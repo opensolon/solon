cdc01a0d1c49e0b326d9533d069fadabf91eec55
noear <noear@live.cn>
2019-10-25 21:45:09 +0800

    1.0.3

--- solon.view.jsp/src/main/java/org/noear/solon/view/jsp/XPluginImp.java
+++ solon.view.jsp/src/main/java/org/noear/solon/view/jsp/XPluginImp.java
@@ -1,12 +1,17 @@
 package org.noear.solon.view.jsp;
 
 import org.noear.solon.XApp;
+import org.noear.solon.XUtil;
 import org.noear.solon.core.XRenderManager;
 import org.noear.solon.core.XPlugin;
 
 public class XPluginImp implements XPlugin {
     @Override
     public void start(XApp app) {
+        if(XUtil.loadClass("javax/servlet/ServletResponse")==null){
+            return;
+        }
+
         JspRender render = new JspRender();
 
         XRenderManager.register(render);
