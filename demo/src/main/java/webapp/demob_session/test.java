18a9679f730c431695b409bcfae1dca0b5eac1ab
noear <noear@live.cn>
2019-10-27 23:30:29 +0800

    1.0.3.6

--- demo/src/main/java/webapp/demob_session/test.java
+++ demo/src/main/java/webapp/demob_session/test.java
@@ -1,4 +1,4 @@
-package webapp.demob_redissessionstate;
+package webapp.demob_session;
 
 import org.noear.solon.annotation.XController;
 import org.noear.solon.annotation.XMapping;
@@ -8,14 +8,14 @@ import java.util.Date;
 
 @XController
 public class test  {
-    @XMapping("/demob/setval")
+    @XMapping("/demob/session/setval")
     public void setVal(XContext ctx){
         ctx.sessionSet("v1", new Date());
         ctx.sessionSet("v2","我是字符串");
         ctx.sessionSet("v3", 121212l);
     }
 
-    @XMapping(value = "/demob/getval", produces = "text/html;charset=utf-8")
+    @XMapping(value = "/demob/session/getval", produces = "text/html;charset=utf-8")
     public void getVal(XContext ctx){
         Object v1 = ctx.session("v1");
         Object v2 = ctx.session("v2");
