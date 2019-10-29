b2afd5dc7901dc036a598b0fd01e9ac77c9c7d95
noear <noear@live.cn>
2019-10-29 14:18:34 +0800

    1.0.3.8

---
+++ _demo/_demo.solon_mvc/src/main/java/webapp/controller/HelloworldController.java
@@ -0,0 +1,25 @@
+package webapp.controller;
+
+import org.noear.solon.annotation.XController;
+import org.noear.solon.annotation.XMapping;
+import org.noear.solon.core.ModelAndView;
+import webapp.model.UserModel;
+
+@XController
+public class HelloworldController {
+    @XMapping("/helloworld")
+    public Object helloworld(){
+        UserModel m = new UserModel();
+        m.id=10;
+        m.name = "刘之西东";
+        m.sex=1;
+
+        ModelAndView vm = new ModelAndView("helloworld.htm");
+
+        vm.put("title","demo");
+        vm.put("message","hello world!");
+        vm.put("m",m);
+
+        return vm;
+    }
+}
