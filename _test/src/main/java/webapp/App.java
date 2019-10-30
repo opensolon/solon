af611dc435a53a68ede4913615f983d13dcc414a
noear <noear@live.cn>
2019-10-30 15:06:46 +0800

    1.0.3.9

--- _test/src/main/java/webapp/App.java
+++ _test/src/main/java/webapp/App.java
@@ -5,7 +5,12 @@ import webapp.demoe_websocket.WsDemoClientTest;
 
 public class App {
     public static void main(String[] args) {
-        XApp.start(App.class, args);
+        XApp app = XApp.start(App.class, args);
+
+
+//        app.send("/seb/test",(c)->{
+//            c.output("收到了...");
+//        });
 
         //web socket test
         //WsDemoClientTest.test();
