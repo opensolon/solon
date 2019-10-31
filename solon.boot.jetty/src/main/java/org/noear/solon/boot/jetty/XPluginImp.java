1459cdb58be115be585cb49ababfdf32a2c0e264
noear <noear@live.cn>
2019-10-31 12:38:52 +0800

    1.0.3.4

--- solon.boot.jetty/src/main/java/org/noear/solon/boot/jetty/XPluginImp.java
+++ solon.boot.jetty/src/main/java/org/noear/solon/boot/jetty/XPluginImp.java
@@ -8,6 +8,9 @@ public final class XPluginImp implements XPlugin {
     private XPlugin _server = null;
     @Override
     public void start(XApp app) {
+        long time_start = System.currentTimeMillis();
+        System.out.println("oejs.Server:main: Jetty 9.4");
+
        Class<?> jspClz = XUtil.loadClass("org.eclipse.jetty.jsp.JettyJspServlet");
 
        XServerProp.init();
@@ -19,5 +22,9 @@ public final class XPluginImp implements XPlugin {
        }
 
        _server.start(app);
+
+        long time_end = System.currentTimeMillis();
+
+        System.out.println("oejs.Server:main: Started @" + (time_end - time_start) + "ms");
     }
 }
