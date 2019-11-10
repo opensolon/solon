b929acb0a1c779d5f2aa8eae025b534e6cd32130
noear <noear@live.cn>
2019-11-10 19:58:34 +0800

    1.0.3.6

--- solon.boot.nettyhttp/src/main/java/org/noear/solon/boot/nettyhttp/NtHttpContext.java
+++ solon.boot.nettyhttp/src/main/java/org/noear/solon/boot/nettyhttp/NtHttpContext.java
@@ -123,12 +123,12 @@ public class NtHttpContext extends XContext {
     }
 
     @Override
-    public String body() throws IOException {
+    public String body() {
         return _request.content().toString();
     }
 
     @Override
-    public InputStream bodyAsStream() throws IOException {
+    public InputStream bodyAsStream() {
         return new ByteArrayInputStream(_request.content().array());
     }
 
