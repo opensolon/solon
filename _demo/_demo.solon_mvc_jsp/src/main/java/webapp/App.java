90631d27708a006ff657b453b5f0534bb709abe3
noear <noear@live.cn>
2019-12-04 17:52:24 +0800

    1.0.3.33

--- _demo/_demo.solon_mvc_jsp/src/main/java/webapp/App.java
+++ _demo/_demo.solon_mvc_jsp/src/main/java/webapp/App.java
@@ -2,6 +2,14 @@ package webapp;
 
 import org.noear.solon.XApp;
 
+/**
+ *
+ * //资源路径说明（不用配置）
+ * resources/application.properties（或 application.yml） 为应用配置文件
+ * resources/static/ 为静态文件根目标
+ * resources/WEB-INF/view/ 为视图文件根目标（支持多视图共存）
+ *
+ * */
 public class App {
     public static void main(String[] args) {
         XApp.start(App.class, args);
