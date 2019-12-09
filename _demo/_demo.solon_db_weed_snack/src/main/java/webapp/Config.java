45fda40a3802026211d48d2e981dc678c3e2dfb8
noear <noear@live.cn>
2019-12-09 15:44:02 +0800

    1.0.3.35

--- _demo/_demo.solon_db_weed_snack/src/main/java/webapp/Config.java
+++ _demo/_demo.solon_db_weed_snack/src/main/java/webapp/Config.java
@@ -6,24 +6,5 @@ import org.noear.solon.core.XMap;
 import org.noear.weed.DbContext;
 
 public class Config {
-    //
-    //直接配置 数据库上下文
-    //
-    public final static DbContext db1 = new DbContext(Aop.prop().getProp("test.db"));
 
-
-    //
-    //使用连接池 配置 数据库上下文
-    //
-    private final static HikariDataSource dataSource(){
-        XMap map = Aop.prop().getXmap("test.db");
-
-        HikariDataSource dataSource = new HikariDataSource();
-        dataSource.setJdbcUrl(map.get("url"));
-        dataSource.setUsername(map.get("username"));
-        dataSource.setPassword(map.get("password"));
-
-        return dataSource;
-    }
-    public final static DbContext db2 = new DbContext().dataSourceSet(dataSource());
 }
