7908bd5c799f943fa6a1162e9ae7b235c2a00c8e
noear <noear@live.cn>
2019-12-16 12:47:54 +0800

    1.0.3.36

--- solon/src/main/java/org/noear/solon/core/XRouterHandler.java
+++ solon/src/main/java/org/noear/solon/core/XRouterHandler.java
@@ -22,16 +22,16 @@ public class XRouterHandler implements XHandler {
         boolean _handled = false;
 
         //前置处理（支持多代理）
-        do_try_handle_multiple(context, XEndpoint.before);
+        handleMultiple(context, XEndpoint.before);
 
         //主体处理
         if (context.getHandled() == false) {
             //（仅支持唯一代理）
-            _handled = do_try_handle_one(context, XEndpoint.main);
+            _handled = handleOne(context, XEndpoint.main);
         }
 
         //后置处理（支持多代理）
-        do_try_handle_multiple(context, XEndpoint.after); //前后不能反 （后置处理由内部进行状态控制）
+        handleMultiple(context, XEndpoint.after); //前后不能反 （后置处理由内部进行状态控制）
 
         //汇总状态
         if (_handled) {
@@ -46,7 +46,7 @@ public class XRouterHandler implements XHandler {
         }
     }
 
-    protected boolean do_try_handle_one(XContext context, int endpoint) throws Throwable {
+    protected boolean handleOne(XContext context, int endpoint) throws Throwable {
         XHandler handler = _router.matchOne(context, endpoint);
 
         if (handler != null) {
@@ -57,7 +57,7 @@ public class XRouterHandler implements XHandler {
         }
     }
 
-    protected void do_try_handle_multiple(XContext context, int endpoint) throws Throwable {
+    protected void handleMultiple(XContext context, int endpoint) throws Throwable {
         for(XHandler handler: _router.matchAll(context,endpoint)){
             handler.handle(context);
         }
