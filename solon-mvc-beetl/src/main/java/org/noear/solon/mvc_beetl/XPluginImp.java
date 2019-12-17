0c8d1f993d732194cee953f537a65fa8b19442ba
noear <noear@live.cn>
2019-12-17 18:00:41 +0800

    1.0.3.38

---
+++ solon-mvc-beetl/src/main/java/org/noear/solon/mvc_beetl/XPluginImp.java
@@ -0,0 +1,11 @@
+package org.noear.solon.mvc_beetl;
+
+import org.noear.solon.XApp;
+import org.noear.solon.core.XPlugin;
+
+public class XPluginImp implements XPlugin {
+    @Override
+    public void start(XApp app) {
+
+    }
+}
