66bfd7b8d6411746b8894d2c0876dcdb2bad5adf
noear <noear@live.cn>
2019-12-23 10:42:43 +0800

    1.0.3.43

---
+++ solon.view.rocker/src/main/java/org/noear/solon/view/rocker/XPluginImp.java
@@ -0,0 +1,15 @@
+package org.noear.solon.view.rocker;
+
+
+import org.noear.solon.XApp;
+import org.noear.solon.core.XPlugin;
+
+@SuppressWarnings("unchecked")
+public class XPluginImp implements XPlugin {
+
+
+    @Override
+    public void start(XApp app) {
+
+    }
+}
