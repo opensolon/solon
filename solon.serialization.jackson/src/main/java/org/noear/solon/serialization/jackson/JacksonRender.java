6e2c0dc3078218aa482fc52bc4e8b1c3ed7021ee
noear <noear@live.cn>
2019-12-25 11:31:12 +0800

    1.0.4

--- solon.serialization.jackson/src/main/java/org/noear/solon/serialization/jackson/JacksonRender.java
+++ solon.serialization.jackson/src/main/java/org/noear/solon/serialization/jackson/JacksonRender.java
@@ -1,8 +1,15 @@
 package org.noear.solon.serialization.jackson;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import org.noear.solon.core.XContext;
 import org.noear.solon.core.XRender;
+
+import java.io.IOException;
+import java.util.Date;
 
 import static com.fasterxml.jackson.databind.ObjectMapper.DefaultTyping.NON_FINAL;
 
@@ -12,6 +19,19 @@ public class JacksonRender implements XRender {
 
     public JacksonRender(){
         mapper_serialize.enableDefaultTypingAsProperty(NON_FINAL, "@type");
+
+
+//        JsonSerializer<Date> dateSerializer = new JsonSerializer<Date>() {
+//            @Override
+//            public void serialize(Date date, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+//                jsonGenerator.writeRaw("new Date(" + date.getTime() + ")");
+//            }
+//        };
+//
+//        SimpleModule simpleModule = new SimpleModule();
+//        simpleModule.addSerializer(Date.class, dateSerializer);
+//
+//        mapper_serialize.registerModule(simpleModule);
     }
 
     @Override
