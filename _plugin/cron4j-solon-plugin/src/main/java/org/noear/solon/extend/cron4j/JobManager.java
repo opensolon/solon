5ce928609da779f7134894af7f04bffad31d078f
noear <noear@live.cn>
2019-12-26 13:02:42 +0800

    1.0.4

---
+++ _plugin/cron4j-solon-plugin/src/main/java/org/noear/solon/extend/cron4j/JobManager.java
@@ -0,0 +1,60 @@
+package org.noear.solon.extend.cron4j;
+
+import it.sauronsoftware.cron4j.Scheduler;
+
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public final class JobManager {
+    static Scheduler _server = null;
+    static ScheduledThreadPoolExecutor _taskScheduler;
+
+    protected static void init(){
+        _server = new Scheduler();
+        _taskScheduler = new ScheduledThreadPoolExecutor(1);
+    }
+
+    protected static void start(){
+        _server.start();
+    }
+
+    protected static void stop(){
+
+        if(_server != null){
+            _server.stop();
+            _server = null;
+        }
+    }
+
+    public static void addJob(String cron4x, Runnable job){
+
+        if (cron4x.indexOf(" ") < 0) {
+            if (cron4x.endsWith("ms")) {
+                long period = Long.parseLong(cron4x.substring(0, cron4x.length() - 2));
+                _taskScheduler.scheduleAtFixedRate(() -> do_exec(job), 0, period, TimeUnit.MILLISECONDS);
+            } else if (cron4x.endsWith("s")) {
+                long period = Long.parseLong(cron4x.substring(0, cron4x.length() - 1));
+                _taskScheduler.scheduleAtFixedRate(() -> do_exec(job), 0, period, TimeUnit.SECONDS);
+            } else if (cron4x.endsWith("m")) {
+                long period = Long.parseLong(cron4x.substring(0, cron4x.length() - 1));
+                _taskScheduler.scheduleAtFixedRate(() -> do_exec(job), 0, period, TimeUnit.MINUTES);
+            } else if (cron4x.endsWith("h")) {
+                long period = Long.parseLong(cron4x.substring(0, cron4x.length() - 1));
+                _taskScheduler.scheduleAtFixedRate(() -> do_exec(job), 0, period, TimeUnit.HOURS);
+            } else if (cron4x.endsWith("d")) {
+                long period = Long.parseLong(cron4x.substring(0, cron4x.length() - 1));
+                _taskScheduler.scheduleAtFixedRate(() -> do_exec(job), 0, period, TimeUnit.DAYS);
+            }
+        } else {
+            _server.schedule(cron4x, () -> do_exec(job));
+        }
+    }
+
+    private static void do_exec(Runnable job) {
+        try {
+            job.run();
+        } catch (Throwable ex) {
+            ex.printStackTrace();
+        }
+    }
+}
--- _plugin/cron4j-solon-plugin/src/main/java/org/noear/solon/extend/cron4j/XPluginImp.java
+++ _plugin/cron4j-solon-plugin/src/main/java/org/noear/solon/extend/cron4j/XPluginImp.java
@@ -1,74 +1,36 @@
 package org.noear.solon.extend.cron4j;
 
-import it.sauronsoftware.cron4j.Scheduler;
-import it.sauronsoftware.cron4j.Task;
 import org.noear.solon.XApp;
 import org.noear.solon.core.Aop;
-import org.noear.solon.core.BeanWrap;
 import org.noear.solon.core.XPlugin;
 
-import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
-
 public class XPluginImp implements XPlugin {
-    Scheduler _server = null;
-    ScheduledThreadPoolExecutor _taskScheduler;
-
     @Override
     public void start(XApp app) {
-        _server = new Scheduler();
-
-        _taskScheduler = new ScheduledThreadPoolExecutor(1);
+        JobManager.init();
 
         Aop.factory().beanLoaderAdd(Job.class, (clz, bw, anno) -> {
 
+            //只对Runnable有效
+            if (Runnable.class.isAssignableFrom(clz) == false) {
+                throw new RuntimeException("Job only supported Runnable：" + clz.getName());
+            }
+
             String cron4x = anno.cron4x();
 
-            if (cron4x.indexOf(" ") < 0) {
-                if (Runnable.class.isAssignableFrom(clz)) {
-                    if (cron4x.endsWith("ms")) {
-                        long period = Long.parseLong(cron4x.substring(0, cron4x.length() - 2));
-                        _taskScheduler.scheduleAtFixedRate(bw.raw(), 0, period, TimeUnit.MILLISECONDS);
-                    } else if (cron4x.endsWith("s")) {
-                        long period = Long.parseLong(cron4x.substring(0, cron4x.length() - 1));
-                        _taskScheduler.scheduleAtFixedRate(bw.raw(), 0, period, TimeUnit.SECONDS);
-                    } else if (cron4x.endsWith("m")) {
-                        long period = Long.parseLong(cron4x.substring(0, cron4x.length() - 1));
-                        _taskScheduler.scheduleAtFixedRate(bw.raw(), 0, period, TimeUnit.MINUTES);
-                    } else if (cron4x.endsWith("h")) {
-                        long period = Long.parseLong(cron4x.substring(0, cron4x.length() - 1));
-                        _taskScheduler.scheduleAtFixedRate(bw.raw(), 0, period, TimeUnit.HOURS);
-                    }else if (cron4x.endsWith("d")) {
-                        long period = Long.parseLong(cron4x.substring(0, cron4x.length() - 1));
-                        _taskScheduler.scheduleAtFixedRate(bw.raw(), 0, period, TimeUnit.DAYS);
-                    }
-                } else {
-                    throw new RuntimeException("Non-cron4 mode, only Runnable is supported：" + clz.getName());
-                }
-            } else {
-                if (Runnable.class.isAssignableFrom(clz)) {
-                    _server.schedule(cron4x, (Runnable) bw.get());
-                }
-
-                if (Task.class.isAssignableFrom(clz)) {
-                    _server.schedule(cron4x, (Task) bw.get());
-                }
-            }
+            JobManager.addJob(cron4x, bw.raw());
 
         });
 
         Aop.beanOnloaded(() -> {
-
-            _server.start();
+            JobManager.start();
         });
     }
 
 
+
     @Override
     public void stop() throws Throwable {
-        if(_server != null){
-            _server.stop();
-            _server = null;
-        }
+        JobManager.stop();
     }
 }
