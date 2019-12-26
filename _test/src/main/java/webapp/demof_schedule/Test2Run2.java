c63bdde7c94edf910901eb24bf3ab6fa4f5f561e
noear <noear@live.cn>
2019-12-26 14:57:13 +0800

    1.0.4

--- _test/src/main/java/webapp/demof_schedule/Test2Run2.java
+++ _test/src/main/java/webapp/demof_schedule/Test2Run2.java
@@ -2,7 +2,7 @@ package webapp.demof_schedule;
 
 import org.noear.solon.extend.cron4j.Job;
 
-//@Job(cron4x = "200ms")
+@Job(cron4x = "200ms")
 public class Test2Run2 implements Runnable {
 
     @Override
