c577eaeab70577433837a6b30a521d9dd7d676be
noear <noear@live.cn>
2019-12-31 09:15:08 +0800

    1.0.4

--- solon.extend.schedule/src/main/java/org/noear/solon/extend/schedule/JobRunner.java
+++ solon.extend.schedule/src/main/java/org/noear/solon/extend/schedule/JobRunner.java
@@ -1,45 +1,38 @@
 package org.noear.solon.extend.schedule;
 
-
-import java.util.Date;
-
 /**
  * 任务运行工具
  * */
 public class JobRunner implements IJobRunner {
     public static IJobRunner global = new JobRunner();
 
-    public boolean allow(JobEntity task){
+    public boolean allow(JobEntity job) {
         return true;
     }
 
-    public void run(JobEntity task) {
-        if (allow(task)) {
-            System.out.print("run::" + task.getName() + "\r\n");
+    public void run(JobEntity job) {
+        if (allow(job)) {
+            System.out.print("schedule run::" + job.getName() + "\r\n");
 
             new Thread(() -> {
-                doRun(task);
+                doRun(job);
             }).start();
         }
     }
 
-    protected void doRun(JobEntity task){
+    protected void doRun(JobEntity job) {
         while (true) {
             try {
-                Date time_start = new Date();
-                System.out.print(task.getName() + "::time_start::" + time_start.toString() + "\r\n");
+                long time_start = System.currentTimeMillis();
+                job.getJob().exec();
+                long time_end = System.currentTimeMillis();
 
-                task.getJob().exec();
-
-                Date time_end = new Date();
-                System.out.print(task.getName() + "::time_end::" + time_end.toString() + "\r\n");
-
-                if(task.getJob().getInterval() == 0){
+                if (job.getJob().getInterval() == 0) {
                     return;
                 }
 
-                if (time_end.getTime() - time_start.getTime() < task.getJob().getInterval()) {
-                    Thread.sleep(task.getJob().getInterval());//0.5s
+                if (time_end - time_start < job.getJob().getInterval()) {
+                    Thread.sleep(job.getJob().getInterval());//0.5s
                 }
 
             } catch (Throwable ex) {
