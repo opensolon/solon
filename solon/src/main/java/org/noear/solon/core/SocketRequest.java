52620274e59c77a8a5ff88595555f543c3008d65
noear <noear@live.cn>
2020-01-02 12:38:18 +0800

    1.0.4.1

--- solon/src/main/java/org/noear/solon/core/SocketRequest.java
+++ solon/src/main/java/org/noear/solon/core/SocketRequest.java
@@ -1,41 +1,32 @@
 package org.noear.solon.core;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 public class SocketRequest {
     public String resourceDescriptor;
-    public InputStream message;
+    public byte[] message;
     public Charset charset = StandardCharsets.UTF_8;
 
     public SocketRequest(String resourceDescriptor, String message) {
         this.resourceDescriptor = resourceDescriptor;
-        this.message = new ByteArrayInputStream(message.getBytes(charset));
+        this.message = message.getBytes(charset);
     }
 
     public SocketRequest(String resourceDescriptor, byte[] bytes) {
         this.resourceDescriptor = resourceDescriptor;
-        this.message = new ByteArrayInputStream(bytes);
+        this.message = bytes;
     }
 
     public ByteBuffer wrap() throws IOException {
-        int len = resourceDescriptor.length();
-        try{ len = len + message.available(); }catch (IOException ex){}
+        int len = resourceDescriptor.length() + message.length;
 
         ByteBuffer buffer = ByteBuffer.allocate(len);
         buffer.put(resourceDescriptor.getBytes(charset));
         buffer.putChar('\n');
-
-
-        byte[] b = new byte[1024];
-        int lens = -1;
-        while ((lens = message.read(b)) > 0) {
-            buffer.put(b, 0, lens);
-        }
+        buffer.put(message);
 
         return buffer;
     }
