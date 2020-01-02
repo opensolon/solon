9af259ecc49360d699f40eb3f1777166ab4e5517
noear <noear@live.cn>
2020-01-02 16:07:10 +0800

    1.0.4.1

--- solon.boot.smartsocket/src/main/java/org/noear/solon/boot/smartsocket/SsProtocol.java
+++ solon.boot.smartsocket/src/main/java/org/noear/solon/boot/smartsocket/SsProtocol.java
@@ -17,55 +17,7 @@ public class SsProtocol implements Protocol<SocketMessage> {
 
     @Override
     public SocketMessage decode(ByteBuffer buffer, AioSession<SocketMessage> session) {
-        //: {key}\n/\n,最少两个
-        if (buffer.limit() < 10) {
-            return null;
-        }
-
-        //1.解码key and uri
-        ByteBuffer sb = ByteBuffer.allocate(Math.min(256, buffer.limit()));
-
-        String key = readStr(buffer, sb);
-        if(key == null){
-            return null;
-        }
-
-        String uri = readStr(buffer, sb);
-        if(uri == null){
-            return null;
-        }
-
-        //2.解码body
-        byte[] bytes = new byte[buffer.limit() - buffer.position()];
-        buffer.get(bytes, 0, buffer.limit() - buffer.position());
-
-        return new SocketMessage(key, uri, bytes);
-    }
-
-    private String readStr(ByteBuffer buffer, ByteBuffer sb) {
-        sb.position(0);
-
-        while (true) {
-            byte c = buffer.get();
-
-            if (c == '\n') {
-                break;
-            } else if (c > 32 || c < 0) {
-                sb.put(c);
-            }
-
-            //url 太长了
-            if (URL_MAX_LEN < buffer.position()) {
-                return null;
-            }
-        }
-
-        sb.flip();
-        if (sb.limit() < 1) {
-            return null;
-        }
-
-        return new String(sb.array(), 0, sb.limit());
+        return SocketMessage.parse(buffer);
     }
 }
 
