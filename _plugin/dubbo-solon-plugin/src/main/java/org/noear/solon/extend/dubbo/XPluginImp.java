e6a624af74bcc342dcb753829187e13f5112e144
noear <noear@live.cn>
2020-01-12 18:26:15 +0800

    1.0.4.2

--- _plugin/dubbo-solon-plugin/src/main/java/org/noear/solon/extend/dubbo/XPluginImp.java
+++ _plugin/dubbo-solon-plugin/src/main/java/org/noear/solon/extend/dubbo/XPluginImp.java
@@ -1,15 +1,45 @@
 package org.noear.solon.extend.dubbo;
 
+import com.alibaba.dubbo.config.ServiceConfig;
+import com.alibaba.dubbo.config.annotation.Service;
 import org.noear.solon.XApp;
+import org.noear.solon.core.Aop;
 import org.noear.solon.core.XPlugin;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 public class XPluginImp implements XPlugin {
+    DubboServer _server;
+
     @Override
     public void start(XApp app) {
-        ClassPathXmlApplicationContext cpxac = new ClassPathXmlApplicationContext("dubbo_provider.xml");
-        cpxac.start();
+
+        _server = new DubboServer(app);
+
+        // 服务提供者暴露服务配置
+        Aop.beanOnloaded(() -> {
+            Aop.beanForeach((name, bw) -> {
+                if (bw.remoting()) {
+
+                    Class<?>[] ifs = bw.clz().getInterfaces();
+                    if (ifs.length == 1) {
+                        Service sAnno = bw.clz().getAnnotation(Service.class);
+                        ServiceConfig service = null;
+                        if (sAnno == null) {
+                            service = new ServiceConfig();
+                            service.setVersion("1.0.0");
+                        } else {
+                            service = new ServiceConfig(sAnno);
+                        }
+
+                        service.setInterface(ifs[0]);
+                        service.setRef(bw.raw());
+
+                        // 暴露及注册服务
+                        _server.regService(service);
+                    }
+                }
+            });
+        });
     }
 
 
