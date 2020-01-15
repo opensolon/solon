c208a1b33be3c1e8728e9e230fb02cb948175577
noear <noear@live.cn>
2020-01-15 15:08:40 +0800

    1.0.4.3

---
+++ _plugin/mqtt-solon-plugin/src/main/java/org/noear/solon/extend/mqtt/XPluginImp.java
@@ -0,0 +1,20 @@
+package org.noear.solon.extend.mqtt;
+
+import org.noear.solon.XApp;
+import org.noear.solon.core.XPlugin;
+
+
+public class XPluginImp implements XPlugin {
+
+
+    @Override
+    public void start(XApp app) {
+
+    }
+
+
+    @Override
+    public void stop() throws Throwable {
+
+    }
+}
