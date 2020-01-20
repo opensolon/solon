90f9554c1b480ccda1b85f52c7386804549e94b6
noear <noear@live.cn>
2020-01-20 16:45:35 +0800

    1.0.4.3

--- solon.serialization.protobuf/src/main/java/org/noear/solon/serialization/protobuf/ProtobufRender.java
+++ solon.serialization.protobuf/src/main/java/org/noear/solon/serialization/protobuf/ProtobufRender.java
@@ -1,10 +1,13 @@
 package org.noear.solon.serialization.protobuf;
 
 import io.edap.protobuf.ProtoBuf;
+import org.noear.solon.core.ModelAndView;
 import org.noear.solon.core.XContext;
 import org.noear.solon.core.XRender;
 
 import java.io.ByteArrayInputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 //不要要入参，方便后面多视图混用
 //
@@ -15,8 +18,12 @@ public class ProtobufRender implements XRender {
         if(XPluginImp.output_meta) {
             ctx.headerSet("solon.serialization", "ProtobufRender");
         }
-
-        byte[] bytes = ProtoBuf.toByteArray(obj);
+        byte[] bytes = null;
+        if(obj instanceof ModelAndView){
+            bytes = ProtoBuf.toByteArray(new HashMap<>(((Map)obj)));
+        }else{
+            bytes = ProtoBuf.toByteArray(obj);
+        }
 
         ctx.attrSet("output", bytes);
         ctx.output(new ByteArrayInputStream(bytes));
