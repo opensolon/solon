69d7d4063294078e59498adb8ac1e15491d45265
noear <noear@live.cn>
2020-01-30 23:24:25 +0800

    1.0.5-SNAPSHOT

--- solon/src/main/java/org/noear/solon/core/AopFactoryBase.java
+++ solon/src/main/java/org/noear/solon/core/AopFactoryBase.java
@@ -1,7 +1,5 @@
 package org.noear.solon.core;
 
-import org.noear.solon.XUtil;
-import org.noear.solon.annotation.XInject;
 import org.noear.solon.ext.Act1;
 
 import java.lang.annotation.Annotation;
