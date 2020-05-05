20855fe146449498859d1c576ee8696bd12f9772
noear <noear@live.cn>
2020-05-05 08:29:52 +0800

    1.0.5.5

--- solon.extend.schedule/src/main/java/org/noear/solon/extend/schedule/XPluginImp.java
+++ solon.extend.schedule/src/main/java/org/noear/solon/extend/schedule/XPluginImp.java
@@ -5,6 +5,12 @@ import org.noear.solon.XApp;
 import org.noear.solon.core.Aop;
 import org.noear.solon.core.XPlugin;
 
+/**
+ * solon.extend.schedule 相对于 cron4j-solon-plugin 的区别：
+ *
+ * getInterval 和 getThreads 可动态控制；例，夜间或流量小时弹性变小数值.
+ *
+ * */
 public class XPluginImp implements XPlugin {
     @Override
     public void start(XApp app) {
