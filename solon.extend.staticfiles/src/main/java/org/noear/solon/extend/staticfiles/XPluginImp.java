c592962a3ec33363c085163f0b91e299611cd8c8
noear <noear@live.cn>
2020-05-08 15:15:50 +0800

    1.0.5.5

--- solon.extend.staticfiles/src/main/java/org/noear/solon/extend/staticfiles/XPluginImp.java
+++ solon.extend.staticfiles/src/main/java/org/noear/solon/extend/staticfiles/XPluginImp.java
@@ -8,6 +8,12 @@ import org.noear.solon.core.XPlugin;
 public class XPluginImp implements XPlugin {
     @Override
     public void start(XApp app) {
+        //通过动态控制是否启用
+        //
+        if("0".equals(System.getProperty("org.noear.solon.extend.staticfiles.enabled"))){
+            return;
+        }
+
         if(XUtil.getResource("/static") != null) {
             XHandlerLink link = new XHandlerLink();
             link.node = new XResourceHandler("/static");
