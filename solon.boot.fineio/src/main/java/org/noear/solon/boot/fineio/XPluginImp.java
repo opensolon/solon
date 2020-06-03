72abd49552d293e6029b55bb6c85212eed0d9f97
noear <noear@live.cn>
2020-06-03 14:36:41 +0800

    1.0.5.13

--- solon.boot.fineio/src/main/java/org/noear/solon/boot/fineio/XPluginImp.java
+++ solon.boot.fineio/src/main/java/org/noear/solon/boot/fineio/XPluginImp.java
@@ -9,7 +9,7 @@ public class XPluginImp implements XPlugin {
 
 
     public static String solon_boot_ver() {
-        return "jdk socket nio jdk8/1.0.5.12";
+        return "jdk socket nio jdk8/1.0.5.13";
     }
 
     NetServer<SocketMessage> _server;
