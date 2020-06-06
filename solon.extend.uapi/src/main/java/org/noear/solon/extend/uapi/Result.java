004d772896d4c8968a7ba1d45ab5c41d0743a73f
noear <noear@live.cn>
2020-06-06 22:10:23 +0800

    1.0.6

---
+++ solon.extend.uapi/src/main/java/org/noear/solon/extend/uapi/Result.java
@@ -0,0 +1,26 @@
+package org.noear.solon.extend.uapi;
+
+import java.io.Serializable;
+
+/**
+ * 结果
+ * */
+public class Result<T> implements Serializable {
+    public int code = 1;
+    public String message;
+    public T data;
+
+    public Result() {
+
+    }
+
+    public Result(int code, String msg) {
+        this(code, msg, null);
+    }
+
+    public Result(int code, String msg, T data) {
+        this.code = code;
+        this.message = msg;
+        this.data = data;
+    }
+}
