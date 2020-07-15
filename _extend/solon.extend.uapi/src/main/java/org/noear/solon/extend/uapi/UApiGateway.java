9e5d99fa142a17b86481091df558fdda5658b8bb
noear <noear@live.cn>
2020-07-15 12:20:28 +0800

    1.0.6.3

--- _extend/solon.extend.uapi/src/main/java/org/noear/solon/extend/uapi/UApiGateway.java
+++ _extend/solon.extend.uapi/src/main/java/org/noear/solon/extend/uapi/UApiGateway.java
@@ -96,7 +96,7 @@ public abstract class UApiGateway implements XHandler , XRender {
 
 
     /**
-     * 添加接口（适用于，从Aop工厂遍历加入）
+     * 添加接口（适用于，从Aop工厂遍历加入；或者把rpc代理包装成bw）
      */
     public void add(BeanWrap bw) {
         if (bw == null) {
