dc78fb87544f1b19c808613a3b896f30122f8e13
noear <noear@live.cn>
2020-07-17 18:40:22 +0800

    1.0.6.4

--- solon/src/main/java/org/noear/solon/annotation/XInit.java
+++ solon/src/main/java/org/noear/solon/annotation/XInit.java
@@ -6,9 +6,9 @@ import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
- * 初始化
+ * 初始化（相当于 PostConstruct）
  *
- * Bean 构建过程：Constructor(构造方法) -> @XInject(依赖注入) -> @XInit(初始化，相当于 PostConstruct)
+ * Bean 构建过程：Constructor(构造方法) -> @XInject(依赖注入) -> @XInit(初始化)
  * */
 @Retention (RUNTIME)
 @Target(METHOD)
