98a0e98067dc06052f2fef3f420750bae6a0570d
noear <noear@live.cn>
2020-07-21 13:20:11 +0800

    1.0.6.5

--- _extend/solon.boot.jdksocket/src/main/java/org/noear/solon/boot/jdksocket/XPluginImp.java
+++ _extend/solon.boot.jdksocket/src/main/java/org/noear/solon/boot/jdksocket/XPluginImp.java
@@ -7,7 +7,7 @@ public class XPluginImp implements XPlugin {
     SocketServer _server;
 
     public static String solon_boot_ver(){
-        return "jdk socket jdk8/1.0.6.4";
+        return "jdk socket jdk8/1.0.6.5";
     }
 
     @Override
