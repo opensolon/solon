e2f79f2098f6cb818837eb17b149f5cd7b62733f
noear <noear@live.cn>
2020-07-26 11:47:30 +0800

    1.0.6.5

---
+++ _extend/solon.extend.uapi/src/main/java/org/noear/solon/extend/uapi/Result.java
@@ -0,0 +1,41 @@
+package org.noear.solon.extend.uapi;
+
+/**
+ * 结果
+ * */
+public class Result<T> {
+    /**
+     * 状态码
+     * */
+    public int code;
+    /**
+     * 状态描述
+     * */
+    public String description;
+    /**
+     * 数据
+     * */
+    public T data;
+
+    /**
+     * 成功
+     * */
+    public static <T> Result<T> succeed(T data){
+        Result<T> result = new Result<>();
+        result.code = 1;
+        result.data = data;
+
+        return result;
+    }
+
+    /**
+     * 失败
+     * */
+    public static <T> Result<T> failure(int code, String description){
+        Result<T> result = new Result<>();
+        result.code = 1;
+        result.description = description;
+
+        return result;
+    }
+}
