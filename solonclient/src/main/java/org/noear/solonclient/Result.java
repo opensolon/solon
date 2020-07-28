d0a4ca46ead9056868bcd10f38dfe879f2b97b18
noear <noear@live.cn>
2020-07-28 11:52:49 +0800

    1.0.6.5

--- solonclient/src/main/java/org/noear/solonclient/Result.java
+++ solonclient/src/main/java/org/noear/solonclient/Result.java
@@ -8,29 +8,41 @@ import java.util.Map;
 
 public class Result {
     public int code;
-    public List<Map.Entry<String,String>> headers;
+    public List<Map.Entry<String, String>> headers;
     public byte[] body;
     public Charset charset;
+    private String body_string;
 
-    public Result(){
+    public Result() {
         headers = new ArrayList<>();
     }
 
-    public Result(Charset charset, byte[] body){
+    public Result(Charset charset, byte[] body) {
         this();
         this.charset = charset;
         this.body = body;
     }
 
-    public void headerAdd(String name, String value){
-        headers.add(new AbstractMap.SimpleEntry<>(name,value));
+    public void headerAdd(String name, String value) {
+        headers.add(new AbstractMap.SimpleEntry<>(name, value));
     }
 
-    public String bodyAsString(){
-        if(charset == null) {
-            return new String(body);
-        }else{
-            return new String(body, charset);
+    public String bodyAsString() {
+        if (body_string == null) {
+            if (charset == null) {
+                body_string = new String(body);
+            } else {
+                body_string = new String(body, charset);
+            }
+
+            //清掉body
+            body = null;
+        }
+
+        if (code >= 400) {
+            throw new RuntimeException(code + "错误：" + body_string);
+        } else {
+            return body_string;
         }
     }
 }
