9fc2077b1698b1596ca5e343f63feb1874d409d2
noear <noear@live.cn>
2020-07-30 17:35:45 +0800

    1.0.7

--- _demo/_demo.solon_helloworld/src/main/java/webapp/HelloApp.java
+++ _demo/_demo.solon_helloworld/src/main/java/webapp/HelloApp.java
@@ -1,21 +1,26 @@
 package webapp;
 
 import org.noear.solon.XApp;
+import org.noear.solon.annotation.XController;
+import org.noear.solon.annotation.XMapping;
+import org.noear.solon.annotation.XSingleton;
 
 /**
  * helloworld 0
  * */
+@XSingleton(false)
+@XController
 public class HelloApp {
     public static void main(String[] args) {
         XApp app = XApp.start(HelloApp.class, args);
 
-        app.http("/", c -> {
-            c.output("hello world!");
-//            try {
-//                throw new RuntimeException("hello world!");
-//            } catch (Exception ex) {
-//                c.output(ex.getMessage());
-//            }
-        });
+//        app.http("/", c -> {
+//            c.output("hello world!");
+//        });
+    }
+
+    @XMapping
+    public String hello(){
+        return "hello world!";
     }
 }
