544e8d6739a5d41525617678d1b62853004f9d69
noear <noear@live.cn>
2020-07-31 11:23:39 +0800

    1.0.7

--- solonclient/src/main/java/org/noear/solonclient/XUpstream.java
+++ solonclient/src/main/java/org/noear/solonclient/XUpstream.java
@@ -1,7 +1,18 @@
 package org.noear.solonclient;
 
+/**
+ * 负载器
+ * */
 @FunctionalInterface
 public interface XUpstream {
+    /**
+     * 设置备份节点
+     * */
     default void setBackup(String server){}
-    String getServer(String name);
+    /**
+     * 获取节点
+     *
+     * @param service 服务名（给多服务网关预留）
+     * */
+    String getServer(String service);
 }
