d1756bbd03e4371026d7b6a1172c9978e218997c
noear <noear@live.cn>
2020-08-02 21:41:58 +0800

    1.0.7

--- _demo/_demo.solon_helloworld/src/main/java/webapp/HelloApp.java
+++ _demo/_demo.solon_helloworld/src/main/java/webapp/HelloApp.java
@@ -5,21 +5,13 @@ import org.noear.solon.annotation.XController;
 import org.noear.solon.annotation.XMapping;
 import org.noear.solon.annotation.XSingleton;
 
-/**
- * helloworld 0
- * */
-@XSingleton(false)
 @XController
 public class HelloApp {
     public static void main(String[] args) {
-        XApp app = XApp.start(HelloApp.class, args);
-
-//        app.http("/", c -> {
-//            c.output("hello world!");
-//        });
+        XApp.start(HelloApp.class, args);
     }
 
-    @XMapping
+    @XMapping("/")
     public String hello(){
         return "hello world!";
     }
