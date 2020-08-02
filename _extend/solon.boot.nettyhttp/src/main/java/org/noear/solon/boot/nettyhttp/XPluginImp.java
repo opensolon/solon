4a2468dddbad863c9bcf28c0fad151893b50e3a1
noear <noear@live.cn>
2020-08-02 14:02:55 +0800

    1.0.7

--- _extend/solon.boot.nettyhttp/src/main/java/org/noear/solon/boot/nettyhttp/XPluginImp.java
+++ _extend/solon.boot.nettyhttp/src/main/java/org/noear/solon/boot/nettyhttp/XPluginImp.java
@@ -7,7 +7,7 @@ public class XPluginImp implements XPlugin {
     HttpServer _server;
 
     public static String solon_boot_ver(){
-        return "netty http 4.1/1.0.7.1";
+        return "netty http 4.1/1.0.7";
     }
 
     @Override
