43c68473b26a9cedfdd27d4ef628a439115c1390
noear <noear@live.cn>
2020-08-04 16:04:57 +0800

    1.0.9

--- _extend/solon.extend.stop/src/main/java/org/noear/solon/extend/stop/XPluginImp.java
+++ _extend/solon.extend.stop/src/main/java/org/noear/solon/extend/stop/XPluginImp.java
@@ -6,18 +6,19 @@ import org.noear.solon.core.XPlugin;
 public class XPluginImp implements XPlugin {
     @Override
     public void start(XApp app) {
-        boolean enabled = app.prop().getBool("solon.stop.enabled",false);
-        String path = app.prop().get("solon.stop.path","/run/stop/");
-        String host = app.prop().get("solon.stop.host","127.0.0.1");
+        boolean enabled = app.prop().getBool("solon.stop.enabled", false);
+        String path = app.prop().get("solon.stop.path", "/run/stop/");
+        String host = app.prop().get("solon.stop.host", "127.0.0.1");
+
+        long delay = XApp.cfg().getLong("solon.stop.delay", 0);
 
 
-
-        if(enabled){
-            app.get(path, (c)->{
-                if("*".equals(host)){
-                    XApp.stop();
-                }else if(host.equals(c.uri().getHost())){
-                    XApp.stop();
+        if (enabled) {
+            app.get(path, (c) -> {
+                if ("*".equals(host)) {
+                    XApp.stop(true, delay);
+                } else if (host.equals(c.uri().getHost())) {
+                    XApp.stop(true, delay);
                 }
             });
         }
