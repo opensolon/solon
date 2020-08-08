cb01c534aa5fa9a6966578b02cd9eecbffb0bd85
noear <noear@live.cn>
2020-08-08 17:34:50 +0800

    1.0.9.1

--- _extend/solon.boot.rapidoid-http-fast/src/main/java/org/noear/solon/boot/rapidoid_http_fast/XPluginImp.java
+++ _extend/solon.boot.rapidoid-http-fast/src/main/java/org/noear/solon/boot/rapidoid_http_fast/XPluginImp.java
@@ -7,7 +7,7 @@ public final class XPluginImp implements XPlugin {
 
 
     public static String solon_boot_ver(){
-        return "jlhttp 2.4/1.0.9";
+        return "jlhttp 2.4/1.0.9.1";
     }
 
     @Override
