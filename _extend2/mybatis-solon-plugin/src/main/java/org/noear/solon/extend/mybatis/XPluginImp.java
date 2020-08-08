78100dbb90fddadadff9880f16799c7c2e2ebe54
noear <noear@live.cn>
2020-08-08 12:55:03 +0800

    1.0.9

--- _extend2/mybatis-solon-plugin/src/main/java/org/noear/solon/extend/mybatis/XPluginImp.java
+++ _extend2/mybatis-solon-plugin/src/main/java/org/noear/solon/extend/mybatis/XPluginImp.java
@@ -1,33 +1,56 @@
 package org.noear.solon.extend.mybatis;
 
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.noear.solon.XApp;
+import org.noear.solon.XUtil;
 import org.noear.solon.core.Aop;
 import org.noear.solon.core.XPlugin;
-
-import java.io.Reader;
+import org.noear.solon.core.XScaner;
 
 public class XPluginImp implements XPlugin {
     @Override
     public void start(XApp app) {
 
-        try {
-            //通过配置文件获取数据库连接信息
-            Reader reader = Resources.getResourceAsReader("mybatis.xml");
+        Aop.factory().beanCreatorAdd(MapperScan.class, (clz0, wrap, anno) -> {
+            String dir = anno.basePackages().replace('.', '/');
+            String sessionFactoryRef = anno.sqlSessionFactoryRef();
 
-            //通过配置信息构建一个SqlSessionFactory
-            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+            Aop.getAsyn(sessionFactoryRef, (bw -> {
+                if (bw.raw() instanceof SqlSessionFactory) {
+                    scanMapper(dir, (SqlSessionFactory) bw.raw());
+                }
+            }));
+        });
 
-            //通过SqlSessionFactory打开一个数据库会话
-            SqlSession sqlsession = sqlSessionFactory.openSession();
+//        try {
+//            //通过配置文件获取数据库连接信息
+//            Reader reader = Resources.getResourceAsReader("mybatis.xml");
+//
+//            //通过配置信息构建一个SqlSessionFactory
+//            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+//
+//            //通过SqlSessionFactory打开一个数据库会话
+//            SqlSession sqlsession = sqlSessionFactory.openSession();
+//
+//            Aop.put(SqlSessionFactory.class, sqlSessionFactory);
+//            Aop.put("sqlsession", sqlsession);
+//        }catch (Exception ex){
+//            throw new RuntimeException(ex);
+//        }
+    }
 
-            Aop.put(SqlSessionFactory.class, sqlSessionFactory);
-            Aop.put("sqlsession", sqlsession);
-        }catch (Exception ex){
-            throw new RuntimeException(ex);
-        }
+    private void scanMapper(String dir, SqlSessionFactory factory) {
+        XScaner.scan(dir, n -> n.endsWith(".class"))
+                .stream()
+                .map(name -> {
+                    String className = name.substring(0, name.length() - 6);
+                    return XUtil.loadClass(className.replace("/", "."));
+                })
+                .forEach((clz) -> {
+                    if (clz != null && clz.isInterface()) {
+                        Object mapper = factory.openSession().getMapper(clz);
+                        Aop.put(clz, mapper);
+                    }
+                });
     }
 }
