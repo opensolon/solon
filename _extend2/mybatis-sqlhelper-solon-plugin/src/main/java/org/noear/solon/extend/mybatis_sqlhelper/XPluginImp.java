3b6002c10528c6cbede8279ac0ed0efb67708341
noear <noear@live.cn>
2020-08-10 14:38:12 +0800

    1.0.9.3

--- _extend2/mybatis-sqlhelper-solon-plugin/src/main/java/org/noear/solon/extend/mybatis_sqlhelper/XPluginImp.java
+++ _extend2/mybatis-sqlhelper-solon-plugin/src/main/java/org/noear/solon/extend/mybatis_sqlhelper/XPluginImp.java
@@ -1,5 +1,6 @@
 package org.noear.solon.extend.mybatis_sqlhelper;
 
+import org.apache.ibatis.session.Configuration;
 import org.noear.solon.XApp;
 import org.noear.solon.core.*;
 
@@ -10,7 +11,9 @@ public class XPluginImp implements XPlugin {
         //
         //在插件里loadBean，会比业务代码里的 bean 优先加载
         //
-        app.loadBean(SqlHelperMybatisAutoConfiguration.class);
+        Aop.beanLoad(SqlHelperMybatisAutoConfiguration.class);
+
+        XEventBus.subscribe(Configuration.class, Aop.get(SqlHelperMybatisAutoConfiguration.class));
 
     }
 }
