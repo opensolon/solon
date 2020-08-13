f2a3776430350da2451ed46a0771da77ff32f8e5
noear <noear@live.cn>
2020-08-13 09:35:28 +0800

    1.0.11

--- solon/src/main/java/org/noear/solon/annotation/XEvent.java
+++ solon/src/main/java/org/noear/solon/annotation/XEvent.java
@@ -3,7 +3,7 @@ package org.noear.solon.annotation;
 import java.lang.annotation.*;
 
 /**
- * 事件订阅
+ * 事件订阅（注解在XEventListener的实现类上，才有效）
  * */
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
