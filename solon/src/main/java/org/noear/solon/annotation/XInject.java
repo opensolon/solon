b7da80ead3ba5cb03e8504418219d3c097a209c1
noear <noear@live.cn>
2020-08-13 10:17:04 +0800

    1.0.10

--- solon/src/main/java/org/noear/solon/annotation/XInject.java
+++ solon/src/main/java/org/noear/solon/annotation/XInject.java
@@ -6,6 +6,8 @@ import java.lang.annotation.*;
  * 注入
  *
  * 可注入到字段或参数（参数只在XConfiguration有效）
+ *
+ * 禁止注入在类型上；可避免让非单例bean的注入变复杂，进而避免影有响性能
  * */
 @Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
