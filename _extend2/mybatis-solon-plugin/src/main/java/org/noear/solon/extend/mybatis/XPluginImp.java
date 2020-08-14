6358dca983ffa777184cc2e34e32a1e50b11fdf8
noear <noear@live.cn>
2020-08-14 16:26:25 +0800

    1.0.11

--- _extend2/mybatis-solon-plugin/src/main/java/org/noear/solon/extend/mybatis/XPluginImp.java
+++ _extend2/mybatis-solon-plugin/src/main/java/org/noear/solon/extend/mybatis/XPluginImp.java
@@ -45,11 +45,6 @@ public class XPluginImp implements XPlugin {
                             return;
                         }
 
-                        if (MybatisProxy.class.isAssignableFrom(varH.getType())) {
-                            varH.setValue(MybatisProxy.get(factory));
-                            return;
-                        }
-
                         if (SqlSession.class.isAssignableFrom(varH.getType())) {
                             varH.setValue(MybatisProxy.get(factory));
                             return;
