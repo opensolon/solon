92c824914e75be7273816ae578029ba3a0dabc69
noear <noear@live.cn>
2020-08-14 14:26:59 +0800

    1.0.11

--- solon/src/main/java/org/noear/solon/core/TranPolicy.java
+++ solon/src/main/java/org/noear/solon/core/TranPolicy.java
@@ -3,7 +3,7 @@ package org.noear.solon.core;
 
 public enum TranPolicy {
     /**
-     * 需要，如果前面在事务则加入，否则新建事务
+     * 必须，如果前面在事务则加入，否则新建事务
      * */
     required,
 
@@ -11,4 +11,9 @@ public enum TranPolicy {
      * 支持，如果前面不存在事务就不使用事务
      * */
     supports,
+
+    /**
+     * 排除
+     * */
+    exclude,
 }
