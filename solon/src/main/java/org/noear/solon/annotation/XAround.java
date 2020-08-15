b5ad06dc936160cde305591ad5df382d489ddbaf
noear <noear@live.cn>
2020-08-15 15:42:43 +0800

    1.0.11

--- solon/src/main/java/org/noear/solon/annotation/XAround.java
+++ solon/src/main/java/org/noear/solon/annotation/XAround.java
@@ -3,6 +3,9 @@ package org.noear.solon.annotation;
 import java.lang.annotation.*;
 import java.lang.reflect.InvocationHandler;
 
+/**
+ * 触发器：围绕处理（替代处理）
+ * */
 @Inherited //要可继承
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
