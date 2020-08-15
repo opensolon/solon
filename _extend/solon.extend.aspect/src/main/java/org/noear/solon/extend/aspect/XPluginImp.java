16973ddd29d507430e9574202f39632dff7f6142
noear <noear@live.cn>
2020-08-15 15:37:04 +0800

    1.0.11

--- _extend/solon.extend.aspect/src/main/java/org/noear/solon/extend/aspect/XPluginImp.java
+++ _extend/solon.extend.aspect/src/main/java/org/noear/solon/extend/aspect/XPluginImp.java
@@ -3,13 +3,18 @@ package org.noear.solon.extend.aspect;
 import org.noear.solon.XApp;
 import org.noear.solon.core.Aop;
 import org.noear.solon.core.XPlugin;
+import org.noear.solon.extend.aspect.annotation.XDao;
 import org.noear.solon.extend.aspect.annotation.XService;
 
 public class XPluginImp implements XPlugin {
     @Override
     public void start(XApp app) {
-        Aop.factory().beanCreatorAdd(XService.class, (clz, wrap, anno) -> {
-            wrap.proxySet(BeanProxyImp.global());
+        Aop.factory().beanCreatorAdd(XDao.class, (clz, bw, anno) -> {
+            bw.proxySet(BeanProxyImp.global());
+        });
+
+        Aop.factory().beanCreatorAdd(XService.class, (clz, bw, anno) -> {
+            bw.proxySet(BeanProxyImp.global());
         });
     }
 }
---
+++ _extend/solon.extend.aspect/src/main/java/org/noear/solon/extend/aspect/annotation/XDao.java
@@ -0,0 +1,9 @@
+package org.noear.solon.extend.aspect.annotation;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface XDao {
+}
