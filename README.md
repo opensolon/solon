381c563795d702b0d91d21a9d1d5f76b46c9bdd3
noear <noear@live.cn>
2020-08-20 20:37:09 +0800

    1.0.14.1

--- README.md
+++ README.md
@@ -64,7 +64,7 @@ public class App{
 <parent>
     <groupId>org.noear</groupId>
     <artifactId>solon-parent</artifactId>
-    <version>1.0.14</version>
+    <version>1.0.14.1</version>
 </parent>
 
 <dependencies>
--- _extend/solon.boot.smarthttp/src/main/java/org/noear/solon/boot/smarthttp/XPluginImp.java
+++ _extend/solon.boot.smarthttp/src/main/java/org/noear/solon/boot/smarthttp/XPluginImp.java
@@ -9,7 +9,7 @@ public final class XPluginImp implements XPlugin {
     HttpBootstrap _server = null;
 
     public static String solon_boot_ver(){
-        return "smart http 1.0.14/1.0.14";
+        return "smart http 1.0.14.1/1.0.14.1";
     }
 
     @Override
@@ -28,7 +28,7 @@ public final class XPluginImp implements XPlugin {
         _server.pipeline().next(_handler);
 
 
-        System.out.println("solon.Server:main: SmartHttpServer 1.0.14");
+        System.out.println("solon.Server:main: SmartHttpServer 1.0.14.1");
 
         try {
 
