0e3a04bdeff4d6808a7e7e10e7bf08371a92ccb9
noear <noear@live.cn>
2020-08-20 20:19:17 +0800

    1.0.15

--- _extend/solon.extend.aspect/src/main/java/org/noear/solon/extend/aspect/XPluginImp.java
+++ _extend/solon.extend.aspect/src/main/java/org/noear/solon/extend/aspect/XPluginImp.java
@@ -11,10 +11,12 @@ public class XPluginImp implements XPlugin {
     public void start(XApp app) {
         Aop.factory().beanCreatorAdd(XDao.class, (clz, bw, anno) -> {
             bw.proxySet(BeanProxyImp.global());
+            Aop.factory().beanNotice(clz,bw);
         });
 
         Aop.factory().beanCreatorAdd(XService.class, (clz, bw, anno) -> {
             bw.proxySet(BeanProxyImp.global());
+            Aop.factory().beanNotice(clz,bw);
         });
     }
 }
