442fdffaecc13475f0d628e37db65cdc801db865
noear <noear@live.cn>
2020-08-21 12:18:39 +0800

    1.0.15

---
+++ solon-test/src/main/java/org/noear/solon/test/HttpTestBase.java
@@ -0,0 +1,13 @@
+package org.noear.solon.test;
+
+import org.noear.solon.XApp;
+
+public class HttpTestBase {
+    public HttpUtils local(String path) {
+        return http("http://localhost:" + XApp.global().port() + path);
+    }
+
+    public HttpUtils http(String url) {
+        return HttpUtils.http(url);
+    }
+}
