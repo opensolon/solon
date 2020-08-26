1aa8b23200b0d6c7a3ba38ee52cbe0d0b090b4c6
noear <noear@live.cn>
2020-08-26 09:39:33 +0800

    1.0.16

--- solon/src/main/java/org/noear/solon/core/AopFactory.java
+++ solon/src/main/java/org/noear/solon/core/AopFactory.java
@@ -107,7 +107,8 @@ public class AopFactory extends AopFactoryBase {
         }
 
         //扫描类文件并处理（采用两段式加载，可以部分bean先处理；剩下的为第二段处理）
-        XScaner.scan(dir, n -> n.endsWith(".class")).forEach(name -> {
+        XScaner.scan(dir, n -> n.endsWith(".class"))
+                .stream().sorted(Comparator.comparing(s -> s.length())).forEach(name -> {
             String className = name.substring(0, name.length() - 6);
 
             Class<?> clz = XUtil.loadClass(className.replace("/", "."));
