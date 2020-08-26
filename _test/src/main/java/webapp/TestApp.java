578d1601fa2c72df68c6d201530703dabc63c6b4
noear <noear@live.cn>
2020-08-26 20:53:59 +0800

    1.0.18

--- _test/src/main/java/webapp/TestApp.java
+++ _test/src/main/java/webapp/TestApp.java
@@ -29,24 +29,7 @@ public class TestApp {
          * */
         XApp app = XApp.start(TestApp.class, args);
 
-        app.add("/demo2x/param", ParamController.class);
-        app.add("/demo2x/mapping", MappingController.class);
 
-        app.get("/",c->c.redirect("/debug.htm"));
-
-        app.before("@@",x -> x.output("test"));
-
-        app.plug(new XPlugin() {
-            @Override
-            public void start(XApp app) {
-
-            }
-
-            @Override
-            public void stop() throws Throwable {
-                System.out.println("通知你一下，我现在要停了");
-            }
-        });
 
 
         //socket client test
---
+++ _test/src/main/java/webapp/TestExt.java
@@ -0,0 +1,32 @@
+package webapp;
+
+import org.noear.solon.XApp;
+import org.noear.solon.annotation.XBean;
+import org.noear.solon.core.XPlugin;
+import webapp.demo2_mvc.MappingController;
+import webapp.demo2_mvc.ParamController;
+
+@XBean
+public class TestExt implements XPlugin {
+    @Override
+    public void start(XApp app) {
+        app.add("/demo2x/param", ParamController.class);
+        app.add("/demo2x/mapping", MappingController.class);
+
+        app.get("/",c->c.redirect("/debug.htm"));
+
+        app.before("@@",x -> x.output("test"));
+
+        app.plug(new XPlugin() {
+            @Override
+            public void start(XApp app) {
+
+            }
+
+            @Override
+            public void stop() throws Throwable {
+                System.out.println("通知你一下，我现在要停了");
+            }
+        });
+    }
+}
