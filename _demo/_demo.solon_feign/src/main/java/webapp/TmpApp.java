75304eeb3f1c930d1b2f7558d7fa346cab6fd4a8
noear <noear@live.cn>
2020-08-26 10:40:18 +0800

    1.0.17

--- _demo/_demo.solon_feign/src/main/java/webapp/TmpApp.java
+++ _demo/_demo.solon_feign/src/main/java/webapp/TmpApp.java
@@ -1,11 +1,20 @@
 package webapp;
 
 import org.noear.solon.XApp;
+import org.noear.solon.core.XBridge;
 import org.noear.solon.extend.feign.EnableFeignClients;
 
 @EnableFeignClients
 public class TmpApp {
     public static void main(String[] args) {
-        XApp.start(TmpApp.class, args);
+        XApp.start(TmpApp.class, args, (app) -> {
+            XBridge.upstreamFactorySet((name) -> {
+                if ("user-service".equals(name)) {
+                    return () -> "http://127.0.0.1:8080";
+                } else {
+                    return null;
+                }
+            });
+        });
     }
 }
