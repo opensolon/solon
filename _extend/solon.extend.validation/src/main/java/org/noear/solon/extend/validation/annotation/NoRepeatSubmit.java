57eea843ce08ff5bd16a03ba98057ac5093b6408
noear <noear@live.cn>
2020-09-07 12:20:06 +0800

    1.0.22

--- _extend/solon.extend.validation/src/main/java/org/noear/solon/extend/validation/annotation/NoRepeatSubmit.java
+++ _extend/solon.extend.validation/src/main/java/org/noear/solon/extend/validation/annotation/NoRepeatSubmit.java
@@ -2,7 +2,7 @@ package org.noear.solon.extend.validation.annotation;
 
 import java.lang.annotation.*;
 
-@Target({ElementType.METHOD})
+@Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface NoRepeatSubmit {
     HttpPart[] value() default {HttpPart.params};
--- _extend/solon.extend.validation/src/main/java/org/noear/solon/extend/validation/annotation/Whitelist.java
+++ _extend/solon.extend.validation/src/main/java/org/noear/solon/extend/validation/annotation/Whitelist.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.METHOD})
+@Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Whitelist {
     @XNote("whitelist tag")
