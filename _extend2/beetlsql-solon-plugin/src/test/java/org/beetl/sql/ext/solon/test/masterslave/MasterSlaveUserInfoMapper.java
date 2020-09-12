9061055eee24cfb767b66f70b0a22d1a895017cb
noear <noear@live.cn>
2020-09-12 21:25:59 +0800

    1.0.26

--- _extend2/beetlsql-solon-plugin/src/test/java/org/beetl/sql/ext/solon/test/masterslave/MasterSlaveUserInfoMapper.java
+++
@@ -1,7 +0,0 @@
-package org.beetl.sql.ext.solon.test.masterslave;
-
-import org.beetl.sql.ext.solon.test.UserInfo;
-import org.beetl.sql.mapper.BaseMapper;
-
-public interface MasterSlaveUserInfoMapper extends BaseMapper<UserInfo> {
-}
--- _extend2/beetlsql-solon-plugin/src/test/java/org/beetl/sql/ext/solon/test/simple/SimpleUserInfoMapper.java
+++
@@ -1,7 +0,0 @@
-package org.beetl.sql.ext.solon.test.simple;
-
-import org.beetl.sql.ext.solon.test.UserInfo;
-import org.beetl.sql.mapper.BaseMapper;
-
-public interface SimpleUserInfoMapper extends BaseMapper<UserInfo> {
-}
