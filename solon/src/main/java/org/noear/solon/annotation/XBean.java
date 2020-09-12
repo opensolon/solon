915fa2294abe7ce09fa74f02bf9199014256c824
noear <noear@live.cn>
2020-09-12 23:48:30 +0800

    1.0.26

--- solon/src/main/java/org/noear/solon/annotation/XBean.java
+++ solon/src/main/java/org/noear/solon/annotation/XBean.java
@@ -18,6 +18,9 @@ public @interface XBean {
     @XNote("标签")
     String tags() default "";
 
+    @XNote("元信息")
+    String meta() default "";
+
     @XNote("同时注册类型，仅当名称非空时有效")
     boolean typed() default false;
 
