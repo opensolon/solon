687f50c7c20feaee907afc9e9c147c81e48c2134
noear <noear@live.cn>
2020-09-16 22:28:48 +0800

    1.0.29

--- _extend2/beetlsql-solon-plugin/src/main/java/org/noear/solon/extend/beetlsql/DsEventListener.java
+++ _extend2/beetlsql-solon-plugin/src/main/java/org/noear/solon/extend/beetlsql/DsEventListener.java
@@ -2,7 +2,6 @@ package org.noear.solon.extend.beetlsql;
 
 import org.noear.solon.core.BeanWrap;
 import org.noear.solon.core.XEventListener;
-import org.noear.solon.extend.beetlsql.DbManager;
 
 import javax.sql.DataSource;
 
@@ -11,7 +10,7 @@ class DsEventListener implements XEventListener<BeanWrap> {
     @Override
     public void onEvent(BeanWrap bw) {
         if (bw.raw() instanceof DataSource) {
-            DbManager.global().get(bw.name(), bw);
+            DbManager.global().get(bw);
         }
     }
 }
