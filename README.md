1923b71ffef3cb4d1a528737ccdb2c63034a672b
noear <noear@live.cn>
2020-09-26 11:37:08 +0800

    1.0.36

--- README.md
+++ README.md
@@ -66,7 +66,7 @@ public class App{
 <parent>
     <groupId>org.noear</groupId>
     <artifactId>solon-parent</artifactId>
-    <version>1.0.35</version>
+    <version>1.0.36</version>
 </parent>
 
 <dependencies>
--- _extend/solon.boot.jdkhttp/src/main/java/org/noear/solon/boot/jdkhttp/XPluginImp.java
+++ _extend/solon.boot.jdkhttp/src/main/java/org/noear/solon/boot/jdkhttp/XPluginImp.java
@@ -12,7 +12,7 @@ public final class XPluginImp implements XPlugin {
     private HttpServer _server = null;
 
     public static String solon_boot_ver(){
-        return "jdk http jdk8/1.0.35";
+        return "jdk http jdk8/1.0.36";
     }
 
     @Override
