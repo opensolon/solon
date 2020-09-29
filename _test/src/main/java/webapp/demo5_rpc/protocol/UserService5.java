a40de4d5ba712703fcb3890f962d0806b8388611
noear <noear@live.cn>
2020-09-29 23:21:29 +0800

    1.0.40

--- _test/src/main/java/webapp/demo5_rpc/protocol/UserService5.java
+++ _test/src/main/java/webapp/demo5_rpc/protocol/UserService5.java
@@ -3,6 +3,6 @@ package webapp.demo5_rpc.protocol;
 import org.noear.fairy.annotation.Mapping;
 
 public interface UserService5 {
-    @Mapping("user/getUser")
+    @Mapping("PUT user/getUser")
     UserModel xxx(Integer userId);
 }
