0fd1ca4033bc9066af7779fd3a8ff7cc0f31228e
noear <noear@live.cn>
2020-09-30 10:29:32 +0800

    1.0.40

--- _demo/_demo.solon_hessian/src/test/java/demo/ProtobufTest.java
+++ _demo/_demo.solon_hessian/src/test/java/demo/ProtobufTest.java
@@ -20,22 +20,22 @@ public class ProtobufTest {
 
     @Test
     public void test2() throws Exception {
-        Map<String,Object> map = new HashMap<>();
-        map.put("name","niko");
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "niko");
 
         byte[] tmp = ProtoBuf.ser(map);
 
-        assert ((Map)ProtoBuf.der(tmp)).size() == 1;
+        assert ((Map) ProtoBuf.der(tmp)).size() == 1;
     }
 
     @Test
     public void test3() throws Exception {
-        Map<String,Object> map = new LinkedHashMap<>();
-        map.put("name","niko");
+        Map<String, Object> map = new LinkedHashMap<>();
+        map.put("name", "niko");
 
         byte[] tmp = ProtoBuf.ser(map);
 
-        assert ((Map)ProtoBuf.der(tmp)).size() == 1;
+        assert ((Map) ProtoBuf.der(tmp)).size() == 1;
     }
 
     @Test
@@ -66,6 +66,43 @@ public class ProtobufTest {
 
         byte[] tmp = ProtoBuf.ser(model);
 
-        assert ((ComplexModel<Point>)ProtoBuf.der(tmp)).getPoints().size() > 1;
+        assert ((ComplexModel<Point>) ProtoBuf.der(tmp)).getPoints().size() > 1;
+    }
+
+    @Test
+    public void test5() throws Exception {
+        ComplexModel<Point> model = new ComplexModel<Point>();
+        model.setId(1);
+        Person person = new Person();
+        person.setName("Tom");
+        person.setAge(86);
+        person.setBirthDay(new Date());
+        person.setSensitiveInformation("This should be private over the wire");
+        model.setPerson(person);
+
+        List<Point> points = new ArrayList<Point>();
+        Point point = new Point();
+        point.setX(3);
+        point.setY(4);
+        points.add(point);
+
+        point = new Point();
+        point.setX(100);
+        point.setY(100);
+        points.add(point);
+
+        //远程方法调用
+        model.setPoints(points);
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("model", model);
+
+
+        byte[] tmp = ProtoBuf.ser(map);
+
+        Map<String, Object> rst = (Map<String, Object>) ProtoBuf.der(tmp);
+
+        assert rst.size() == 1;
+        assert ((ComplexModel<Point>) rst.get("model")).getPoints().size() > 1;
     }
 }
