dcc8b388be29d0fbea83301eb426d47fcc9819cb
noear <noear@live.cn>
2020-10-02 19:33:37 +0800

    1.0.40

--- _test/src/main/java/webapp/demoh_xsocket/XSocketListenerImp.java
+++ _test/src/main/java/webapp/demoh_xsocket/XSocketListenerImp.java
@@ -1,6 +1,8 @@
 package webapp.demoh_xsocket;
 
+import org.noear.solon.XApp;
 import org.noear.solon.annotation.XBean;
+import org.noear.solon.core.XSignal;
 import org.noear.solonx.socket.api.XSession;
 import org.noear.solonx.socket.api.XSocketListener;
 import org.noear.solonx.socket.api.XSocketMessage;
@@ -9,15 +11,21 @@ import org.noear.solonx.socket.api.XSocketMessage;
 public class XSocketListenerImp implements XSocketListener {
     @Override
     public void onMessage(XSession session, XSocketMessage message) {
-        //System.out.println("X我收到了::" + message.toString());
-        //session.send("X我收到了::" + message.toString());
-
         System.out.println(message.resourceDescriptor());
 
-        message.setHandled(true);
+        if(XApp.cfg().isDebugMode()){
+            return;
+        }
 
-        session.getOpenSessions().forEach(s -> {
-            s.send(message.toString());
-        });
+        if (session.signal() == XSignal.WEBSOCKET) {
+            message.setHandled(true);
+
+            session.getOpenSessions().forEach(s -> {
+                s.send(message.toString());
+            });
+        } else {
+            System.out.println("X我收到了::" + message.toString());
+            //session.send("X我收到了::" + message.toString());
+        }
     }
 }
