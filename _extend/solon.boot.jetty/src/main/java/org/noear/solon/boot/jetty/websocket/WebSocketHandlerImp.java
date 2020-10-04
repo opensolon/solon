b3d91fff0b610158ce976a7ae95695b3a4a7f841
noear <noear@live.cn>
2020-10-04 12:03:00 +0800

    1.0.40

--- _extend/solon.boot.jetty/src/main/java/org/noear/solon/boot/jetty/websocket/WebSocketHandlerImp.java
+++ _extend/solon.boot.jetty/src/main/java/org/noear/solon/boot/jetty/websocket/WebSocketHandlerImp.java
@@ -1,13 +1,8 @@
 package org.noear.solon.boot.jetty.websocket;
 
-import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.websocket.server.WebSocketHandler;
 import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public class WebSocketHandlerImp extends WebSocketHandler {
 
@@ -18,11 +13,6 @@ public class WebSocketHandlerImp extends WebSocketHandler {
 
         //注册览听器
         factory.register(WebSocketListenerImp.class);
-    }
-
-    @Override
-    public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        super.handle(target, baseRequest, request, response);
     }
 }
 
