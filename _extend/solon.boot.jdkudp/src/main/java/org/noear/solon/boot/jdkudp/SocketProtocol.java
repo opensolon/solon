f3827b3810e4f490d3a754aa6a89676679754f6d
noear <noear@live.cn>
2020-10-09 16:13:14 +0800

    1.0.43

---
+++ _extend/solon.boot.jdkudp/src/main/java/org/noear/solon/boot/jdkudp/SocketProtocol.java
@@ -0,0 +1,57 @@
+package org.noear.solon.boot.jdkudp;
+
+import org.noear.solon.core.XMessage;
+import org.noear.solon.extend.xsocket.XMessageUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+
+public class SocketProtocol {
+    public XMessage decode(InputStream input) throws IOException {
+        if(input == null){
+            return null;
+        }
+
+        byte[] lenBts = new byte[4];
+        if (input.read(lenBts) < -1) {
+            return null;
+        }
+
+        int len = bytesToInt32(lenBts);
+
+        if(len < 6){
+            return null;
+        }
+
+        byte[] bytes = new byte[len];
+        bytes[0] = lenBts[0];
+        bytes[1] = lenBts[1];
+        bytes[2] = lenBts[2];
+        bytes[3] = lenBts[3];
+
+        input.read(bytes, 4, len - 4);
+
+        return XMessageUtils.decode(ByteBuffer.wrap(bytes));
+    }
+
+
+    private static int bytesToInt32(byte[] bs) {
+        //获取最高八位
+        int num1 = 0;
+        num1 = (num1 ^ (int) bs[0]);
+        num1 = num1 << 24;
+        //获取第二高八位
+        int num2 = 0;
+        num2 = (num2 ^ (int) bs[1]);
+        num2 = num2 << 16;
+        //获取第二低八位
+        int num3 = 0;
+        num3 = (num3 ^ (int) bs[2]);
+        num3 = num3 << 8;
+        //获取低八位
+        int num4 = 0;
+        num4 = (num4 ^ (int) bs[3]);
+        return num1 ^ num2 ^ num3 ^ num4;
+    }
+}
---
+++ _extend/solon.boot.jdkudp/src/main/java/org/noear/solon/boot/jdkudp/SocketServer.java
@@ -0,0 +1,87 @@
+package org.noear.solon.boot.jdkudp;
+
+import org.noear.solon.core.XListener;
+import org.noear.solon.core.XMessage;
+import org.noear.solon.core.XMethod;
+import org.noear.solon.core.XSession;
+import org.noear.solon.extend.xsocket.XListenerProxy;
+import org.noear.solon.extend.xsocket.XSocketContextHandler;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SocketServer {
+    private ServerSocket server;
+    private SocketProtocol protocol;
+    private ExecutorService pool = Executors.newCachedThreadPool();
+
+    private XSocketContextHandler handler = new XSocketContextHandler(XMethod.SOCKET);
+    private XListener listener = XListenerProxy.getGlobal();
+
+    public void setProtocol(SocketProtocol protocol) {
+        this.protocol = protocol;
+    }
+
+    public void start(int port) {
+        new Thread(() -> {
+            try {
+                start0(port);
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+        }).start();
+    }
+
+    private void start0(int port) throws IOException {
+        server = new ServerSocket(port);
+
+        System.out.println("Server started, waiting for customer connection...");
+
+        while (true) {
+            Socket socket = server.accept();
+
+            XSession session = _SocketSession.get(socket);
+            listener.onOpen(session);
+
+            pool.execute(() -> {
+                while (true) {
+                    if (socket.isClosed()) {
+                        listener.onClose(session);
+                        break;
+                    }
+
+                    XMessage message = _SocketSession.receive(socket, protocol);
+                    if (message != null) {
+                        pool.execute(() -> {
+                            try {
+                                listener.onMessage(session, message);
+
+                                if(message.getHandled() == false){
+                                    handler.handle(session,message,false);
+                                }
+                            } catch (Throwable ex) {
+                                listener.onError(session, ex);
+                            }
+                        });
+                    }
+
+                }
+            });
+        }
+    }
+
+    public void stop() {
+        if (server.isClosed()) {
+            return;
+        }
+
+        try {
+            server.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
