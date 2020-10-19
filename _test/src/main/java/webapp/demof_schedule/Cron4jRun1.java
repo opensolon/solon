e29761ddd46abe9179307bcce40c7d12d29d1f5c
noear <noear@live.cn>
2020-10-19 16:31:40 +0800

    1.1.5

--- _test/src/main/java/webapp/demof_schedule/Cron4jRun1.java
+++ _test/src/main/java/webapp/demof_schedule/Cron4jRun1.java
@@ -5,12 +5,13 @@ import org.noear.solon.extend.cron4j.Cron4j;
 
 import java.util.Date;
 
-//@Cron4j(cron5x = "200ms")
+@Cron4j(cron5x = "200ms", name = "Cron4jRun1")
 public class Cron4jRun1 implements Runnable {
 
     @Override
     public void run() {
         System.out.println("我是定时任务: Cron4jRun1(200ms) -- " + new Date().toString());
         //throw new RuntimeException("异常");
+        System.out.println("如果间隔太长，我可能被配置给控制了");
     }
 }
--- _test/src/main/java/webapp/demof_schedule/QuartzRun1.java
+++ _test/src/main/java/webapp/demof_schedule/QuartzRun1.java
@@ -4,11 +4,12 @@ import org.noear.solon.extend.quartz.Quartz;
 
 import java.util.Date;
 
-@Quartz(cron7x = "200ms")
+@Quartz(cron7x = "200ms" ,name = "QuartzRun1")
 public class QuartzRun1 implements Runnable {
     @Override
     public void run() {
         System.out.println("我是定时任务: QuartzRun1(200ms) -- " + new Date().toString());
         //throw new RuntimeException("异常");
+        System.out.println("如果间隔太长，我可能被配置给控制了");
     }
 }
