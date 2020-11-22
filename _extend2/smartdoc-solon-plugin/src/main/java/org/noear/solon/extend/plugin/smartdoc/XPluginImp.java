bab01e0859b3cea2c2e3a810eb60e9424c39d6f7
noear <noear@live.cn>
2020-11-22 20:08:18 +0800

    1.2.1

--- _extend2/smartdoc-solon-plugin/src/main/java/org/noear/solon/extend/plugin/smartdoc/XPluginImp.java
+++ _extend2/smartdoc-solon-plugin/src/main/java/org/noear/solon/extend/plugin/smartdoc/XPluginImp.java
@@ -5,7 +5,7 @@ import org.noear.solon.core.Plugin;
 
 public class XPluginImp implements Plugin {
     @Override
-    public void start(Solon app) {
+    public void start(SolonApp app) {
 
     }
 }
