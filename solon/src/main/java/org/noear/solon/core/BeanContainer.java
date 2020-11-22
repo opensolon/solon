0f193be7a8fe10cc97ca4b7dfa74cc944bc5bf15
noear <noear@live.cn>
2020-11-22 20:50:11 +0800

    1.2.1

--- solon/src/main/java/org/noear/solon/core/BeanContainer.java
+++ solon/src/main/java/org/noear/solon/core/BeanContainer.java
@@ -19,7 +19,7 @@ import java.util.function.Consumer;
  * @author noear
  * @since 1.0
  * */
-public class BeanContainer {
+public abstract class BeanContainer {
     //////////////////////////
     //
     // 容器存储
