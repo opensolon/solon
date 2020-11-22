9e26a6ec727f381dd61b83f9232af4a3fc453491
noear <noear@live.cn>
2020-11-22 20:09:33 +0800

    1.2.1

--- _extend2/smartdoc-solon-plugin/src/main/java/org/noear/solon/extend/plugin/smartdoc/XPluginImp.java
+++ _extend2/smartdoc-solon-plugin/src/main/java/org/noear/solon/extend/plugin/smartdoc/XPluginImp.java
@@ -1,6 +1,7 @@
 package org.noear.solon.extend.plugin.smartdoc;
 
-import org.noear.solon.Solon;
+
+import org.noear.solon.SolonApp;
 import org.noear.solon.core.Plugin;
 
 public class XPluginImp implements Plugin {
