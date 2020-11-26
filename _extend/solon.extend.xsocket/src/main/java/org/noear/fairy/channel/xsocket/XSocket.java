e40f7b132f97d26c05a57abe7ef338c62d10842e
noear <noear@live.cn>
2020-11-26 19:43:09 +0800

    1.2.1

--- _extend/solon.extend.xsocket/src/main/java/org/noear/fairy/channel/xsocket/XSocket.java
+++ _extend/solon.extend.xsocket/src/main/java/org/noear/fairy/channel/xsocket/XSocket.java
@@ -1,30 +1,29 @@
 package org.noear.fairy.channel.xsocket;
 
 import org.noear.fairy.Fairy;
+import org.noear.fairy.IDecoder;
+import org.noear.fairy.IEncoder;
 import org.noear.fairy.decoder.SnackDecoder;
 import org.noear.fairy.encoder.SnackEncoder;
 import org.noear.solon.core.message.Session;
 import org.noear.solon.extend.xsocket.SessionFactory;
 
 public class XSocket {
-    public static <T> T get(String host, int port, Class<T> service) {
+    public static <T> T create(String host, int port, Class<T> service) {
         Session session = SessionFactory.create(host, port, true);
-        SocketChannel channel = new SocketChannel(() -> session);
-
-        return Fairy.builder()
-                .encoder(SnackEncoder.instance)
-                .decoder(SnackDecoder.instance)
-                .upstream(() -> "tpc://xsocket")
-                .channel(channel)
-                .create(service);
+        return create(session, service);
     }
 
-    public static <T> T get(Session session, Class<T> service) {
+    public static <T> T create(Session session, Class<T> service) {
+        return create(session, SnackEncoder.instance, SnackDecoder.instance, service);
+    }
+
+    public static <T> T create(Session session, IEncoder encoder, IDecoder decoder, Class<T> service) {
         SocketChannel channel = new SocketChannel(() -> session);
 
         return Fairy.builder()
-                .encoder(SnackEncoder.instance)
-                .decoder(SnackDecoder.instance)
+                .encoder(encoder)
+                .decoder(decoder)
                 .upstream(() -> "tpc://xsocket")
                 .channel(channel)
                 .create(service);
