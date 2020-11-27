ce7d7a40a555ea20dcd4484da1ab19ccbe7db1c6
noear <noear@live.cn>
2020-11-27 08:04:43 +0800

    1.2.2

--- _extend/solon.extend.xsocket/src/main/java/org/noear/fairy/channel/xsocket/XSocket.java
+++ _extend/solon.extend.xsocket/src/main/java/org/noear/fairy/channel/xsocket/XSocket.java
@@ -5,6 +5,7 @@ import org.noear.fairy.Decoder;
 import org.noear.fairy.Encoder;
 import org.noear.fairy.decoder.SnackDecoder;
 import org.noear.fairy.encoder.SnackEncoder;
+import org.noear.solon.core.handle.Context;
 import org.noear.solon.core.message.Session;
 import org.noear.solon.extend.xsocket.SessionFactory;
 
@@ -12,6 +13,14 @@ public class XSocket {
     public static <T> T create(String host, int port, Class<T> service) {
         Session session = SessionFactory.create(host, port, true);
         return create(session, service);
+    }
+
+    public static <T> T create(Context context, Class<T> service) {
+        if (context.request() instanceof Session) {
+            return create((Session) context.request(), SnackEncoder.instance, SnackDecoder.instance, service);
+        } else {
+            throw new IllegalArgumentException("Request context nonsupport xsocket");
+        }
     }
 
     public static <T> T create(Session session, Class<T> service) {
