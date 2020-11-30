740f562cfacd557b119e4ca2967e8684fb988171
noear <noear@live.cn>
2020-11-30 19:21:53 +0800

    1.2.2

--- _extend/solon.extend.xsocket/src/main/java/org/noear/nami/channel/xsocket/XSocket.java
+++ _extend/solon.extend.xsocket/src/main/java/org/noear/nami/channel/xsocket/XSocket.java
@@ -4,7 +4,7 @@ import org.noear.nami.Decoder;
 import org.noear.nami.Encoder;
 import org.noear.nami.Nami;
 import org.noear.nami.decoder.SnackDecoder;
-import org.noear.nami.encoder.SnackEncoder;
+import org.noear.nami.encoder.SnackTypeEncoder;
 import org.noear.solon.core.handle.Context;
 import org.noear.solon.core.message.Session;
 import org.noear.solon.extend.xsocket.SessionFactory;
@@ -17,14 +17,14 @@ public class XSocket {
 
     public static <T> T create(Context context, Class<T> service) {
         if (context.request() instanceof Session) {
-            return create((Session) context.request(), SnackEncoder.instance, SnackDecoder.instance, service);
+            return create((Session) context.request(), SnackTypeEncoder.instance, SnackDecoder.instance, service);
         } else {
             throw new IllegalArgumentException("Request context nonsupport xsocket");
         }
     }
 
     public static <T> T create(Session session, Class<T> service) {
-        return create(session, SnackEncoder.instance, SnackDecoder.instance, service);
+        return create(session, SnackTypeEncoder.instance, SnackDecoder.instance, service);
     }
 
     public static <T> T create(Session session, Encoder encoder, Decoder decoder, Class<T> service) {
