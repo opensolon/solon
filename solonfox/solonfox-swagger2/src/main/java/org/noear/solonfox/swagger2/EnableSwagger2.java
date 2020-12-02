818ba5af42b685cb31ba2f947b94edb98496103e
noear <noear@live.cn>
2020-12-02 23:01:16 +0800

    1.2.5

---
+++ solonfox/solonfox-swagger2/src/main/java/org/noear/solonfox/swagger2/EnableSwagger2.java
@@ -0,0 +1,9 @@
+package org.noear.solonfox.swagger2;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface EnableSwagger2 {
+}
