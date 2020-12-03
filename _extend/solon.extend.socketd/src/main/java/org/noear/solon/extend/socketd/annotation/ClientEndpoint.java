72f2ae8c37380787ef78ece65c1c6ed68fff53a0
noear <noear@live.cn>
2020-12-03 15:26:45 +0800

    1.2.5

--- _extend/solon.extend.socketd/src/main/java/org/noear/solon/extend/socketd/annotation/ClientEndpoint.java
+++ _extend/solon.extend.socketd/src/main/java/org/noear/solon/extend/socketd/annotation/ClientEndpoint.java
@@ -1,11 +1,9 @@
 package org.noear.solon.extend.socketd.annotation;
 
+import org.noear.solon.annotation.Note;
 import org.noear.solon.core.handle.MethodType;
-import org.noear.solon.core.message.Message;
-import org.noear.solon.extend.socketd.MessageWrapper;
 
 import java.lang.annotation.*;
-import java.util.function.Supplier;
 
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
@@ -34,10 +32,12 @@ public @interface ClientEndpoint {
     /**
      * 握手包头
      * */
+    @Note("握手包头")
     String handshakeHeader() default "";
 
     /**
      * 心跳频率（单位：秒）
      */
+    @Note("心跳频率（单位：秒）")
     int heartbeatRate() default 30;
 }
