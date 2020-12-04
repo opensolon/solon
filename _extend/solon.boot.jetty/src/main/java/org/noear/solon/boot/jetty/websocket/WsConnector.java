12540cac96a07d481a7b3ae341b4e2074bf1a76d
noear <noear@live.cn>
2020-12-04 08:53:18 +0800

    1.2.5

--- _extend/solon.boot.jetty/src/main/java/org/noear/solon/boot/jetty/websocket/WsConnector.java
+++ _extend/solon.boot.jetty/src/main/java/org/noear/solon/boot/jetty/websocket/WsConnector.java
@@ -1,7 +1,19 @@
 package org.noear.solon.boot.jetty.websocket;
 
-public class WsConnector {
-    public WsConnector(String host, int port){
+import org.noear.solon.core.message.Session;
 
+public class WsConnector {
+    public WsConnector(String host, int port) {
+
+    }
+
+    Session start() {
+//        WebSocketClient client = new WebSocketClient();
+
+
+//         WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+//         String uri = "ws://localhost:8080/broadcast";
+
+        return null;
     }
 }
