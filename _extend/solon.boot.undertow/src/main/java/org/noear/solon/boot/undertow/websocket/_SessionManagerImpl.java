984550035f3f39dbbc507be852a17d680432d25d
noear <noear@live.cn>
2020-12-04 18:19:41 +0800

    1.2.5

--- _extend/solon.boot.undertow/src/main/java/org/noear/solon/boot/undertow/websocket/_SessionManagerImpl.java
+++ _extend/solon.boot.undertow/src/main/java/org/noear/solon/boot/undertow/websocket/_SessionManagerImpl.java
@@ -1,23 +1,33 @@
 package org.noear.solon.boot.undertow.websocket;
 
+import io.undertow.websockets.core.WebSocketChannel;
 import org.noear.solon.core.message.Session;
 import org.noear.solon.extend.socketd.SessionManager;
 
 import java.util.Collection;
+import java.util.Collections;
 
 public class _SessionManagerImpl extends SessionManager {
     @Override
     protected Session getSession(Object conn) {
-        return null;
+        if (conn instanceof WebSocketChannel) {
+            return _SocketSession.get((WebSocketChannel) conn);
+        } else {
+            throw new IllegalArgumentException("This conn requires a undertow WebSocketChannel type");
+        }
     }
 
     @Override
     protected Collection<Session> getOpenSessions() {
-        return null;
+        return Collections.unmodifiableCollection(_SocketSession.sessions.values());
     }
 
     @Override
     protected void removeSession(Object conn) {
-
+        if (conn instanceof WebSocketChannel) {
+            _SocketSession.remove((WebSocketChannel) conn);
+        } else {
+            throw new IllegalArgumentException("This conn requires a undertow WebSocketChannel type");
+        }
     }
 }
