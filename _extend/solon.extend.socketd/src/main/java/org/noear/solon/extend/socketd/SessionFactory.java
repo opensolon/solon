01868fc9cbfff977f0e8b16e59c9be6b752fae12
noear <noear@live.cn>
2020-12-04 12:28:25 +0800

    1.2.5

--- _extend/solon.extend.socketd/src/main/java/org/noear/solon/extend/socketd/SessionFactory.java
+++ _extend/solon.extend.socketd/src/main/java/org/noear/solon/extend/socketd/SessionFactory.java
@@ -4,6 +4,12 @@ import org.noear.solon.core.message.Session;
 
 import java.net.URI;
 
+/**
+ * 为客户端提供统一的会话创建界面
+ *
+ * @author noear
+ * @since 1.2
+ * */
 public interface SessionFactory {
     String[] schemes();
 
--- _extend/solon.extend.socketd/src/main/java/org/noear/solon/extend/socketd/SessionManager.java
+++ _extend/solon.extend.socketd/src/main/java/org/noear/solon/extend/socketd/SessionManager.java
@@ -4,6 +4,12 @@ import org.noear.solon.core.message.Session;
 
 import java.util.Collection;
 
+/**
+ * 为服务端提供统一的管理接口
+ *
+ * @author noear
+ * @since 1.2
+ * */
 public abstract class SessionManager {
     private static SessionManager instance;
 
