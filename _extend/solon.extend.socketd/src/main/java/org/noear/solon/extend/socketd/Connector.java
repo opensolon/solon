165ceffc23e2003ae723f418e09b723c4798b825
noear <noear@live.cn>
2020-12-07 14:05:24 +0800

    1.2.7

--- _extend/solon.extend.socketd/src/main/java/org/noear/solon/extend/socketd/Connector.java
+++ _extend/solon.extend.socketd/src/main/java/org/noear/solon/extend/socketd/Connector.java
@@ -5,6 +5,12 @@ import org.noear.solon.core.message.Session;
 import java.io.IOException;
 import java.net.URI;
 
+/**
+ * 链接器
+ *
+ * @author noear
+ * @since 1.2
+ * */
 public interface Connector<T> {
     URI getUri();
 
