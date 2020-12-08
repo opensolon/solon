5c4e9fe9fe77569810d8f57cca1b4009854dec50
noear <noear@live.cn>
2020-12-08 23:43:41 +0800

    1.2.9

--- solonfox/solonfox-swagger2/src/main/java/org/noear/solonfox/swagger2/XPluginImp.java
+++ solonfox/solonfox-swagger2/src/main/java/org/noear/solonfox/swagger2/XPluginImp.java
@@ -5,16 +5,19 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.models.*;
 import io.swagger.models.parameters.FormParameter;
 import io.swagger.models.parameters.Parameter;
+import io.swagger.models.parameters.PathParameter;
 import io.swagger.models.parameters.QueryParameter;
 import org.noear.snack.ONode;
 import org.noear.solon.Solon;
 import org.noear.solon.SolonApp;
+import org.noear.solon.Utils;
 import org.noear.solon.annotation.Controller;
 import org.noear.solon.core.Aop;
 import org.noear.solon.core.Plugin;
 import org.noear.solon.core.handle.Action;
 import org.noear.solon.core.handle.Handler;
 import org.noear.solon.core.handle.MethodType;
+import org.noear.solon.core.handle.UploadedFile;
 import org.noear.solon.core.route.RouteTable;
 
 import java.util.LinkedHashMap;
@@ -55,7 +58,7 @@ public class XPluginImp implements Plugin {
             swagger.info(info);
         }
 
-        swagger.host("127.0.0.1");
+        swagger.host("localhost:" + Solon.global().port());
         swagger.basePath("/");
 
         buildTags();
@@ -82,11 +85,39 @@ public class XPluginImp implements Plugin {
                 Action action = (Action) route.target;
 
                 Path path = new Path();
-                for (java.lang.reflect.Parameter p0 : action.method().getParameters()) {
-                    Parameter p1 = (route.method == MethodType.GET ? new QueryParameter() : new FormParameter());
-                    p1.setName(p0.getName());
-
-                    path.addParameter(p1);
+                {
+                    switch (route.method) {
+                        case GET: {
+                            path.get(buildPathPperation(route, true));
+                            break;
+                        }
+                        case POST: {
+                            path.post(buildPathPperation(route, false));
+                            break;
+                        }
+                        case PUT: {
+                            path.put(buildPathPperation(route, false));
+                            break;
+                        }
+                        case DELETE: {
+                            path.delete(buildPathPperation(route, false));
+                            break;
+                        }
+                        case PATCH: {
+                            path.patch(buildPathPperation(route, false));
+                            break;
+                        }
+                        case HTTP: {
+                            //path.get(buildPathPperation(route, true));
+                            path.post(buildPathPperation(route, false));
+                            //path.put(buildPathPperation(route, false));
+                            //path.delete(buildPathPperation(route, false));
+                            //path.patch(buildPathPperation(route, false));
+                            break;
+                        } default:{
+                            path.post(buildPathPperation(route, false));
+                        }
+                    }
                 }
 
                 pathMap.put(route.path, path);
@@ -94,5 +125,44 @@ public class XPluginImp implements Plugin {
         }
 
         swagger.setPaths(pathMap);
+    }
+
+    private Operation buildPathPperation(RouteTable.Route<Handler> route, boolean isGet) {
+        Action action = (Action) route.target;
+
+
+        Operation operation = new Operation();
+        operation.addTag(action.bean().clz().getName());
+
+        if (Utils.isNotEmpty(action.produces())) {
+            operation.addProduces(action.produces());
+        }else{
+            operation.addProduces("*/*");
+        }
+
+        if (Utils.isNotEmpty(action.consumes())) {
+            operation.addConsumes(action.consumes());
+        }
+
+        for (java.lang.reflect.Parameter p0 : action.method().getParameters()) {
+            String n1 = "{" + p0.getName() + "}";
+            Parameter p1 = null;
+
+            if (route.path.indexOf(n1) > 0) {
+                p1 = new PathParameter();
+            } else {
+                if (isGet) {
+                    p1 = new QueryParameter();
+                } else {
+                    p1 = new FormParameter();
+                }
+            }
+
+            p1.setName(p0.getName());
+
+            operation.addParameter(p1);
+        }
+
+        return operation;
     }
 }
