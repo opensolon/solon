390e159c85f7510a8bb5c2ebc1cf44d274d8d624
noear <noear@live.cn>
2020-12-11 16:07:10 +0800

    1.2.9

--- _extend/solon.extend.socketd/src/main/java/org/noear/solon/extend/socketd/protocol/MessageProtocolCompress.java
+++ _extend/solon.extend.socketd/src/main/java/org/noear/solon/extend/socketd/protocol/MessageProtocolCompress.java
@@ -26,8 +26,6 @@ public class MessageProtocolCompress implements MessageProtocol {
     }
 
 
-
-
     public byte[] compress(byte[] bytes) throws Exception {
         return GzipUtil.compress(bytes);
     }
