841583266f829d954924814927a59dc6a18bba45
noear <noear@live.cn>
2020-12-11 12:51:41 +0800

    1.2.9

--- _extend/solon.extend.socketd/src/main/java/org/noear/solon/extend/socketd/MessageProtocolManager.java
+++ _extend/solon.extend.socketd/src/main/java/org/noear/solon/extend/socketd/MessageProtocolManager.java
@@ -4,15 +4,21 @@ import org.noear.solon.core.message.Message;
 import org.noear.solon.extend.socketd.protocol.MessageProtocol;
 import org.noear.solon.extend.socketd.protocol.MessageProtocolDefault;
 
+import javax.net.ssl.SSLContext;
 import java.nio.ByteBuffer;
 
 public class MessageProtocolManager {
     private static MessageProtocol protocol = new MessageProtocolDefault();
+    private static SSLContext sslContext;
 
     public static void setProtocol(MessageProtocol protocol) {
         if (protocol != null) {
             MessageProtocolManager.protocol = protocol;
         }
+    }
+
+    public static void setSslContext(SSLContext sslContext){
+        MessageProtocolManager.sslContext = sslContext;
     }
 
 
