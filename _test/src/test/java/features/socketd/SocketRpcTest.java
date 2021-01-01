aaa96e4c9b8bb67f3c808c8ff40efb3de0c5b0af
noear <noear@live.cn>
2021-01-01 17:29:24 +0800

    1.2.14

--- _test/src/test/java/features/socketd/SocketRpcTest.java
+++ _test/src/test/java/features/socketd/SocketRpcTest.java
@@ -3,6 +3,7 @@ package features.socketd;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.noear.nami.Nami;
+import org.noear.solon.Solon;
 import org.noear.solon.extend.socketd.SocketD;
 import org.noear.solon.test.SolonJUnit4ClassRunner;
 import org.noear.solon.test.SolonTest;
@@ -27,14 +28,11 @@ public class SocketRpcTest {
 
     @Test
     public void test_rpc_api2() throws Throwable {
-        int _port = 8080 + 20000;
-
         HelloRpcService rpc = Nami.builder()
-                .uri("tcp://localhost:" + _port + "/demoh/rpc")
+                .uri("tcp://localhost:" + (Solon.global().port() + 20000) + "/demoh/rpc")
                 .create(HelloRpcService.class);
 
         String rst = rpc.hello("noear");
-
         System.out.println(rst);
 
         assert "name=noear".equals(rst);
@@ -42,14 +40,11 @@ public class SocketRpcTest {
 
     @Test
     public void test_rpc_api3() throws Throwable {
-        int _port = 8080 + 20000;
-
         HelloRpcService rpc = Nami.builder()
-                .upstream(() -> "tcp://localhost:" + _port)
+                .upstream(() -> "tcp://localhost:" + (Solon.global().port() + 20000))
                 .create(HelloRpcService.class);
 
         String rst = rpc.hello("noear");
-
         System.out.println(rst);
 
         assert "name=noear".equals(rst);
@@ -57,14 +52,23 @@ public class SocketRpcTest {
 
     @Test
     public void test_rpc_api_http1() throws Throwable {
-        int _port = 8080;
-
         HelloRpcService rpc = Nami.builder()
-                .upstream(() -> "http://localhost:" + _port)
+                .upstream(() -> "http://localhost:" + Solon.global().port())
                 .create(HelloRpcService.class);
 
         String rst = rpc.hello("noear");
+        System.out.println(rst);
 
+        assert "name=noear".equals(rst);
+    }
+
+    @Test
+    public void test_rpc_api_ws1() throws Throwable {
+        HelloRpcService rpc = Nami.builder()
+                .upstream(() -> "ws://localhost:" + (Solon.global().port() + 10000))
+                .create(HelloRpcService.class);
+
+        String rst = rpc.hello("noear");
         System.out.println(rst);
 
         assert "name=noear".equals(rst);
