723e245f6061e2365540ae5ccaee022bd5db607d
noear <noear@live.cn>
2021-01-02 18:13:18 +0800

    1.2.14

--- _extend2/springboot-solon-plugin/src/main/java/org/noear/solon/extend/springboot/ConfigurationSolon.java
+++ _extend2/springboot-solon-plugin/src/main/java/org/noear/solon/extend/springboot/ConfigurationSolon.java
@@ -1,16 +1,22 @@
 package org.noear.solon.extend.springboot;
 
+import org.noear.solon.Solon;
 import org.noear.solon.extend.servlet.SolonServletFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.context.event.ApplicationStartingEvent;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextRefreshedEvent;
 
 /**
  * @author noear 2020/12/28 created
  */
 @Configuration
-public class ConfigurationSolon {
+public class ConfigurationSolon implements ApplicationListener<ApplicationStartingEvent> {
     @Bean
     public FilterRegistrationBean servletRegistrationBean() {
         FilterRegistrationBean registration = new FilterRegistrationBean();
@@ -22,5 +28,10 @@ public class ConfigurationSolon {
     @Bean
     public BeanPostProcessor beanPostProcessor(){
         return new BeanPostProcessorSolon();
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationStartingEvent event) {
+        Solon.start(event.getSource().getClass(), event.getArgs());
     }
 }
