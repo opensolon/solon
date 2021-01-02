d677930f856e8119dc9cb3e2941b0f1cc8183799
noear <noear@live.cn>
2021-01-02 19:05:09 +0800

    1.2.14

--- _extend2/springboot-solon-plugin/src/main/java/org/noear/solon/extend/springboot/ApplicationStartingSolon.java
+++ _extend2/springboot-solon-plugin/src/main/java/org/noear/solon/extend/springboot/ApplicationStartingSolon.java
@@ -1,6 +1,7 @@
 package org.noear.solon.extend.springboot;
 
 import org.noear.solon.Solon;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.context.event.ApplicationStartingEvent;
 import org.springframework.context.ApplicationListener;
 
@@ -11,6 +12,10 @@ import org.springframework.context.ApplicationListener;
 public class ApplicationStartingSolon implements ApplicationListener<ApplicationStartingEvent> {
     @Override
     public void onApplicationEvent(ApplicationStartingEvent event) {
-        Solon.start(event.getSource().getClass(), event.getArgs());
+        if (event.getSource() instanceof SpringApplication) {
+            SpringApplication sa = (SpringApplication) event.getSource();
+
+            Solon.start(sa.getMainApplicationClass(), event.getArgs());
+        }
     }
 }
