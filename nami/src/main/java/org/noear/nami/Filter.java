eee925d9cabccba2383688c51f45df86f6e1805f
noear <noear@live.cn>
2021-01-03 19:40:55 +0800

    1.2.14

--- nami/src/main/java/org/noear/nami/Filter.java
+++ nami/src/main/java/org/noear/nami/Filter.java
@@ -9,5 +9,5 @@ public interface Filter {
     /**
      * 过滤
      */
-    void filter(NamiConfig cfg, String method, String url, Map<String, String> headers, Map<String, Object> args);
+    void filter(NamiConfig cfg, String action, String url, Map<String, String> headers, Map<String, Object> args);
 }
