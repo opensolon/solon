fcadf1e5382c9895714a392d1a1cd7d36a323b7a
noear <noear@live.cn>
2021-01-06 07:58:19 +0800

    1.2.16

--- _extend2/springboot-solon-plugin/src/main/java/org/noear/solon/extend/springboot/ConfigurationSolon.java
+++ _extend2/springboot-solon-plugin/src/main/java/org/noear/solon/extend/springboot/ConfigurationSolon.java
@@ -1,6 +1,9 @@
 package org.noear.solon.extend.springboot;
 
+import org.noear.solon.extend.servlet.SolonServletFilter;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,4 +18,11 @@ public class ConfigurationSolon  {
         return new BeanPostProcessorSolon();
     }
 
+    @Bean
+    @ConditionalOnWebApplication
+    public FilterRegistrationBean servletRegistrationBean() {
+        FilterRegistrationBean registration = new FilterRegistrationBean<>();
+        registration.setFilter(new SolonServletFilter());
+        return registration;
+    }
 }
