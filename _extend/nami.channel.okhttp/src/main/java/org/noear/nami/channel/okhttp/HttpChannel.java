4e1b8e6c9b0497d77ff0dbb5d976faa99ca1d3fe
noear <noear@live.cn>
2021-01-05 10:29:18 +0800

    1.2.15

--- _extend/nami.channel.okhttp/src/main/java/org/noear/nami/channel/okhttp/HttpChannel.java
+++ _extend/nami.channel.okhttp/src/main/java/org/noear/nami/channel/okhttp/HttpChannel.java
@@ -4,7 +4,6 @@ import okhttp3.MediaType;
 import okhttp3.Response;
 import org.noear.nami.*;
 import org.noear.nami.common.Constants;
-import org.noear.nami.common.MethodWrap;
 import org.noear.nami.common.Result;
 
 import java.lang.reflect.Method;
