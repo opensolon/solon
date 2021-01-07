bdee073c6033b51d8ae6fa95839fc0ef7f4a41e8
noear <noear@live.cn>
2021-01-07 13:11:58 +0800

    1.2.17

--- _test/src/test/java/features/nami/NamiTest.java
+++ _test/src/test/java/features/nami/NamiTest.java
@@ -11,35 +11,35 @@ import java.util.List;
 /**
  * @author noear 2021/1/1 created
  */
-@RunWith(SolonJUnit4ClassRunner.class)
+//@RunWith(SolonJUnit4ClassRunner.class)
 public class NamiTest {
-    @Test
-    public void test() {
-        GitHub github = Nami.builder()
-                .decoder(SnackDecoder.instance)
-                .upstream(() -> "https://api.github.com")
-                .create(GitHub.class);
-
-        List<Contributor> contributors = github.contributors("OpenFeign", "feign");
-        for (Contributor contributor : contributors) {
-            System.out.println(contributor.login + " (" + contributor.contributions + ")");
-        }
-
-        assert contributors.size() > 1;
-    }
-
-    @Test
-    public void test2(){
-        GitHub github = Nami.builder()
-                .decoder(SnackDecoder.instance)
-                .upstream(() -> "https://api.github.com")
-                .create(GitHub.class);
-
-        Issue issue = new Issue();
-        issue.title = "测试";
-        issue.body = "同题";
-
-
-        github.createIssue(issue,"OpenFeign", "feign");
-    }
+//    @Test
+//    public void test() {
+//        GitHub github = Nami.builder()
+//                .decoder(SnackDecoder.instance)
+//                .upstream(() -> "https://api.github.com")
+//                .create(GitHub.class);
+//
+//        List<Contributor> contributors = github.contributors("OpenFeign", "feign");
+//        for (Contributor contributor : contributors) {
+//            System.out.println(contributor.login + " (" + contributor.contributions + ")");
+//        }
+//
+//        assert contributors.size() > 1;
+//    }
+//
+//    @Test
+//    public void test2(){
+//        GitHub github = Nami.builder()
+//                .decoder(SnackDecoder.instance)
+//                .upstream(() -> "https://api.github.com")
+//                .create(GitHub.class);
+//
+//        Issue issue = new Issue();
+//        issue.title = "测试";
+//        issue.body = "同题";
+//
+//
+//        github.createIssue(issue,"OpenFeign", "feign");
+//    }
 }
