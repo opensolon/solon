2cf5c8a9b6fc61ed723f5ea5953ede851ca7d9ac
noear <noear@live.cn>
2021-01-15 09:54:34 +0800

    1.2.21

--- _extend/solon.extend.cloud/src/main/java/org/noear/solon/extend/cloud/CloudManager.java
+++ _extend/solon.extend.cloud/src/main/java/org/noear/solon/extend/cloud/CloudManager.java
@@ -3,8 +3,11 @@ package org.noear.solon.extend.cloud;
 import org.noear.solon.extend.cloud.annotation.CloudConfig;
 import org.noear.solon.extend.cloud.annotation.CloudDiscovery;
 import org.noear.solon.extend.cloud.annotation.CloudEvent;
+import org.noear.solon.extend.cloud.service.ConfigService;
 import org.noear.solon.extend.cloud.service.RegisterService;
 
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -15,11 +18,12 @@ import java.util.Set;
  * @since 1.2
  */
 public class CloudManager {
-    private static Set<RegisterService> registerServiceSet;
+    private static Set<RegisterService> registerServiceSet = new LinkedHashSet<>();
+    private static Set<ConfigService> configServiceSet = new LinkedHashSet<>();
 
-    private static Map<String, CloudConfigHandler> configHandlerMap;
-    private static Map<String, CloudDiscoveryHandler> discoveryHandlerMap;
-    private static Map<String, CloudEventHandler> eventHandlerMap;
+    private static Map<String, CloudConfigHandler> configHandlerMap = new LinkedHashMap<>();
+    private static Map<String, CloudDiscoveryHandler> discoveryHandlerMap = new LinkedHashMap<>();
+    private static Map<String, CloudEventHandler> eventHandlerMap = new LinkedHashMap<>();
 
     public static void register(CloudConfig anno, CloudConfigHandler handler) {
         configHandlerMap.put(anno.value(), handler);
@@ -31,5 +35,13 @@ public class CloudManager {
 
     public static void register(CloudEvent anno, CloudEventHandler handler) {
         eventHandlerMap.put(anno.value(), handler);
+    }
+
+    public static void register(ConfigService service) {
+        configServiceSet.add(service);
+    }
+
+    public static void register(RegisterService service) {
+        registerServiceSet.add(service);
     }
 }
