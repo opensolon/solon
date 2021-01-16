770c0771969f8e5c56fd6b3dab79eefec095a713
noear <noear@live.cn>
2021-01-16 17:06:43 +0800

    1.2.21

--- _extend/solon.extend.cloud/src/main/java/org/noear/solon/extend/cloud/XPluginImp.java
+++ _extend/solon.extend.cloud/src/main/java/org/noear/solon/extend/cloud/XPluginImp.java
@@ -1,14 +1,20 @@
 package org.noear.solon.extend.cloud;
 
+import org.noear.solon.Solon;
 import org.noear.solon.SolonApp;
+import org.noear.solon.Utils;
 import org.noear.solon.core.Aop;
+import org.noear.solon.core.Bridge;
 import org.noear.solon.core.Plugin;
 import org.noear.solon.core.wrap.ClassWrap;
 import org.noear.solon.extend.cloud.annotation.CloudConfig;
 import org.noear.solon.extend.cloud.annotation.CloudDiscovery;
 import org.noear.solon.extend.cloud.annotation.CloudEvent;
 import org.noear.solon.extend.cloud.impl.CloudBeanInjector;
+import org.noear.solon.extend.cloud.impl.CloudLoadBalanceFactory;
 import org.noear.solon.extend.cloud.model.Config;
+import org.noear.solon.extend.cloud.model.Node;
+import org.noear.solon.extend.cloud.utils.LocalUtils;
 
 import java.util.Properties;
 
@@ -22,7 +28,7 @@ public class XPluginImp implements Plugin {
         Aop.context().beanBuilderAdd(CloudConfig.class, (clz, bw, anno) -> {
             if (bw.raw() instanceof CloudConfigHandler) {
                 CloudManager.register(anno, bw.raw());
-            }else {
+            } else {
                 CloudManager.register(anno, new CloudConfigHandler() {
                     @Override
                     public void handler(Config cfg) {
@@ -73,5 +79,55 @@ public class XPluginImp implements Plugin {
                 });
             }
         });
+
+
+        if (CloudManager.configService() != null) {
+
+            //加载配置
+            if (Utils.isNotEmpty(CloudProps.getConfigLoad())) {
+                String[] ss = CloudProps.getConfigLoad().split("/");
+                String group = ss[0];
+                String key = (ss.length > 1 ? ss[1] : "*");
+                Config config = CloudManager.configService().get(group, key);
+
+                if (config != null && Utils.isNotEmpty(config.value)) {
+                    Properties properties = Utils.buildProperties(config.value);
+                    Solon.cfg().loadAdd(properties);
+                }
+            }
+        }
+
+        if (CloudManager.registerService() != null) {
+            //设置负载工厂
+            Bridge.upstreamFactorySet(CloudLoadBalanceFactory.instance);
+
+            //注册服务
+            if (Utils.isNotEmpty(Solon.cfg().appName())) {
+                Node node = new Node();
+                node.service = Solon.cfg().appName();
+                node.ip = LocalUtils.getLocalAddress();
+                node.port = Solon.global().port();
+                node.protocol = "http";
+
+                CloudManager.registerService().register(node);
+            }
+        }
+    }
+
+    @Override
+    public void stop() throws Throwable {
+        if (Solon.cfg().isDriftMode()) {
+            if (CloudManager.registerService() != null) {
+                if (Utils.isNotEmpty(Solon.cfg().appName())) {
+                    Node node = new Node();
+                    node.service = Solon.cfg().appName();
+                    node.ip = LocalUtils.getLocalAddress();
+                    node.port = Solon.global().port();
+                    node.protocol = "http";
+
+                    CloudManager.registerService().deregister(node);
+                }
+            }
+        }
     }
 }
