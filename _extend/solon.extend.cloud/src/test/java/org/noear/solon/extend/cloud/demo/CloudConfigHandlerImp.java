2c350ac07e9272b7a38c40a017be9982dc9408f7
noear <noear@live.cn>
2021-01-16 15:29:32 +0800

    1.2.21

--- _extend/solon.extend.cloud/src/test/java/org/noear/solon/extend/cloud/demo/CloudConfigHandlerImp.java
+++ _extend/solon.extend.cloud/src/test/java/org/noear/solon/extend/cloud/demo/CloudConfigHandlerImp.java
@@ -2,15 +2,15 @@ package org.noear.solon.extend.cloud.demo;
 
 import org.noear.solon.extend.cloud.CloudConfigHandler;
 import org.noear.solon.extend.cloud.annotation.CloudConfig;
-import org.noear.solon.extend.cloud.model.ConfigSet;
+import org.noear.solon.extend.cloud.model.Config;
 
 /**
  * @author noear 2021/1/14 created
  */
-@CloudConfig("water")
+@CloudConfig("water/water")
 public class CloudConfigHandlerImp implements CloudConfigHandler {
     @Override
-    public void handler(ConfigSet configSet) {
+    public void handler(Config config) {
 
     }
 }
