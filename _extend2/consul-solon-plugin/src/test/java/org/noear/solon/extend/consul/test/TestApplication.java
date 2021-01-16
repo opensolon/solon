2c26d84c322750152184305ad1eef6c967d8b9d0
noear <noear@live.cn>
2021-01-16 17:57:35 +0800

    1.2.21

--- _extend2/consul-solon-plugin/src/test/java/org/noear/solon/extend/consul/test/TestApplication.java
+++ _extend2/consul-solon-plugin/src/test/java/org/noear/solon/extend/consul/test/TestApplication.java
@@ -1,10 +1,8 @@
 package org.noear.solon.extend.consul.test;
 
-import org.noear.nami.integration.solon.EnableNamiClient;
 import org.noear.solon.Solon;
 import org.noear.solon.extend.consul.ConsulKvUpdateEvent;
 
-@EnableNamiClient
 public class TestApplication {
     public static void main(String[] args) {
         Solon.start(TestApplication.class, args,app->{
