923290cbff029431f76dda4b1ff7651a99ca3a2e
noear <noear@live.cn>
2021-01-19 08:40:45 +0800

    1.2.22

--- _demo/_demo.solon_hessian/src/main/java/server/dso/IComplexModelService.java
+++ _demo/_demo.solon_hessian/src/main/java/server/dso/IComplexModelService.java
@@ -1,11 +1,9 @@
 package server.dso;
 
-import org.noear.nami.annotation.Body;
 import org.noear.nami.annotation.NamiClient;
 import server.model.ComplexModel;
-import server.model.Point;
 
-@NamiClient("test:/ComplexModelService/")
+@NamiClient(name = "test", path = "/ComplexModelService/")
 public interface IComplexModelService {
     //持久化
     void save(ComplexModel model);
