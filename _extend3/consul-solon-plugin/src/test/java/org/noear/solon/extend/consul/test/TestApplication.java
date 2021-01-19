e39dd724250d9a885ac513f4474b6d505e1e2807
noear <noear@live.cn>
2021-01-19 15:59:21 +0800

    1.2.22

--- _extend3/consul-solon-plugin/src/test/java/org/noear/solon/extend/consul/test/TestApplication.java
+++ _extend3/consul-solon-plugin/src/test/java/org/noear/solon/extend/consul/test/TestApplication.java
@@ -2,9 +2,16 @@ package org.noear.solon.extend.consul.test;
 
 
 import org.noear.solon.Solon;
+import org.noear.solon.annotation.Component;
 
-public class TestApplication {
+@Component(remoting = true)
+public class TestApplication implements HelloInterface{
     public static void main(String[] args) {
         Solon.start(TestApplication.class, args);
+    }
+
+    @Override
+    public String hello0() {
+        return "你好：（";
     }
 }
