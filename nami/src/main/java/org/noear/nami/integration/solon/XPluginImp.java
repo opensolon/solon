a7b8a9bb42b4f72da2e0f96a66e41bc32adbb6d0
noear <noear@live.cn>
2021-01-19 09:36:19 +0800

    1.2.22

--- nami/src/main/java/org/noear/nami/integration/solon/XPluginImp.java
+++ nami/src/main/java/org/noear/nami/integration/solon/XPluginImp.java
@@ -26,14 +26,14 @@ public class XPluginImp implements Plugin {
                 return;
             }
 
-            if (Utils.isEmpty(anno.url()) && Utils.isEmpty(anno.name()) && Utils.isEmpty(anno.path())) {
+            if (Utils.isEmpty(anno.url()) && Utils.isEmpty(anno.name())) {
                 NamiClient anno2 = varH.getType().getAnnotation(NamiClient.class);
                 if (anno2 != null) {
                     anno = anno2;
                 }
             }
 
-            if (Utils.isEmpty(anno.url()) && Utils.isEmpty(anno.name()) && Utils.isEmpty(anno.path()) && anno.upstream().length==0)  {
+            if (Utils.isEmpty(anno.url()) && Utils.isEmpty(anno.name()) && anno.upstream().length==0)  {
                 throw new NamiException("@NamiClient configuration error!");
             }
 
