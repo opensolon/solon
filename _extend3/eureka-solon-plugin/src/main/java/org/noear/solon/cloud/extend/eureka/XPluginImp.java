cc1c46ee6134239afe0a08fe654390f3fa50f1b1
noear <noear@live.cn>
2021-01-21 21:51:41 +0800

    1.2.24

---
+++ _extend3/eureka-solon-plugin/src/main/java/org/noear/solon/cloud/extend/eureka/XPluginImp.java
@@ -0,0 +1,14 @@
+package org.noear.solon.cloud.extend.eureka;
+
+import org.noear.solon.SolonApp;
+import org.noear.solon.core.Plugin;
+
+/**
+ * @author noear 2021/1/21 created
+ */
+public class XPluginImp implements Plugin {
+    @Override
+    public void start(SolonApp app) {
+
+    }
+}
