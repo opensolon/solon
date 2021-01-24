61d1d179b1808d4ec46de6a50bffa84c7856cb10
noear <noear@live.cn>
2021-01-24 20:44:20 +0800

    1.2.26

---
+++ _extend3/kafka-solon-plugin/src/main/java/org/noear/solon/cloud/extend/kafka/KafkaProps.java
@@ -0,0 +1,11 @@
+package org.noear.solon.cloud.extend.kafka;
+
+import org.noear.solon.cloud.CloudProps;
+
+/**
+ * @author noear
+ * @since 1.2
+ */
+public class KafkaProps {
+    public static final CloudProps instance = new CloudProps("kafka");
+}
