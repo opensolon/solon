51b7a7d584b3d1fc34aadc4d4c503cacb8d99296
noear <noear@live.cn>
2021-02-01 08:20:41 +0800

    1.3.3

--- _extend/nami.channel.http.okhttp/src/main/java/org/noear/nami/channel/http/okhttp/HttpUtils.java
+++ _extend/nami.channel.http.okhttp/src/main/java/org/noear/nami/channel/http/okhttp/HttpUtils.java
@@ -13,8 +13,8 @@ import java.util.concurrent.TimeUnit;
 class HttpUtils {
     private final static Dispatcher dispatcher() {
         Dispatcher temp = new Dispatcher();
-        temp.setMaxRequests(3000);
-        temp.setMaxRequestsPerHost(600);
+        temp.setMaxRequests(20000);
+        temp.setMaxRequestsPerHost(10000);
         return temp;
     }
 
