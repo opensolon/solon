f65984ad13675598a002d8eb3ac4b3ec35567e44
noear <noear@live.cn>
2021-02-15 14:51:32 +0800

    1.3.4

--- _test/src/main/java/webapp/demob_session/test.java
+++ _test/src/main/java/webapp/demob_session/test.java
@@ -7,22 +7,22 @@ import org.noear.solon.core.handle.Context;
 import java.util.Date;
 
 @Controller
-public class test  {
+public class test {
     @Mapping("/demob/session/setval")
-    public void setVal(Context ctx){
+    public void setVal(Context ctx) {
         ctx.sessionSet("v1", new Date());
-        ctx.sessionSet("v2","我是字符串");
+        ctx.sessionSet("v2", "我是字符串");
         ctx.sessionSet("v3", 121212l);
     }
 
     @Mapping(value = "/demob/session/getval", produces = "text/html;charset=utf-8")
-    public void getVal(Context ctx){
+    public void getVal(Context ctx) {
         Object v1 = ctx.session("v1");
         Object v2 = ctx.session("v2");
         Object v3 = ctx.session("v3");
 
-        ctx.output(String.valueOf(v1) + "<br/>");
-        ctx.output(String.valueOf(v2) + "<br/>");
-        ctx.output(String.valueOf(v3) + "<br/>");
+        ctx.output(v1 + "<br/>");
+        ctx.output(v2 + "<br/>");
+        ctx.output(v3 + "<br/>");
     }
 }
---
+++ _test/src/test/java/features/SessionTest.java
@@ -0,0 +1,40 @@
+package features;
+
+import okhttp3.Headers;
+import okhttp3.Response;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.noear.solon.test.HttpTestBase;
+import org.noear.solon.test.HttpUtils;
+import org.noear.solon.test.SolonJUnit4ClassRunner;
+import org.noear.solon.test.SolonTest;
+
+import java.util.List;
+
+/**
+ * @author noear 2021/2/15 created
+ */
+@RunWith(SolonJUnit4ClassRunner.class)
+@SolonTest(webapp.TestApp.class)
+public class SessionTest extends HttpTestBase {
+    @Test
+    public void test() throws Exception {
+        Response response = path("/demob/session/setval").exec("get");
+        System.out.println(response.body().string());
+
+        List<String> cookies = response.headers().values("Set-Cookie");
+
+        HttpUtils httpUtils = path("/demob/session/getval");
+
+        for (String c1 : cookies) {
+            String kv = c1.split(";")[0];
+            if (kv.contains("=")) {
+                httpUtils.headerAdd("Cookie", kv);
+            }
+        }
+
+        String html = httpUtils.get();
+
+        assert html.contains("121212");
+    }
+}
