35a47f4a3532cb6043434609f61f9fb0ce38d294
noear <noear@live.cn>
2021-02-19 17:18:13 +0800

    Update InfoConfig.java

--- _test/src/main/java/webapp/swagger/InfoConfig.java
+++ _test/src/main/java/webapp/swagger/InfoConfig.java
@@ -1,17 +1,17 @@
 package webapp.swagger;
 
-import io.swagger.models.Contact;
-import io.swagger.models.Info;
+//import io.swagger.models.Contact;
+//import io.swagger.models.Info;
 import org.noear.solon.annotation.Bean;
 import org.noear.solon.annotation.Configuration;
 
 @Configuration
 public class InfoConfig {
-    @Bean
-    public Info build() {
-        return new Info()
-                .contact(new Contact().name("noear").email("noear@live.cn"))
-                .title("Interface Document for solon.test")
-                .version("test");
-    }
+//    @Bean
+//    public Info build() {
+//        return new Info()
+//                .contact(new Contact().name("noear").email("noear@live.cn"))
+//                .title("Interface Document for solon.test")
+//                .version("test");
+//    }
 }
