4a9de15b42668a1c4042c8796db0ab37f609f87a
noear <noear@live.cn>
2021-02-20 10:55:47 +0800

    1.3.7

--- solonfox/solonfox-swagger-ui/src/main/java/org/noear/solonfox/swagger/ui/XPluginImp.java
+++
@@ -1,5 +0,0 @@
-package org.noear.solonfox.swagger.ui;
-
-public class XPluginImp {
-
-}
--- solonfox/solonfox-swagger2/src/main/java/org/noear/solonfox/swagger2/EnableSwagger2.java
+++
@@ -1,9 +0,0 @@
-package org.noear.solonfox.swagger2;
-
-import java.lang.annotation.*;
-
-@Target({ElementType.TYPE})
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
-public @interface EnableSwagger2 {
-}
