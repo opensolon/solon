7b6b7b4bbe1630cf575f6206b42a37dc5ef05610
noear <noear@live.cn>
2021-02-23 11:08:00 +0800

    1.3.8-m3

--- _extend/solon.extend.validation/src/main/java/org/noear/solon/extend/validation/annotation/Logined.java
+++ _extend/solon.extend.validation/src/main/java/org/noear/solon/extend/validation/annotation/Logined.java
@@ -2,10 +2,7 @@ package org.noear.solon.extend.validation.annotation;
 
 import org.noear.solon.annotation.Note;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 /**
  *
