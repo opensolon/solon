3a2db99661958a964cda4a0de9910b32a6243940
noear <noear@live.cn>
2021-02-26 10:41:51 +0800

    1.3.9

--- solon/src/main/java/org/noear/solon/Solon.java
+++ solon/src/main/java/org/noear/solon/Solon.java
@@ -70,7 +70,7 @@ public class Solon {
         JarClassLoader.bindingThread();
 
 
-        PrintUtil.wran("solon.app: Start loading");
+        PrintUtil.info("Solon","solon.app: Start loading");
 
         //1.创建应用
         global = new SolonApp(source, argx);
@@ -101,9 +101,9 @@ public class Solon {
 
         RuntimeMXBean rb = ManagementFactory.getRuntimeMXBean();
         if (rb == null) {
-            PrintUtil.wran("solon.app: End loading elapsed=" + global.elapsedTimes() + "ms");
+            PrintUtil.info("Solon","solon.app: End loading elapsed=" + global.elapsedTimes() + "ms");
         } else {
-            PrintUtil.wran("solon.app: End loading elapsed=" + global.elapsedTimes() + "ms pid=" + rb.getName());
+            PrintUtil.info("Solon","solon.app: End loading elapsed=" + global.elapsedTimes() + "ms pid=" + rb.getName());
         }
         return global;
     }
