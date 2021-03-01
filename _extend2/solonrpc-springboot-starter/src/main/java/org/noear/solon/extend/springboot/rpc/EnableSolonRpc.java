3f5e08bfbb0985d2f52ba9139239b96458e2e153
noear <noear@live.cn>
2021-03-01 16:45:38 +0800

    1.3.10

--- _extend2/solonrpc-springboot-starter/src/main/java/org/noear/solon/extend/springboot/rpc/EnableSolonRpc.java
+++ _extend2/solonrpc-springboot-starter/src/main/java/org/noear/solon/extend/springboot/rpc/EnableSolonRpc.java
@@ -1,13 +1,14 @@
 package org.noear.solon.extend.springboot.rpc;
 
-import org.noear.solon.annotation.Import;
+import org.springframework.context.annotation.Import;
 
 import java.lang.annotation.*;
 
 /**
  * @author noear 2021/3/1 created
  */
-@Import(AutoConfiguration.class)
+@Import({org.noear.nami.integration.springboot.AutoConfiguration.class,
+        org.noear.solon.cloud.integration.springboot.AutoConfiguration.class})
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
