d9c1c49bf2693b8888dbf596f9d06665d1fcd7ef
noear <noear@live.cn>
2021-03-03 14:47:09 +0800

    1.3.10

--- _extend/solon.cloud/src/main/java/org/noear/solon/cloud/service/CloudLockService.java
+++ _extend/solon.cloud/src/main/java/org/noear/solon/cloud/service/CloudLockService.java
@@ -1,5 +1,7 @@
 package org.noear.solon.cloud.service;
 
+import org.noear.solon.Solon;
+
 /**
  * 云端锁服务
  *
@@ -10,55 +12,103 @@ public interface CloudLockService {
     /**
      * 锁住
      *
-     * @param group 锁分组
-     * @param key 锁键
+     * @param group   锁分组
+     * @param key     锁键
      * @param seconds 锁定时间（过期失效）
-     * @param holder 持有人
-     * */
+     * @param holder  持有人
+     */
     boolean lock(String group, String key, int seconds, String holder);
 
     /**
      * 锁住
      *
-     * @param group 锁分组
-     * @param key 锁键
+     * @param key     锁键
      * @param seconds 锁定时间（过期失效）
-     * */
+     * @param holder  持有人
+     */
+    default boolean lock(String key, int seconds, String holder) {
+        return lock(Solon.cfg().appName(), key, seconds, holder);
+    }
+
+    /**
+     * 锁住
+     *
+     * @param group   锁分组
+     * @param key     锁键
+     * @param seconds 锁定时间（过期失效）
+     */
     default boolean lock(String group, String key, int seconds) {
         return lock(group, key, seconds, null);
+    }
+
+    /**
+     * 锁住
+     *
+     * @param key     锁键
+     * @param seconds 锁定时间（过期失效）
+     */
+    default boolean lock(String key, int seconds) {
+        return lock(Solon.cfg().appName(), key, seconds);
     }
 
     /**
      * 解锁
      *
      * @param group 锁分组
-     * @param key 锁键
-     * */
+     * @param key   锁键
+     */
     void unlock(String group, String key, String holder);
 
     /**
      * 解锁
      *
      * @param group 锁分组
-     * @param key 锁键
-     * */
+     * @param key   锁键
+     */
     default void unlock(String group, String key) {
         unlock(group, key, null);
+    }
+
+    /**
+     * 解锁
+     *
+     * @param key   锁键
+     */
+    default void unlock(String key) {
+        unlock(Solon.cfg().appName(), key, null);
     }
 
     /**
      * 检测是否存在锁
      *
      * @param group 锁分组
-     * @param key 锁键
-     * */
+     * @param key   锁键
+     */
     boolean isLocked(String group, String key);
+
+    /**
+     * 检测是否存在锁
+     *
+     * @param key   锁键
+     */
+    default boolean isLocked(String key) {
+        return isLocked(Solon.cfg().appName(), key);
+    }
 
     /**
      * 获取锁的持有人
      *
      * @param group 锁分组
-     * @param key 锁键
-     * */
+     * @param key   锁键
+     */
     String getLockHolder(String group, String key);
+
+    /**
+     * 获取锁的持有人
+     *
+     * @param key   锁键
+     */
+    default String getLockHolder(String key) {
+        return getLockHolder(Solon.cfg().appName(), key);
+    }
 }
