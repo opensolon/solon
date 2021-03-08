2f639898e600d14906ce6fc78a82c940295c581c
noear <noear@live.cn>
2021-03-08 17:42:54 +0800

    1.3.10

---
+++ _test/src/main/java/webapp/demox_log/FilterDemo.java
@@ -0,0 +1,18 @@
+package webapp.demox_log;
+
+import org.noear.solon.annotation.Component;
+import org.noear.solon.core.handle.Context;
+import org.noear.solon.core.handle.Filter;
+import org.noear.solon.core.handle.FilterChain;
+
+/**
+ * @author noear 2021/3/8 created
+ */
+@Component
+public class FilterDemo implements Filter {
+    @Override
+    public void doFilter(Context ctx, FilterChain chain) throws Throwable {
+        System.out.println("我是好人过滤器!!!path="+ctx.path());
+        chain.doFilter(ctx);
+    }
+}
