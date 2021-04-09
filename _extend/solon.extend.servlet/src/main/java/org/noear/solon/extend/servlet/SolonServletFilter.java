79a7e40a1758b5a535b9e56d33d1ddb447b4eba2
noear <noear@live.cn>
2021-04-09 12:52:53 +0800

    1.3.20-m8

--- _extend/solon.extend.servlet/src/main/java/org/noear/solon/extend/servlet/SolonServletFilter.java
+++ _extend/solon.extend.servlet/src/main/java/org/noear/solon/extend/servlet/SolonServletFilter.java
@@ -1,8 +1,10 @@
 package org.noear.solon.extend.servlet;
 
 import org.noear.solon.Solon;
+import org.noear.solon.core.event.EventBus;
 import org.noear.solon.core.handle.Context;
 import org.noear.solon.core.handle.ContextUtil;
+import org.noear.solon.core.handle.Handler;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
@@ -14,6 +16,9 @@ import java.io.IOException;
  * @since 1.2
  * */
 public class SolonServletFilter implements Filter {
+    public static Handler onFilterStart;
+    public static Handler onFilterEnd;
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         //兼容 servlet 3.1.0
@@ -23,6 +28,10 @@ public class SolonServletFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
         if (request instanceof HttpServletRequest) {
             Context ctx = new SolonServletContext((HttpServletRequest) request, (HttpServletResponse) response);
+
+            //过滤开始
+            doFilterStart(ctx);
+
             Solon.global().tryHandle(ctx);
 
             if (ctx.getHandled() == false) {
@@ -33,8 +42,31 @@ public class SolonServletFilter implements Filter {
                     ContextUtil.currentRemove();
                 }
             }
+
+            //过滤结束
+            doFilterEnd(ctx);
         } else {
             filterChain.doFilter(request, response);
+        }
+    }
+
+    public void doFilterStart(Context ctx) {
+        if (onFilterStart != null) {
+            try {
+                onFilterStart.handle(ctx);
+            } catch (Throwable ex) {
+                EventBus.push(ex);
+            }
+        }
+    }
+
+    public void doFilterEnd(Context ctx) {
+        if (onFilterEnd != null) {
+            try {
+                onFilterEnd.handle(ctx);
+            } catch (Throwable ex) {
+                EventBus.push(ex);
+            }
         }
     }
 
