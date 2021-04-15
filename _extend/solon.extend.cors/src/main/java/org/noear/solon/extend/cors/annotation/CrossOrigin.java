8f350deaff40af4485e9208d62be2650c4df64b8
noear <noear@live.cn>
2021-04-15 16:52:19 +0800

    1.3.21

--- _extend/solon.extend.cors/src/main/java/org/noear/solon/extend/cors/annotation/CrossOrigin.java
+++ _extend/solon.extend.cors/src/main/java/org/noear/solon/extend/cors/annotation/CrossOrigin.java
@@ -1,6 +1,7 @@
 package org.noear.solon.extend.cors.annotation;
 
 import org.noear.solon.annotation.Before;
+import org.noear.solon.annotation.Note;
 import org.noear.solon.extend.cors.CrossOriginInterceptor;
 
 import java.lang.annotation.*;
@@ -14,6 +15,7 @@ import java.lang.annotation.*;
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface CrossOrigin {
+    @Note("支持配置模式： ${xxx}")
     String origins() default "*";
     /**
      * 默认：10分钟检测一次
