eb2d9972e27b4a86802bbf8f2627c9fc146ff968
noear <noear@live.cn>
2021-04-27 10:17:47 +0800

    1.3.27

---
+++ _test/src/main/java/webapp/dso/NamiConfigurationImpl.java
@@ -0,0 +1,30 @@
+package webapp.dso;
+
+import org.noear.nami.Filter;
+import org.noear.nami.Nami;
+import org.noear.nami.NamiConfig;
+import org.noear.nami.NamiConfiguration;
+import org.noear.nami.annotation.NamiClient;
+import org.noear.solon.annotation.Component;
+
+import java.util.Map;
+
+/**
+ * @author noear 2021/4/27 created
+ */
+//@Component
+//public class NamiConfigurationImpl implements NamiConfiguration {
+//    @Override
+//    public void config(NamiClient client, Nami.Builder builder) {
+//        builder.filterAdd(FilterImpl.instance);
+//    }
+//
+//    public static class FilterImpl implements Filter {
+//        public static final Filter instance = new FilterImpl();
+//
+//        @Override
+//        public void filter(NamiConfig cfg, String action, String url, Map<String, String> headers, Map<String, Object> args) {
+//            headers.put("key","val");
+//        }
+//    }
+//}
