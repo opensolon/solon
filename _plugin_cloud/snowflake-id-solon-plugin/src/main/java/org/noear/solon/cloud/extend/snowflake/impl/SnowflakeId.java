223a796028ccb1fdc67b19c8c053871c4180e030
noear <noear@live.cn>
2021-05-04 08:07:19 +0800

    1.3.32

--- _plugin_cloud/snowflake-id-solon-plugin/src/main/java/org/noear/solon/cloud/extend/snowflake/impl/SnowflakeId.java
+++ _plugin_cloud/snowflake-id-solon-plugin/src/main/java/org/noear/solon/cloud/extend/snowflake/impl/SnowflakeId.java
@@ -11,30 +11,32 @@ import java.util.Random;
  * @since 1.3
  */
 public class SnowflakeId {
-    //起始时间 2020-01-01 00:00:00
+    /**
+     * 默认起始时间 2020-01-01 00:00:00
+     * */
     private static final long START_TIME_DEF = 1577808000000L;
     private String dataBlock;
 
     public SnowflakeId() {
         this.dataBlock = Solon.cfg().appGroup() + "::" + Solon.cfg().appName();
 
-        START_TIME = START_TIME_DEF;
+        startTime = START_TIME_DEF;
 
-        DATA_ID = getDataId();
-        WORK_ID = getWorkId();
+        dataId = getDataId();
+        workId = getWorkId();
     }
 
     public SnowflakeId(String dataBlock, long startTime) {
         this.dataBlock = dataBlock;
 
         if(startTime > 0) {
-            START_TIME = startTime;
+            this.startTime = startTime;
         }else{
-            START_TIME = START_TIME_DEF;
+            this.startTime = START_TIME_DEF;
         }
 
-        DATA_ID = getDataId();
-        WORK_ID = getWorkId();
+        dataId = getDataId();
+        workId = getWorkId();
     }
 
     public SnowflakeId(long dataId, long workId) {
@@ -42,22 +44,22 @@ public class SnowflakeId {
     }
 
     public SnowflakeId(long dataId, long workId, long startTime) {
-        if (dataId > DATA_MAX_NUM || dataId < 0) {
+        if (dataId > dataMaxNum || dataId < 0) {
             throw new IllegalArgumentException("dataId can't be greater than DATA_MAX_NUM or less than 0");
         }
-        if (workId > WORK_MAX_NUM || workId < 0) {
+        if (workId > workMaxNum || workId < 0) {
             throw new IllegalArgumentException("workId can't be greater than WORK_MAX_NUM or less than 0");
         }
 
         //默认起始时间 2020-01-01 00:00:00
         if(startTime > 0) {
-            START_TIME = startTime;
+            this.startTime = startTime;
         }else{
-            START_TIME = START_TIME_DEF;
+            this.startTime = START_TIME_DEF;
         }
 
-        DATA_ID = dataId;
-        WORK_ID = workId;
+        this.dataId = dataId;
+        this.workId = workId;
     }
 
     ////////////////////////////
@@ -65,97 +67,97 @@ public class SnowflakeId {
     /**
      * 时间部分所占长度
      */
-    private final int TIME_LEN = 41;
+    private final int timeLen = 41;
     /**
      * 数据中心id所占长度
      */
-    private final int DATA_LEN = 5;
+    private final int dataLen = 5;
     /**
      * 机器id所占长度
      */
-    private final int WORK_LEN = 5;
+    private final int workLen = 5;
     /**
      * 毫秒内序列所占长度
      */
-    private final int SEQ_LEN = 12;
+    private final int seqLen = 12;
 
     /**
      * 定义起始时间 2020-01-01 00:00:00
      */
-    private final long START_TIME;
+    private final long startTime;
     /**
      * 上次生成ID的时间截
      */
-    private long LAST_TIME_STAMP = -1L;
+    private long lastTimeStamp = -1L;
     /**
      * 时间部分向左移动的位数 22
      */
-    private final int TIME_LEFT_BIT = 64 - 1 - TIME_LEN;
+    private final int timeLeftBit = 64 - 1 - timeLen;
 
     /**
      * 自动获取数据中心id（可以手动定义 0-31之间的数）
      */
-    private final long DATA_ID;
+    private final long dataId;
     /**
      * 自动机器id（可以手动定义 0-31之间的数）
      */
-    private final long WORK_ID;
+    private final long workId;
     /**
      * 数据中心id最大值 31
      */
-    private final int DATA_MAX_NUM = ~(-1 << DATA_LEN);
+    private final int dataMaxNum = ~(-1 << dataLen);
     /**
      * 机器id最大值 31
      */
-    private final int WORK_MAX_NUM = ~(-1 << WORK_LEN);
+    private final int workMaxNum = ~(-1 << workLen);
     /**
      * 随机获取数据中心id的参数 32
      */
-    private final int DATA_RANDOM = DATA_MAX_NUM + 1;
+    private final int dataRandom = dataMaxNum + 1;
     /**
      * 随机获取机器id的参数 32
      */
-    private final int WORK_RANDOM = WORK_MAX_NUM + 1;
+    private final int workRandom = workMaxNum + 1;
     /**
      * 数据中心id左移位数 17
      */
-    private final int DATA_LEFT_BIT = TIME_LEFT_BIT - DATA_LEN;
+    private final int dataLeftBit = timeLeftBit - dataLen;
     /**
      * 机器id左移位数 12
      */
-    private final int WORK_LEFT_BIT = DATA_LEFT_BIT - WORK_LEN;
+    private final int workLeftBit = dataLeftBit - workLen;
 
     /**
      * 上一次的毫秒内序列值
      */
-    private long LAST_SEQ = 0L;
+    private long seqLastVal = 0L;
     /**
      * 毫秒内序列的最大值 4095
      */
-    private final long SEQ_MAX_NUM = ~(-1 << SEQ_LEN);
+    private final long seqMaxNum = ~(-1 << seqLen);
 
 
     public synchronized long nextId() {
         long now = System.currentTimeMillis();
 
         //如果当前时间小于上一次ID生成的时间戳，说明系统时钟回退过这个时候应当抛出异常
-        if (now < LAST_TIME_STAMP) {
+        if (now < lastTimeStamp) {
             throw new RuntimeException("System time error refused to generate snowflake ID!");
         }
 
-        if (now == LAST_TIME_STAMP) {
-            LAST_SEQ = (LAST_SEQ + 1) & SEQ_MAX_NUM;
-            if (LAST_SEQ == 0) {
-                now = nextMillis(LAST_TIME_STAMP);
+        if (now == lastTimeStamp) {
+            seqLastVal = (seqLastVal + 1) & seqMaxNum;
+            if (seqLastVal == 0) {
+                now = nextMillis(lastTimeStamp);
             }
         } else {
-            LAST_SEQ = 0;
+            seqLastVal = 0;
         }
 
         //上次生成ID的时间截
-        LAST_TIME_STAMP = now;
+        lastTimeStamp = now;
 
-        return ((now - START_TIME) << TIME_LEFT_BIT) | (DATA_ID << DATA_LEFT_BIT) | (WORK_ID << WORK_LEFT_BIT) | LAST_SEQ;
+        return ((now - startTime) << timeLeftBit) | (dataId << dataLeftBit) | (workId << workLeftBit) | seqLastVal;
     }
 
 
@@ -187,16 +189,16 @@ public class SnowflakeId {
      */
     protected int getWorkId() {
         try {
-            return getHostId(Inet4Address.getLocalHost().getHostAddress(), WORK_MAX_NUM);
+            return getHostId(Inet4Address.getLocalHost().getHostAddress(), workMaxNum);
         } catch (UnknownHostException e) {
-            return new Random().nextInt(WORK_RANDOM);
+            return new Random().nextInt(workRandom);
         }
     }
 
     /**
-     * 根据 block 取余，发生异常就获取 0到31之间的随机数
+     * 根据 data block 取余，发生异常就获取 0到31之间的随机数
      */
     protected int getDataId() {
-        return getHostId(dataBlock, DATA_MAX_NUM);
+        return getHostId(dataBlock, dataMaxNum);
     }
 }
