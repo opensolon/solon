3154f7af4d0fb5f43cd858dc4fa4270e426b0c76
noear <noear@live.cn>
2021-05-04 11:27:09 +0800

    1.3.32

--- _plugin_cloud/kafka-solon-plugin/src/main/java/org/noear/solon/cloud/extend/kafka/XPluginImp.java
+++ _plugin_cloud/kafka-solon-plugin/src/main/java/org/noear/solon/cloud/extend/kafka/XPluginImp.java
@@ -1,14 +1,28 @@
 package org.noear.solon.cloud.extend.kafka;
 
 import org.noear.solon.SolonApp;
+import org.noear.solon.Utils;
+import org.noear.solon.cloud.CloudManager;
+import org.noear.solon.cloud.extend.kafka.service.CloudEventServiceKafkaImp;
+import org.noear.solon.core.Aop;
 import org.noear.solon.core.Plugin;
 
 /**
- * @author noear 2021/5/4 created
+ * @author noear
+ * @since 1.3
  */
 public class XPluginImp implements Plugin {
     @Override
     public void start(SolonApp app) {
+        String server = KafkaProps.instance.getEventServer();
 
+        if (Utils.isNotEmpty(server)) {
+            if (KafkaProps.instance.getEventEnable()) {
+                CloudEventServiceKafkaImp eventServiceImp = CloudEventServiceKafkaImp.getInstance();
+                CloudManager.register(KafkaProps.instance.getEventChannel(), eventServiceImp);
+
+                Aop.beanOnloaded(eventServiceImp::subscribe);
+            }
+        }
     }
 }
--- _plugin_cloud/kafka-solon-plugin/src/main/java/org/noear/solon/cloud/extend/kafka/service/CloudEventServiceKafkaImp.java
+++ _plugin_cloud/kafka-solon-plugin/src/main/java/org/noear/solon/cloud/extend/kafka/service/CloudEventServiceKafkaImp.java
@@ -1,28 +1,63 @@
 package org.noear.solon.cloud.extend.kafka.service;
 
-import io.netty.handler.codec.string.StringEncoder;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.noear.solon.Solon;
+import org.noear.solon.Utils;
 import org.noear.solon.cloud.CloudEventHandler;
 import org.noear.solon.cloud.annotation.EventLevel;
 import org.noear.solon.cloud.exception.CloudEventException;
 import org.noear.solon.cloud.extend.kafka.KafkaProps;
 import org.noear.solon.cloud.model.Event;
+import org.noear.solon.cloud.service.CloudEventObserverEntity;
 import org.noear.solon.cloud.service.CloudEventService;
+import org.noear.solon.core.event.EventBus;
+import org.noear.solon.ext.WarnThrowable;
 
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 /**
- * @author noear 2021/5/4 created
+ * @author noear
+ * @since 1.3
  */
 public class CloudEventServiceKafkaImp implements CloudEventService {
-    private final Producer<String, String> producer;
-    private long timeout;
+    private static CloudEventServiceKafkaImp instance;
+    public static synchronized CloudEventServiceKafkaImp getInstance() {
+        if (instance == null) {
+            instance = new CloudEventServiceKafkaImp();
+        }
 
-    public CloudEventServiceKafkaImp(String server){
+        return instance;
+    }
+
+
+
+    private KafkaProducer<String, String> producer;
+    private KafkaConsumer<String, String> consumer;
+
+    private long timeout;
+    private String server;
+    String eventChannelName;
+
+    public CloudEventServiceKafkaImp() {
         this.timeout = KafkaProps.instance.getEventPublishTimeout();
+        this.server = KafkaProps.instance.getEventServer();
+        this.eventChannelName = KafkaProps.instance.getEventChannel();
+    }
+
+    private synchronized void initProducer() {
+        if (producer != null) {
+            return;
+        }
 
         Properties props = new Properties();
 
@@ -36,9 +71,35 @@ public class CloudEventServiceKafkaImp implements CloudEventService {
         producer = new KafkaProducer<String, String>(props);
     }
 
+    private synchronized void initConsumer() {
+        if (consumer != null) {
+            return;
+        }
+
+        Properties props = new Properties();
+
+        props.put("bootstrap.servers", server);
+        props.put("group.id", Solon.cfg().appGroup() + "::" + Solon.cfg().appName());
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("session.timeout.ms", "30000");
+        props.put("max.poll.records", 1000);
+        props.put("auto.offset.reset", "earliest");
+        props.put("key.deserializer", StringDeserializer.class.getName());
+        props.put("value.deserializer", StringDeserializer.class.getName());
+
+        consumer = new KafkaConsumer<String, String>(props);
+    }
+
 
     @Override
     public boolean publish(Event event) throws CloudEventException {
+        initProducer();
+
+        if(Utils.isEmpty(event.key())){
+            event.key(Utils.guid());
+        }
+
         Future<RecordMetadata> future = producer.send(new ProducerRecord<String, String>(event.topic(), event.key(), event.content()));
         if (event.qos() > 0) {
             try {
@@ -51,8 +112,69 @@ public class CloudEventServiceKafkaImp implements CloudEventService {
         return true;
     }
 
+    Map<String, CloudEventObserverEntity> observerMap = new HashMap<>();
+
     @Override
     public void attention(EventLevel level, String channel, String group, String topic, CloudEventHandler observer) {
+        if (observerMap.containsKey(topic)) {
+            return;
+        }
 
+        observerMap.put(topic, new CloudEventObserverEntity(level, group, topic, observer));
+    }
+
+    public void subscribe() {
+        try {
+            initConsumer();
+
+            if (observerMap.size() > 0) {
+                consumer.subscribe(observerMap.keySet());
+            }
+        } catch (Throwable ex) {
+            throw new RuntimeException(ex);
+        }
+
+        new Thread(this::subscribePull).start();
+    }
+
+    private void subscribePull() {
+        while (true) {
+            boolean isOk = true;
+
+            try {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+                for (ConsumerRecord<String, String> record : records) {
+                    Event event = new Event(record.topic(), record.value())
+                            .key(record.key())
+                            .channel(eventChannelName);
+
+                    isOk = isOk && onReceive(event);
+                }
+
+                if (isOk) {
+                    consumer.commitAsync();
+                }
+            } catch (Throwable e) {
+                EventBus.push(e);
+            }
+        }
+    }
+
+    /**
+     * 处理接收事件
+     */
+    public boolean onReceive(Event event) throws Throwable {
+        boolean isOk = true;
+        CloudEventObserverEntity entity = null;
+
+        entity = observerMap.get(event.topic());
+        if (entity != null) {
+            isOk = entity.handler(event);
+        }else{
+            //只需要记录一下
+            EventBus.push(new WarnThrowable(event, "There is no observer for this event topic[" + event.topic() + "]"));
+        }
+
+        return isOk;
     }
 }
