56545bdf0292d6fbc65460352374c26d7b777e67
noear <noear@live.cn>
2021-05-10 23:24:24 +0800

    1.3.36

--- _test/src/main/java/webapp/demox_log_breaker/TestAppender.java
+++ _test/src/main/java/webapp/demox_log_breaker/TestAppender.java
@@ -1,12 +1,12 @@
 package webapp.demox_log_breaker;
 
-import org.noear.solon.logging.event.Appender;
+import org.noear.solon.logging.event.AppenderBase;
 import org.noear.solon.logging.event.LogEvent;
 
 /**
  * @author noear 2021/2/25 created
  */
-public class TestAppender implements Appender {
+public class TestAppender extends AppenderBase {
 
     @Override
     public void append(LogEvent logEvent) {
