9652ff169ebbbd5622bf0f91eca4ed26d868d59f
noear <noear@live.cn>
2021-05-10 17:00:42 +0800

    1.3.36

---
+++ _test/src/test/java/test1/LogTest.java
@@ -0,0 +1,15 @@
+package test1;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+/**
+ * @author noear 2021/5/10 created
+ */
+@Slf4j
+public class LogTest {
+    @Test
+    public void test(){
+        log.info("test");
+    }
+}
