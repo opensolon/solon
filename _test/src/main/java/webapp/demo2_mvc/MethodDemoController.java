2c3484199469a8e061dd40b510c65261a0b49511
noear <noear@live.cn>
2021-05-10 12:10:58 +0800

    1.3.36

--- _test/src/main/java/webapp/demo2_mvc/MethodDemoController.java
+++ _test/src/main/java/webapp/demo2_mvc/MethodDemoController.java
@@ -1,36 +1,49 @@
 package webapp.demo2_mvc;
 
 import org.noear.solon.annotation.*;
-import org.noear.solon.core.handle.Context;
 import org.noear.solon.core.handle.MethodType;
 
 @Mapping("/demo2/method0")
 @Controller
 public class MethodDemoController {
+    /**
+     * (1)没事儿，这么写就够了
+     */
+
     //经典写法
-    @Mapping(value = "post0", method = MethodType.POST)
-    public String test_post0(Context context) {
-        return context.param("name");
+    @Mapping("demo00")
+    public String demo00(String name) {
+        return name;
+    }
+
+    /**
+     * (2)像 REST api，强调 method的
+     */
+
+    //经典写法
+    @Mapping(value = "demo10", method = MethodType.POST)
+    public String demo10(String name) {
+        return name;
     }
 
     //新增写法（新旧，2种都可以玩）
     @Post
-    @Mapping("post")
-    public String test_post(Context context) {
-        return context.param("name");
+    @Mapping("demo11")
+    public String demo11(String name) {
+        return name;
     }
 
     //经典写法
-    @Mapping(value = "http0", method = {MethodType.HTTP, MethodType.SOCKET})
-    public String test_http0(Context context) {
-        return context.param("name");
+    @Mapping(value = "demo20", method = {MethodType.HTTP, MethodType.SOCKET})
+    public String demo20(String name) {
+        return name;
     }
 
     //新增写法（新旧，2种都可以玩）
     @Socket
     @Http
-    @Mapping("http")
-    public String test_http(Context context) {
-        return context.param("name");
+    @Mapping("demo21")
+    public String demo21(String name) {
+        return name;
     }
 }
