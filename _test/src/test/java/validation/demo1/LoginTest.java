40b24c27d80663c3c96f6aee12565cfd30721617
noear <noear@live.cn>
2021-05-11 15:00:44 +0800

    1.3.36

--- _test/src/test/java/validation/demo1/LoginTest.java
+++ _test/src/test/java/validation/demo1/LoginTest.java
@@ -2,6 +2,7 @@ package validation.demo1;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.noear.solon.core.handle.Result;
 import org.noear.solon.extend.jsr303.ValidationUtils;
 import org.noear.solon.test.SolonJUnit4ClassRunner;
 
@@ -11,12 +12,27 @@ import org.noear.solon.test.SolonJUnit4ClassRunner;
 @RunWith(SolonJUnit4ClassRunner.class)
 public class LoginTest {
     @Test
-    public void login() throws Exception{
+    public void login() throws Exception {
         LoginForm loginForm = new LoginForm();
         loginForm.setMobile("xxx");
 
         System.out.println("已进入login" + loginForm);
 
-        ValidationUtils.validate(loginForm);
+        Result result  = ValidationUtils.validate(loginForm);
+
+        assert result.getCode() == Result.FAILURE_CODE;
+    }
+
+    @Test
+    public void login2() throws Exception {
+        LoginForm loginForm = new LoginForm();
+        loginForm.setMobile("xxx");
+        loginForm.setPassword("xx");
+
+        System.out.println("已进入login" + loginForm);
+
+        Result result  = ValidationUtils.validate(loginForm);
+
+        assert result.getCode() == Result.SUCCEED_CODE;
     }
 }
