33b96c39ddd488b58634fec0bb5c0b529de41ffc
noear <noear@live.cn>
2021-05-13 23:49:43 +0800

    1.3.38

--- _plugin/shiro-solon-plugin/src/test/java/demo/AuthController.java
+++ _plugin/shiro-solon-plugin/src/test/java/demo/AuthController.java
@@ -17,7 +17,7 @@ public class AuthController {
         //验证用户是否登录，等同于方法subject.isAuthenticated() 结果为true时。
     }
     @RequiresGuest
-    @Mapping("/t1")
+    @Mapping("/t2")
     public void testGuest(){
         /**
          * 验证是否是一个guest的请求，与@RequiresUser完全相反。
@@ -26,7 +26,7 @@ public class AuthController {
          * */
     }
     @RequiresUser
-    @Mapping("/t1")
+    @Mapping("/t3")
     public void testUser(){
         /**
          * 验证用户是否被记忆，user有两种含义：
@@ -35,12 +35,12 @@ public class AuthController {
          * */
     }
     @RequiresRoles("admin")
-    @Mapping("/t1")
+    @Mapping("/t4")
     public void testRoles(){
         //如果subject中有aRoleName角色才可以访问方法someMethod。如果没有这个权限则会抛出异常AuthorizationException。
     }
     @RequiresPermissions({"file:read", "write:aFile.txt", "xx:xx:xx"} )
-    @Mapping("/t1")
+    @Mapping("/t5")
     public void xxx(){
         //要求subject中必须同时含有file:read和write:aFile.txt的权限才能执行方法someMethod()。否则抛出异常AuthorizationException。
     }
