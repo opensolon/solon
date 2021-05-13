236b859e531a3a4feb6171959f8a94f783809c95
noear <noear@live.cn>
2021-05-13 22:48:05 +0800

    1.3.38

--- _test/src/main/java/webapp/demoa_interceptor/AroundHandler.java
+++ _test/src/main/java/webapp/demoa_interceptor/AroundHandler.java
@@ -3,12 +3,14 @@ package webapp.demoa_interceptor;
 import org.noear.solon.core.handle.InterceptorChain;
 import org.noear.solon.core.handle.Interceptor;
 import org.noear.solon.core.handle.Context;
+import org.noear.solon.core.handle.Invocation;
+import org.omg.CORBA.DynAnyPackage.Invalid;
 
 public class AroundHandler implements Interceptor {
 
     @Override
-    public Object doIntercept(Object obj, Object[] args, InterceptorChain chain) throws Throwable {
+    public Object doIntercept(Invocation inv) throws Throwable {
         Context.current().output("@Around:我也加一点；");
-        return chain.doIntercept(obj, args);
+        return inv.invoke();
     }
 }
