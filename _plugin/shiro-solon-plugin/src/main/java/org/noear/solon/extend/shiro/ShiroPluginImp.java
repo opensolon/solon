4a6878f5c1532d7d933f71b913913a34a7025b15
noear <noear@live.cn>
2021-05-14 12:33:57 +0800

    1.3.38

--- _plugin/shiro-solon-plugin/src/main/java/org/noear/solon/extend/shiro/ShiroPluginImp.java
+++ _plugin/shiro-solon-plugin/src/main/java/org/noear/solon/extend/shiro/ShiroPluginImp.java
@@ -13,8 +13,6 @@ import org.noear.solon.extend.shiro.config.ShiroConfiguration;
  * @since 2021/5/12 23:20
  */
 public class ShiroPluginImp implements Plugin {
-
-    @SuppressWarnings("unchecked")
     @Override
     public void start(SolonApp app) {
         //换了种方式，注册AOP环绕处理，获得一级拦截权限（之前的方案，需要类上有：@Valid 注解；借助它的拦截，获得二级执行权限有）
