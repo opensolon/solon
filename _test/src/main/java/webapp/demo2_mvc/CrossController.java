a64f6f8cf288e8a5a06ae450d38b09c8cc836b5f
noear <noear@live.cn>
2021-05-22 06:40:00 +0800

    1.4.2

---
+++ _test/src/main/java/webapp/demo2_mvc/CrossController.java
@@ -0,0 +1,18 @@
+package webapp.demo2_mvc;
+
+import org.noear.solon.annotation.Controller;
+import org.noear.solon.annotation.Mapping;
+import org.noear.solon.extend.cors.annotation.CrossOrigin;
+
+/**
+ * @author noear 2021/5/22 created
+ */
+@Mapping("/demo2/cross")
+@CrossOrigin(origins = "*")
+@Controller
+public class CrossController {
+    @Mapping("test")
+    public void test(){
+
+    }
+}
