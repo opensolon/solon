e11385b3c54677d990fe8b91eb6ccf0ef66ea273
noear <noear@live.cn>
2021-05-24 15:00:51 +0800

    1.4.4

--- _plugin/activerecord-solon-plugin/src/main/java/org/noear/solon/extend/activerecord/XPluginImp.java
+++ _plugin/activerecord-solon-plugin/src/main/java/org/noear/solon/extend/activerecord/XPluginImp.java
@@ -1,7 +1,10 @@
 package org.noear.solon.extend.activerecord;
 
 import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.DbKit;
 import com.jfinal.plugin.activerecord.IDataSourceProvider;
+import com.jfinal.template.source.ClassPathSourceFactory;
 import org.noear.solon.SolonApp;
 import org.noear.solon.core.Aop;
 import org.noear.solon.core.Plugin;
@@ -17,7 +20,7 @@ import javax.sql.DataSource;
 public class XPluginImp implements Plugin {
     @Override
     public void start(SolonApp app) {
-        Aop.getAsyn(DataSource.class, bw->{
+        Aop.getAsyn(DataSource.class, bw -> {
             initActiveRecord(bw.raw());
         });
     }
@@ -26,6 +29,9 @@ public class XPluginImp implements Plugin {
         IDataSourceProvider dsp = new DataSourceProviderWrap(ds);
 
         ActiveRecordPlugin arp = new ActiveRecordPlugin(dsp);
+
+//        arp.getEngine().setBaseTemplatePath("/activerecord/");
+        arp.getEngine().setSourceFactory(new ClassPathSourceFactory());
 
         ResourceScaner.scan("activerecord", n -> n.endsWith(".sql"))
                 .forEach(url -> {
