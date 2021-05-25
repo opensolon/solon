b5f4de6b2745d84022d54e32a310e7d766a863b5
noear <noear@live.cn>
2021-05-25 23:45:39 +0800

    1.4.5

--- _extend/solon.extend.auth/src/main/java/org/noear/solon/extend/security/AuthService.java
+++ _extend/solon.extend.auth/src/main/java/org/noear/solon/extend/security/AuthService.java
@@ -1,6 +1,6 @@
 package org.noear.solon.extend.security;
 
-import org.noear.solon.core.handle.MethodType;
+import org.noear.solon.core.handle.Context;
 import org.noear.solon.extend.security.annotation.Logical;
 import org.noear.solon.extend.security.model.UserModel;
 
@@ -11,37 +11,36 @@ import org.noear.solon.extend.security.model.UserModel;
 public interface AuthService {
     /**
      * 用账号密码登录
-     * */
+     */
     boolean login(String username, String password);
 
     /**
      * 用领牌登录
-     * */
+     */
     boolean login(String token);
 
     /**
      * 登录后加载模型（可用于自动登录）
-     * */
+     */
     void loginLoad(UserModel user);
 
     /**
      * 验证登录状态
-     * */
+     */
     boolean verifyLogined();
 
     /**
      * 验证路径（一般使用路径验证）
-     * */
+     */
     boolean verifyPath(String path, String method);
 
     /**
      * 验证权限（有特殊情况用权限验证）
-     * */
+     */
     boolean verifyPermissions(String[] permissions, Logical logical);
 
     /**
      * 验证角色（有特殊情况用角色验证）
-     * */
+     */
     boolean verifyRoles(String[] roles, Logical logical);
-
 }
