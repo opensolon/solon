ae8be85311cd802ef5fc9609b649ee721ebcbbe3
noear <noear@live.cn>
2021-05-25 20:48:55 +0800

    1.4.4

--- _extend/solon.extend.security/src/main/java/org/noear/solon/extend/security/model/MenuNode.java
+++ _extend/solon.extend.security/src/main/java/org/noear/solon/extend/security/model/MenuNode.java
@@ -4,12 +4,31 @@ import java.util.Map;
 
 /**
  * @author noear
+ * @since 1.4
  */
 public class MenuNode {
+    /**
+     * 名字（代号）
+     * */
     String name;
+    /**
+     * 显示名
+     * */
     String displayName;
+    /**
+     * 地址
+     * */
     String url;
+    /**
+     * 窗口止录
+     * */
     String target;
+    /**
+     * 图标
+     * */
     String icon;
+    /**
+     * 元信息
+     * */
     Map<String,String> meta;
 }
