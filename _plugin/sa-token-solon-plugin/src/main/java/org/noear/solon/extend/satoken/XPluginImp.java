78497f052c590ee13b8ffc162285afe8a04e6fcc
noear <noear@live.cn>
2021-05-29 23:57:03 +0800

    1.4.8

---
+++ _plugin/sa-token-solon-plugin/src/main/java/org/noear/solon/extend/satoken/XPluginImp.java
@@ -0,0 +1,15 @@
+package org.noear.solon.extend.satoken;
+
+import org.noear.solon.SolonApp;
+import org.noear.solon.core.Plugin;
+
+/**
+ * @author noear
+ * @since 1.4
+ */
+public class XPluginImp implements Plugin {
+    @Override
+    public void start(SolonApp app) {
+
+    }
+}
