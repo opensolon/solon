6f626c8efb48254030dea8dfee8f9ad7432e9c73
noear <noear@live.cn>
2021-06-02 10:46:04 +0800

    1.4.9

--- _extend/solon.extend.auth/src/main/java/org/noear/solon/extend/auth/AuthException.java
+++ _extend/solon.extend.auth/src/main/java/org/noear/solon/extend/auth/AuthException.java
@@ -1,15 +1,13 @@
 package org.noear.solon.extend.auth;
 
 /**
+ * 认证异常
+ *
  * @author noear
  * @since 1.4
  */
 public class AuthException extends RuntimeException {
     public AuthException(String message) {
         super(message);
-    }
-
-    public AuthException(Throwable cause) {
-        super(cause);
     }
 }
