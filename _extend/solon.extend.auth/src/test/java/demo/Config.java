a0902b74b2b172c4e967980f5187c19c286f721e
noear <noear@live.cn>
2021-06-02 11:09:39 +0800

    1.4.9

--- _extend/solon.extend.auth/src/test/java/demo/Config.java
+++ _extend/solon.extend.auth/src/test/java/demo/Config.java
@@ -10,16 +10,16 @@ import org.noear.solon.extend.auth.AuthAdapter;
 @Configuration
 public class Config {
     @Bean
-    public AuthAdapter adapter(){
+    public AuthAdapter adapter() {
         return new AuthAdapter()
                 .loginUrl("/login") //设定登录地址，未登录时自动跳转
-                .addRule(b->b.include("").exclude("").verifyIp())
-                .addRule(b->b.include("").exclude("").verifyLogined())
-                .addRule(b->b.include("").exclude("").verifyPath())
-                .addRule(b->b.include("").exclude("").verifyPermissions(""))
-                .addRule(b->b.include("").exclude("").verifyPermissionsAnd(""))
-                .addRule(b->b.include("").exclude("").verifyRoles(""))
-                .addRule(b->b.include("").exclude("").verifyRolesAnd(""))
+                .addRule(b -> b.include("").exclude("").verifyIp().failure((c, r) -> c.output("你的ip不是白名单")))
+                .addRule(b -> b.include("").exclude("").verifyLogined())
+                .addRule(b -> b.include("").exclude("").verifyPath())
+                .addRule(b -> b.include("").exclude("").verifyPermissions(""))
+                .addRule(b -> b.include("").exclude("").verifyPermissionsAnd(""))
+                .addRule(b -> b.include("").exclude("").verifyRoles(""))
+                .addRule(b -> b.include("").exclude("").verifyRolesAnd(""))
                 .processor(null) //设定认证处理器
                 .failure((ctx, rst) -> { //设定验证失败代理
                     ctx.render(rst);
