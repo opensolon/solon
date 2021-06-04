01c4b6c9e4146330f1f8c04e849e9f269abf3a92
noear <noear@live.cn>
2021-06-04 21:38:46 +0800

    1.4.11

---
+++ _extend/solon.cloud/src/main/java/org/noear/solon/cloud/CloudJobHandler.java
@@ -0,0 +1,11 @@
+package org.noear.solon.cloud;
+
+import org.noear.solon.core.handle.Handler;
+
+/**
+ * @author noear
+ * @since 1.4
+ */
+@FunctionalInterface
+public interface CloudJobHandler extends Handler {
+}
