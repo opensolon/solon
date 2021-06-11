aa5392352732dc479e402b7513424e2e4ae924d4
noear <noear@live.cn>
2021-06-11 16:24:17 +0800

    1.4.14

--- _extend/solon.view.velocity/src/main/java/org/noear/solon/view/velocity/tags/HasRoleTag.java
+++ _extend/solon.view.velocity/src/main/java/org/noear/solon/view/velocity/tags/HasRoleTag.java
@@ -6,7 +6,10 @@ import org.apache.velocity.exception.ParseErrorException;
 import org.apache.velocity.exception.ResourceNotFoundException;
 import org.apache.velocity.runtime.directive.Directive;
 import org.apache.velocity.runtime.parser.node.Node;
+import org.noear.solon.Utils;
 import org.noear.solon.annotation.Component;
+import org.noear.solon.extend.auth.AuthUtil;
+import org.noear.solon.extend.auth.annotation.Logical;
 
 import java.io.IOException;
 import java.io.Writer;
@@ -29,6 +32,33 @@ public class HasRoleTag extends Directive {
 
     @Override
     public boolean render(InternalContextAdapter context, Writer writer, Node node) throws IOException, ResourceNotFoundException, ParseErrorException, MethodInvocationException {
-        return true;
+        int attrNum = node.jjtGetNumChildren();
+
+        if (attrNum < 0) {
+            return false;
+        }
+
+        String nameStr = (String) node.jjtGetChild(0).value(context);
+        String logicalStr = null;
+
+        if (attrNum > 1) {
+            logicalStr = (String) node.jjtGetChild(1).value(context);
+        }
+
+        if (Utils.isEmpty(nameStr)) {
+            return false;
+        }
+
+        String[] names = nameStr.split(",");
+
+        if (names.length == 0) {
+            return false;
+        }
+
+        if (AuthUtil.verifyRoles(names, Logical.of(logicalStr))) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
