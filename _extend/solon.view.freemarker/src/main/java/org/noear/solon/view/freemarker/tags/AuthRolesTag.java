6eea431e0141fc67e4f1b3fd98701e590b6a6294
noear <noear@live.cn>
2021-06-12 18:26:27 +0800

    1.5.0

--- _extend/solon.view.freemarker/src/main/java/org/noear/solon/view/freemarker/tags/AuthRolesTag.java
+++ _extend/solon.view.freemarker/src/main/java/org/noear/solon/view/freemarker/tags/AuthRolesTag.java
@@ -19,13 +19,31 @@ import java.util.Map;
  * @since 1.4
  */
 public class AuthRolesTag implements TemplateDirectiveModel {
+    final String nameValue;
+
+    public AuthRolesTag() {
+        nameValue = null;
+    }
+
+    public AuthRolesTag(String names) {
+        nameValue = names;
+    }
+
     @Override
     public void execute(Environment env, Map map, TemplateModel[] templateModels, TemplateDirectiveBody body) throws TemplateException, IOException {
-        NvMap mapExt = new NvMap(map);
+        if (nameValue == null) {
+            NvMap mapExt = new NvMap(map);
 
-        String nameStr = mapExt.get(Constants.ATTR_name);
-        String logicalStr = mapExt.get(Constants.ATTR_logical);
+            String nameStr = mapExt.get(Constants.ATTR_name);
+            String logicalStr = mapExt.get(Constants.ATTR_logical);
 
+            executeDo(env, body, nameStr, logicalStr);
+        } else {
+            executeDo(env, body, nameValue, null);
+        }
+    }
+
+    private void executeDo(Environment env, TemplateDirectiveBody body, String nameStr, String logicalStr) throws TemplateException, IOException {
         if (Utils.isEmpty(nameStr)) {
             return;
         }
