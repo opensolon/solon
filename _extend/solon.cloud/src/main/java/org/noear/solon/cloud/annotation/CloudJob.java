de2de94e72c4e447a66b1951bb35b188de24c436
noear <noear@live.cn>
2021-06-25 18:04:19 +0800

    1.5.7

--- _extend/solon.cloud/src/main/java/org/noear/solon/cloud/annotation/CloudJob.java
+++ _extend/solon.cloud/src/main/java/org/noear/solon/cloud/annotation/CloudJob.java
@@ -19,14 +19,15 @@ public @interface CloudJob {
      * */
     @Note("name")
     String value();
-    /**
-     * 描述, 支持${xxx}配置
-     * */
-    String description() default "";
 
     /**
      * 时间表达式（具体由适配框架支持）
      * */
     @Note("或cron：支持7位（秒，分，时，日期ofM，月，星期ofW，年）； 或简配：s，m，h，d")
-    String cron7x();
+    String cron7x() default "";
+
+    /**
+     * 描述, 支持${xxx}配置
+     * */
+    String description() default "";
 }
