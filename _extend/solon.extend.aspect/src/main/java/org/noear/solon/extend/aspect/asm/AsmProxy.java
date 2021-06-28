76c9964a3840ec39def5928461375c6c20cf5cac
noear <noear@live.cn>
2021-06-28 13:45:10 +0800

    1.5.7

--- _extend/solon.extend.aspect/src/main/java/org/noear/solon/extend/aspect/asm/AsmProxy.java
+++ _extend/solon.extend.aspect/src/main/java/org/noear/solon/extend/aspect/asm/AsmProxy.java
@@ -15,8 +15,8 @@ import java.util.List;
 import java.util.Map;
 
 public class AsmProxy {
-    public static final int ASM_VERSION = Opcodes.ASM7;
-    public static final int ASM_JDK_VERSION = Opcodes.V1_7;
+    public static final int ASM_VERSION = Opcodes.ASM8;
+    public static final int ASM_JDK_VERSION = Opcodes.V1_8;
 
     // 动态生成代理类的前缀
     public static final String PROXY_CLASSNAME_PREFIX = "$Proxy_";
