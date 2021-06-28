ca0e4a6d941539260820adb5aa96abe99257c1e2
noear <noear@live.cn>
2021-06-28 13:59:23 +0800

    1.5.7

--- _test/src/main/java/webapp/demo2_mvc/CmdController.java
+++ _test/src/main/java/webapp/demo2_mvc/CmdController.java
@@ -1,5 +1,6 @@
 package webapp.demo2_mvc;
 
+import org.noear.solon.annotation.Inject;
 import org.noear.solon.annotation.Singleton;
 import org.noear.solon.annotation.Mapping;
 import org.noear.solon.annotation.Controller;
@@ -8,12 +9,16 @@ import org.noear.solon.core.handle.Context;
 @Singleton(false)
 @Controller
 public class CmdController {
+
+    @Inject
+    CmdService cmdService;
+
     @Mapping("/demo2/CMD/{cmd_name}")
     public void cmd(Context ctx, String cmd_name) throws Exception{
         switch (cmd_name) {
-            case "A.0.1": ctx.output(cmd_name);break;
-            case "A.0.2": ctx.output(cmd_name);break;
-            case "A.0.3": ctx.output(cmd_name);break;
+            case "A.0.1": ctx.output(cmdService.name(cmd_name));break;
+            case "A.0.2": ctx.output(cmdService.name(cmd_name));break;
+            case "A.0.3": ctx.output(cmdService.name(cmd_name));break;
             default:ctx.statusSet(404);break;
         }
     }
---
+++ _test/src/main/java/webapp/demo2_mvc/CmdService.java
@@ -0,0 +1,8 @@
+package webapp.demo2_mvc;
+
+/**
+ * @author noear 2021/6/28 created
+ */
+public interface CmdService {
+    String name(String name);
+}
