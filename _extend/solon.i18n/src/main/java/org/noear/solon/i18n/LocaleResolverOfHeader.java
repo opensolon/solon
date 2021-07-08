0b83a20a0bd22256caee05714a01092194084f56
noear <noear@live.cn>
2021-07-08 16:58:14 +0800

    1.5.10

--- _extend/solon.i18n/src/main/java/org/noear/solon/i18n/LocaleResolverOfHeader.java
+++ _extend/solon.i18n/src/main/java/org/noear/solon/i18n/LocaleResolverOfHeader.java
@@ -28,13 +28,14 @@ public class LocaleResolverOfHeader implements LocaleResolver {
     public Locale getLocale(Context ctx) {
         if (ctx.getLocale() == null) {
             String lang = ctx.header(headerName);
-            if(lang.contains(",")){
-                lang = lang.split(",")[0];
-            }
 
             if (Utils.isEmpty(lang)) {
                 ctx.setLocale(Locale.getDefault());
             } else {
+                if(lang.contains(",")){
+                    lang = lang.split(",")[0];
+                }
+
                 ctx.setLocale(LocaleUtil.toLocale(lang));
             }
         }
