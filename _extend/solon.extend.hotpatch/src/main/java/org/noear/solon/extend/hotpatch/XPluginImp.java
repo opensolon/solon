1be7c55165220742e07946c7fb7417e6feed5734
noear <noear@live.cn>
2021-07-23 13:57:11 +0800

    1.5.17

---
+++ _extend/solon.extend.hotpatch/src/main/java/org/noear/solon/extend/hotpatch/XPluginImp.java
@@ -0,0 +1,15 @@
+package org.noear.solon.extend.hotpatch;
+
+import org.noear.solon.SolonApp;
+import org.noear.solon.core.Plugin;
+
+/**
+ * @author noear
+ * @since 1.5
+ */
+public class XPluginImp implements Plugin {
+    @Override
+    public void start(SolonApp app) {
+
+    }
+}
