2fe64260497051b353d591bc041d306b60f8fb3a
noear <noear@live.cn>
2021-07-23 13:00:48 +0800

    1.5.16

--- solon/src/main/java/org/noear/solon/core/Aop.java
+++ solon/src/main/java/org/noear/solon/core/Aop.java
@@ -2,7 +2,6 @@ package org.noear.solon.core;
 
 import org.noear.solon.Utils;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 import java.util.function.BiConsumer;
