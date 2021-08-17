49f506da456ebbe1dbe793cfe15cad46d29a4a58
noear <noear@live.cn>
2021-08-17 11:03:22 +0800

    1.5.25

---
+++ _plugin/solon-springboot-starter/src/main/java/org/noear/solon/extend/springboot/web/HandlerInterceptorSolon.java
@@ -0,0 +1,45 @@
+package org.noear.solon.extend.springboot.web;
+
+import org.noear.solon.core.handle.Context;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Web 拦截器处理，为 Context 添加 result 便于日志
+ *
+ * <pre><code>
+ * @Configuration
+ * public class WebMvcConfigurerSolon implements WebMvcConfigurer {
+ *     @Override
+ *     public void addInterceptors(InterceptorRegistry registry) {
+ *         registry.addInterceptor(new HandlerInterceptorSolon())
+ *                 .addPathPatterns("/**");
+ *     }
+ * }
+ * </code></pre>
+ *
+ * @author noear
+ * @since 1.5
+ */
+public class HandlerInterceptorSolon implements HandlerInterceptor {
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        Context ctx = Context.current();
+        if (ctx != null) {
+            ctx.result = modelAndView;
+        }
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        if (ex != null) {
+            Context ctx = Context.current();
+            if (ctx != null) {
+                ctx.errors = ex;
+            }
+        }
+    }
+}
