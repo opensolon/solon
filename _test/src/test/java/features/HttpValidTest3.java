1fc2a26d8342b5822d459b07d04aa83874ded025
noear <noear@live.cn>
2021-09-05 11:48:42 +0800

    1.5.29

--- _test/src/test/java/features/HttpValidTest3.java
+++ _test/src/test/java/features/HttpValidTest3.java
@@ -19,23 +19,46 @@ public class HttpValidTest3 extends HttpTestBase {
     @Test
     public void test0() throws IOException {
         assert path("/demo2/valid/bean3").get().contains("field10");
-    }
 
-    @Test
-    public void test1() throws IOException {
         Map<String, String> data = new LinkedHashMap<>();
         data.put("field1","");
-
         assert path("/demo2/valid/bean3").data(data).post().contains("field10");
     }
 
     @Test
-    public void test2() throws IOException {
+    public void test1() throws IOException {
         Map<String, String> data = new LinkedHashMap<>();
         data.put("field1","2020-12-12T12:12:12");
         assert path("/demo2/valid/bean3").data(data).post().contains("field10");
 
         data.put("field1","2020-12-12 12:12:12");
         assert path("/demo2/valid/bean3").data(data).post().contains("field1");
+    }
+
+    @Test
+    public void test10() throws IOException {
+        Map<String, String> data = new LinkedHashMap<>();
+        data.put("field1","");
+        data.put("field10","2020-12-12T12:12:12");
+        assert path("/demo2/valid/bean3").data(data).post().contains("field2");
+
+        data.put("field10","");
+        assert path("/demo2/valid/bean3").data(data).post().contains("field10");
+    }
+
+    @Test
+    public void test2() throws IOException {
+        Map<String, String> data = new LinkedHashMap<>();
+        data.put("field1","");
+        data.put("field10","2020-12-12T12:12:12");
+        data.put("field2","noear@live.cn");
+        assert path("/demo2/valid/bean3").data(data).post().contains("field20");
+
+        data.put("field2","");
+        assert path("/demo2/valid/bean3").data(data).post().contains("field20");
+
+
+        data.put("field20","noear@live.cn");
+        assert path("/demo2/valid/bean3").data(data).post().contains("field30");
     }
 }
