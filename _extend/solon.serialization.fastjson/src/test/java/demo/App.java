81847216e2e4b23202abba3c5dc984ed26c1e741
noear <noear@live.cn>
2021-10-09 15:06:56 +0800

    1.5.41

---
+++ _extend/solon.serialization.fastjson/src/test/java/demo/App.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.noear.solon.Solon;
+
+/**
+ * @author noear 2021/10/9 created
+ */
+public class App {
+    public static void main(String[] args){
+        Solon.start(App.class, args);
+    }
+}
