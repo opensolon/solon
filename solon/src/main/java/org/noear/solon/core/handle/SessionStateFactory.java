6588ccb7c47bbd712583a2cea028de66b8c37168
noear <noear@live.cn>
2021-10-10 13:21:57 +0800

    1.5.41

--- solon/src/main/java/org/noear/solon/core/handle/SessionStateFactory.java
+++ solon/src/main/java/org/noear/solon/core/handle/SessionStateFactory.java
@@ -1,11 +1,19 @@
 package org.noear.solon.core.handle;
 
 /**
- * @author noear 2021/2/14 created
+ * Session 状态器工厂
+ *
+ * @author noear
+ * @since 1.3
  */
 public interface SessionStateFactory {
-    /** 优先级 */
+    /**
+     * 优先级
+     * */
     default int priority(){return 0;}
 
+    /**
+     * 创建会话状态
+     * */
     SessionState create(Context ctx);
 }
