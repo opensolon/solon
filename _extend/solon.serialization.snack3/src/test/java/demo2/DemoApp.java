d56950c539a246a1a6e17180cb5022a6af09f0a1
noear <noear@live.cn>
2021-10-19 12:16:17 +0800

    1.5.46

--- _extend/solon.serialization.snack3/src/test/java/demo2/DemoApp.java
+++ _extend/solon.serialization.snack3/src/test/java/demo2/DemoApp.java
@@ -1,7 +1,6 @@
 package demo2;
 
 import org.noear.solon.Solon;
-import org.noear.solon.serialization.JsonLongConverter;
 import org.noear.solon.serialization.JsonConverter;
 import org.noear.solon.serialization.snack3.SnackRenderFactory;
 
@@ -13,26 +12,26 @@ import java.util.Date;
  * @author noear 2021/10/12 created
  */
 public class DemoApp {
-    public static void main(String[] args){
-        Solon.start(DemoApp.class, args, app->{
+    public static void main(String[] args) {
+        Solon.start(DemoApp.class, args, app -> {
             initMvcJsonCustom();
         });
     }
 
     /**
      * 初始化json定制（需要在插件运行前定制）
-     * */
-    private static void initMvcJsonCustom(){
+     */
+    private static void initMvcJsonCustom() {
         //通过转换器，做简单类型的定制
-        SnackRenderFactory.global.addConvertor(Date.class,
-                (JsonLongConverter<Date>) source -> source.getTime());
+        SnackRenderFactory.global
+                .addConvertor(Date.class, s -> s.getTime());
 
-        SnackRenderFactory.global.addConvertor(LocalDate.class,
-                (JsonConverter<LocalDate>) source -> source.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+        SnackRenderFactory.global
+                .addConvertor(LocalDate.class, s -> s.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
 
 
         //通过编码器，做复杂类型的原生定制（基于框架原生接口）
-        SnackRenderFactory.global.addEncoder(Date.class, (data, node) ->
-                node.val().setNumber(data.getTime()));
+        SnackRenderFactory.global
+                .addEncoder(Date.class, (data, node) -> node.val().setNumber(data.getTime()));
     }
 }
