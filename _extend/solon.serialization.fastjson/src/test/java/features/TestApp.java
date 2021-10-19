027d1d7998495d64180930509b6c188bf5166ed6
noear <noear@live.cn>
2021-10-19 12:26:00 +0800

    1.5.46

--- _extend/solon.serialization.fastjson/src/test/java/features/TestApp.java
+++ _extend/solon.serialization.fastjson/src/test/java/features/TestApp.java
@@ -3,8 +3,6 @@ package features;
 import org.noear.solon.Solon;
 import org.noear.solon.annotation.Controller;
 import org.noear.solon.annotation.Mapping;
-import org.noear.solon.serialization.JsonLongConverter;
-import org.noear.solon.serialization.JsonConverter;
 import org.noear.solon.serialization.fastjson.FastjsonRenderFactory;
 
 import java.time.LocalDate;
@@ -20,30 +18,30 @@ import java.util.Map;
 @Controller
 public class TestApp {
     public static void main(String[] args) {
-        Solon.start(TestApp.class, args, app->{
+        Solon.start(TestApp.class, args, app -> {
             initMvcJsonCustom();
         });
     }
 
     /**
      * 初始化json定制（需要在插件运行前定制）
-     * */
-    private static void initMvcJsonCustom(){
+     */
+    private static void initMvcJsonCustom() {
         //通过转换器，做简单类型的定制
-        FastjsonRenderFactory.global.addConvertor(Date.class,
-                (JsonLongConverter<Date>) source -> source.getTime());
+        FastjsonRenderFactory.global
+                .addConvertor(Date.class, s -> s.getTime());
 
-        FastjsonRenderFactory.global.addConvertor(LocalDate.class,
-                (JsonConverter<LocalDate>) source -> source.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+        FastjsonRenderFactory.global
+                .addConvertor(LocalDate.class, s -> s.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
 
-        FastjsonRenderFactory.global.addConvertor(LocalDateTime.class,
-                (JsonConverter<LocalDateTime>) source -> source.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
+        FastjsonRenderFactory.global
+                .addConvertor(LocalDateTime.class, s -> s.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
 
     }
 
     @Mapping("/")
-    public Object home(){
-        Map<String,Object> data = new LinkedHashMap<>();
+    public Object home() {
+        Map<String, Object> data = new LinkedHashMap<>();
         data.put("time1", LocalDateTime.now());
         data.put("time2", LocalDate.now());
         data.put("time3", new Date());
