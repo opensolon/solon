ea6aa37ab3acb2c3bec713cb3631426bf38c2437
noear <noear@live.cn>
2021-10-30 19:44:01 +0800

    1.5.55

--- _plugin_cloud/consul-solon-plugin/src/main/java/org/noear/solon/cloud/extend/consul/ConsulProps.java
+++ _plugin_cloud/consul-solon-plugin/src/main/java/org/noear/solon/cloud/extend/consul/ConsulProps.java
@@ -3,7 +3,8 @@ package org.noear.solon.cloud.extend.consul;
 import org.noear.solon.cloud.CloudProps;
 
 /**
- * @author noear 2021/1/16 created
+ * @author noear
+ * @since 1.2
  */
 public class ConsulProps {
     public static final CloudProps instance = new CloudProps("consul");
