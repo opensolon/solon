b79515a218a950d0bc91f78e982c7bad5b697439
noear <noear@live.cn>
2021-11-04 15:23:27 +0800

    1.5.57

---
+++ _plugin/sqltoy-solon-plugin/src/main/java/org/noear/solon/extend/sqltoy/XPluginImp.java
@@ -0,0 +1,11 @@
+package org.noear.solon.extend.sqltoy;
+
+import org.noear.solon.SolonApp;
+import org.noear.solon.core.Plugin;
+
+public class XPluginImp implements Plugin {
+    @Override
+    public void start(SolonApp app) {
+
+    }
+}
