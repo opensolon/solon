aa626822dbb7d84834c7062982cd6e3bfe31b600
noear <noear@live.cn>
2021-11-10 23:55:05 +0800

    1.5.63

--- _extend/solon.extend.graalvm/src/main/java/org/noear/solon/extend/graalvm/GraalvmUtil.java
+++ _extend/solon.extend.graalvm/src/main/java/org/noear/solon/extend/graalvm/GraalvmUtil.java
@@ -18,27 +18,27 @@ import java.util.function.Predicate;
  */
 public class GraalvmUtil {
 
-    private  static Set<String> resources =new HashSet();
-    private static boolean scaned=false;
+    private static Set<String> resources = new HashSet();
+    private static boolean scaned = false;
 
     /**
      * graalvm 里的 scan 通过预处理，存放到配置文件，key= solon.scan (@since 1.6)
      *
      * @param path   路径
      * @param filter 过滤条件
-     * @param urls  扫描到的路径 作为返回
+     * @param urls   扫描到的路径 作为返回
      */
-    public  static void scanResource(String path, Predicate<String> filter, Set<String> urls) {
+    public static void scanResource(String path, Predicate<String> filter, Set<String> urls) {
 
-            if (!scaned) {
-                readNativeResourceConfig();
-                readNativeReflectConfig();
-                scaned=true;
+        if (!scaned) {
+            readNativeResourceConfig();
+            readNativeReflectConfig();
+            scaned = true;
+        }
+        for (String f : resources) {
+            if (f.startsWith(path) && filter.test(f)) {
+                urls.add(f);
             }
-            for(String f :resources){
-                if (f.startsWith(path) && filter.test(f)) {
-                    urls.add(f);
-             }
         }
     }
 
@@ -49,7 +49,7 @@ public class GraalvmUtil {
         try {
             List<ClassLoader> loaderList = ExtendLoader.load(Solon.cfg().extend(), false);
             for (ClassLoader loader : loaderList) {
-                Enumeration<URL> rs = Utils.getResources(loader,"META-INF/native-image/reflect-config.json");
+                Enumeration<URL> rs = Utils.getResources(loader, "META-INF/native-image/reflect-config.json");
                 while (rs.hasMoreElements()) {
                     InputStream inputStream = rs.nextElement().openStream();
                     String s = readFileByLines(inputStream);
@@ -64,7 +64,7 @@ public class GraalvmUtil {
                 PrintUtil.info("load reflect-config completed: ", resources.toString());
             }
         } catch (Exception e) {
-            PrintUtil.yellowln("read reflect-config error :"+e.getLocalizedMessage());
+            PrintUtil.yellowln("read reflect-config error :" + e.getLocalizedMessage());
             EventBus.push(e);
         }
     }
@@ -72,48 +72,49 @@ public class GraalvmUtil {
     /**
      * 读取resource-config.json 获取需要扫描的资源文件列表
      */
-    private static void readNativeResourceConfig( ) {
+    private static void readNativeResourceConfig() {
         try {
             List<ClassLoader> loaderList = ExtendLoader.load(Solon.cfg().extend(), false);
-             for (ClassLoader loader : loaderList) {
-                 Enumeration<URL> rs = Utils.getResources(loader,"META-INF/native-image/resource-config.json");
-                 while (rs.hasMoreElements()) {
-                     InputStream inputStream = rs.nextElement().openStream();
-                     String s = readFileByLines(inputStream);
-                     ONode o = ONode.load(s);
-                     ONode includes = o.select("$.resources.includes");
-                     includes.forEach(on -> {
-                         String name = on.get("pattern").getString().replaceAll("\\\\\\\\Q", "").replaceAll("\\\\\\\\E", "");
+            for (ClassLoader loader : loaderList) {
+                Enumeration<URL> rs = Utils.getResources(loader, "META-INF/native-image/resource-config.json");
+                while (rs.hasMoreElements()) {
+                    InputStream inputStream = rs.nextElement().openStream();
+                    String s = readFileByLines(inputStream);
+                    ONode o = ONode.load(s);
+                    ONode includes = o.select("$.resources.includes");
+                    includes.forEach(on -> {
+                        String name = on.get("pattern").getString().replaceAll("\\\\\\\\Q", "").replaceAll("\\\\\\\\E", "");
 
-                         if (name.startsWith("META-INF")) {
-                             resources.add(name);
-                         }
-                     });
-                 }
-             }
+                        if (name.startsWith("META-INF")) {
+                            resources.add(name);
+                        }
+                    });
+                }
+            }
             if (Solon.cfg().isDebugMode()) {
                 PrintUtil.info("load resource-config completed: ", resources.toString());
             }
         } catch (Exception e) {
-            PrintUtil.yellowln("read resource-config.json error :"+e.getLocalizedMessage());
+            PrintUtil.yellowln("read resource-config.json error :" + e.getLocalizedMessage());
             EventBus.push(e);
         }
     }
 
     /**
      * 以行为单位读取文件
+     *
      * @param inputStream 输入流
      * @return
      */
     public static String readFileByLines(InputStream inputStream) {
-        String content="";
+        String content = "";
         BufferedReader reader = null;
         try {
             reader = new BufferedReader(new InputStreamReader(inputStream));
             String tempString = null;
 
             while ((tempString = reader.readLine()) != null) {
-                content+=tempString+"\r\n";
+                content += tempString + "\r\n";
             }
             reader.close();
         } catch (IOException e) {
@@ -131,7 +132,6 @@ public class GraalvmUtil {
                 }
             }
         }
-        return  content;
+        return content;
     }
-
 }
