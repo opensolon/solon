b13041ef9a30476c9bda4c1802ebf37af9f2f36e
noear <noear@live.cn>
2021-11-11 13:47:45 +0800

    1.5.64

--- _extend/solon.extend.hotdev/src/test/java/demo/TestController.java
+++ _extend/solon.extend.hotdev/src/test/java/demo/TestController.java
@@ -4,10 +4,9 @@ import org.noear.solon.annotation.Controller;
 import org.noear.solon.annotation.Mapping;
 
 @Controller
-@Mapping("test")
 public class TestController {
-    @Mapping("sayhello")
-    public String sayhello(){
-        return "hello solon world1";
+    @Mapping("/")
+    public String hello() {
+        return "hello solon world";
     }
 }
