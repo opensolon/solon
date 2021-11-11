0c038d4d59a80f35d425914cbb488a998db25d2f
desire <251070051@qq.com>
2021-11-11 11:20:09 +0800

    开发热重启工具

---
+++ _extend/solon.extend.hotswap/src/main/java/org/noear/solon/extend/hotswap/SolonHelper.java
@@ -0,0 +1,16 @@
+package org.noear.solon.extend.hotswap;
+
+import org.noear.solon.Solon;
+
+/**
+ * 这里其实可以直接用Solon,但是Solon的stop在线程里面，重新启动实例时如果端口还在占用，就会重启失败
+ */
+public class SolonHelper {
+    public static void start(String source,String[] args) throws ClassNotFoundException {
+        Solon.start(Class.forName(source),args);
+    }
+    public static void stop(){
+        Solon.cfg().plugs().forEach(p -> p.prestop());
+        Solon.cfg().plugs().forEach(p -> p.stop());
+    }
+}
---
+++ _extend/solon.extend.hotswap/src/test/java/org/noear/solon/demo/TestController.java
@@ -0,0 +1,13 @@
+package org.noear.solon.demo;
+
+import org.noear.solon.annotation.Controller;
+import org.noear.solon.annotation.Mapping;
+
+@Controller
+@Mapping("test")
+public class TestController {
+    @Mapping("sayhello")
+    public String sayhello(){
+        return "hello solon world";
+    }
+}
