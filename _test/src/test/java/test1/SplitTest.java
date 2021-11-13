1ec0596494318e6b89be23f274715c1a6d71430b
noear <noear@live.cn>
2021-11-13 11:33:57 +0800

    1.5.67

--- _test/src/test/java/test1/SplitTest.java
+++ _test/src/test/java/test1/SplitTest.java
@@ -2,14 +2,41 @@ package test1;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Locale;
+
 /**
  * @author noear 2021/11/13 created
  */
 public class SplitTest {
     @Test
-    public void test(){
-        System.out.println("zh_CN".split("_|-"));
-        System.out.println("zh-CN".split("_|-"));
-        System.out.println("zh_CN-HANS".split("_|-"));
+    public void test() {
+        String[] ss;
+
+        ss = "zh_CN".split("_|-");
+        System.out.println(Arrays.asList(ss));
+
+        ss = "zh-CN".split("_|-");
+        System.out.println(Arrays.asList(ss));
+
+        ss = "zh_CN-HANS".split("_|-");
+        System.out.println(Arrays.asList(ss));
+    }
+
+    @Test
+    public void test2() {
+        String[] ss;
+
+        ss = "zh".split("_|-");
+        System.out.println(new Locale(ss[0]));
+
+        ss = "zh_CN".split("_|-");
+        System.out.println(new Locale(ss[0], ss[1]));
+
+        ss = "zh-cn".split("_|-");
+        System.out.println(new Locale(ss[0], ss[1]));
+
+        ss = "zh_CN-HANS".split("_|-");
+        System.out.println(new Locale(ss[0], ss[1], ss[2]));
     }
 }
