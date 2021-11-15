a053b4db61c11987ae441892e209abc74dafa06a
noear <noear@live.cn>
2021-11-15 12:31:47 +0800

    1.5.67

--- _extend/solon.extend.hotdev/src/test/java/demo/TestController.java
+++ _extend/solon.extend.hotdev/src/test/java/demo/TestController.java
@@ -3,10 +3,18 @@ package demo;
 import org.noear.solon.annotation.Controller;
 import org.noear.solon.annotation.Mapping;
 
+import java.util.Date;
+
 @Controller
 public class TestController {
+    static Date date; //静态变量，也会重构
+
     @Mapping("/")
     public String hello() {
-        return "hello world";
+        if (date == null) {
+            date = new Date();
+        }
+
+        return "hello world1, " + date.getTime();
     }
 }
