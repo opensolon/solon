9e518366a065bc51f7c5a64d4c357512b5d08e30
noear <noear@live.cn>
2021-11-23 18:34:03 +0800

    1.6.0

--- _test/src/main/java/webapp/Config.java
+++ _test/src/main/java/webapp/Config.java
@@ -2,6 +2,8 @@ package webapp;
 
 import org.noear.solon.annotation.Bean;
 import org.noear.solon.annotation.Configuration;
+import org.noear.solon.annotation.Inject;
+import org.noear.solon.core.BeanWrap;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,13 +13,19 @@ import java.util.Map;
  */
 @Configuration
 public class Config {
-    @Bean
+    @Bean("map1")
     public Map<String, Object> map() {
         Map<String, Object> map = new HashMap<>();
         map.put("1", 1);
         map.put("2", 2);
 
         return map;
+    }
+
+    @Bean
+    public void test(@Inject("map1") BeanWrap bw) {
+        Map map = bw.get();
+        System.out.println("map::" + map.toString());
     }
 
 //    @Bean
