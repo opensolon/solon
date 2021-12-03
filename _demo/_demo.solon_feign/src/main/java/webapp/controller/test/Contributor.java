2c7262e6c85af3814a9d0768c475a523851a5995
noear <noear@live.cn>
2021-12-03 23:10:20 +0800

    1.6.4

--- _demo/_demo.solon_feign/src/main/java/webapp/controller/test/Contributor.java
+++
@@ -1,9 +0,0 @@
-package webapp.controller.test;
-
-/**
- * @author noear 2021/1/1 created
- */
-public class Contributor {
-    public String login;
-    public int contributions;
-}
--- _demo/_demo.solon_hessian/src/main/java/client/ClientApp.java
+++
@@ -1,7 +0,0 @@
-package client;
-
-/**
- * @author noear 2021/6/6 created
- */
-public class ClientApp {
-}
