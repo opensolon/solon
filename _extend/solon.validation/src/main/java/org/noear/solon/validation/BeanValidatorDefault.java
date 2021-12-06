d6fc1e93dfd659c81bc84544d5f7d167930016b6
noear <noear@live.cn>
2021-12-06 11:55:46 +0800

    1.6.6

--- _extend/solon.validation/src/main/java/org/noear/solon/validation/BeanValidatorDefault.java
+++ _extend/solon.validation/src/main/java/org/noear/solon/validation/BeanValidatorDefault.java
@@ -2,6 +2,8 @@ package org.noear.solon.validation;
 
 import org.noear.solon.core.handle.Result;
 
+import java.util.Iterator;
+
 /**
  * Bean 验证默认实现
  *
@@ -12,12 +14,29 @@ class BeanValidatorDefault implements BeanValidator {
     /**
      * 验证
      *
-     * @param obj 实体对象
+     * @param obj    实体对象
      * @param groups 分组（有些实现，可能不支持）
      * @return 验证结果
-     * */
+     */
     @Override
     public Result validate(Object obj, Class<?>... groups) {
-        return ValidatorManager.validateOfEntity(obj);
+        if (obj instanceof Iterable) {
+            Iterator iterator = ((Iterable) obj).iterator();
+            while (iterator.hasNext()) {
+                Object val2 = iterator.next();
+
+                if (val2 != null) {
+                    Result rst = ValidatorManager.validateOfEntity(val2);
+
+                    if (rst.getCode() != Result.SUCCEED_CODE) {
+                        return rst;
+                    }
+                }
+            }
+
+            return Result.succeed();
+        } else {
+            return ValidatorManager.validateOfEntity(obj);
+        }
     }
 }
