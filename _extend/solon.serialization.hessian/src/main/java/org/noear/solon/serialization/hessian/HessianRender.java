13ff8c60c1c880a1b75761a4f8564cf3122ac418
noear <noear@live.cn>
2021-12-16 19:13:25 +0800

    1.6.8

--- _extend/solon.serialization.hessian/src/main/java/org/noear/solon/serialization/hessian/HessianRender.java
+++ _extend/solon.serialization.hessian/src/main/java/org/noear/solon/serialization/hessian/HessianRender.java
@@ -6,12 +6,25 @@ import org.noear.solon.core.handle.Context;
 import org.noear.solon.core.handle.Render;
 
 import java.io.ByteArrayOutputStream;
+import java.util.Base64;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 //不要要入参，方便后面多视图混用
 //
 public class HessianRender implements Render {
+
+    @Override
+    public String renderAndReturn(Object data, Context ctx) throws Throwable {
+        byte[] bytes = null;
+        if (data instanceof ModelAndView) {
+            bytes = serializeDo(new LinkedHashMap((Map) data));
+        } else {
+            bytes = serializeDo(data);
+        }
+
+        return Base64.getEncoder().encodeToString(bytes);
+    }
 
     @Override
     public void render(Object obj, Context ctx) throws Throwable {
--- _extend/solon.serialization.protostuff/src/main/java/org/noear/solon/serialization/protostuff/ProtostuffRender.java
+++ _extend/solon.serialization.protostuff/src/main/java/org/noear/solon/serialization/protostuff/ProtostuffRender.java
@@ -1,13 +1,25 @@
 package org.noear.solon.serialization.protostuff;
 
 import org.noear.solon.core.handle.Context;
+import org.noear.solon.core.handle.ModelAndView;
 import org.noear.solon.core.handle.Render;
+
+import java.util.Base64;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * @author noear
  * @since 1.2
  */
 public class ProtostuffRender implements Render {
+
+    @Override
+    public String renderAndReturn(Object data, Context ctx) throws Throwable {
+        byte[] bytes = serializeDo(data);
+        return Base64.getEncoder().encodeToString(bytes);
+    }
+
     @Override
     public void render(Object obj, Context ctx) throws Throwable {
         if (XPluginImp.output_meta) {
