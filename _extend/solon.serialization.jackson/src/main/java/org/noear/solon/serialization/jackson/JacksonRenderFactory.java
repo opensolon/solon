e991955d839ab1e4e15b95b8087e08c7de3d6872
noear <noear@live.cn>
2021-12-18 21:50:10 +0800

    1.6.9

--- _extend/solon.serialization.jackson/src/main/java/org/noear/solon/serialization/jackson/JacksonRenderFactory.java
+++ _extend/solon.serialization.jackson/src/main/java/org/noear/solon/serialization/jackson/JacksonRenderFactory.java
@@ -6,7 +6,10 @@ import org.noear.solon.core.handle.Render;
 import org.noear.solon.serialization.StringSerializerRender;
 
 /**
- * @author noear 2021/10/11 created
+ * Json 渲染器工厂
+ *
+ * @author noear
+ * @since 1.5
  */
 public class JacksonRenderFactory extends JacksonRenderFactoryBase {
     public static final JacksonRenderFactory global = new JacksonRenderFactory();
--- _extend/solon.serialization.jackson/src/main/java/org/noear/solon/serialization/jackson/JacksonRenderTypedFactory.java
+++ _extend/solon.serialization.jackson/src/main/java/org/noear/solon/serialization/jackson/JacksonRenderTypedFactory.java
@@ -9,7 +9,10 @@ import org.noear.solon.core.handle.Render;
 import org.noear.solon.serialization.StringSerializerRender;
 
 /**
- * @author noear 2021/10/11 created
+ * Json 类型化渲染器工厂
+ *
+ * @author noear
+ * @since 1.5
  */
 public class JacksonRenderTypedFactory extends JacksonRenderFactoryBase {
     public static final JacksonRenderTypedFactory global = new JacksonRenderTypedFactory();
