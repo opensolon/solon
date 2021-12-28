11cc4616f19d28b7381b6eecd0becd5e7c34d2a3
noear <noear@live.cn>
2021-12-28 16:22:25 +0800

    1.6.11

--- _test/src/test/java/features/HttpTest2.java
+++ _test/src/test/java/features/HttpTest2.java
@@ -6,7 +6,10 @@ import org.noear.solon.test.HttpTestBase;
 import org.noear.solon.test.SolonJUnit4ClassRunner;
 import org.noear.solon.test.SolonTest;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @author noear 2020/12/24 created
@@ -30,4 +33,13 @@ public class HttpTest2 extends HttpTestBase {
 //            assert path("/demo2/servlet/hello?name=noear").get().equals("Ok");
 //        }
 //    }
+
+
+    @Test
+    public void upload() throws IOException {
+        InputStream inputStream = new ByteArrayInputStream("test".getBytes(StandardCharsets.UTF_8));
+        assert path("/demo3/upload/f1")
+                .data("file", "装修-水电-视频.mp4", inputStream, "video/mp4")
+                .post().equals("成功：装修-水电-视频.mp4");
+    }
 }
