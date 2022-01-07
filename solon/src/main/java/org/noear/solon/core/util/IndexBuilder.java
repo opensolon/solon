edb091c4f3a9e01f4ebd43e107665f1c8670a347
cym1102 <cym1102@qq.com>
2022-01-07 16:54:17 +0800

    处理循环依赖

--- solon/src/main/java/org/noear/solon/core/util/IndexBuilder.java
+++ solon/src/main/java/org/noear/solon/core/util/IndexBuilder.java
@@ -10,56 +10,55 @@ import org.noear.solon.annotation.Inject;
 
 /**
  * 构建初始化的index
+ * 
  * @author cym1102
  *
  */
 public class IndexBuilder {
 
-	public static Map<String, Integer> map = new HashMap<>();
-	public static ArrayList<String> classNameSet = new ArrayList<>();
+	private static Map<String, Integer> map = new HashMap<>();
+	private static ArrayList<String> classStack = new ArrayList<>();
 
+	/**
+	 * 获取bean的初始化index
+	 * 
+	 * @param clazz bean类
+	 * @return 顺序index
+	 */
 	public static int buildIndex(Class<?> clazz) {
 		return buildIndex(clazz, true);
 	}
 
 	/**
+	 * 获取bean的初始化index
 	 * 
-	 * @param clazz bean类
-	 * @param stackTop 是否为查找起始
+	 * @param clazz    bean类
+	 * @param stackTop 是否为查找栈顶
 	 * @return 顺序index
 	 */
-	public static int buildIndex(Class<?> clazz, Boolean stackTop) {
+	private static int buildIndex(Class<?> clazz, Boolean stackTop) {
 		if (stackTop) {
-			classNameSet.clear();
-		} else {
-			if (classNameSet.contains(clazz.getName())) {
+			classStack.clear();
+
+			if (isLoopRelate(clazz, clazz.getName())) {
 				String link = "";
-				for(String name:classNameSet) {
-					link += (name + " -> ");
+				for (int i = 0; i < classStack.size(); i++) {
+					link += classStack.get(i);
+					if (i != classStack.size() - 1) {
+						link += " -> ";
+					}
 				}
-				link += classNameSet.get(0); 
+
 				throw new RuntimeException("发生依赖循环:" + link);
 			}
+
 		}
-		classNameSet.add(clazz.getName());
-		
+
 		if (map.get(clazz.getName()) != null) {
 			return map.get(clazz.getName());
 		} else {
 			// 找到他的依赖类
-			List<Class<?>> clazzList = new ArrayList<>();
-			Field[] fields = clazz.getDeclaredFields();
-			for (Field field : fields) {
-				if (field.isAnnotationPresent(Inject.class)) {
-					Inject inject = field.getAnnotation(Inject.class);
-					if (inject.value().contains("${")) {
-						// 注入的是参数, 略过
-						continue;
-					}
-
-					clazzList.add(field.getType());
-				}
-			}
+			List<Class<?>> clazzList = findRelateClass(clazz);
 
 			// 没有依赖类, 直接返回0
 			if (clazzList.size() == 0) {
@@ -84,6 +83,57 @@ public class IndexBuilder {
 			return miniIndex + 1;
 		}
 
+	}
+
+	/**
+	 * 寻找依赖类
+	 * 
+	 * @param clazz
+	 * @return 依赖类集合
+	 */
+	private static List<Class<?>> findRelateClass(Class<?> clazz) {
+		List<Class<?>> clazzList = new ArrayList<>();
+		Field[] fields = clazz.getDeclaredFields();
+		for (Field field : fields) {
+			if (field.isAnnotationPresent(Inject.class)) {
+				Inject inject = field.getAnnotation(Inject.class);
+				if (inject.value().contains("${")) {
+					// 注入的是参数, 略过
+					continue;
+				}
+
+				clazzList.add(field.getType());
+			}
+		}
+		return clazzList;
+	}
+
+	/**
+	 * 检查是否循环依赖
+	 * 
+	 * @param clazz
+	 * @return 是否循环依赖
+	 */
+	private static boolean isLoopRelate(Class<?> clazz, String topName) {
+		classStack.add(clazz.getName()); // 入栈
+
+		List<Class<?>> clazzList = findRelateClass(clazz);
+
+		for (Class<?> clazzRelate : clazzList) {
+			if (clazzRelate.getName().equals(topName)) {
+				classStack.add(clazzRelate.getName()); // 入栈
+				return true;
+			}
+		}
+
+		for (Class<?> clazzRelate : clazzList) {
+			if (isLoopRelate(clazzRelate, topName)) {
+				return true;
+			}
+		}
+
+		classStack.remove(clazz.getName()); // 出栈
+		return false;
 	}
 
 }
