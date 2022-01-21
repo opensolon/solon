22de21fa54136bf5049c66c4899f00bfe2e24923
noear <noear@live.cn>
2022-01-21 17:47:49 +0800

    1.6.18

--- _extend/solon.extend.aspect/src/main/java/org/noear/solon/extend/aspect/BeanProxy.java
+++ _extend/solon.extend.aspect/src/main/java/org/noear/solon/extend/aspect/BeanProxy.java
@@ -3,8 +3,6 @@ package org.noear.solon.extend.aspect;
 import org.noear.solon.core.Aop;
 import org.noear.solon.core.BeanWrap;
 
-import java.lang.reflect.InvocationHandler;
-
 /**
  * Bean 动态代理
  *
@@ -55,15 +53,5 @@ public class BeanProxy implements BeanWrap.Proxy {
     @Override
     public Object getProxy(Object bean) {
         return new BeanInvocationHandler(bean).getProxy();
-    }
-
-
-    /**
-     * 获取一个代理，并转发给 handler
-     *
-     * @since 1.6
-     */
-    public <T> T getProxy(T bean, InvocationHandler handler) {
-        return (T) new BeanInvocationHandler(bean, handler).getProxy();
     }
 }
