ac14380676cbb469f535675461ae1ee68a871079
noear <noear@live.cn>
2022-02-10 18:47:10 +0800

    1.6.21

--- _extend/solon.extend.staticfiles/src/test/java/demo/InitPluginDemo.java
+++ _extend/solon.extend.staticfiles/src/test/java/demo/InitPluginDemo.java
@@ -4,6 +4,7 @@ import org.noear.solon.SolonApp;
 import org.noear.solon.annotation.Component;
 import org.noear.solon.core.Plugin;
 import org.noear.solon.extend.staticfiles.StaticMappings;
+import org.noear.solon.extend.staticfiles.StaticMimes;
 import org.noear.solon.extend.staticfiles.repository.ClassPathStaticRepository;
 import org.noear.solon.extend.staticfiles.repository.ExtendStaticRepository;
 import org.noear.solon.extend.staticfiles.repository.FileStaticRepository;
@@ -23,5 +24,9 @@ public class InitPluginDemo implements Plugin {
         StaticMappings.add("/", new FileStaticRepository("/data/sss/water/water_ext/"));
         //3.添加资源路径
         StaticMappings.add("/", new ClassPathStaticRepository("user"));
+
+
+        //添加mime
+        StaticMimes.add(".vue", "text/html");
     }
 }
