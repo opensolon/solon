e9e69e47fa2e465b0de46875dc8820616bd26e84
noear <noear@live.cn>
2022-02-16 09:24:51 +0800

    1.6.24

---
+++ _plugin/jap-solon-plugin/src/main/java/org/noear/solon/plugin/jap/XPluginImpl.java
@@ -0,0 +1,15 @@
+package org.noear.solon.plugin.jap;
+
+import org.noear.solon.SolonApp;
+import org.noear.solon.core.Plugin;
+
+/**
+ * @author noear
+ * @since 1.6
+ */
+public class XPluginImpl implements Plugin {
+    @Override
+    public void start(SolonApp app) {
+
+    }
+}
