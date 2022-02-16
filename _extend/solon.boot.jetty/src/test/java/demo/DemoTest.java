f25d3b4885f86b201178f4d149699379e82a8535
noear <noear@live.cn>
2022-02-16 11:41:31 +0800

    1.6.24

--- _extend/solon.boot.jetty/src/test/java/demo/DemoTest.java
+++ _extend/solon.boot.jetty/src/test/java/demo/DemoTest.java
@@ -1,5 +1,6 @@
 package demo;
 
+import org.noear.solon.annotation.Controller;
 import org.noear.solon.annotation.Mapping;
 import org.noear.solon.core.handle.Context;
 
@@ -8,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 /**
  * @author noear 2022/1/18 created
  */
+@Controller
 public class DemoTest {
     @Mapping("demo")
     public void demo(Context ctx, HttpServletRequest request) {
