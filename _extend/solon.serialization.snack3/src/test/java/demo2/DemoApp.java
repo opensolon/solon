07a992a54c12bd5547e1db53a6507c054b3bf2a8
noear <noear@live.cn>
2022-02-19 14:51:41 +0800

    1.6.25

--- _extend/solon.serialization.snack3/src/test/java/demo2/DemoApp.java
+++ _extend/solon.serialization.snack3/src/test/java/demo2/DemoApp.java
@@ -1,5 +1,7 @@
 package demo2;
 
+import org.noear.snack.core.Feature;
+import org.noear.snack.core.Options;
 import org.noear.solon.Solon;
 import org.noear.solon.serialization.JsonConverter;
 import org.noear.solon.serialization.snack3.SnackRenderFactory;
@@ -14,26 +16,29 @@ import java.util.Date;
 public class DemoApp {
     public static void main(String[] args) {
         Solon.start(DemoApp.class, args, app -> {
-            initMvcJsonCustom();
+            app.onEvent(SnackRenderFactory.class, e -> {
+                initMvcJsonCustom(e);
+            });
         });
     }
 
-    /**
-     * 初始化json定制（需要在插件运行前定制）
-     */
-    private static void initMvcJsonCustom() {
-        //通过转换器，做简单类型的定制
-        SnackRenderFactory.global
-                .addConvertor(Date.class, s -> s.getTime());
+    private static void initMvcJsonCustom(SnackRenderFactory factory) {
+        //示例1：通过转换器，做简单类型的定制
+        factory.addConvertor(Date.class, s -> s.getTime());
 
-        SnackRenderFactory.global
-                .addConvertor(LocalDate.class, s -> s.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+        factory.addConvertor(LocalDate.class, s -> s.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
 
-        SnackRenderFactory.global
-                .addConvertor(Double.class, s -> String.valueOf(s));
+        factory.addConvertor(Double.class, s -> String.valueOf(s));
 
-        //通过编码器，做复杂类型的原生定制（基于框架原生接口）
-        SnackRenderFactory.global
-                .addEncoder(Date.class, (data, node) -> node.val().setNumber(data.getTime()));
+        //示例2：通过编码器，做复杂类型的原生定制（基于框架原生接口）
+        factory.addEncoder(Date.class, (data, node) -> node.val().setNumber(data.getTime()));
+
+        //示例3：添加序列化null的特性
+        factory.setFeatures(
+                Feature.OrderedField,
+                Feature.WriteDateUseTicks,
+                Feature.StringNullAsEmpty,
+                Feature.QuoteFieldNames,
+                Feature.SerializeNulls);
     }
 }
