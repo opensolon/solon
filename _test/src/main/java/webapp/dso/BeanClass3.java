8a5f89f1c22b70e4238b7aba3a2e409d61758b41
noear <noear@live.cn>
2022-02-25 23:02:47 +0800

    1.6.29

---
+++ _test/src/main/java/webapp/dso/BeanClass3.java
@@ -0,0 +1,10 @@
+package webapp.dso;
+
+import org.noear.solon.annotation.Component;
+
+/**
+ * @author noear 2022/2/25 created
+ */
+@Component
+public class BeanClass3 extends BeanClass2<Integer>{
+}
