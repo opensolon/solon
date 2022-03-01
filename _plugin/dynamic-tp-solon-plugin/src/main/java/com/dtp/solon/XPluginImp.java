94efb150f72899ca254581ceb03f141b45541e5d
noear <noear@live.cn>
2022-03-01 11:52:15 +0800

    1.6.30

---
+++ _plugin/dynamic-tp-solon-plugin/src/main/java/com/dtp/solon/XPluginImp.java
@@ -0,0 +1,11 @@
+package com.dtp.solon;
+
+import org.noear.solon.SolonApp;
+import org.noear.solon.core.Plugin;
+
+public class XPluginImp implements Plugin {
+    @Override
+    public void start(SolonApp app) {
+
+    }
+}
