5731becc2cb24ac1881819e91cec2830926678ed
noear <noear@live.cn>
2022-03-02 14:42:04 +0800

    1.6.30

---
+++ _plugin_cloud/minio7-solon-plugin/src/main/java/org/noear/solon/cloud/extend/minio/XPluginImp.java
@@ -0,0 +1,24 @@
+package org.noear.solon.cloud.extend.minio;
+
+import org.noear.solon.SolonApp;
+import org.noear.solon.Utils;
+import org.noear.solon.cloud.CloudManager;
+import org.noear.solon.cloud.extend.minio.service.CloudFileServiceMinioImp;
+import org.noear.solon.core.Plugin;
+
+/**
+ * @author iYarnFog
+ * @since 1.5
+ */
+public class XPluginImp implements Plugin {
+    @Override
+    public void start(SolonApp app) {
+        if (Utils.isEmpty(MinioProps.INSTANCE.getFileAccessKey())) {
+            return;
+        }
+
+        if (MinioProps.INSTANCE.getFileEnable()) {
+            CloudManager.register(new CloudFileServiceMinioImp(MinioProps.INSTANCE));
+        }
+    }
+}
