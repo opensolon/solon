3c0949789edcc4794870eb089f5b50adc8cfe759
noear <noear@live.cn>
2022-03-21 22:32:41 +0800

    1.6.34

---
+++ _plugin_cloud/nacos2-solon-plugin/src/main/java/org/noear/solon/cloud/extend/nacos2/impl/InstanceWrap.java
@@ -0,0 +1,10 @@
+package org.noear.solon.cloud.extend.nacos2.impl;
+
+import com.alibaba.nacos.api.naming.pojo.Instance;
+
+/**
+ * @author noear
+ * @since 1.2
+ */
+public class InstanceWrap extends Instance {
+}
