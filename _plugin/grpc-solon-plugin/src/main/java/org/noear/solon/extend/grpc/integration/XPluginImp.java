7f23a8bde4b41261481550b814eb6bb46b464750
noear <noear@live.cn>
2022-03-23 22:19:14 +0800

    1.6.34

---
+++ _plugin/grpc-solon-plugin/src/main/java/org/noear/solon/extend/grpc/integration/XPluginImp.java
@@ -0,0 +1,17 @@
+package org.noear.solon.extend.grpc.integration;
+
+import org.noear.solon.SolonApp;
+import org.noear.solon.core.Aop;
+import org.noear.solon.core.Plugin;
+import org.noear.solon.extend.grpc.EnableGrpc;
+
+
+public class XPluginImp implements Plugin {
+
+    @Override
+    public void start(SolonApp app) {
+        if (app.source().getAnnotation(EnableGrpc.class) == null) {
+            return;
+        }
+    }
+}
