7b209ed08972be005e4aecd63be8ea44344e74a3
noear <noear@live.cn>
2022-03-28 16:37:14 +0800

    1.6.35

---
+++ _plugin/mybatisplus-solon-plugin/src/main/java/com/baomidou/mybatisplus/solon/service/IService.java
@@ -0,0 +1,234 @@
+package com.baomidou.mybatisplus.solon.service;
+
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.Assert;
+import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.conditions.query.LambdaQueryChainWrapper;
+import com.baomidou.mybatisplus.extension.conditions.query.QueryChainWrapper;
+import com.baomidou.mybatisplus.extension.conditions.update.LambdaUpdateChainWrapper;
+import com.baomidou.mybatisplus.extension.conditions.update.UpdateChainWrapper;
+import com.baomidou.mybatisplus.extension.kotlin.KtQueryChainWrapper;
+import com.baomidou.mybatisplus.extension.kotlin.KtUpdateChainWrapper;
+import com.baomidou.mybatisplus.extension.toolkit.ChainWrappers;
+import com.baomidou.mybatisplus.extension.toolkit.SqlHelper;
+import org.noear.solon.data.annotation.Tran;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * @author noear 2022/3/28 created
+ */
+
+public interface IService<T> {
+    int DEFAULT_BATCH_SIZE = 1000;
+
+    default boolean save(T entity) {
+        return SqlHelper.retBool(this.getBaseMapper().insert(entity));
+    }
+
+    @Tran
+    default boolean saveBatch(Collection<T> entityList) {
+        return this.saveBatch(entityList, 1000);
+    }
+
+    boolean saveBatch(Collection<T> entityList, int batchSize);
+
+    @Tran
+    default boolean saveOrUpdateBatch(Collection<T> entityList) {
+        return this.saveOrUpdateBatch(entityList, 1000);
+    }
+
+    boolean saveOrUpdateBatch(Collection<T> entityList, int batchSize);
+
+    default boolean removeById(Serializable id) {
+        return SqlHelper.retBool(this.getBaseMapper().deleteById(id));
+    }
+
+    default boolean removeById(Serializable id, boolean useFill) {
+        throw new UnsupportedOperationException("不支持的方法!");
+    }
+
+    default boolean removeById(T entity) {
+        return SqlHelper.retBool(this.getBaseMapper().deleteById(entity));
+    }
+
+    default boolean removeByMap(Map<String, Object> columnMap) {
+        Assert.notEmpty(columnMap, "error: columnMap must not be empty", new Object[0]);
+        return SqlHelper.retBool(this.getBaseMapper().deleteByMap(columnMap));
+    }
+
+    default boolean remove(Wrapper<T> queryWrapper) {
+        return SqlHelper.retBool(this.getBaseMapper().delete(queryWrapper));
+    }
+
+    default boolean removeByIds(Collection<?> list) {
+        return CollectionUtils.isEmpty(list) ? false : SqlHelper.retBool(this.getBaseMapper().deleteBatchIds(list));
+    }
+
+    @Tran
+    default boolean removeByIds(Collection<?> list, boolean useFill) {
+        if (CollectionUtils.isEmpty(list)) {
+            return false;
+        } else {
+            return useFill ? this.removeBatchByIds(list, true) : SqlHelper.retBool(this.getBaseMapper().deleteBatchIds(list));
+        }
+    }
+
+    @Tran
+    default boolean removeBatchByIds(Collection<?> list) {
+        return this.removeBatchByIds(list, 1000);
+    }
+
+    @Tran
+    default boolean removeBatchByIds(Collection<?> list, boolean useFill) {
+        return this.removeBatchByIds(list, 1000, useFill);
+    }
+
+    default boolean removeBatchByIds(Collection<?> list, int batchSize) {
+        throw new UnsupportedOperationException("不支持的方法!");
+    }
+
+    default boolean removeBatchByIds(Collection<?> list, int batchSize, boolean useFill) {
+        throw new UnsupportedOperationException("不支持的方法!");
+    }
+
+    default boolean updateById(T entity) {
+        return SqlHelper.retBool(this.getBaseMapper().updateById(entity));
+    }
+
+    default boolean update(Wrapper<T> updateWrapper) {
+        return this.update((T)null, updateWrapper);
+    }
+
+    default boolean update(T entity, Wrapper<T> updateWrapper) {
+        return SqlHelper.retBool(this.getBaseMapper().update(entity, updateWrapper));
+    }
+
+    @Tran
+    default boolean updateBatchById(Collection<T> entityList) {
+        return this.updateBatchById(entityList, 1000);
+    }
+
+    boolean updateBatchById(Collection<T> entityList, int batchSize);
+
+    boolean saveOrUpdate(T entity);
+
+    default T getById(Serializable id) {
+        return this.getBaseMapper().selectById(id);
+    }
+
+    default List<T> listByIds(Collection<? extends Serializable> idList) {
+        return this.getBaseMapper().selectBatchIds(idList);
+    }
+
+    default List<T> listByMap(Map<String, Object> columnMap) {
+        return this.getBaseMapper().selectByMap(columnMap);
+    }
+
+    default T getOne(Wrapper<T> queryWrapper) {
+        return this.getOne(queryWrapper, true);
+    }
+
+    T getOne(Wrapper<T> queryWrapper, boolean throwEx);
+
+    Map<String, Object> getMap(Wrapper<T> queryWrapper);
+
+    <V> V getObj(Wrapper<T> queryWrapper, Function<? super Object, V> mapper);
+
+    default long count() {
+        return this.count(Wrappers.emptyWrapper());
+    }
+
+    default long count(Wrapper<T> queryWrapper) {
+        return SqlHelper.retCount(this.getBaseMapper().selectCount(queryWrapper));
+    }
+
+    default List<T> list(Wrapper<T> queryWrapper) {
+        return this.getBaseMapper().selectList(queryWrapper);
+    }
+
+    default List<T> list() {
+        return this.list(Wrappers.emptyWrapper());
+    }
+
+    default <E extends IPage<T>> E page(E page, Wrapper<T> queryWrapper) {
+        return this.getBaseMapper().selectPage(page, queryWrapper);
+    }
+
+    default <E extends IPage<T>> E page(E page) {
+        return this.page(page, Wrappers.emptyWrapper());
+    }
+
+    default List<Map<String, Object>> listMaps(Wrapper<T> queryWrapper) {
+        return this.getBaseMapper().selectMaps(queryWrapper);
+    }
+
+    default List<Map<String, Object>> listMaps() {
+        return this.listMaps(Wrappers.emptyWrapper());
+    }
+
+    default List<Object> listObjs() {
+        return this.listObjs(Function.identity());
+    }
+
+    default <V> List<V> listObjs(Function<? super Object, V> mapper) {
+        return this.listObjs(Wrappers.emptyWrapper(), mapper);
+    }
+
+    default List<Object> listObjs(Wrapper<T> queryWrapper) {
+        return this.listObjs(queryWrapper, Function.identity());
+    }
+
+    default <V> List<V> listObjs(Wrapper<T> queryWrapper, Function<? super Object, V> mapper) {
+        return (List)this.getBaseMapper().selectObjs(queryWrapper).stream().filter(Objects::nonNull).map(mapper).collect(Collectors.toList());
+    }
+
+    default <E extends IPage<Map<String, Object>>> E pageMaps(E page, Wrapper<T> queryWrapper) {
+        return this.getBaseMapper().selectMapsPage(page, queryWrapper);
+    }
+
+    default <E extends IPage<Map<String, Object>>> E pageMaps(E page) {
+        return this.pageMaps(page, Wrappers.emptyWrapper());
+    }
+
+    BaseMapper<T> getBaseMapper();
+
+    Class<T> getEntityClass();
+
+    default QueryChainWrapper<T> query() {
+        return ChainWrappers.queryChain(this.getBaseMapper());
+    }
+
+    default LambdaQueryChainWrapper<T> lambdaQuery() {
+        return ChainWrappers.lambdaQueryChain(this.getBaseMapper());
+    }
+
+    default KtQueryChainWrapper<T> ktQuery() {
+        return ChainWrappers.ktQueryChain(this.getBaseMapper(), this.getEntityClass());
+    }
+
+    default KtUpdateChainWrapper<T> ktUpdate() {
+        return ChainWrappers.ktUpdateChain(this.getBaseMapper(), this.getEntityClass());
+    }
+
+    default UpdateChainWrapper<T> update() {
+        return ChainWrappers.updateChain(this.getBaseMapper());
+    }
+
+    default LambdaUpdateChainWrapper<T> lambdaUpdate() {
+        return ChainWrappers.lambdaUpdateChain(this.getBaseMapper());
+    }
+
+    default boolean saveOrUpdate(T entity, Wrapper<T> updateWrapper) {
+        return this.update(entity, updateWrapper) || this.saveOrUpdate(entity);
+    }
+}
---
+++ _plugin/mybatisplus-solon-plugin/src/main/java/com/baomidou/mybatisplus/solon/service/impl/ServiceImpl.java
@@ -0,0 +1,211 @@
+package com.baomidou.mybatisplus.solon.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.enums.SqlMethod;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.TableInfo;
+import com.baomidou.mybatisplus.core.metadata.TableInfoHelper;
+import com.baomidou.mybatisplus.core.toolkit.*;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.baomidou.mybatisplus.extension.toolkit.SqlHelper;
+import org.apache.ibatis.binding.MapperMethod;
+import org.apache.ibatis.logging.Log;
+import org.apache.ibatis.logging.LogFactory;
+import org.apache.ibatis.session.SqlSession;
+import org.mybatis.spring.SqlSessionUtils;
+import org.noear.solon.annotation.Inject;
+import org.noear.solon.data.annotation.Tran;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+/**
+ * @author noear
+ * @since 1.6
+ */
+public class ServiceImpl<M extends BaseMapper<T>, T> implements IService<T> {
+    protected Log log = LogFactory.getLog(this.getClass());
+    @Inject
+    protected M baseMapper;
+    protected Class<T> entityClass = this.currentModelClass();
+    protected Class<M> mapperClass = this.currentMapperClass();
+
+    public ServiceImpl() {
+    }
+
+    public M getBaseMapper() {
+        return this.baseMapper;
+    }
+
+    public Class<T> getEntityClass() {
+        return this.entityClass;
+    }
+
+    /** @deprecated */
+    @Deprecated
+    protected boolean retBool(Integer result) {
+        return SqlHelper.retBool(result);
+    }
+
+    protected Class<M> currentMapperClass() {
+        return (Class<M>)ReflectionKit.getSuperClassGenericType(this.getClass(), ServiceImpl.class, 0);
+    }
+
+    protected Class<T> currentModelClass() {
+        return (Class<T>)ReflectionKit.getSuperClassGenericType(this.getClass(), ServiceImpl.class, 1);
+    }
+
+    /** @deprecated */
+    @Deprecated
+    protected SqlSession sqlSessionBatch() {
+        return SqlHelper.sqlSessionBatch(this.entityClass);
+    }
+
+    /** @deprecated */
+    @Deprecated
+    protected void closeSqlSession(SqlSession sqlSession) {
+        SqlSessionUtils.closeSqlSession(sqlSession, GlobalConfigUtils.currentSessionFactory(this.entityClass));
+    }
+
+    /** @deprecated */
+    @Deprecated
+    protected String sqlStatement(SqlMethod sqlMethod) {
+        return SqlHelper.table(this.entityClass).getSqlStatement(sqlMethod.getMethod());
+    }
+
+    @Tran
+    public boolean saveBatch(Collection<T> entityList, int batchSize) {
+        String sqlStatement = this.getSqlStatement(SqlMethod.INSERT_ONE);
+        return this.executeBatch(entityList, batchSize, (sqlSession, entity) -> {
+            sqlSession.insert(sqlStatement, entity);
+        });
+    }
+
+    protected String getSqlStatement(SqlMethod sqlMethod) {
+        return SqlHelper.getSqlStatement(this.mapperClass, sqlMethod);
+    }
+
+    @Tran
+    public boolean saveOrUpdate(T entity) {
+        if (null == entity) {
+            return false;
+        } else {
+            TableInfo tableInfo = TableInfoHelper.getTableInfo(this.entityClass);
+            Assert.notNull(tableInfo, "error: can not execute. because can not find cache of TableInfo for entity!", new Object[0]);
+            String keyProperty = tableInfo.getKeyProperty();
+            Assert.notEmpty(keyProperty, "error: can not execute. because can not find column for id from entity!", new Object[0]);
+            Object idVal = tableInfo.getPropertyValue(entity, tableInfo.getKeyProperty());
+            return !StringUtils.checkValNull(idVal) && !Objects.isNull(this.getById((Serializable)idVal)) ? this.updateById(entity) : this.save(entity);
+        }
+    }
+
+    @Tran
+    public boolean saveOrUpdateBatch(Collection<T> entityList, int batchSize) {
+        TableInfo tableInfo = TableInfoHelper.getTableInfo(this.entityClass);
+        Assert.notNull(tableInfo, "error: can not execute. because can not find cache of TableInfo for entity!", new Object[0]);
+        String keyProperty = tableInfo.getKeyProperty();
+        Assert.notEmpty(keyProperty, "error: can not execute. because can not find column for id from entity!", new Object[0]);
+        return SqlHelper.saveOrUpdateBatch(this.entityClass, this.mapperClass, this.log, entityList, batchSize, (sqlSession, entity) -> {
+            Object idVal = tableInfo.getPropertyValue(entity, keyProperty);
+            return StringUtils.checkValNull(idVal) || CollectionUtils.isEmpty(sqlSession.selectList(this.getSqlStatement(SqlMethod.SELECT_BY_ID), entity));
+        }, (sqlSession, entity) -> {
+            MapperMethod.ParamMap<T> param = new MapperMethod.ParamMap();
+            param.put("et", entity);
+            sqlSession.update(this.getSqlStatement(SqlMethod.UPDATE_BY_ID), param);
+        });
+    }
+
+    @Tran
+    public boolean updateBatchById(Collection<T> entityList, int batchSize) {
+        String sqlStatement = this.getSqlStatement(SqlMethod.UPDATE_BY_ID);
+        return this.executeBatch(entityList, batchSize, (sqlSession, entity) -> {
+            MapperMethod.ParamMap<T> param = new MapperMethod.ParamMap();
+            param.put("et", entity);
+            sqlSession.update(sqlStatement, param);
+        });
+    }
+
+    public T getOne(Wrapper<T> queryWrapper, boolean throwEx) {
+        return throwEx ? this.baseMapper.selectOne(queryWrapper) : SqlHelper.getObject(this.log, this.baseMapper.selectList(queryWrapper));
+    }
+
+    public Map<String, Object> getMap(Wrapper<T> queryWrapper) {
+        return (Map)SqlHelper.getObject(this.log, this.baseMapper.selectMaps(queryWrapper));
+    }
+
+    public <V> V getObj(Wrapper<T> queryWrapper, Function<? super Object, V> mapper) {
+        return SqlHelper.getObject(this.log, this.listObjs(queryWrapper, mapper));
+    }
+
+    /** @deprecated */
+    @Deprecated
+    protected boolean executeBatch(Consumer<SqlSession> consumer) {
+        return SqlHelper.executeBatch(this.entityClass, this.log, consumer);
+    }
+
+    protected <E> boolean executeBatch(Collection<E> list, int batchSize, BiConsumer<SqlSession, E> consumer) {
+        return SqlHelper.executeBatch(this.entityClass, this.log, list, batchSize, consumer);
+    }
+
+    protected <E> boolean executeBatch(Collection<E> list, BiConsumer<SqlSession, E> consumer) {
+        return this.executeBatch(list, 1000, consumer);
+    }
+
+    public boolean removeById(Serializable id) {
+        TableInfo tableInfo = TableInfoHelper.getTableInfo(this.getEntityClass());
+        return tableInfo.isWithLogicDelete() && tableInfo.isWithUpdateFill() ? this.removeById(id, true) : SqlHelper.retBool(this.getBaseMapper().deleteById(id));
+    }
+
+    @Tran
+    public boolean removeByIds(Collection<?> list) {
+        if (CollectionUtils.isEmpty(list)) {
+            return false;
+        } else {
+            TableInfo tableInfo = TableInfoHelper.getTableInfo(this.getEntityClass());
+            return tableInfo.isWithLogicDelete() && tableInfo.isWithUpdateFill() ? this.removeBatchByIds(list, true) : SqlHelper.retBool(this.getBaseMapper().deleteBatchIds(list));
+        }
+    }
+
+    public boolean removeById(Serializable id, boolean useFill) {
+        TableInfo tableInfo = TableInfoHelper.getTableInfo(this.entityClass);
+        if (useFill && tableInfo.isWithLogicDelete() && !this.entityClass.isAssignableFrom(id.getClass())) {
+            T instance = tableInfo.newInstance();
+            tableInfo.setPropertyValue(instance, tableInfo.getKeyProperty(), new Object[]{id});
+            return this.removeById(instance);
+        } else {
+            return SqlHelper.retBool(this.getBaseMapper().deleteById(id));
+        }
+    }
+
+    @Tran
+    public boolean removeBatchByIds(Collection<?> list, int batchSize) {
+        TableInfo tableInfo = TableInfoHelper.getTableInfo(this.entityClass);
+        return this.removeBatchByIds(list, batchSize, tableInfo.isWithLogicDelete() && tableInfo.isWithUpdateFill());
+    }
+
+    @Tran
+    public boolean removeBatchByIds(Collection<?> list, int batchSize, boolean useFill) {
+        String sqlStatement = this.getSqlStatement(SqlMethod.DELETE_BY_ID);
+        TableInfo tableInfo = TableInfoHelper.getTableInfo(this.entityClass);
+        return this.executeBatch(list, batchSize, (sqlSession, e) -> {
+            if (useFill && tableInfo.isWithLogicDelete()) {
+                if (this.entityClass.isAssignableFrom(e.getClass())) {
+                    sqlSession.update(sqlStatement, e);
+                } else {
+                    T instance = tableInfo.newInstance();
+                    tableInfo.setPropertyValue(instance, tableInfo.getKeyProperty(), new Object[]{e});
+                    sqlSession.update(sqlStatement, instance);
+                }
+            } else {
+                sqlSession.update(sqlStatement, e);
+            }
+
+        });
+    }
+}
+
