b6f0b9681c22613f7c2c2600f32259722785a54e
noear <noear@live.cn>
2022-03-30 13:28:02 +0800

    1.6.35

--- _plugin_cloud/httputils-solon-plugin/src/main/java/org/noear/solon/cloud/utils/http/HttpUtils.java
+++ _plugin_cloud/httputils-solon-plugin/src/main/java/org/noear/solon/cloud/utils/http/HttpUtils.java
@@ -35,7 +35,7 @@ public class HttpUtils {
 
     private final static OkHttpClient httpClientDefault = new OkHttpClient.Builder()
             .connectTimeout(10, TimeUnit.SECONDS)
-            .writeTimeout(5, TimeUnit.SECONDS)
+            .writeTimeout(10, TimeUnit.SECONDS)
             .readTimeout(60, TimeUnit.SECONDS)
             .dispatcher(httpClientDefaultDispatcher.get())
             .build();
