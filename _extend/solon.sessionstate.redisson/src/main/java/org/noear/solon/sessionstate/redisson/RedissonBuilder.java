6d93e08f0a0967cf102502295bc441555832f29e
noear <noear@live.cn>
2022-04-14 21:21:24 +0800

    1.7.2

--- _extend/solon.sessionstate.redisson/src/main/java/org/noear/solon/sessionstate/redisson/RedissonBuilder.java
+++ _extend/solon.sessionstate.redisson/src/main/java/org/noear/solon/sessionstate/redisson/RedissonBuilder.java
@@ -15,9 +15,6 @@ public class RedissonBuilder {
     /**
      * <code><pre>
      * test.cache1:
-     *   driverType: "redis"
-     *   keyHeader: "demo" #默认为 ${solon.app.name} ，可不配置
-     *   defSeconds: 30 #默认为 30，可不配置
      *   server: "localhost:6379"
      *   db: 0 #默认为 0，可不配置
      *   password: ""
