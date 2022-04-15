02556058cc4f7f00827de065422c055fb3903074
noear <noear@live.cn>
2022-04-15 12:43:30 +0800

    1.7.2

---
+++ _extend/solon.cache.redisson/src/test/java/features/App.java
@@ -0,0 +1,41 @@
+package features;
+
+import org.noear.solon.Solon;
+import org.noear.solon.annotation.Bean;
+import org.noear.solon.annotation.Configuration;
+import org.noear.solon.annotation.Inject;
+import org.noear.solon.cache.redisson.RedissonCacheService;
+import org.noear.solon.core.Aop;
+import org.noear.solon.data.cache.CacheService;
+
+/**
+ * @author noear 2022/4/15 created
+ */
+@Configuration
+public class App {
+    public static void main(String[] args) throws Exception {
+        Solon.start(App.class, args);
+
+        CacheService cacheService = Aop.get(CacheService.class);
+
+        cacheService.store("test", "1", 1);
+        assert "1".equals(cacheService.get("test"));
+        Thread.sleep(2000);
+        assert cacheService.get("test") == null;
+
+
+        UserM userM = new UserM();
+        userM.id = 12;
+        userM.name = "test";
+
+        cacheService.store("test", userM, 1);
+        assert userM.id == ((UserM)cacheService.get("test")).id;
+        Thread.sleep(2000);
+        assert cacheService.get("test") == null;
+    }
+
+    @Bean
+    public CacheService cacheService(@Inject("${test.rd1}") RedissonCacheService cacheService) {
+        return cacheService;
+    }
+}
---
+++ _extend/solon.cache.redisson/src/test/java/features/UserM.java
@@ -0,0 +1,11 @@
+package features;
+
+import java.io.Serializable;
+
+/**
+ * @author noear 2022/4/15 created
+ */
+public class UserM implements Serializable {
+    public long id;
+    public String name;
+}
