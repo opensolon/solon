a1360820d5d3dfefe3be83055597f5658f337cbb
noear <noear@live.cn>
2022-04-20 15:06:33 +0800

    1.7.3

--- _plugin/dubbo3-solon-plugin/src/main/java/org/noear/solon/extend/dubbo3/integration/XPluginImp.java
+++ _plugin/dubbo3-solon-plugin/src/main/java/org/noear/solon/extend/dubbo3/integration/XPluginImp.java
@@ -1,6 +1,8 @@
 package org.noear.solon.extend.dubbo3.integration;
 
 import org.apache.dubbo.config.ServiceConfig;
+import org.apache.dubbo.config.annotation.DubboReference;
+import org.apache.dubbo.config.annotation.DubboService;
 import org.apache.dubbo.config.annotation.Reference;
 import org.apache.dubbo.config.annotation.Service;
 import org.noear.solon.SolonApp;
@@ -21,6 +23,11 @@ public class XPluginImp implements Plugin {
 
         _server = DubboAdapter.global(app);
 
+        startForOldAnno();
+        startForNewAnno();
+    }
+
+    private void startForOldAnno(){
         //支持duboo.Service注解
         Aop.context().beanBuilderAdd(Service.class, ((clz, bw, anno) -> {
             Class<?>[] ifs = bw.clz().getInterfaces();
@@ -40,6 +47,31 @@ public class XPluginImp implements Plugin {
         Aop.context().beanInjectorAdd(Reference.class, ((fwT, anno) -> {
             if (fwT.getType().isInterface()) {
                 Object raw = _server.getService(fwT.getType(), new ReferenceAnno(anno));
+                fwT.setValue(raw);
+            }
+        }));
+    }
+
+    private void startForNewAnno(){
+        //支持duboo.Service注解
+        Aop.context().beanBuilderAdd(DubboService.class, ((clz, bw, anno) -> {
+            Class<?>[] ifs = bw.clz().getInterfaces();
+            if (ifs.length > 0) {
+                ServiceConfig cfg = new ServiceConfig(new DubboServiceAnno(anno));
+                if (cfg.getInterface() == null) {
+                    cfg.setInterface(ifs[0]);
+                }
+                cfg.setRef(bw.raw());
+
+                // 暴露及注册服务
+                _server.regService(cfg);
+            }
+        }));
+
+        //支持dubbo.Reference注入
+        Aop.context().beanInjectorAdd(DubboReference.class, ((fwT, anno) -> {
+            if (fwT.getType().isInterface()) {
+                Object raw = _server.getService(fwT.getType(), new DubboReferenceAnno(anno));
                 fwT.setValue(raw);
             }
         }));
