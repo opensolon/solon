090d3d1b94e5d7890b03d67580c4efa7c0fa90b3
noear <noear@live.cn>
2022-04-28 16:56:41 +0800

    1.7.5

--- _extend/solon.extend.cors/src/test/java/demo/DemoController.java
+++ _extend/solon.extend.cors/src/test/java/demo/DemoController.java
@@ -1,14 +1,17 @@
 package demo;
 
 import org.noear.solon.annotation.Controller;
+import org.noear.solon.annotation.Get;
 import org.noear.solon.annotation.Mapping;
-import org.noear.solon.extend.cors.annotation.CrossOrigin;
+import org.noear.solon.annotation.Post;
 
 /**
  * @author noear 2022/4/28 created
  */
 @Controller
 public class DemoController extends BaseController{
+    @Get
+    @Post
     @Mapping("hello")
     public String hello(){
         return "hello";
