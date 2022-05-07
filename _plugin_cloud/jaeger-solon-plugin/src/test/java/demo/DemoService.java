cd15ebeaf9ea1cf63cacccae576c8cb8b657b28d
noear <noear@live.cn>
2022-05-08 00:25:22 +0800

    1.7.6

---
+++ _plugin_cloud/jaeger-solon-plugin/src/test/java/demo/DemoService.java
@@ -0,0 +1,20 @@
+package demo;
+
+import org.noear.nami.annotation.Mapping;
+import org.noear.solon.annotation.Controller;
+import org.noear.solon.cloud.opentracing.Spans;
+
+/**
+ * @author noear 2022/5/7 created
+ */
+@Controller
+public class DemoService {
+    @Mapping("hello")
+    public String hello() {
+        Spans.active(span -> span.setTag("订单", 12));
+        //或
+        Spans.active().setTag("订单", 12);
+
+        return "hello world";
+    }
+}
