354cc43510b5d9029f69da35cfa55c4eee9318fe
noear <noear@live.cn>
2022-05-10 13:33:33 +0800

    1.7.7

--- _plugin/sa-token-solon-plugin/src/main/java/cn/dev33/satoken/solon/XPluginImp.java
+++ _plugin/sa-token-solon-plugin/src/main/java/cn/dev33/satoken/solon/XPluginImp.java
@@ -1,7 +1,6 @@
 package cn.dev33.satoken.solon;
 
 import cn.dev33.satoken.SaManager;
-import cn.dev33.satoken.action.SaTokenAction;
 import cn.dev33.satoken.annotation.*;
 import cn.dev33.satoken.basic.SaBasicTemplate;
 import cn.dev33.satoken.basic.SaBasicUtil;
@@ -10,11 +9,11 @@ import cn.dev33.satoken.context.second.SaTokenSecondContextCreator;
 import cn.dev33.satoken.dao.SaTokenDao;
 import cn.dev33.satoken.id.SaIdTemplate;
 import cn.dev33.satoken.id.SaIdUtil;
+import cn.dev33.satoken.json.SaJsonTemplate;
 import cn.dev33.satoken.listener.SaTokenListener;
+import cn.dev33.satoken.sign.SaSignTemplate;
 import cn.dev33.satoken.solon.integration.SaContextForSolon;
 import cn.dev33.satoken.solon.integration.SaTokenMethodInterceptor;
-import cn.dev33.satoken.sso.SaSsoTemplate;
-import cn.dev33.satoken.sso.SaSsoUtil;
 import cn.dev33.satoken.stp.StpInterface;
 import cn.dev33.satoken.stp.StpLogic;
 import cn.dev33.satoken.stp.StpUtil;
@@ -59,53 +58,53 @@ public class XPluginImp implements Plugin {
         });
 
         // 注入二级上下文 Bean
-        Aop.getAsyn(SaTokenSecondContextCreator.class, bw -> {
+        Aop.getAsyn(SaTokenSecondContextCreator.class, bw->{
             SaTokenSecondContextCreator raw = bw.raw();
             SaManager.setSaTokenSecondContext(raw.create());
         });
 
         // 注入侦听器 Bean
-        Aop.getAsyn(SaTokenListener.class, bw -> {
+        Aop.getAsyn(SaTokenListener.class, bw->{
             SaManager.setSaTokenListener(bw.raw());
         });
 
-        // 注入框架行为 Bean
-        Aop.getAsyn(SaTokenAction.class, bw -> {
-            SaManager.setSaTokenAction(bw.raw());
-        });
-
         // 注入权限认证 Bean
-        Aop.getAsyn(StpInterface.class, bw -> {
+        Aop.getAsyn(StpInterface.class, bw->{
             SaManager.setStpInterface(bw.raw());
         });
 
         // 注入持久化 Bean
-        Aop.getAsyn(SaTokenDao.class, bw -> {
+        Aop.getAsyn(SaTokenDao.class, bw->{
             SaManager.setSaTokenDao(bw.raw());
         });
 
         // 临时令牌验证模块 Bean
-        Aop.getAsyn(SaTempInterface.class, bw -> {
+        Aop.getAsyn(SaTempInterface.class, bw->{
             SaManager.setSaTemp(bw.raw());
         });
 
         // Sa-Token-Id 身份凭证模块 Bean
-        Aop.getAsyn(SaIdTemplate.class, bw -> {
+        Aop.getAsyn(SaIdTemplate.class, bw->{
             SaIdUtil.saIdTemplate = bw.raw();
         });
 
         // Sa-Token Http Basic 认证模块 Bean
-        Aop.getAsyn(SaBasicTemplate.class, bw -> {
+        Aop.getAsyn(SaBasicTemplate.class, bw->{
             SaBasicUtil.saBasicTemplate = bw.raw();
         });
 
-        // Sa-Token-SSO 单点登录模块 Bean
-        Aop.getAsyn(SaSsoTemplate.class, bw -> {
-            SaSsoUtil.saSsoTemplate = bw.raw();
+        // Sa-Token JSON 转换器 Bean
+        Aop.getAsyn(SaJsonTemplate.class, bw->{
+            SaManager.setSaJsonTemplate(bw.raw());
+        });
+
+        // Sa-Token 参数签名算法 Bean
+        Aop.getAsyn(SaSignTemplate.class, bw->{
+            SaManager.setSaSignTemplate(bw.raw());
         });
 
         // 自定义 StpLogic 对象
-        Aop.getAsyn(StpLogic.class, bw -> {
+        Aop.getAsyn(StpLogic.class, bw->{
             StpUtil.setStpLogic(bw.raw());
         });
     }
