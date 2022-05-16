0c344a9043b237504ae07c8b7643b0187c6ab7b3
noear <noear@live.cn>
2022-05-16 16:33:55 +0800

    1.7.9

--- _extend/solon.plugind/src/main/java/org/noear/solon/plugind/PluginPlus.java
+++ _extend/solon.plugind/src/main/java/org/noear/solon/plugind/PluginPlus.java
@@ -12,23 +12,36 @@ import org.noear.solon.core.Props;
  * @since 1.7
  */
 public abstract class PluginPlus implements Plugin {
-    private AopContext context;
-    private Props props;
-
-    public PluginPlus(){
-        this(null);
-    }
-    public PluginPlus(Props props){
-        this.props = props;
-    }
+    /**
+     * 属性是否隔离
+     */
+    protected abstract boolean isPropIsolation();
 
     /**
-     * 上下文
-     * */
+     * Bean是否隔离
+     */
+    protected abstract boolean isBeanIsolation();
+
+
+    private AopContext context;
+
+    /**
+     * 上下文（提供隔离支持）
+     */
     public AopContext context() {
-        if(context == null){
-            context = Aop.context().copy(props);
+        if (context == null) {
+            if (isBeanIsolation()) {
+                if (isPropIsolation()) {
+                    context = Aop.context().copy(new Props());
+                } else {
+                    context = Aop.context().copy();
+                }
+            } else {
+                context = Aop.context();
+            }
+
         }
+
         return context;
     }
 }
