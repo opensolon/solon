b6cb9f83a4f5286c36448919abf97807ebed5293
noear <noear@live.cn>
2022-05-26 18:49:30 +0800

    1.8.1

--- solon/src/main/java/org/noear/solon/core/AopContext.java
+++ solon/src/main/java/org/noear/solon/core/AopContext.java
@@ -313,7 +313,7 @@ public class AopContext extends BeanContainer {
     public void beanScan(Class<?> source) {
         //确定文件夹名
         if (source.getPackage() != null) {
-            beanScan(source.getPackage().getName());
+            beanScan(source.getClassLoader(), source.getPackage().getName());
         }
     }
 
@@ -321,20 +321,31 @@ public class AopContext extends BeanContainer {
      * ::扫描源下的所有 bean 及对应处理
      */
     public void beanScan(String basePackage) {
+        beanScan(getClassLoader(), basePackage);
+    }
+
+    /**
+     * ::扫描源下的所有 bean 及对应处理
+     */
+    public void beanScan(ClassLoader classLoader,String basePackage) {
         if (Utils.isEmpty(basePackage)) {
+            return;
+        }
+
+        if (classLoader == null) {
             return;
         }
 
         String dir = basePackage.replace('.', '/');
 
         //扫描类文件并处理（采用两段式加载，可以部分bean先处理；剩下的为第二段处理）
-        ScanUtil.scan(getClassLoader(), dir, n -> n.endsWith(".class"))
+        ScanUtil.scan(classLoader, dir, n -> n.endsWith(".class"))
                 .stream()
                 .sorted(Comparator.comparing(s -> s.length()))
                 .forEach(name -> {
                     String className = name.substring(0, name.length() - 6);
 
-                    Class<?> clz = Utils.loadClass(getClassLoader(), className.replace("/", "."));
+                    Class<?> clz = Utils.loadClass(classLoader, className.replace("/", "."));
                     if (clz != null) {
                         tryCreateBean(clz);
                     }
