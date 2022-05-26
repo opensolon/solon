f8853433e8273b8c5ff4d0df0220903afeee20e7
noear <noear@live.cn>
2022-05-26 18:21:33 +0800

    1.8.1

--- _solon_extend/solon.extend.staticfiles/src/main/java/org/noear/solon/extend/staticfiles/repository/ClassPathStaticRepository.java
+++ _solon_extend/solon.extend.staticfiles/src/main/java/org/noear/solon/extend/staticfiles/repository/ClassPathStaticRepository.java
@@ -2,6 +2,7 @@ package org.noear.solon.extend.staticfiles.repository;
 
 import org.noear.solon.Solon;
 import org.noear.solon.Utils;
+import org.noear.solon.core.JarClassLoader;
 import org.noear.solon.extend.staticfiles.StaticRepository;
 
 import java.io.File;
@@ -17,22 +18,29 @@ import java.net.URL;
 public class ClassPathStaticRepository implements StaticRepository {
     String location;
     String locationDebug;
+    ClassLoader classLoader;
 
 
     /**
      * 构建函数
      *
      * @param location 位置
-     * */
+     */
     public ClassPathStaticRepository(String location) {
+        this(JarClassLoader.global(), location);
+    }
+
+    public ClassPathStaticRepository(ClassLoader classLoader, String location) {
+        this.classLoader = classLoader;
+
         setLocation(location);
     }
 
     /**
      * @param location 位置
-     * */
+     */
     protected void setLocation(String location) {
-        if(location == null){
+        if (location == null) {
             return;
         }
 
@@ -48,7 +56,7 @@ public class ClassPathStaticRepository implements StaticRepository {
         this.location = location;
 
         if (Solon.cfg().isDebugMode()) {
-            URL rooturi = Utils.getResource("/");
+            URL rooturi = Utils.getResource(classLoader, "/");
 
             if (rooturi != null) {
                 String rootdir = rooturi.toString()
@@ -72,6 +80,6 @@ public class ClassPathStaticRepository implements StaticRepository {
             }
         }
 
-        return Utils.getResource(location + path);
+        return Utils.getResource(classLoader, location + path);
     }
 }
