a4698b49a488b8b65bb771c7db6138ee9ac9085c
noear <noear@live.cn>
2022-06-09 14:53:14 +0800

    1.8.3

--- __test/src/main/java/webapp/TestApp.java
+++ __test/src/main/java/webapp/TestApp.java
@@ -5,6 +5,7 @@ import org.noear.solon.SolonApp;
 import org.noear.solon.SolonBuilder;
 import org.noear.solon.Utils;
 import org.noear.solon.annotation.Import;
+import org.noear.solon.boot.filter.ContextPathFilter;
 import org.noear.solon.cloud.CloudClient;
 import org.noear.solon.core.ExtendLoader;
 import org.noear.solon.core.handle.MethodType;
@@ -67,6 +68,8 @@ public class TestApp {
             x.enableSocketD(true);
             x.enableWebSocket(true);
             x.pluginAdd(1, new AppPluginImp());
+
+            x.filter(new ContextPathFilter("/test/"));
 
             x.onEvent(JsonRenderFactory.class, e->{
                System.out.println("JsonRenderFactory event: xxxxx: " + e.getClass().getSimpleName());
---
+++ _solon_extend/solon.boot/src/main/java/org/noear/solon/boot/filter/ContextPathFilter.java
@@ -0,0 +1,31 @@
+package org.noear.solon.boot.filter;
+
+import org.noear.solon.core.handle.Context;
+import org.noear.solon.core.handle.Filter;
+import org.noear.solon.core.handle.FilterChain;
+
+/**
+ * 提供 ContextPath 类似的功能
+ *
+ * @author noear
+ * @since 1.8
+ */
+public class ContextPathFilter implements Filter {
+    private String path;
+
+    /**
+     * @param path '/demo/'
+     * */
+    public ContextPathFilter(String path) {
+        this.path = path;
+    }
+
+    @Override
+    public void doFilter(Context ctx, FilterChain chain) throws Throwable {
+        if (ctx.pathNew().startsWith(path)) {
+            ctx.pathNew(ctx.path().substring(path.length() - 1));
+        }
+
+        chain.doFilter(ctx);
+    }
+}
