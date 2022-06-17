487cc97965dc2fb3fdc4140a8342c9c6a7cc77bb
noear <noear@live.cn>
2022-06-17 17:50:45 +0800

    1.8.4

---
+++ _solon_extend/solon.web.cors/src/test/java/demo/BaseController.java
@@ -0,0 +1,10 @@
+package demo;
+
+import org.noear.solon.web.cors.annotation.CrossOrigin;
+
+/**
+ * @author noear 2022/4/28 created
+ */
+@CrossOrigin(origins = "*")
+public class BaseController {
+}
