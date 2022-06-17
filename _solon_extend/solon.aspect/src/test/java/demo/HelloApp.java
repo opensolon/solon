752ef9e44e5a944ffa200772ea95e68535ad5690
noear <noear@live.cn>
2022-06-17 21:25:22 +0800

    1.8.4

---
+++ _solon_extend/solon.aspect/src/test/java/demo/HelloApp.java
@@ -0,0 +1,13 @@
+package demo;
+
+
+import org.noear.solon.Solon;
+
+/**
+ * @author noear 2022/5/26 created
+ */
+public class HelloApp {
+    public static void main(String[] args) {
+        Solon.start(HelloApp.class, args);
+    }
+}
