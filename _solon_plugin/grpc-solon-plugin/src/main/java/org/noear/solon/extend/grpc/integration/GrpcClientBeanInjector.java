1ff5b2eb02a668cd4607264714fd5abe2b097545
noear <noear@live.cn>
2022-06-19 18:52:16 +0800

    1.9.0

--- _solon_plugin/grpc-solon-plugin/src/main/java/org/noear/solon/extend/grpc/integration/GrpcClientBeanInjector.java
+++ _solon_plugin/grpc-solon-plugin/src/main/java/org/noear/solon/extend/grpc/integration/GrpcClientBeanInjector.java
@@ -1,5 +1,7 @@
 package org.noear.solon.extend.grpc.integration;
 
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import org.noear.solon.core.BeanInjector;
 import org.noear.solon.core.VarHolder;
 import org.noear.solon.extend.grpc.annotation.GrpcClient;
@@ -11,6 +13,8 @@ import org.noear.solon.extend.grpc.annotation.GrpcClient;
 public class GrpcClientBeanInjector implements BeanInjector<GrpcClient> {
     @Override
     public void doInject(VarHolder varH, GrpcClient anno) {
+        ManagedChannel channel = ManagedChannelBuilder.forTarget(anno.name()).usePlaintext().build();
+
 
     }
 }
