463cf32dff1ae1d6fbda1e0f8ca4c1824cab2414
noear <noear@live.cn>
2022-06-28 21:18:28 +0800

    1.9.1

--- _solon_extend/solon.boot.tomcat/src/main/java/org/noear/solon/boot/tomcat/TomcatServer.java
+++ _solon_extend/solon.boot.tomcat/src/main/java/org/noear/solon/boot/tomcat/TomcatServer.java
@@ -2,6 +2,7 @@ package org.noear.solon.boot.tomcat;
 
 import org.apache.catalina.Context;
 import org.apache.catalina.startup.Tomcat;
+import org.noear.solon.Utils;
 import org.noear.solon.boot.ServerLifecycle;
 import org.noear.solon.boot.ServerProps;
 import org.noear.solon.boot.tomcat.http.TCHttpContextHandler;
@@ -20,6 +21,10 @@ public class TomcatServer implements ServerLifecycle {
         tomcat = new Tomcat();
         tomcat.setBaseDir(System.getProperty("java.io.tmpdir"));
         tomcat.setPort(port);
+        if(Utils.isNotEmpty(host)) {
+            tomcat.setHostname(host);
+        }
+
         //context configuration start 开始上下文相关配置
         Context context = stepContext();
 
@@ -31,13 +36,20 @@ public class TomcatServer implements ServerLifecycle {
         context.setAllowCasualMultipartParsing(true);
 
         tomcat.start();
+        new Thread(()->{
+            tomcat.getServer().await();
+        }).start();
     }
 
     protected Context stepContext() throws Throwable{
-        String servletName = "solon";
-        tomcat.addServlet("/", servletName, new TCHttpContextHandler());
+        //1.初始化上下文
+        Context context = tomcat.addContext("", null);//第二个参数与文档相关
 
-        Context context = tomcat.addContext("/", null);//第二个参数与文档相关
+        //2.添加 servlet
+        String servletName = "solon";
+        tomcat.addServlet(context, servletName, new TCHttpContextHandler());
+
+        //3.建立 servlet 印射
         context.addServletMappingDecoded("/", servletName);//Servlet与对应uri映射
 
         return context;
