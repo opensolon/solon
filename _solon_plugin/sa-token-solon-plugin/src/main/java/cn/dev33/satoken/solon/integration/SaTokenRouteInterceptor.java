4c69f89c86a98274942ee23ee35b294cd624c1e1
noear <noear@live.cn>
2022-07-10 23:42:55 +0800

    1.9.2

---
+++ _solon_plugin/sa-token-solon-plugin/src/main/java/cn/dev33/satoken/solon/integration/SaTokenRouteInterceptor.java
@@ -0,0 +1,63 @@
+package cn.dev33.satoken.solon.integration;
+
+import cn.dev33.satoken.exception.BackResultException;
+import cn.dev33.satoken.exception.StopMatchException;
+import cn.dev33.satoken.router.SaRouteFunction;
+import cn.dev33.satoken.solon.model.SaRequestForSolon;
+import cn.dev33.satoken.solon.model.SaResponseForSolon;
+import cn.dev33.satoken.stp.StpUtil;
+import org.noear.solon.core.handle.Context;
+import org.noear.solon.core.handle.Handler;
+
+/**
+ * sa-token基于路由的拦截式鉴权 
+ * @author kong
+ */
+public class SaTokenRouteInterceptor implements Handler {
+
+	/**
+	 * 每次进入拦截器的[执行函数]，默认为登录校验
+	 */
+	protected SaRouteFunction function = (req, res, handler) -> StpUtil.checkLogin();
+
+	/**
+	 * 创建一个路由拦截器
+	 */
+	public SaTokenRouteInterceptor() {
+	}
+
+	/**
+	 * 创建, 并指定[执行函数]
+	 * @param function [执行函数]
+	 */
+	public SaTokenRouteInterceptor(SaRouteFunction function) {
+		this.function = function;
+	}
+
+	/**
+	 * 静态方法快速构建一个 
+	 * @param function 自定义模式下的执行函数
+	 * @return sa路由拦截器 
+	 */
+	public static SaTokenRouteInterceptor newInstance(SaRouteFunction function) {
+		return new SaTokenRouteInterceptor(function);
+	}
+	
+
+
+	@Override
+	public void handle(Context ctx) throws Throwable {
+		try {
+			function.run(new SaRequestForSolon(), new SaResponseForSolon(), ctx);
+		} catch (StopMatchException e) {
+			// 停止匹配，进入Controller
+		} catch (BackResultException e) {
+			// 停止匹配，向前端输出结果
+			// 停止匹配，向前端输出结果
+			ctx.contentType("text/plain; charset=utf-8");
+			ctx.output(e.getMessage());
+			ctx.setHandled(true);
+			return;
+		}
+	}
+}
--- _solon_plugin/sa-token-solon-plugin/src/test/java/demo/Config.java
+++ _solon_plugin/sa-token-solon-plugin/src/test/java/demo/Config.java
@@ -3,7 +3,11 @@ package demo;
 import cn.dev33.satoken.SaManager;
 import cn.dev33.satoken.config.SaTokenConfig;
 import cn.dev33.satoken.dao.SaTokenDao;
+import cn.dev33.satoken.router.SaRouter;
 import cn.dev33.satoken.solon.dao.SaTokenDaoOfRedis;
+import cn.dev33.satoken.solon.integration.SaTokenRouteInterceptor;
+import cn.dev33.satoken.stp.StpUtil;
+import org.noear.solon.Solon;
 import org.noear.solon.annotation.Bean;
 import org.noear.solon.annotation.Configuration;
 import org.noear.solon.annotation.Inject;
@@ -19,11 +23,21 @@ public class Config {
     }
 
     @Bean
-    public void saTokenConfigInit(){
+    public void saTokenConfigInit() {
         SaTokenConfig saTokenConfig = new SaTokenConfig();
         //..
 
         SaManager.setConfig(saTokenConfig);
     }
 
+    @Bean
+    public void saTokenRouteInterceptor() {
+        Solon.app().before(SaTokenRouteInterceptor.newInstance((req, res, o) -> {
+            // 根据路由划分模块，不同模块不同鉴权
+            SaRouter.match("/user/**", r -> StpUtil.checkPermission("user"));
+            SaRouter.match("/admin/**", r -> StpUtil.checkPermission("admin"));
+            SaRouter.match("/goods/**", r -> StpUtil.checkPermission("goods"));
+            SaRouter.match("/orders/**", r -> StpUtil.checkPermission("orders"));
+        }));
+    }
 }
