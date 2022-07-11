ac17e256201c40b6181cf6385e3bff99094d1294
noear <noear@live.cn>
2022-07-11 12:12:18 +0800

    1.9.2

--- _solon_plugin/sa-token-solon-plugin/src/test/java/demo2/Config.java
+++ _solon_plugin/sa-token-solon-plugin/src/test/java/demo2/Config.java
@@ -15,11 +15,24 @@ public class Config {
     @Bean
     public void saTokenPathInterceptor() {
         Solon.app().before(new SaTokenRouterInterceptor((req, res, e) -> {
+            SaRouter.match("/**", StpUtil::checkLogin);
             // 根据路由划分模块，不同模块不同鉴权
             SaRouter.match("/user/**", r -> StpUtil.checkPermission("user"));
             SaRouter.match("/admin/**", r -> StpUtil.checkPermission("admin"));
             SaRouter.match("/goods/**", r -> StpUtil.checkPermission("goods"));
             SaRouter.match("/orders/**", r -> StpUtil.checkPermission("orders"));
         }));
+    }
+
+    @Bean
+    public void saTokenPathInterceptor2() {
+        Solon.app().before((ctx) -> {
+            SaRouter.match("/**", StpUtil::checkLogin);
+            // 根据路由划分模块，不同模块不同鉴权
+            SaRouter.match("/user/**", r -> StpUtil.checkPermission("user"));
+            SaRouter.match("/admin/**", r -> StpUtil.checkPermission("admin"));
+            SaRouter.match("/goods/**", r -> StpUtil.checkPermission("goods"));
+            SaRouter.match("/orders/**", r -> StpUtil.checkPermission("orders"));
+        });
     }
 }
