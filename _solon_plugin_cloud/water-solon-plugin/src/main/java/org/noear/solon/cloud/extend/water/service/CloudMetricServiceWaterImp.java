6d1e4c2c873200a773c1387a66865d8f2edd8364
noear <noear@live.cn>
2022-07-20 16:09:11 +0800

    1.9.3

--- _solon_plugin_cloud/water-solon-plugin/src/main/java/org/noear/solon/cloud/extend/water/service/CloudMetricServiceWaterImp.java
+++ _solon_plugin_cloud/water-solon-plugin/src/main/java/org/noear/solon/cloud/extend/water/service/CloudMetricServiceWaterImp.java
@@ -13,23 +13,23 @@ import org.noear.water.WaterClient;
 public class CloudMetricServiceWaterImp implements CloudMetricService {
     @Override
     public void addCount(String group, String category, String item, long val) {
-        WaterClient.Track.trackCount(group, category, item, val);
+        WaterClient.Track.addCount(group, category, item, val);
 
     }
 
     @Override
     public void addMeter(String group, String category, String item, long val) {
         if (WW.track_service.equals(group)) {
-            WaterClient.Track.trackNode(category, item, val);
+            WaterClient.Track.addMeterByNode(category, item, val);
         } else if (WW.track_from.equals(group)) {
-            WaterClient.Track.trackFrom(category, item, val);
+            WaterClient.Track.addMeterByFrom(category, item, val);
         } else {
-            WaterClient.Track.track(group, category, item, val);
+            WaterClient.Track.addMeter(group, category, item, val);
         }
     }
 
     @Override
     public void addGauge(String group, String category, String item, long val) {
-        WaterClient.Track.track(group + "_", category, item, val);
+        WaterClient.Track.addGauge(group, category, item, val);
     }
 }
