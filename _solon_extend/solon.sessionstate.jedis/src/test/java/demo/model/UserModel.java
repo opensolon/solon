7680e85a8e11a224e50465f76a54366f36145e53
noear <noear@live.cn>
2022-08-23 07:59:15 +0800

    1.10.1

--- _solon_extend/solon.sessionstate.jedis/src/test/java/demo/model/UserModel.java
+++ _solon_extend/solon.sessionstate.jedis/src/test/java/demo/model/UserModel.java
@@ -1,9 +1,11 @@
 package demo.model;
 
+import java.io.Serializable;
+
 /**
  * @author noear 2022/1/18 created
  */
-public class UserModel {
+public class UserModel implements Serializable {
     public Integer id;
     public String name;
 }
