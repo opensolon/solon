e27a9a99169fb5c1ea0ad30c7ff541c94bdd078b
tangyukai <tangyukai@corp.netease.com>
2022-08-26 10:35:08 +0800

    tomcat9 stepup

--- _solon_extend/solon.boot.tomcat/src/main/java/org/noear/solon/boot/tomcat/TomcatServer.java
+++ _solon_extend/solon.boot.tomcat/src/main/java/org/noear/solon/boot/tomcat/TomcatServer.java
@@ -1,11 +1,19 @@
 package org.noear.solon.boot.tomcat;
 
 import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
 import org.noear.solon.Utils;
 import org.noear.solon.boot.ServerLifecycle;
 import org.noear.solon.boot.ServerProps;
 import org.noear.solon.boot.tomcat.http.TCHttpContextHandler;
+import org.noear.solon.core.util.NamedThreadFactory;
+import org.noear.solon.core.util.PrintUtil;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.noear.solon.boot.tomcat.XPluginImp.solon_boot_ver;
 
 
 /**
@@ -14,52 +22,66 @@ import org.noear.solon.boot.tomcat.http.TCHttpContextHandler;
  * @Description : Yukai is so handsome xxD
  */
 public class TomcatServer implements ServerLifecycle {
-    private Tomcat tomcat;
+    private Tomcat _server; // 单线程加载机制 无需锁保护
+    private static final String TOMCAT_PREFIX = "tomcat-";
+    private final ExecutorService _executor = Executors.newSingleThreadExecutor(new NamedThreadFactory(TOMCAT_PREFIX));
+
 
     @Override
     public void start(String host, int port) throws Throwable {
-        tomcat = new Tomcat();
-        tomcat.setBaseDir(System.getProperty("java.io.tmpdir"));
-        tomcat.setPort(port);
-        if(Utils.isNotEmpty(host)) {
-            tomcat.setHostname(host);
+        stepTomcat(host, port);
+        stepContext();
+        startTom();
+    }
+
+    private void stepTomcat(String host, int port) {
+        _server = new Tomcat();
+        _server.setBaseDir(System.getProperty("java.io.tmpdir"));
+        _server.setPort(port);
+        _server.getConnector();
+        if (Utils.isNotEmpty(host)) {
+            _server.setHostname(host);
         }
+    }
 
+    private Context stepContext() {
         //context configuration start 开始上下文相关配置
-        Context context = stepContext();
-
+        //1.初始化上下文
+        Context context = _server.addContext("", null);//第二个参数与文档相关
+        //2.添加 servlet
+        String servletName = "solon";
+        Tomcat.addServlet(context, servletName, new TCHttpContextHandler());
+        //3.建立 servlet 印射
+        context.addServletMappingDecoded("/", servletName);//Servlet与对应uri映射
         //**************session time setting start Session时间相关*****************
         if (ServerProps.session_timeout > 0) {
             context.setSessionTimeout(ServerProps.session_timeout);
         }
         //**************session time setting end*****************
         context.setAllowCasualMultipartParsing(true);
-
-        tomcat.start();
-        new Thread(()->{
-            tomcat.getServer().await();
-        }).start();
-    }
-
-    protected Context stepContext() throws Throwable{
-        //1.初始化上下文
-        Context context = tomcat.addContext("", null);//第二个参数与文档相关
-
-        //2.添加 servlet
-        String servletName = "solon";
-        tomcat.addServlet(context, servletName, new TCHttpContextHandler());
-
-        //3.建立 servlet 印射
-        context.addServletMappingDecoded("/", servletName);//Servlet与对应uri映射
-
         return context;
     }
 
     @Override
     public void stop() throws Throwable {
-        if (tomcat != null) {
-            tomcat.stop();
-            tomcat = null;
+        if (_server != null) {
+            _server.stop();
+            _server = null;
         }
+
+        PrintUtil.info("Server:main: tomcat: Has Stopped " + solon_boot_ver());
+    }
+
+
+
+
+
+    private void startTom() throws LifecycleException, InterruptedException {
+        _server.start();
+        _executor.execute(() -> {
+            _server.getServer().await();
+        });
+        //给org.apache.catalina.Server.await 点时间
+        Thread.sleep(1);
     }
 }
