91844cffb57bbdc5832a7e34c8b75fd55229e639
noear <noear@live.cn>
2022-08-30 17:00:30 +0800

    1.10.2

--- _solon_extend/solon.boot.tomcat/src/main/java/org/noear/solon/boot/tomcat/TomcatServerBase.java
+++ _solon_extend/solon.boot.tomcat/src/main/java/org/noear/solon/boot/tomcat/TomcatServerBase.java
@@ -1,21 +1,15 @@
 package org.noear.solon.boot.tomcat;
 import org.apache.catalina.Context;
-import org.apache.catalina.LifecycleException;
-import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.connector.Connector;
 import org.apache.catalina.startup.Tomcat;
-import org.apache.catalina.webresources.DirResourceSet;
-import org.apache.catalina.webresources.StandardRoot;
 import org.noear.solon.Utils;
 import org.noear.solon.boot.ServerLifecycle;
 import org.noear.solon.boot.ServerProps;
-import org.noear.solon.core.util.NamedThreadFactory;
 import org.noear.solon.core.util.PrintUtil;
 
 import java.io.File;
 import java.net.URISyntaxException;
 import java.util.Optional;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 import static org.noear.solon.boot.tomcat.XPluginImp.solon_boot_ver;
 
@@ -67,9 +61,15 @@ public abstract class TomcatServerBase implements ServerLifecycle {
         _server = new Tomcat();
         _server.setBaseDir(System.getProperty("java.io.tmpdir"));
         _server.setPort(port);
-        _server.getConnector();
+
         if (Utils.isNotEmpty(host)) {
             _server.setHostname(host);
+        }
+
+        Connector connector = _server.getConnector();
+
+        if (ServerProps.request_maxBodySize > 0) {
+            connector.setMaxPostSize(ServerProps.request_maxBodySize);
         }
     }
 
