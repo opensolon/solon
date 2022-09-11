77e0fe68671557aa6920f0f4abf99cbe8be2a197
noear <noear@live.cn>
2022-09-11 11:34:55 +0800

    1.10.3

---
+++ _solon_plugin_cloud/kubernetes-solon-plugin/src/main/java/org/noear/solon/cloud/extend/kubernetes/KubernetesProps.java
@@ -0,0 +1,11 @@
+package org.noear.solon.cloud.extend.kubernetes;
+
+import org.noear.solon.cloud.CloudProps;
+
+/**
+ * @author noear
+ * @since 1.10
+ */
+public class KubernetesProps {
+    public static final CloudProps instance = new CloudProps("kubernetes");
+}
