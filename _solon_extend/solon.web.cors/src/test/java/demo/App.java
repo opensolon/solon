13c104baa57e6c77c154c5a239c4e64123e07c12
noear <noear@live.cn>
2022-09-16 17:48:33 +0800

    1.10.3

--- _solon_extend/solon.web.cors/src/test/java/demo/App.java
+++ _solon_extend/solon.web.cors/src/test/java/demo/App.java
@@ -1,12 +1,23 @@
 package demo;
 
 import org.noear.solon.Solon;
+import org.noear.solon.web.cors.CrossFilter;
+import org.noear.solon.web.cors.CrossHandler;
 
 /**
  * @author noear 2022/4/28 created
  */
 public class App {
     public static void main(String[] args){
-        Solon.start(App.class, args);
+        Solon.start(App.class, args, app->{
+            //例：增加全局处理（用过滤器模式）
+            app.filter(-1, new CrossFilter().allowedOrigins("*")); //加-1 优先级更高
+
+            //例：增加全局处理（用处理链模式）
+            app.before(new CrossHandler().allowedOrigins("*"));
+
+            //例：或者增某段路径的处理
+            app.before("/user/**", new CrossHandler().allowedOrigins("*"));
+        });
     }
 }
