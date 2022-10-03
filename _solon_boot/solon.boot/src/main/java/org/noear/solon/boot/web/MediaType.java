143e3d8cd3f1317bdd7efd421f1b02b7faee4c9b
noear <noear@live.cn>
2022-10-03 20:16:48 +0800

    1.10.5

---
+++ _solon_boot/solon.boot/src/main/java/org/noear/solon/boot/web/MediaType.java
@@ -0,0 +1,31 @@
+package org.noear.solon.boot.web;
+
+/**
+ * @author noear
+ * @since 1.10
+ */
+public final class MediaType {
+    public static final String ALL_VALUE = "*/*";
+    public static final String APPLICATION_ATOM_XML_VALUE = "application/atom+xml";
+    public static final String APPLICATION_FORM_URLENCODED_VALUE = "application/x-www-form-urlencoded";
+    public static final String APPLICATION_JSON_VALUE = "application/json";
+    public static final String APPLICATION_JSON_UTF8_VALUE = "application/json;charset=UTF-8";
+    public static final String APPLICATION_OCTET_STREAM_VALUE = "application/octet-stream";
+    public static final String APPLICATION_PDF_VALUE = "application/pdf";
+    public static final String APPLICATION_PROBLEM_JSON_VALUE = "application/problem+json";
+    public static final String APPLICATION_PROBLEM_JSON_UTF8_VALUE = "application/problem+json;charset=UTF-8";
+    public static final String APPLICATION_PROBLEM_XML_VALUE = "application/problem+xml";
+    public static final String APPLICATION_RSS_XML_VALUE = "application/rss+xml";
+    public static final String APPLICATION_STREAM_JSON_VALUE = "application/stream+json";
+    public static final String APPLICATION_XHTML_XML_VALUE = "application/xhtml+xml";
+    public static final String APPLICATION_XML_VALUE = "application/xml";
+    public static final String IMAGE_GIF_VALUE = "image/gif";
+    public static final String IMAGE_JPEG_VALUE = "image/jpeg";
+    public static final String IMAGE_PNG_VALUE = "image/png";
+    public static final String MULTIPART_FORM_DATA_VALUE = "multipart/form-data";
+    public static final String TEXT_EVENT_STREAM_VALUE = "text/event-stream";
+    public static final String TEXT_HTML_VALUE = "text/html";
+    public static final String TEXT_MARKDOWN_VALUE = "text/markdown";
+    public static final String TEXT_PLAIN_VALUE = "text/plain";
+    public static final String TEXT_XML = "text/xml";
+}
