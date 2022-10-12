455918b52280589277d503de35f1e55f4a9a0d63
noear <noear@live.cn>
2022-10-12 17:15:58 +0800

    1.10.7

--- _solon_plugin_cloud2/aws-s3-solon-cloud-plugin/src/test/java/demo/DemoTest.java
+++ _solon_plugin_cloud2/aws-s3-solon-cloud-plugin/src/test/java/demo/DemoTest.java
@@ -1,22 +1,23 @@
 package demo;
 
-import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.noear.solon.Solon;
 import org.noear.solon.cloud.CloudClient;
 import org.noear.solon.cloud.model.Media;
 import org.noear.solon.core.handle.Result;
 import org.noear.solon.test.SolonJUnit4ClassRunner;
 import org.noear.solon.test.SolonTest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author noear 2021/10/18 created
  */
-@Slf4j
 @RunWith(SolonJUnit4ClassRunner.class)
 @SolonTest(DemoApp.class)
 public class DemoTest {
+    static Logger log = LoggerFactory.getLogger(DemoTest.class);
+
     final String contentBody = "test s3 rest api";
     final Media media = new Media(contentBody);
     final String key = "test0/test.txt";
