43edffcba4100cc8b45bfc53e7aecdaa3e6ac9fb
noear <noear@live.cn>
2022-10-19 11:44:18 +0800

    1.10.8

---
+++ _solon_plugin/swagger-solon-plugin/src/main/java/org/noear/solon/extend/swagger/XPluginImp.java
@@ -0,0 +1,16 @@
+package org.noear.solon.extend.swagger;
+
+
+import org.noear.solon.core.AopContext;
+import org.noear.solon.core.Plugin;
+
+/**
+ * @author noear
+ * @since 1.10
+ */
+public class XPluginImp implements Plugin {
+    @Override
+    public void start(AopContext context) {
+
+    }
+}
--- _solon_plugin/vaptcha-solon-plugin/src/main/java/org/noear/solon/extend/vaptcha/XPluginImp.java
+++ _solon_plugin/vaptcha-solon-plugin/src/main/java/org/noear/solon/extend/vaptcha/XPluginImp.java
@@ -1,6 +1,5 @@
 package org.noear.solon.extend.vaptcha;
 
-import org.noear.solon.SolonApp;
 import org.noear.solon.core.AopContext;
 import org.noear.solon.core.Plugin;
 import org.noear.solon.extend.vaptcha.http.request.validators.Vaptcha;
