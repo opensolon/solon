c12e3743e278f7ede358ac02bf354e9927e80cd3
noear <noear@live.cn>
2022-10-27 11:03:16 +0800

    1.10.10

--- _solon_extend/solon.sessionstate.jwt/src/main/java/io/jsonwebtoken/snack/io/SnackDeserializer.java
+++ _solon_extend/solon.sessionstate.jwt/src/main/java/io/jsonwebtoken/snack/io/SnackDeserializer.java
@@ -3,12 +3,14 @@ package io.jsonwebtoken.snack.io;
 import io.jsonwebtoken.io.DeserializationException;
 import io.jsonwebtoken.io.Deserializer;
 import org.noear.snack.ONode;
-import org.noear.snack.core.DEFAULTS;
 import org.noear.snack.core.Feature;
 import org.noear.snack.core.Options;
 
 /**
- * @author noear 2022/10/27 created
+ * Snack3 的 Deserializer 实现
+ *
+ * @author noear
+ * @since 1.10
  */
 public class SnackDeserializer<T> implements Deserializer<T> {
     Options options = Options.serialize().add(Feature.EnumUsingName);
@@ -17,7 +19,8 @@ public class SnackDeserializer<T> implements Deserializer<T> {
     public T deserialize(byte[] bytes) throws DeserializationException {
         String json = new String(bytes);
         ONode oNode = ONode.loadStr(json, options);
-        oNode.remove(DEFAULTS.DEF_TYPE_PROPERTY_NAME);
+        oNode.remove(options.getTypePropertyName());
+
         return oNode.toObject(Object.class);
     }
 }
--- _solon_extend/solon.sessionstate.jwt/src/main/java/io/jsonwebtoken/snack/io/SnackSerializer.java
+++ _solon_extend/solon.sessionstate.jwt/src/main/java/io/jsonwebtoken/snack/io/SnackSerializer.java
@@ -3,18 +3,24 @@ package io.jsonwebtoken.snack.io;
 import io.jsonwebtoken.io.SerializationException;
 import io.jsonwebtoken.io.Serializer;
 import org.noear.snack.ONode;
-import org.noear.snack.core.DEFAULTS;
 import org.noear.snack.core.Feature;
 import org.noear.snack.core.Options;
 
 import java.nio.charset.StandardCharsets;
 
+/**
+ * Snack3 的 Serializer 实现，支持 bean 序列化
+ *
+ * @author noear
+ * @since 1.10
+ */
 public class SnackSerializer<T> implements Serializer<T> {
     Options options = Options.serialize().add(Feature.EnumUsingName);
     @Override
     public byte[] serialize(T t) throws SerializationException {
         ONode oNode = ONode.loadObj(t, options);
-        oNode.remove(DEFAULTS.DEF_TYPE_PROPERTY_NAME);
+        oNode.remove(options.getTypePropertyName());
+
         String json = oNode.toJson();
         return json.getBytes(StandardCharsets.UTF_8);
     }
