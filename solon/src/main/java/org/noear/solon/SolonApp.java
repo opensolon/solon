ea53d6b2e411ccaa59656ea723a8488648dc11d3
noear <noear@live.cn>
2022-11-06 15:48:29 +0800

    1.10.11

--- solon/src/main/java/org/noear/solon/SolonApp.java
+++ solon/src/main/java/org/noear/solon/SolonApp.java
@@ -119,7 +119,7 @@ public class SolonApp extends RouterAdapter {
         //event::1.0.x推送Plugin init end事件
         EventBus.push(PluginInitEndEvent.instance);
 
-        LogUtil.global().trace("App: Plugin start loading");
+        LogUtil.global().trace("App: Plugin starting up");
 
         //1.1.尝试启动插件（顺序不能乱） //不能用forEach，以免当中有插进来
         for (int i = 0, len = plugs.size(); i < len; i++) {
