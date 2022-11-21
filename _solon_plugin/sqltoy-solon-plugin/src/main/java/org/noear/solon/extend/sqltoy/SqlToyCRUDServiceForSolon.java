3736d99ee4d8fee97fd08a825448bfc2a85b9e8a
noear <noear@live.cn>
2022-11-21 14:10:26 +0800

    1.11.1

--- _solon_plugin/sqltoy-solon-plugin/src/main/java/org/noear/solon/extend/sqltoy/SqlToyCRUDServiceForSolon.java
+++ _solon_plugin/sqltoy-solon-plugin/src/main/java/org/noear/solon/extend/sqltoy/SqlToyCRUDServiceForSolon.java
@@ -1,371 +1,213 @@
 package org.noear.solon.extend.sqltoy;
 
+import org.noear.solon.annotation.Singleton;
 import org.noear.solon.data.annotation.Tran;
 import org.noear.solon.aspect.annotation.Service;
 import org.sagacity.sqltoy.dao.SqlToyLazyDao;
 import org.sagacity.sqltoy.model.*;
+import org.sagacity.sqltoy.service.SqlToyCRUDService;
 import org.sagacity.sqltoy.service.impl.SqlToyCRUDServiceImpl;
 import org.sagacity.sqltoy.translate.TranslateHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.Serializable;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 
 /**
  *
  * @author 夜の孤城
- * @since 1.2
+ * @since 1.10
  * */
+@Singleton(false) //因为会被多数据源使用，所以不能是单例
 @Service
-public class SqlToyCRUDServiceForSolon extends SqlToyCRUDServiceImpl {
+public class SqlToyCRUDServiceForSolon implements SqlToyCRUDService {
+    protected final Logger logger = LoggerFactory.getLogger(SqlToyCRUDServiceImpl.class);
+    protected SqlToyLazyDao sqlToyLazyDao;
 
-    public SqlToyCRUDServiceForSolon() {
-//        Aop.getAsyn(SqlToyLazyDao.class, bw -> {
-//            sqlToyLazyDao = bw.raw();
-//        });
+
+    public void setSqlToyLazyDao(SqlToyLazyDao sqlToyLazyDao) {
+        this.sqlToyLazyDao = sqlToyLazyDao;
     }
-    public SqlToyCRUDServiceForSolon(SqlToyLazyDao dao) {
-        sqlToyLazyDao=dao;
-    }
-    /*
-     * (non-Javadoc)
-     *
-     * @see org.sagacity.sqltoy.service.SqlToyCRUDService#save(java.io.Serializable
-     * )
-     */
-    @Override
+
     @Tran
     public Object save(Serializable entity) {
-        return sqlToyLazyDao.save(entity);
+        return this.sqlToyLazyDao.save(entity);
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see org.sagacity.sqltoy.service.SqlToyCRUDService#saveAll(java.util.List)
-     */
-    @Override
     @Tran
     public <T extends Serializable> Long saveAll(List<T> entities) {
-        return sqlToyLazyDao.saveAll(entities);
+        return this.sqlToyLazyDao.saveAll(entities);
     }
 
-    @Override
     @Tran
     public <T extends Serializable> Long saveAllIgnoreExist(List<T> entities) {
-        return sqlToyLazyDao.saveAllIgnoreExist(entities);
+        return this.sqlToyLazyDao.saveAllIgnoreExist(entities);
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see
-     * org.sagacity.sqltoy.service.SqlToyCRUDService#update(java.io.Serializable ,
-     * java.lang.String[])
-     */
-    @Override
     @Tran
     public Long update(Serializable entity, String... forceUpdateProps) {
         if (null == entity) {
             throw new IllegalArgumentException("update 数据对象为null!");
+        } else {
+            return this.sqlToyLazyDao.update(entity, forceUpdateProps);
         }
-        return sqlToyLazyDao.update(entity, forceUpdateProps);
     }
 
-    @Override
     @Tran
     public Long updateCascade(Serializable entity, String... forceUpdateProps) {
         if (null == entity) {
             throw new IllegalArgumentException("update 数据对象为null!");
+        } else {
+            return this.sqlToyLazyDao.updateCascade(entity, forceUpdateProps, (Class[])null, (HashMap)null);
         }
-        return sqlToyLazyDao.updateCascade(entity, forceUpdateProps, null, null);
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see
-     * org.sagacity.sqltoy.service.SqlToyCRUDService#update(java.io.Serializable ,
-     * boolean)
-     */
-    @Override
     @Tran
     public Long updateDeeply(Serializable entity) {
         if (null == entity) {
             throw new IllegalArgumentException("updateDeeply 数据对象为null!");
+        } else {
+            return this.sqlToyLazyDao.updateDeeply(entity);
         }
-        return sqlToyLazyDao.updateDeeply(entity);
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see org.sagacity.sqltoy.service.SqlToyCRUDService#updateAll(java.util .List,
-     * java.lang.String[])
-     */
-    @Override
     @Tran
     public <T extends Serializable> Long updateAll(List<T> entities, String... forceUpdateProps) {
-        return sqlToyLazyDao.updateAll(entities, forceUpdateProps);
+        return this.sqlToyLazyDao.updateAll(entities, forceUpdateProps);
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see org.sagacity.sqltoy.service.SqlToyCRUDService#updateAllDeeply(java
-     * .util.List)
-     */
-    @Override
     @Tran
     public <T extends Serializable> Long updateAllDeeply(List<T> entities) {
-        return sqlToyLazyDao.updateAllDeeply(entities);
+        return this.sqlToyLazyDao.updateAllDeeply(entities);
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see org.sagacity.sqltoy.service.SqlToyCRUDService#saveOrUpdate(java.io
-     * .Serializable, java.lang.String[])
-     */
-    @Override
     @Tran
     public Long saveOrUpdate(Serializable entity, String... forceUpdateProps) {
         if (null == entity) {
             throw new IllegalArgumentException("saveOrUpdate  数据对象为null!");
+        } else {
+            return this.sqlToyLazyDao.saveOrUpdate(entity, forceUpdateProps);
         }
-        return sqlToyLazyDao.saveOrUpdate(entity, forceUpdateProps);
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see org.sagacity.sqltoy.service.SqlToyCRUDService#saveOrUpdateAll(java
-     * .io.Serializable, java.lang.String[])
-     */
-    @Override
     @Tran
     public <T extends Serializable> Long saveOrUpdateAll(List<T> entities, String... forceUpdateProps) {
-        return sqlToyLazyDao.saveOrUpdateAll(entities, forceUpdateProps);
+        return this.sqlToyLazyDao.saveOrUpdateAll(entities, forceUpdateProps);
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see org.sagacity.sqltoy.service.SqlToyCRUDService#load(java.io.Serializable
-     * )
-     */
-    @Override
     @Tran
     public <T extends Serializable> T load(T entity) {
-        return sqlToyLazyDao.load(entity);
+        return this.sqlToyLazyDao.load(entity);
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see org.sagacity.sqltoy.service.SqlToyCRUDService#loadCascade(java.io.
-     * Serializable)
-     */
-    @Override
     @Tran
     public <T extends Serializable> T loadCascade(T entity) {
-        return sqlToyLazyDao.loadCascade(entity, null);
+        return this.sqlToyLazyDao.loadCascade(entity, (LockMode)null, new Class[0]);
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see org.sagacity.sqltoy.service.SqlToyCRUDService#loadAll(java.util.List)
-     */
-    @Override
     @Tran
     public <T extends Serializable> List<T> loadAll(List<T> entities) {
-        return sqlToyLazyDao.loadAll(entities);
+        return this.sqlToyLazyDao.loadAll(entities);
     }
 
-    @Override
     @Tran
-    public <T extends Serializable> List<T> loadAllCascade(List<T> entities, final Class... cascadeTypes) {
-        return sqlToyLazyDao.loadAllCascade(entities, cascadeTypes);
+    public <T extends Serializable> List<T> loadAllCascade(List<T> entities, Class... cascadeTypes) {
+        return this.sqlToyLazyDao.loadAllCascade(entities, cascadeTypes);
     }
 
-    @Override
     @Tran
     public <T extends Serializable> List<T> loadByIds(Class<T> voClass, Object... ids) {
-        return sqlToyLazyDao.loadByIds(voClass, ids);
+        return this.sqlToyLazyDao.loadByIds(voClass, ids);
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see
-     * org.sagacity.sqltoy.service.SqlToyCRUDService#delete(java.io.Serializable )
-     */
-    @Override
     @Tran
     public Long delete(Serializable entity) {
-        return sqlToyLazyDao.delete(entity);
+        return this.sqlToyLazyDao.delete(entity);
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see org.sagacity.sqltoy.service.SqlToyCRUDService#deleteAll(java.util .List)
-     */
-    @Override
     @Tran
     public <T extends Serializable> Long deleteAll(List<T> entities) {
-        return sqlToyLazyDao.deleteAll(entities);
+        return this.sqlToyLazyDao.deleteAll(entities);
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see org.sagacity.sqltoy.service.SqlToyCRUDService#truncate(java.io.
-     * Serializable)
-     */
-    @Override
+    public Long deleteByIds(Class entityClass, Object... ids) {
+        return this.sqlToyLazyDao.deleteByIds(entityClass, ids);
+    }
+
     @Tran
-    public void truncate(final Class entityClass) {
-        sqlToyLazyDao.truncate(entityClass);
+    public void truncate(Class entityClass) {
+        this.sqlToyLazyDao.truncate(entityClass);
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see org.sagacity.sqltoy.service.SqlToyCRUDService#isUnique(java.io.
-     * Serializable, java.lang.String[], java.lang.String)
-     */
-    @Override
     @Tran
     public boolean isUnique(Serializable entity, String... paramsNamed) {
-        return sqlToyLazyDao.isUnique(entity, paramsNamed);
+        return this.sqlToyLazyDao.isUnique(entity, paramsNamed);
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see org.sagacity.sqltoy.service.SqlToyCRUDService#wrapTreeTableRoute(
-     * java.io.Serializable, java.lang.String)
-     */
-    @Override
     @Tran
     public boolean wrapTreeTableRoute(Serializable entity, String pidField) {
-        return sqlToyLazyDao.wrapTreeTableRoute(new TreeTableModel(entity).pidField(pidField));
+        return this.sqlToyLazyDao.wrapTreeTableRoute((new TreeTableModel(entity)).pidField(pidField));
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see
-     * org.sagacity.sqltoy.service.SqlToyCRUDService#wrapTreeTableRoute(java.io.
-     * Serializable, java.lang.String, int)
-     */
-    @Override
     @Tran
     public boolean wrapTreeTableRoute(Serializable entity, String pidField, int appendIdSize) {
-        return sqlToyLazyDao.wrapTreeTableRoute(new TreeTableModel(entity).pidField(pidField).idLength(appendIdSize));
+        return this.sqlToyLazyDao.wrapTreeTableRoute((new TreeTableModel(entity)).pidField(pidField).idLength(appendIdSize));
     }
 
-    @Override
     @Tran
-    public <T> List<QueryResult<T>> parallQuery(List<ParallQuery> parallQueryList, String[] paramNames,
-                                                Object[] paramValues) {
-        return sqlToyLazyDao.parallQuery(parallQueryList, paramNames, paramValues, null);
+    public <T> List<QueryResult<T>> parallQuery(List<ParallQuery> parallQueryList, String[] paramNames, Object[] paramValues) {
+        return this.sqlToyLazyDao.parallQuery(parallQueryList, paramNames, paramValues, (ParallelConfig)null);
     }
 
-    @Override
     @Tran
-    public <T> List<QueryResult<T>> parallQuery(List<ParallQuery> parallQueryList, String[] paramNames,
-                                                Object[] paramValues, ParallelConfig parallelConfig) {
-        return sqlToyLazyDao.parallQuery(parallQueryList, paramNames, paramValues, parallelConfig);
+    public <T> List<QueryResult<T>> parallQuery(List<ParallQuery> parallQueryList, String[] paramNames, Object[] paramValues, ParallelConfig parallelConfig) {
+        return this.sqlToyLazyDao.parallQuery(parallQueryList, paramNames, paramValues, parallelConfig);
     }
 
-    @Override
     @Tran
-    public <T> List<QueryResult<T>> parallQuery(List<ParallQuery> parallQueryList, Map<String, Object> paramsMap,
-                                                ParallelConfig parallelConfig) {
-        return sqlToyLazyDao.parallQuery(parallQueryList, paramsMap, parallelConfig);
+    public <T> List<QueryResult<T>> parallQuery(List<ParallQuery> parallQueryList, Map<String, Object> paramsMap, ParallelConfig parallelConfig) {
+        return this.sqlToyLazyDao.parallQuery(parallQueryList, paramsMap, parallelConfig);
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see
-     * org.sagacity.sqltoy.service.SqlToyCRUDService#generateBizId(java.lang.String,
-     * int)
-     */
-    @Override
     public long generateBizId(String signature, int increment) {
-        return sqlToyLazyDao.generateBizId(signature, increment);
+        return this.sqlToyLazyDao.generateBizId(signature, increment);
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see org.sagacity.sqltoy.service.SqlToyCRUDService#generateBizId(java.io.
-     * Serializable)
-     */
-    @Override
     public String generateBizId(Serializable entity) {
-        return sqlToyLazyDao.generateBizId(entity);
+        return this.sqlToyLazyDao.generateBizId(entity);
     }
 
-    @Override
     public void translate(Collection dataSet, String cacheName, TranslateHandler handler) {
-        sqlToyLazyDao.translate(dataSet, cacheName, null, 1, handler);
+        this.sqlToyLazyDao.translate(dataSet, cacheName, (String)null, 1, handler);
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see
-     * org.sagacity.sqltoy.service.SqlToyCRUDService#translate(java.util.Collection,
-     * java.lang.String, java.lang.String, java.lang.Integer,
-     * org.sagacity.sqltoy.plugin.TranslateHandler)
-     */
-    @Override
-    public void translate(Collection dataSet, String cacheName, String dictType, Integer index,
-                          TranslateHandler handler) {
-        sqlToyLazyDao.translate(dataSet, cacheName, dictType, index, handler);
+    public void translate(Collection dataSet, String cacheName, String dictType, Integer index, TranslateHandler handler) {
+        this.sqlToyLazyDao.translate(dataSet, cacheName, dictType, index, handler);
     }
 
-    /**
-     * @todo 判断缓存是否存在
-     * @param cacheName
-     * @return
-     */
-    @Override
     public boolean existCache(String cacheName) {
-        return sqlToyLazyDao.existCache(cacheName);
+        return this.sqlToyLazyDao.existCache(cacheName);
     }
 
-    @Override
     public Set<String> getCacheNames() {
-        return sqlToyLazyDao.getCacheNames();
+        return this.sqlToyLazyDao.getCacheNames();
     }
 
-    @Override
     public String[] cacheMatchKeys(String matchRegex, CacheMatchFilter cacheMatchFilter) {
-        return sqlToyLazyDao.cacheMatchKeys(matchRegex, cacheMatchFilter);
+        return this.sqlToyLazyDao.cacheMatchKeys(matchRegex, cacheMatchFilter);
     }
 
-    @Override
     public <T extends Serializable> List<T> convertType(List sourceList, Class<T> resultType) {
-        return sqlToyLazyDao.convertType(sourceList, resultType);
+        return this.sqlToyLazyDao.convertType(sourceList, resultType, new String[0]);
     }
 
-    @Override
     public <T extends Serializable> T convertType(Serializable source, Class<T> resultType) {
-        return sqlToyLazyDao.convertType(source, resultType);
+        return this.sqlToyLazyDao.convertType(source, resultType, new String[0]);
     }
 
-    @Override
     public <T extends Serializable> Page<T> convertType(Page sourcePage, Class<T> resultType) {
-        return sqlToyLazyDao.convertType(sourcePage, resultType);
+        return this.sqlToyLazyDao.convertType(sourcePage, resultType, new String[0]);
     }
-
 }
