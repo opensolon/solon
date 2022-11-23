1139453c2209fee4079697e55bfc452727a09cb3
noear <noear@live.cn>
2022-11-23 22:24:29 +0800

    1.11.1

--- _solon_base/solon.data.datasource/src/main/java/org/noear/solon/data/datasource/DynamicDataSource.java
+++ _solon_base/solon.data.datasource/src/main/java/org/noear/solon/data/datasource/DynamicDataSource.java
@@ -1,12 +1,7 @@
 package org.noear.solon.data.datasource;
 
-import org.noear.solon.Utils;
-import org.noear.solon.core.Props;
-import org.noear.solon.core.PropsConverter;
-
 import javax.sql.DataSource;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
@@ -23,57 +18,23 @@ public class DynamicDataSource extends AbstractRoutingDataSource {
             throw new IllegalStateException("Missing dynamic data source configuration");
         }
 
-        //::类型
-        String typeStr = props.getProperty("type");
         String strictStr = props.getProperty("strict", "false");
-
-        if (Utils.isEmpty(typeStr)) {
-            //缺少类型配置
-            throw new IllegalStateException("Missing type configuration");
-        }
-        props.remove("type");
         props.remove("strict");
 
-        Class<?> typeClz = Utils.loadClass(typeStr);
-
-        if (typeClz == null || DataSource.class.isAssignableFrom(typeClz) == false) {
-            throw new IllegalStateException("Type configuration not is data source");
-        }
-
-
-        //::数据源构建
-        Props rootProps;
-        if (props instanceof Props) {
-            rootProps = ((Props) props);
-        } else {
-            rootProps = new Props();
-            rootProps.putAll(props);
-        }
-
-        Map<String, Props> groupProps = rootProps.getGroupedProp("");
-
-        if (groupProps.size() == 0) {
-            //缺少数据源配置
-            throw new IllegalStateException("Missing dynamic data source configuration");
-        }
-
-
-        Map<String, DataSource> dataSourceMap = new HashMap<>();
-        groupProps.forEach((key, prop) -> {
-            if (prop.size() > 1) {
-                //超过1个以上的，才可能是数据源属性
-                DataSource source = (DataSource) PropsConverter.global().convert(prop, typeClz);
-                dataSourceMap.put(key, source);
-            }
-        });
-
+        Map<String, DataSource> dataSourceMap = DynamicDsUtils.buildDsMap(props);
 
         //::获取默认数据源
         DataSource defSource = dataSourceMap.get("default");
 
+        if (defSource == null) {
+            throw new IllegalStateException("Missing default data source configuration");
+        }
+
         //::初始化
         init(defSource, dataSourceMap, Boolean.parseBoolean(strictStr));
     }
+
+
 
     public DynamicDataSource(DataSource defaultTargetDataSource, Map<String, DataSource> targetDataSources) {
         init(defaultTargetDataSource, targetDataSources, false);
