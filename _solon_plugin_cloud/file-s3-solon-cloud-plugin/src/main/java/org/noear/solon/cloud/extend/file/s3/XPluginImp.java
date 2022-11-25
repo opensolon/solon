babdbbbc8288af6b7bb0198f82e210f9b13d55fb
noear <noear@live.cn>
2022-11-25 18:31:40 +0800

    1.11.3

--- _solon_plugin_cloud/file-s3-solon-cloud-plugin/src/main/java/org/noear/solon/cloud/extend/file/s3/XPluginImp.java
+++ _solon_plugin_cloud/file-s3-solon-cloud-plugin/src/main/java/org/noear/solon/cloud/extend/file/s3/XPluginImp.java
@@ -1,5 +1,8 @@
 package org.noear.solon.cloud.extend.file.s3;
 
+import org.noear.solon.Utils;
+import org.noear.solon.cloud.CloudManager;
+import org.noear.solon.cloud.extend.file.s3.service.CloudFileServiceS3Imp;
 import org.noear.solon.core.AopContext;
 import org.noear.solon.core.Plugin;
 
@@ -7,9 +10,15 @@ import org.noear.solon.core.Plugin;
  * @author noear
  * @since 1.10
  */
-public class XPluginImp implements Plugin{
+public class XPluginImp implements Plugin {
     @Override
     public void start(AopContext context) throws Throwable {
+        if (Utils.isEmpty(S3Props.instance.getFileAccessKey())) {
+            return;
+        }
 
+        if (S3Props.instance.getFileEnable()) {
+            CloudManager.register(new CloudFileServiceS3Imp(S3Props.instance));
+        }
     }
 }
---
+++ _solon_plugin_cloud/file-s3-solon-cloud-plugin/src/main/java/org/noear/solon/cloud/extend/file/s3/service/CloudFileServiceS3Imp.java
@@ -0,0 +1,32 @@
+package org.noear.solon.cloud.extend.file.s3.service;
+
+import org.noear.solon.cloud.CloudProps;
+import org.noear.solon.cloud.exception.CloudFileException;
+import org.noear.solon.cloud.model.Media;
+import org.noear.solon.cloud.service.CloudFileService;
+import org.noear.solon.core.handle.Result;
+
+/**
+ * @author noear
+ * @since 1.10
+ */
+public class CloudFileServiceS3Imp implements CloudFileService {
+    public CloudFileServiceS3Imp(CloudProps cloudProps){
+
+    }
+
+    @Override
+    public Media get(String bucket, String key) throws CloudFileException {
+        return null;
+    }
+
+    @Override
+    public Result put(String bucket, String key, Media media) throws CloudFileException {
+        return null;
+    }
+
+    @Override
+    public Result delete(String bucket, String key) throws CloudFileException {
+        return null;
+    }
+}
