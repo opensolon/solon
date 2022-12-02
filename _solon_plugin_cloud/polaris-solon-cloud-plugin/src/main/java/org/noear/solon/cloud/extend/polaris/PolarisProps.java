6709f6af7c30a725b7e57351adea9ec0c201518f
何荣振 <hrzpaul@foxmail.com>
2022-12-02 14:32:45 +0800

    feat: 服务治理polaris集成

---
+++ _solon_plugin_cloud/polaris-solon-cloud-plugin/src/main/java/org/noear/solon/cloud/extend/polaris/PolarisProps.java
@@ -0,0 +1,11 @@
+package org.noear.solon.cloud.extend.polaris;
+
+import org.noear.solon.cloud.CloudProps;
+
+/**
+ * @author noear
+ * @since 1.2
+ */
+public class PolarisProps {
+    public static final CloudProps instance = new CloudProps("polaris");
+}
--- _solon_plugin_cloud/polaris-solon-cloud-plugin/src/main/java/org/noear/solon/cloud/extend/polaris/XPluginImp.java
+++ _solon_plugin_cloud/polaris-solon-cloud-plugin/src/main/java/org/noear/solon/cloud/extend/polaris/XPluginImp.java
@@ -1,5 +1,8 @@
 package org.noear.solon.cloud.extend.polaris;
 
+import org.noear.solon.cloud.CloudManager;
+import org.noear.solon.cloud.extend.polaris.service.CloudConfigServicePolarisImp;
+import org.noear.solon.cloud.extend.polaris.service.CloudDiscoveryServicePolarisImp;
 import org.noear.solon.core.AopContext;
 import org.noear.solon.core.Plugin;
 
@@ -10,6 +13,14 @@ import org.noear.solon.core.Plugin;
 public class XPluginImp implements Plugin {
     @Override
     public void start(AopContext context) throws Throwable {
+        //1.登记配置服务
+        if (PolarisProps.instance.getConfigEnable()) {
+            CloudManager.register(new CloudConfigServicePolarisImp(PolarisProps.instance));
+        }
 
+        //2.登记发现服务
+        if (PolarisProps.instance.getDiscoveryEnable()) {
+            CloudManager.register(new CloudDiscoveryServicePolarisImp(PolarisProps.instance));
+        }
     }
 }
