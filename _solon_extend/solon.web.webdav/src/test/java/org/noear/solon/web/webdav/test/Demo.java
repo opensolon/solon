1923875c2a8bcb8e2df4abdc9b6d93505538c807
noear <noear@live.cn>
2022-12-03 09:11:04 +0800

    1.11.4

--- _solon_extend/solon.web.webdav/src/test/java/org/noear/solon/web/webdav/test/Demo.java
+++ _solon_extend/solon.web.webdav/src/test/java/org/noear/solon/web/webdav/test/Demo.java
@@ -1,9 +1,11 @@
-package org.noear.solon.web.webdav;
+package org.noear.solon.web.webdav.test;
 
 import org.noear.solon.Solon;
 import org.noear.solon.core.handle.Context;
 import org.noear.solon.core.handle.Handler;
 import org.noear.solon.core.handle.MethodType;
+import org.noear.solon.web.webdav.AbstractHandler;
+import org.noear.solon.web.webdav.FileSystem;
 import org.noear.solon.web.webdav.impl.LocalFileSystem;
 
 /**
