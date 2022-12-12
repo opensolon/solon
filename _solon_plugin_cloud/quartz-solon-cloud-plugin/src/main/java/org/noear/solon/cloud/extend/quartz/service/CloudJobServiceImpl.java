fa1da5ae19c9eda26cdd5d8d447d2cc02df04313
noear <noear@live.cn>
2022-12-12 18:14:48 +0800

    1.11.5

--- _solon_plugin_cloud/quartz-solon-cloud-plugin/src/main/java/org/noear/solon/cloud/extend/quartz/service/CloudJobServiceImpl.java
+++ _solon_plugin_cloud/quartz-solon-cloud-plugin/src/main/java/org/noear/solon/cloud/extend/quartz/service/CloudJobServiceImpl.java
@@ -8,6 +8,8 @@ import org.noear.solon.cloud.service.CloudJobService;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author noear
  * @since 1.11
@@ -45,21 +47,26 @@ public class CloudJobServiceImpl implements CloudJobService {
             tryInitScheduler();
 
             if (_scheduler.checkExists(jobKey) == false) {
-                JobDetail jobDetail = JobBuilder.newJob(JobQuartzProxy.class)
-                        .withDescription(description)
-                        .withIdentity(jobKey)
-                        .setJobData(new JobDataMap())
-                        .build();
-
-                CronScheduleBuilder builder = CronScheduleBuilder.cronSchedule(cron7x);
-
-                Trigger trigger = TriggerBuilder.newTrigger()
-                        .withIdentity(name, "solon")
-                        .startNow()
-                        .withSchedule(builder)
-                        .build();
-
-                _scheduler.scheduleJob(jobDetail, trigger);
+                if (cron7x.indexOf(" ") < 0) {
+                    if (cron7x.endsWith("ms")) {
+                        long period = Long.parseLong(cron7x.substring(0, cron7x.length() - 2));
+                        regJobByPeriod(jobKey, name, description, period, TimeUnit.MILLISECONDS);
+                    } else if (cron7x.endsWith("s")) {
+                        long period = Long.parseLong(cron7x.substring(0, cron7x.length() - 1));
+                        regJobByPeriod(jobKey, name, description, period, TimeUnit.SECONDS);
+                    } else if (cron7x.endsWith("m")) {
+                        long period = Long.parseLong(cron7x.substring(0, cron7x.length() - 1));
+                        regJobByPeriod(jobKey, name, description, period, TimeUnit.MINUTES);
+                    } else if (cron7x.endsWith("h")) {
+                        long period = Long.parseLong(cron7x.substring(0, cron7x.length() - 1));
+                        regJobByPeriod(jobKey, name, description, period, TimeUnit.HOURS);
+                    } else if (cron7x.endsWith("d")) {
+                        long period = Long.parseLong(cron7x.substring(0, cron7x.length() - 1));
+                        regJobByPeriod(jobKey, name, description, period, TimeUnit.DAYS);
+                    }
+                } else {
+                    regJobByCronx(jobKey, cron7x, name, description);
+                }
             }
         } catch (SchedulerException e) {
             throw new CloudJobException(e);
@@ -75,12 +82,71 @@ public class CloudJobServiceImpl implements CloudJobService {
 
     /**
      * 启用
-     * */
+     */
     public void start() throws SchedulerException {
         tryInitScheduler();
 
         if (_scheduler != null) {
             _scheduler.start();
+        }
+    }
+
+    private void regJobByCronx(JobKey jobKey, String name, String cron7x, String description) throws SchedulerException {
+        JobDetail jobDetail = JobBuilder.newJob(JobQuartzProxy.class)
+                .withDescription(description)
+                .withIdentity(jobKey)
+                .setJobData(new JobDataMap())
+                .build();
+
+        CronScheduleBuilder builder = CronScheduleBuilder.cronSchedule(cron7x);
+
+        Trigger trigger = TriggerBuilder.newTrigger()
+                .withIdentity(name, "solon")
+                .startNow()
+                .withSchedule(builder)
+                .build();
+
+        _scheduler.scheduleJob(jobDetail, trigger);
+    }
+
+    private void regJobByPeriod(JobKey jobKey, String name, String description, long period, TimeUnit unit) throws SchedulerException {
+        JobDetail jobDetail = JobBuilder.newJob(JobQuartzProxy.class)
+                .withDescription(description)
+                .withIdentity(jobKey)
+                .setJobData(new JobDataMap())
+                .build();
+
+        if (_scheduler.checkExists(jobDetail.getKey()) == false) {
+            SimpleScheduleBuilder builder = SimpleScheduleBuilder.simpleSchedule();
+            switch (unit) {
+                case MILLISECONDS:
+                    builder.withIntervalInMilliseconds(period);
+                    break;
+                case SECONDS:
+                    builder.withIntervalInSeconds((int) period);
+                    break;
+                case MINUTES:
+                    builder.withIntervalInMinutes((int) period);
+                    break;
+                case HOURS:
+                    builder.withIntervalInHours((int) period);
+                    break;
+                case DAYS:
+                    builder.withIntervalInHours((int) (period * 24));
+                    break;
+                default:
+                    return;
+            }
+
+            builder.repeatForever();
+
+            Trigger trigger = TriggerBuilder.newTrigger()
+                    .withIdentity(name, "solon")
+                    .startNow()
+                    .withSchedule(builder)//
+                    .build();
+
+            _scheduler.scheduleJob(jobDetail, trigger);
         }
     }
 }
