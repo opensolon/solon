7f7b71a5dabc9f8e75714d1467b48aab6102221e
noear <noear@live.cn>
2022-12-22 13:21:34 +0800

    1.11.7

--- _solon_plugin/liteflow-solon-plugin/src/main/java/com/yomahub/liteflow/solon/integration/XPluginImpl.java
+++ _solon_plugin/liteflow-solon-plugin/src/main/java/com/yomahub/liteflow/solon/integration/XPluginImpl.java
@@ -1,9 +1,14 @@
 package com.yomahub.liteflow.solon.integration;
 
 import com.yomahub.liteflow.annotation.LiteflowComponent;
+import com.yomahub.liteflow.annotation.LiteflowMethod;
 import com.yomahub.liteflow.core.NodeComponent;
 import com.yomahub.liteflow.flow.FlowBus;
-import com.yomahub.liteflow.solon.LiteflowProperty;
+import com.yomahub.liteflow.solon.NodeComponentOfMethod;
+import com.yomahub.liteflow.solon.config.LiteflowAutoConfiguration;
+import com.yomahub.liteflow.solon.config.LiteflowMainAutoConfiguration;
+import com.yomahub.liteflow.solon.config.LiteflowMonitorProperty;
+import com.yomahub.liteflow.solon.config.LiteflowProperty;
 import org.noear.solon.Utils;
 import org.noear.solon.core.AopContext;
 import org.noear.solon.core.Plugin;
@@ -32,6 +37,11 @@ public class XPluginImpl implements Plugin {
             return;
         }
 
+        //放到前面
+        context.beanMake(LiteflowProperty.class);
+        context.beanMake(LiteflowMonitorProperty.class);
+        context.beanMake(LiteflowAutoConfiguration.class);
+
         //订阅 NodeComponent 组件
         context.subWrapsOfType(NodeComponent.class, bw -> {
             NodeComponent node1 = bw.raw();
@@ -40,22 +50,33 @@ public class XPluginImpl implements Plugin {
             FlowBus.addSpringScanNode(bw.name(), bw.raw());
         });
 
+        context.beanExtractorAdd(LiteflowMethod.class, (bw, method, anno) -> {
+            NodeComponent node1 = new NodeComponentOfMethod(bw, method, anno.value());
+            String nodeId = Utils.annoAlias(anno.nodeId(), bw.name());
+            node1.setNodeId(nodeId);
+            node1.setType(anno.nodeType());
+
+            FlowBus.addSpringScanNode(nodeId, node1);
+        });
+
         context.beanBuilderAdd(LiteflowComponent.class, (clz, bw, anno) -> {
             if(NodeComponent.class.isAssignableFrom(clz)) {
                 NodeComponent node1 = bw.raw();
-                String id1 = Utils.annoAlias(anno.id(), anno.value());
-                String name1 =Utils.annoAlias(anno.name(), id1);
+                String nodeId = Utils.annoAlias(anno.id(), anno.value());
 
-                node1.setNodeId(id1);
-                node1.setName(name1);
+                node1.setNodeId(nodeId);
+                node1.setName(anno.name());
 
-                FlowBus.addSpringScanNode(node1.getNodeId(), node1);
+                FlowBus.addSpringScanNode(nodeId, node1);
+            }else{
+                context.beanExtract(bw); //尝试提取 LiteflowMethod 函数
             }
         });
 
+
         //扫描相关组件
         context.beanOnloaded((ctx)->{
-            context.beanScan(LiteflowProperty.class);
+            context.beanMake(LiteflowMainAutoConfiguration.class);
         });
     }
 }
