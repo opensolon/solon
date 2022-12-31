c61e02107d007c178f7e9ea7bc18246870ed214e
noear <noear@live.cn>
2022-12-31 09:35:04 +0800

    1.12.1

---
+++ _solon_plugin_cloud/fastdfs-solon-cloud-plugin/src/test/java/demo/DemoApp.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.noear.solon.Solon;
+
+/**
+ * @author 等風來再離開 2022/11/29 created
+ */
+public class DemoApp {
+    public static void main(String[] args) {
+        Solon.start(DemoApp.class, args);
+    }
+}
