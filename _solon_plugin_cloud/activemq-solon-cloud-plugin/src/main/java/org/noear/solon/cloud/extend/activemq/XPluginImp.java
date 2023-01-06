3f454a2e00012e8fcd49853dd4aacc83c63be68a
noear <noear@live.cn>
2023-01-06 16:15:48 +0800

    1.12.1

---
+++ _solon_plugin_cloud/activemq-solon-cloud-plugin/src/main/java/org/noear/solon/cloud/extend/activemq/XPluginImp.java
@@ -0,0 +1,15 @@
+package org.noear.solon.cloud.extend.activemq;
+
+import org.noear.solon.core.AopContext;
+import org.noear.solon.core.Plugin;
+
+/**
+ * @author noear
+ * @since 1.12
+ */
+public class XPluginImp implements Plugin {
+    @Override
+    public void start(AopContext context) throws Throwable {
+
+    }
+}
