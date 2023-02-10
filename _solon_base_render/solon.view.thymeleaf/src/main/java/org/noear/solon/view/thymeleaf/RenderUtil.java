c4e39a95981ad28e3c463d973a33d78f6806b726
larry <1762861715@qq.com>
2023-02-10 09:12:58 +0800

    thymeleaf渲染字符串

---
+++ _solon_base_render/solon.view.thymeleaf/src/main/java/org/noear/solon/view/thymeleaf/RenderUtil.java
@@ -0,0 +1,23 @@
+package org.noear.solon.view.thymeleaf;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.StringTemplateResolver;
+
+import java.io.StringWriter;
+import java.util.Map;
+
+public class RenderUtil {
+    public static String render(String template, Map<String, Object> model) throws Exception {
+        TemplateEngine engine = new TemplateEngine();
+        StringTemplateResolver resolver = new StringTemplateResolver();
+        engine.setTemplateResolver(resolver);
+        Context context = new Context();
+        context.setVariables(model);
+        StringWriter writer = new StringWriter();
+        engine.process(template, context, writer);
+        return writer.toString();
+
+    }
+}
+
