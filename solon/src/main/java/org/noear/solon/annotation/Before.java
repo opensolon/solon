bb6dd89d30d41278cd1e8778588b66e54b6b5455
lionel <lionelk@163.com>
2023-02-14 13:15:51 +0800

    修改错别字

--- solon/src/main/java/org/noear/solon/annotation/Before.java
+++ solon/src/main/java/org/noear/solon/annotation/Before.java
@@ -4,7 +4,7 @@ import org.noear.solon.core.handle.Handler;
 import java.lang.annotation.*;
 
 /**
- * 触发器：前置处理（争对 XController 和 XAction 的拦截器）
+ * 触发器：前置处理（针对 Controller 和 Action 的拦截器）
  *
  * <pre><code>
  * @Before({StartHandler.class, IpHandler.class})
