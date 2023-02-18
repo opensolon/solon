f7deb3d2ab3a46c1aae21f36f02d162ef5f48343
noear <noear@live.cn>
2023-02-18 13:35:31 +0800

    调整两个测试类名

--- __test/src/test/java/features/MockTest.java
+++ __test/src/test/java/features/MockTest.java
@@ -1,28 +1,65 @@
 package features;
 
-import okhttp3.mockwebserver.MockResponse;
-import okhttp3.mockwebserver.MockWebServer;
-import org.junit.Rule;
-import org.junit.Test;
-import org.noear.solon.test.AbstractHttpTester;
 
-import java.io.IOException;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 /**
  * @author noear 2023/2/14 created
  */
-public class MockTest extends AbstractHttpTester {
-    public final static String EXPECTED = "{\"status\": \"ok\"}";
-    @Rule
-    public MockWebServer server = new MockWebServer();
+public class MockTest {
+    @Test
+    public void testBehavior() {
+        List<String> list = mock(List.class);
+        list.add("1");
+        list.add("2");
+
+        System.out.println(list.get(0)); // 会得到null ，前面只是在记录行为而已，没有往list中添加数据
+
+        assertFalse(verify(list).add("1")); // 正确，因为该行为被记住了
+
+        assertThrows(Error.class, () -> {
+            verify(list).add("3");//报错，因为前面没有记录这个行为
+        });
+    }
 
     @Test
-    public void test() throws IOException {
-        server.enqueue(new MockResponse().setBody(EXPECTED));
+    void testStub() {
+        List<Integer> l = mock(ArrayList.class);
 
-        String rst = http(server.getPort()).get();
+        when(l.get(0)).thenReturn(10);
+        when(l.get(1)).thenReturn(20);
+        when(l.get(2)).thenThrow(new RuntimeException("no such element"));
 
-        assert rst != null;
-        assert EXPECTED.equals(rst);
+        assertEquals(l.get(0), 10);
+        assertEquals(l.get(1), 20);
+        assertNull(l.get(4));
+        assertThrows(RuntimeException.class, () -> {
+            int x = l.get(2);
+        });
+    }
+
+    @Test
+    void testVoidStub() {
+        List<Integer> l = mock(ArrayList.class);
+        doReturn(10).when(l).get(1);
+        doThrow(new RuntimeException("you cant clear this List")).when(l).clear();
+
+        assertEquals(l.get(1), 10);
+        assertThrows(RuntimeException.class, () -> l.clear());
+    }
+
+    @Test
+    void testMatchers() {
+        List<Integer> l = mock(ArrayList.class);
+        when(l.get(anyInt())).thenReturn(100);
+
+        assertEquals(l.get(999), 100);
     }
 }
---
+++ __test/src/test/java/features/MockWebTest.java
@@ -0,0 +1,28 @@
+package features;
+
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import org.junit.Rule;
+import org.junit.Test;
+import org.noear.solon.test.AbstractHttpTester;
+
+import java.io.IOException;
+
+/**
+ * @author noear 2023/2/14 created
+ */
+public class MockWebTest extends AbstractHttpTester {
+    public final static String EXPECTED = "{\"status\": \"ok\"}";
+    @Rule
+    public MockWebServer server = new MockWebServer();
+
+    @Test
+    public void testSimple() throws IOException {
+        server.enqueue(new MockResponse().setBody(EXPECTED));
+
+        String rst = http(server.getPort()).get();
+
+        assert rst != null;
+        assert EXPECTED.equals(rst);
+    }
+}
