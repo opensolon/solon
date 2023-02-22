bd6e9f94de5e3df95936371f23b6c1a658f8d16c
Shihwan <hulytu@163.com>
2023-02-22 19:46:01 +0800

    修复构建打包问题，添加自动查找main方法

--- _solon_tool/solon-gradle-plugin/src/main/java/org/noear/solon/gradle/plugin/JavaPluginAction.java
+++ _solon_tool/solon-gradle-plugin/src/main/java/org/noear/solon/gradle/plugin/JavaPluginAction.java
@@ -2,22 +2,32 @@ package org.noear.solon.gradle.plugin;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
-import org.gradle.api.plugins.BasePlugin;
-import org.gradle.api.plugins.JavaPlugin;
+import org.gradle.api.file.FileCollection;
+import org.gradle.api.plugins.*;
 import org.gradle.api.tasks.SourceSet;
+import org.gradle.api.tasks.SourceSetContainer;
+import org.gradle.api.tasks.TaskProvider;
 import org.gradle.api.tasks.bundling.Jar;
 import org.gradle.api.tasks.compile.CompileOptions;
 import org.gradle.api.tasks.compile.JavaCompile;
 import org.jetbrains.annotations.NotNull;
+import org.noear.solon.gradle.dsl.SolonExtension;
 import org.noear.solon.gradle.tasks.bundling.SolonJar;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.Callable;
 import java.util.stream.Collectors;
 
 final class JavaPluginAction implements PluginApplicationAction {
 
     private static final String PARAMETERS_COMPILER_ARG = "-parameters";
+
+    private final SinglePublishedArtifact singlePublishedArtifact;
+
+    JavaPluginAction(SinglePublishedArtifact artifact) {
+        this.singlePublishedArtifact = artifact;
+    }
 
     @Override
     public Class<? extends Plugin<? extends Project>> getPluginClass() {
@@ -27,11 +37,56 @@ final class JavaPluginAction implements PluginApplicationAction {
     @Override
     public void execute(@NotNull Project project) {
         classifyJarTask(project);
-
-        configureSolonJarTask(project);
-
+        configureBuildTask(project);
+        TaskProvider<ResolveMainClassName> resolveMainClassName = configureResolveMainClassNameTask(project);
+        TaskProvider<SolonJar> solonJar = configureSolonJarTask(project, resolveMainClassName);
+        configureArtifactPublication(solonJar);
         project.afterEvaluate(this::configureUtf8Encoding);
         configureParametersCompilerArg(project);
+    }
+
+    private void configureBuildTask(Project project) {
+        project.getTasks().named(BasePlugin.ASSEMBLE_TASK_NAME)
+                .configure((task) -> task.dependsOn(this.singlePublishedArtifact));
+    }
+
+    private TaskProvider<ResolveMainClassName> configureResolveMainClassNameTask(Project project) {
+        return project.getTasks().register(SolonPlugin.RESOLVE_MAIN_CLASS_NAME_TASK_NAME,
+                ResolveMainClassName.class, (resolveMainClassName) -> {
+                    ExtensionContainer extensions = project.getExtensions();
+                    resolveMainClassName.setDescription("Resolves the name of the application's main class.");
+                    resolveMainClassName.setGroup(BasePlugin.BUILD_GROUP);
+                    Callable<FileCollection> classpath = () -> project.getExtensions()
+                            .getByType(SourceSetContainer.class).getByName(SourceSet.MAIN_SOURCE_SET_NAME).getOutput();
+                    resolveMainClassName.setClasspath(classpath);
+                    resolveMainClassName.getConfiguredMainClassName().convention(project.provider(() -> {
+
+                        String javaApplicationMainClass = getJavaApplicationMainClass(extensions);
+
+                        if (javaApplicationMainClass != null) {
+                            return javaApplicationMainClass;
+                        }
+
+                        SolonExtension springBootExtension = project.getExtensions()
+                                .findByType(SolonExtension.class);
+
+                        return Objects.requireNonNull(springBootExtension).getMainClass().getOrNull();
+                    }));
+                    resolveMainClassName.getOutputFile()
+                            .set(project.getLayout().getBuildDirectory().file("resolvedMainClassName"));
+                });
+    }
+
+    private static String getJavaApplicationMainClass(ExtensionContainer extensions) {
+        JavaApplication javaApplication = extensions.findByType(JavaApplication.class);
+        if (javaApplication == null) {
+            return null;
+        }
+        return javaApplication.getMainClass().getOrNull();
+    }
+
+    private JavaPluginExtension javaPluginExtension(Project project) {
+        return project.getExtensions().getByType(JavaPluginExtension.class);
     }
 
     private void classifyJarTask(Project project) {
@@ -39,6 +94,9 @@ final class JavaPluginAction implements PluginApplicationAction {
                 .configure((task) -> task.getArchiveClassifier().convention("plain"));
     }
 
+    private void configureArtifactPublication(TaskProvider<SolonJar> bootJar) {
+        this.singlePublishedArtifact.addJarCandidate(bootJar);
+    }
 
     private void configureUtf8Encoding(Project evaluatedProject) {
         evaluatedProject.getTasks().withType(JavaCompile.class).configureEach(this::configureUtf8Encoding);
@@ -60,20 +118,21 @@ final class JavaPluginAction implements PluginApplicationAction {
         });
     }
 
-    private void configureSolonJarTask(Project project) {
-        SourceSet mainSourceSet = PluginApplicationAction.javaPluginExtension(project).getSourceSets()
+    private TaskProvider<SolonJar> configureSolonJarTask(Project project, TaskProvider<ResolveMainClassName> resolveMainClassName) {
+        SourceSet mainSourceSet = javaPluginExtension(project).getSourceSets()
                 .getByName(SourceSet.MAIN_SOURCE_SET_NAME);
 
         Callable<List<Object>> classpath = () -> mainSourceSet.getRuntimeClasspath().getFiles().stream().map(file -> file.isDirectory() ? file : project.zipTree(file)).collect(Collectors.toList());
 
-        project.getTasks().register(SolonPlugin.SOLON_JAR_TASK_NAME, SolonJar.class, (bootJar) -> {
+        return project.getTasks().register(SolonPlugin.SOLON_JAR_TASK_NAME, SolonJar.class, (bootJar) -> {
             bootJar.setDescription("Assembles an executable jar archive containing the main classes and their dependencies.");
             bootJar.setGroup(BasePlugin.BUILD_GROUP);
             bootJar.classpath(classpath);
-            bootJar.getMainClass().convention(PluginApplicationAction.configureResolveMainClassName(project));
+
+            bootJar.getMainClass().convention(resolveMainClassName.flatMap((resolver) -> resolveMainClassName.get().readMainClassName()));
 
             bootJar.getTargetJavaVersion()
-                    .set(project.provider(() -> PluginApplicationAction.javaPluginExtension(project).getTargetCompatibility()));
+                    .set(project.provider(() -> javaPluginExtension(project).getTargetCompatibility()));
         });
     }
 }
