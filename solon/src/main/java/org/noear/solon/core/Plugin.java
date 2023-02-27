8131f0d9db456f8cfe0323ea2dddd7e4a5f3d269
noear <noear@live.cn>
2023-02-28 01:19:41 +0800

    取消 init 接口

--- solon/src/main/java/org/noear/solon/core/Plugin.java
+++ solon/src/main/java/org/noear/solon/core/Plugin.java
@@ -9,12 +9,6 @@ package org.noear.solon.core;
 @FunctionalInterface
 public interface Plugin {
     /**
-     * 初始化（不允许做Aop相关的事情）
-     * */
-    @Deprecated
-    default void init(AopContext context) throws Throwable{}
-
-    /**
      * 启动 （stop 可通过: app.onStop(..) 实现）
      */
     void start(AopContext context) throws Throwable;
