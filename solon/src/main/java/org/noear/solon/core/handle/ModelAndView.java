c638867057e8d1b956d7d29c069fc02b8f68006a
noear <noear@live.cn>
2023-03-17 13:42:38 +0800

    增加 ModelAndView 操作便利性

--- solon/src/main/java/org/noear/solon/core/handle/ModelAndView.java
+++ solon/src/main/java/org/noear/solon/core/handle/ModelAndView.java
@@ -34,11 +34,15 @@ public class ModelAndView implements Serializable {
     private String view;
     private Map<String, Object> model = new LinkedHashMap<>();
 
-    public ModelAndView(){super();}
+    public ModelAndView() {
+        super();
+    }
+
     public ModelAndView(String view) {
         this();
         this.view = view;
     }
+
     public ModelAndView(String view, Map<String, ?> model) {
         this(view);
 
@@ -48,37 +52,74 @@ public class ModelAndView implements Serializable {
     }
 
 
-    /** 视图 */
+    /**
+     * 获取视图
+     */
     public String view() {
         return view;
     }
-    public ModelAndView view(String view){
+
+    /**
+     * 设置视图
+     */
+    public ModelAndView view(String view) {
         this.view = view;
         return this;
     }
 
-    /** 模型 */
-    public Map<String, Object> model() { return model; }
+    /**
+     * 获取模型
+     */
+    public Map<String, Object> model() {
+        return model;
+    }
 
-    public void put(String key, Object value){
+    /**
+     * 设置模型
+     *
+     * @since 2.2
+     */
+    public ModelAndView model(Map<String, Object> model) {
+        this.model = model;
+        return this;
+    }
+
+    /**
+     * 添加模型值
+     */
+    public ModelAndView put(String key, Object value) {
         model.put(key, value);
+        return this;
     }
 
-    public void putIfAbsent(String key, Object value){
+    /**
+     * 添加模型值
+     */
+    public ModelAndView putIfAbsent(String key, Object value) {
         model.putIfAbsent(key, value);
+        return this;
     }
 
-    public void putAll(Map<String, ?> keyValues){
+    /**
+     * 添加模型值
+     */
+    public ModelAndView putAll(Map<String, ?> keyValues) {
         model.putAll(keyValues);
+        return this;
     }
 
+    /**
+     * 清空
+     */
     public void clear() {
         model.clear();
         view = null;
     }
 
-    /** 是否为空 */
+    /**
+     * 是否为空
+     */
     public boolean isEmpty() {
-        return view == null && model.size()==0;
+        return view == null && model.size() == 0;
     }
 }
