b01bdd9632d6d50495b8f3c494ccb2bdcb5d6f1a
noear <noear@live.cn>
2023-04-03 09:01:07 +0800

    jackson 增加几个新的默认特性

--- _solon_base_render/solon.serialization.jackson/src/main/java/org/noear/solon/serialization/jackson/XPluginImp.java
+++ _solon_base_render/solon.serialization.jackson/src/main/java/org/noear/solon/serialization/jackson/XPluginImp.java
@@ -1,6 +1,7 @@
 package org.noear.solon.serialization.jackson;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import org.noear.solon.Solon;
@@ -11,6 +12,11 @@ import org.noear.solon.core.event.EventBus;
 import org.noear.solon.core.handle.RenderManager;
 import org.noear.solon.serialization.prop.JsonProps;
 import org.noear.solon.serialization.prop.JsonPropsUtil;
+
+import java.util.LinkedHashMap;
+
+import static com.fasterxml.jackson.databind.MapperFeature.PROPAGATE_TRANSIENT_MARKER;
+import static com.fasterxml.jackson.databind.MapperFeature.SORT_PROPERTIES_ALPHABETICALLY;
 
 public class XPluginImp implements Plugin {
 
@@ -73,6 +79,18 @@ public class XPluginImp implements Plugin {
         if (writeNulls == false) {
             factory.config().setSerializationInclusion(JsonInclude.Include.NON_NULL);
         }
+
+        //启用 transient 关键字
+        factory.config().configure(PROPAGATE_TRANSIENT_MARKER,true);
+        //启用排序（即使用 LinkedHashMap）
+        factory.config().configure(SORT_PROPERTIES_ALPHABETICALLY, true);
+        //是否识别不带引号的key
+        factory.config().configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
+        //是否识别单引号的key
+        factory.config().configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
+        //浮点数默认类型（dubbod 转 BigDecimal）
+        factory.config().configure(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS, true);
+
 
         //反序列化时候遇到不匹配的属性并不抛出异常
         factory.config().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
