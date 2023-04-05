97d4ebd135a681fedc8ac92125e4520f04f83545
noear <noear@live.cn>
2023-04-05 18:49:11 +0800

    添加 seata-solon-plugin 空项目

--- _solon_plugin/graphql-solon-plugin/src/test/java/features/GraphqlTest.java
+++
@@ -1,9 +0,0 @@
-package features;
-
-import org.noear.solon.Solon;
-
-public class GraphqlTest {
-    public static void main(String[] args) {
-        Solon.start(GraphqlTest.class, args);
-    }
-}
