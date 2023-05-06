972e38c738fd81b1dec664ebce778a398c48bf66
HikariLan <shaokeyibb@126.com>
2023-05-07 00:32:08 +0800

    feat(solon-admin:server-ui): Initial commit
    Signed-off-by: HikariLan <shaokeyibb@126.com>

---
+++ _solon_tool/solon-admin-server-ui/src/main/vue/src/App.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ _solon_tool/solon-admin-server-ui/src/main/vue/vite.config.ts
@@ -0,0 +1,11 @@
+import {defineConfig} from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    target: "es2020",
+    outDir: "../../../target/dist",
+  }
+})
