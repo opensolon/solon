8bf33ee978a6cece36438f24374deaa5e5984c1c
HikariLan <shaokeyibb@126.com>
2023-05-10 19:24:28 +0800

    feature(solon-admin:server-ui): add arco.design
    Signed-off-by: HikariLan <shaokeyibb@126.com>

--- _solon_tool/solon-admin-server-ui/src/main/vue/vite.config.ts
+++ _solon_tool/solon-admin-server-ui/src/main/vue/vite.config.ts
@@ -1,11 +1,26 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite';
+import {ArcoResolver} from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  build: {
-    target: "es2020",
-    outDir: "../../../target/dist",
-  }
+    plugins: [
+        vue(),
+        AutoImport({
+            resolvers: [ArcoResolver()],
+        }),
+        Components({
+            resolvers: [
+                ArcoResolver({
+                    sideEffect: true
+                })
+            ]
+        })
+    ],
+    build: {
+        target: "es2020",
+        outDir: "../../../target/dist",
+    }
 })
