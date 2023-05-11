56243dc554adaffb4d6eb31dfdf1641821767395
HikariLan <shaokeyibb@126.com>
2023-05-11 23:04:42 +0800

    feat(solon-admin:server): properties loading
    Signed-off-by: HikariLan <shaokeyibb@126.com>

--- _solon_tool/solon-admin-server/src/main/java/org/noear/solon/admin/server/config/AdminServerBootstrapConfiguration.java
+++ _solon_tool/solon-admin-server/src/main/java/org/noear/solon/admin/server/config/AdminServerBootstrapConfiguration.java
@@ -1,9 +1,24 @@
 package org.noear.solon.admin.server.config;
 
+import org.noear.solon.annotation.Bean;
+import org.noear.solon.annotation.Condition;
 import org.noear.solon.annotation.Configuration;
 
+@Condition(onProperty = "${solon.admin.server.enabled} = true")
 @Configuration
 public class AdminServerBootstrapConfiguration {
 
+    @Bean
+    public ServerProperties serverProperties() {
+        return new ServerProperties();
+    }
+
+    @Bean
+    public MarkedServerEnabled markedServerEnabled() {
+        return new MarkedServerEnabled();
+    }
+
+    public static class MarkedServerEnabled {
+    }
 
 }
---
+++ _solon_tool/solon-admin-server/src/main/java/org/noear/solon/admin/server/config/Application.java
@@ -0,0 +1,7 @@
+package org.noear.solon.admin.server.config;
+
+import lombok.Data;
+
+@Data
+public class Application {
+}
