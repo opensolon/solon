44ebc5548d7d2a1fe91afbc569bb917194f01626
HikariLan <shaokeyibb@126.com>
2023-05-11 19:47:34 +0800

    feat(solon-admin:server-ui): empty project dashboard
    Signed-off-by: HikariLan <shaokeyibb@126.com>

--- solon-projects/solon-tool/solon-admin-server-ui/src/main/vue/src/locale/en_us/index.ts
+++ solon-projects/solon-tool/solon-admin-server-ui/src/main/vue/src/locale/en_us/index.ts
@@ -2,5 +2,6 @@ export default {
     "language": "English",
     "header.title": "Solon Admin",
     "header.item.home": "Home",
-    "header.action.locale": "Successfully switch language to {language}"
+    "header.action.locale": "Successfully switch language to {language}",
+    "home.getting-started": "To get started, please launching a Solon Admin client project first.",
 }
--- solon-projects/solon-tool/solon-admin-server-ui/src/main/vue/src/pages/Home.vue
+++ solon-projects/solon-tool/solon-admin-server-ui/src/main/vue/src/pages/Home.vue
@@ -1,11 +1,35 @@
 <script lang="ts" setup>
 
+import {useI18n} from "vue-i18n";
+
+const {t} = useI18n()
+
 </script>
 
 <template>
-
+    <div class="dashboard">
+        <div class="empty">
+            <a-empty :description="t('home.getting-started')">
+                <template #image>
+                    <icon-info-circle-fill/>
+                </template>
+            </a-empty>
+        </div>
+    </div>
 </template>
 
 <style scoped>
+.dashboard {
+    display: flex;
 
+    min-height: 50vh;
+    border-radius: 5px;
+}
+
+.empty {
+    margin: auto;
+}
+
+.empty {
+}
 </style>
