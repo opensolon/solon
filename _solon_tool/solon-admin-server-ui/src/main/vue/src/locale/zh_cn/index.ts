7a456e9cacd315452f61c830b290888f98e3a6df
HikariLan <shaokeyibb@126.com>
2023-05-11 19:47:34 +0800

    feat(solon-admin:server-ui): empty project dashboard
    Signed-off-by: HikariLan <shaokeyibb@126.com>

--- _solon_tool/solon-admin-server-ui/src/main/vue/src/locale/zh_cn/index.ts
+++ _solon_tool/solon-admin-server-ui/src/main/vue/src/locale/zh_cn/index.ts
@@ -2,5 +2,6 @@ export default {
     "language": "简体中文",
     "header.title": "Solon Admin",
     "header.item.home": "主页",
-    "header.action.locale": "成功切换语言至 {language}"
+    "header.action.locale": "成功切换语言至 {language}",
+    "home.getting-started": "要开始，请先启动一个 Solon Admin 客户端。"
 }
