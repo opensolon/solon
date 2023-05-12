ed182ff3d6b46929dffee9c8d3306859a8adb29f
noear <noear@live.cn>
2023-05-12 20:28:17 +0800

    增加 solon.boot.jdkhttp 对 HttpServerConfigure 接口的支持，方便添加端口及ssl的编程控制

--- _solon_boot/solon.boot.jdkhttp/src/main/java/org/noear/solon/boot/jdkhttp/XPluginImp.java
+++ _solon_boot/solon.boot.jdkhttp/src/main/java/org/noear/solon/boot/jdkhttp/XPluginImp.java
@@ -20,7 +20,7 @@ public final class XPluginImp implements Plugin {
         return "jdk http/" + Solon.version();
     }
 
-    JdkHttpServer _server;
+    JdkHttpServerComb _server;
 
     @Override
     public void start(AopContext context) {
@@ -59,18 +59,19 @@ public final class XPluginImp implements Plugin {
 
         long time_start = System.currentTimeMillis();
 
-        final String _wrapHost = props.getWrapHost();
-        final int _wrapPort = props.getWrapPort();
-        _signal = new SignalSim(_name, _wrapHost, _wrapPort, "http", SignalType.HTTP);
 
-        _server = new JdkHttpServer();
+        _server = new JdkHttpServerComb();
         _server.setExecutor(props.getBioExecutor("jdkhttp-"));
         _server.setHandler(Solon.app()::tryHandle);
-        _server.start(_host, _port);
 
         //尝试事件扩展
         EventBus.push(_server);
+        _server.start(_host, _port);
 
+
+        final String _wrapHost = props.getWrapHost();
+        final int _wrapPort = props.getWrapPort();
+        _signal = new SignalSim(_name, _wrapHost, _wrapPort, "http", SignalType.HTTP);
         app.signalAdd(_signal);
 
         long time_end = System.currentTimeMillis();
