a7a27ac6e6e156de1214686e16b3b9b0e558d783
HikariLan <shaokeyibb@126.com>
2023-05-13 15:28:06 +0800

    feat(solon-admin): add enable log
    Signed-off-by: HikariLan <shaokeyibb@126.com>

--- solon-projects/solon-tool/solon-admin-server/src/main/java/org/noear/solon/admin/server/config/AdminServerBootstrapConfiguration.java
+++ solon-projects/solon-tool/solon-admin-server/src/main/java/org/noear/solon/admin/server/config/AdminServerBootstrapConfiguration.java
@@ -1,11 +1,13 @@
 package org.noear.solon.admin.server.config;
 
 import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
 import org.noear.solon.Solon;
 import org.noear.solon.annotation.Bean;
 import org.noear.solon.annotation.Condition;
 import org.noear.solon.annotation.Configuration;
 
+@Slf4j
 @Configuration
 public class AdminServerBootstrapConfiguration {
 
@@ -18,6 +20,12 @@ public class AdminServerBootstrapConfiguration {
     @Value
     public static class MarkedServerEnabled {
         String mode;
+
+        public MarkedServerEnabled(String mode) {
+            this.mode = mode;
+
+            log.info("Solon Admin server has been successfully enabled in {} mode.", this.mode);
+        }
     }
 
 }
