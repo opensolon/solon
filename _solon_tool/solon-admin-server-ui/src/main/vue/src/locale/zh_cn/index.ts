5a92efbf5399ff904ae16df1f6cfc9616dccfdf8
HikariLan <shaokeyibb@126.com>
2023-05-13 22:15:28 +0800

    feat(solon-admin): application service
    Signed-off-by: HikariLan <shaokeyibb@126.com>

--- _solon_tool/solon-admin-server-ui/src/main/vue/src/locale/zh_cn/index.ts
+++ _solon_tool/solon-admin-server-ui/src/main/vue/src/locale/zh_cn/index.ts
@@ -1,7 +1,11 @@
 export default {
     "language": "简体中文",
+    "loading": "加载中，请稍后...",
     "header.title": "Solon Admin",
     "header.item.home": "主页",
     "header.action.locale": "成功切换语言至 {language}",
-    "home.getting-started": "要开始，请先启动一个 Solon Admin 客户端。"
+    "home.getting-started": "要开始，请先启动一个 Solon Admin 客户端。",
+    "home.summary.abnormal": "部分应用状态异常",
+    "home.summary.normal": "一切正常",
+    "home.applications.title": "{count} 个应用程序"
 }
--- _solon_tool/solon-admin-server-ui/src/main/vue/src/pages/Home.vue
+++ _solon_tool/solon-admin-server-ui/src/main/vue/src/pages/Home.vue
@@ -1,49 +1,115 @@
 <script lang="ts" setup>
 
 import {useI18n} from "vue-i18n";
+import {Application, ApplicationStatus} from "../data";
+import {computed, ref} from "vue";
+import {computedAsync} from "@vueuse/core";
 
 const {t} = useI18n()
 
-const bool = true;
+const isLoading = ref(false);
+const applications = computedAsync<Application[]>(async () => {
+    return await fetch("/api/application/all")
+        .then(response => response.json())
+}, null!!, isLoading)
+
+const isEmpty = computed(() => applications.value.length === 0)
+const isAbnormal = computed(() => applications.value.some(app => app.status !== ApplicationStatus.UP))
 
 </script>
 
 <template>
     <div class="dashboard">
-        <div class="empty" v-if="bool">
-            <a-empty :description="t('home.getting-started')">
-                <template #image>
-                    <icon-info-circle-fill/>
-                </template>
-            </a-empty>
+        <div class="summary">
+            <div v-if="isLoading" class="loading">
+                <div>
+                    <a-spin :tip="t('loading')"/>
+                </div>
+            </div>
+            <div v-else-if="isEmpty" class="empty">
+                <a-empty :description="t('home.getting-started')">
+                    <template #image>
+                        <icon-info-circle-fill/>
+                    </template>
+                </a-empty>
+            </div>
+            <div v-else-if="isAbnormal" class="abnormal">
+                <a-empty :description="t('home.summary.abnormal')">
+                    <template #image>
+                        <icon-exclamation-circle-fill style="color: rgb(var(--red-6))"/>
+                    </template>
+                </a-empty>
+            </div>
+            <div v-else class="normal">
+                <a-empty :description="t('home.summary.normal')">
+                    <template #image>
+                        <icon-check-circle-fill style="color: rgb(var(--green-6))"/>
+                    </template>
+                </a-empty>
+            </div>
         </div>
-        <div class="items" v-else>
+        <div v-if="!isEmpty" class="applications">
             <a-list>
                 <template #header>
-                    List title
+                    <span>{{ t('home.applications.title', {count: applications.length}) }}</span>
                 </template>
-                <a-list-item>Beijing Bytedance Technology Co., Ltd.</a-list-item>
-                <a-list-item>Bytedance Technology Co., Ltd.</a-list-item>
-                <a-list-item>Beijing Toutiao Technology Co., Ltd.</a-list-item>
-                <a-list-item>Beijing Volcengine Technology Co., Ltd.</a-list-item>
-                <a-list-item>China Beijing Bytedance Technology Co., Ltd.</a-list-item>
+                <template v-for="app in applications">
+                    <a-list-item>
+                        <a-list-item-meta :title="app.name">
+                            <template #avatar>
+                                <template v-if="app.status===ApplicationStatus.UP">
+                                    <icon-check-circle-fill style="color: rgb(var(--green-6))"/>
+                                </template>
+                                <template v-else>
+                                    <icon-exclamation-circle-fill style="color: rgb(var(--red-6))"/>
+                                </template>
+                            </template>
+                            <template #description>
+                                <a-link :href="app.baseUrl" icon>{{ app.baseUrl }}</a-link>
+                            </template>
+                        </a-list-item-meta>
+                    </a-list-item>
+                </template>
             </a-list>
         </div>
     </div>
+
 </template>
 
 <style scoped>
 .dashboard {
-    display: flex;
-
-    min-height: 50vh;
+    margin: 2.0rem;
 }
 
-.empty {
+.summary {
+    margin-bottom: 1.5rem;
+    padding: 20px;
+
+    border: darkgrey 1px solid;
+    border-radius: 5px;
+}
+
+.loading div {
+    display: inline;
     margin: auto;
 }
 
-.items{
-    width: 100%;
+:deep(.arco-empty-description) {
+    color: BLACK;
+    font-size: 1.5rem;
+    line-height: 2rem;
+    font-weight: 700;
+}
+
+:deep(.arco-empty-image) {
+    font-size: 100px;
+}
+
+:deep(.arco-list-item-meta-avatar) {
+    font-size: 30px;
+}
+
+:deep(.arco-list-item-meta-description) .arco-link {
+    padding: 0;
 }
 </style>
