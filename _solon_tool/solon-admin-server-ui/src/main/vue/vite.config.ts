2bd3be88e492c1ed7230fb16fbce211966b30b37
HikariLan <shaokeyibb@126.com>
2023-05-13 16:16:27 +0800

    fix(solon-admin:server-ui): fix esm-bundler warn for vue-i18n
    Signed-off-by: HikariLan <shaokeyibb@126.com>

--- _solon_tool/solon-admin-server-ui/src/main/vue/vite.config.ts
+++ _solon_tool/solon-admin-server-ui/src/main/vue/vite.config.ts
@@ -7,6 +7,11 @@ import {vitePluginForArco} from '@arco-plugins/vite-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    define: {
+        __VUE_I18N_FULL_INSTALL__: true,
+        __VUE_I18N_LEGACY_API__: false,
+        __INTLIFY_PROD_DEVTOOLS__: false,
+    },
     plugins: [
         vue(),
         vitePluginForArco({
