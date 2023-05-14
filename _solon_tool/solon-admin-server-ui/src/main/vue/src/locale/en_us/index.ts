a8882a9a7897e77db7bdfc508fbd9129f24bd0d8
HikariLan <shaokeyibb@126.com>
2023-05-14 17:51:25 +0800

    feat(solon-admin): unregister application
    Signed-off-by: HikariLan <shaokeyibb@126.com>

--- _solon_tool/solon-admin-server-ui/src/main/vue/src/locale/en_us/index.ts
+++ _solon_tool/solon-admin-server-ui/src/main/vue/src/locale/en_us/index.ts
@@ -7,5 +7,9 @@ export default {
     "home.getting-started": "To get started, please launching a Solon Admin client project first.",
     "home.summary.abnormal": "Some applications down",
     "home.summary.normal": "All up",
-    "home.applications.title": "{count} application(s)"
+    "home.applications.title": "{count} application(s)",
+    "home.applications.actions.remove.tooltip": "Remove application",
+    "home.applications.actions.remove.ask": "Are you sure you want to delete?",
+    "home.applications.actions.remove.fail": "Failed to remove application",
+    "home.applications.actions.remove.success": "Successfully remove application"
 }
--- _solon_tool/solon-admin-server-ui/src/main/vue/src/locale/zh_cn/index.ts
+++ _solon_tool/solon-admin-server-ui/src/main/vue/src/locale/zh_cn/index.ts
@@ -7,5 +7,9 @@ export default {
     "home.getting-started": "要开始，请先启动一个 Solon Admin 客户端。",
     "home.summary.abnormal": "部分应用状态异常",
     "home.summary.normal": "一切正常",
-    "home.applications.title": "{count} 个应用程序"
+    "home.applications.title": "{count} 个应用程序",
+    "home.applications.actions.remove.tooltip": "移除应用程序",
+    "home.applications.actions.remove.ask": "确定要移除该应用程序吗？",
+    "home.applications.actions.remove.fail": "应用移除失败",
+    "home.applications.actions.remove.success": "应用移除成功"
 }
