1ca6428b6c6fdfb9192777bffff2951f635a43af
Sorghum <sorghum2021@163.com>
2023-05-28 09:34:31 +0800

    feat(插件适配): README.md 文档完善，添加测试代码。
    Closes #146

--- solon-projects/solon-plugin-data/shardingsphere-solon-plugin/src/test/java/demo/DemoApp.java
+++ solon-projects/solon-plugin-data/shardingsphere-solon-plugin/src/test/java/demo/DemoApp.java
@@ -2,8 +2,6 @@ package demo;
 
 import org.apache.shardingsphere.driver.jdbc.core.datasource.ShardingSphereDataSource;
 import org.noear.solon.Solon;
-import org.noear.solon.SolonApp;
-import org.noear.solon.SolonBuilder;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -12,7 +10,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 /**
- * @author noear 2021/7/12 created
+ * @author noear 2023/05/28 created
  */
 public class DemoApp {
     public static void main(String[] args) throws SQLException {
@@ -31,6 +29,25 @@ public class DemoApp {
         Connection connectionWrite = shardingSphereDataSource.getConnection();
         connectionWrite.setAutoCommit(false);
         Statement statementWrite = connectionWrite.createStatement();
+        statementWrite.executeUpdate("INSERT INTO e_dict (create_by, create_time, update_by, update_time, code, name, remark, erupt_tenant_id)\n" +
+                "VALUES (null, null, null, null, null, '1', null, null);\n" +
+                "\n");
+        connectionWrite.rollback();
+
+        System.out.println("------------------ Sharding Ds 2 ------------------");
+        shardingSphereDataSource = (ShardingSphereDataSource) Solon.context().getBean("shardingDs2");
+        // 读
+        connectionRead = shardingSphereDataSource.getConnection();
+        statementRead = connectionRead.createStatement();
+        resultSet = statementRead.executeQuery("select 1");
+        System.out.println("execute = " + resultSet);
+        statementRead.close();
+        connectionRead.close();
+        // ---------------------------------------------------------------------------
+        // 写
+        connectionWrite = shardingSphereDataSource.getConnection();
+        connectionWrite.setAutoCommit(false);
+        statementWrite = connectionWrite.createStatement();
         statementWrite.executeUpdate("INSERT INTO e_dict (create_by, create_time, update_by, update_time, code, name, remark, erupt_tenant_id)\n" +
                 "VALUES (null, null, null, null, null, '1', null, null);\n" +
                 "\n");
