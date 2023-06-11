5e3ad5008f6ec5535b3118fbf46610f6df330364
songyinyin <song_yinyin@163.com>
2023-06-11 18:57:30 +0800

    支持 mybatis-plus native

--- solon-projects/solon-plugin-data/mybatis-plus-solon-plugin/src/main/java/com/baomidou/mybatisplus/solon/integration/XPluginImpl.java
+++ solon-projects/solon-plugin-data/mybatis-plus-solon-plugin/src/main/java/com/baomidou/mybatisplus/solon/integration/XPluginImpl.java
@@ -1,7 +1,12 @@
 package com.baomidou.mybatisplus.solon.integration;
 
 import com.baomidou.mybatisplus.core.toolkit.reflect.GenericTypeUtils;
+import com.baomidou.mybatisplus.solon.integration.aot.MybatisPlusRuntimeNativeRegistrar;
+import org.apache.ibatis.solon.aot.MybatisRuntimeNativeRegistrar;
+import org.noear.solon.aot.RuntimeNativeRegistrar;
 import org.noear.solon.core.AopContext;
+import org.noear.solon.core.runtime.NativeDetector;
+import org.noear.solon.core.util.ClassUtil;
 import org.noear.solon.core.util.GenericUtil;
 import org.apache.ibatis.solon.integration.MybatisAdapterManager;
 import org.noear.solon.core.Plugin;
@@ -19,6 +24,11 @@ public class XPluginImpl implements Plugin {
         MybatisAdapterManager.setAdapterFactory(new MybatisAdapterFactoryPlus());
         GenericTypeUtils.setGenericTypeResolver(((clazz, genericIfc) ->
                 GenericUtil.resolveTypeArguments(clazz, genericIfc)));
+
+        // aot
+        if (NativeDetector.isAotRuntime() && ClassUtil.hasClass(() -> RuntimeNativeRegistrar.class)) {
+            context.wrapAndPut(MybatisPlusRuntimeNativeRegistrar.class);
+        }
 
     }
 }
