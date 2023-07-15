bc6b7f790952c850a149f4e0eaae9b1352f796b5
noear <noear@live.cn>
2023-07-15 10:07:45 +0800

    添加个研究示例

---
+++ __test/src/test/java/labs/ExeTest.java
@@ -0,0 +1,26 @@
+package labs;
+
+import org.noear.solon.Solon;
+import org.noear.solon.core.handle.ActionExecuteHandlerDefault;
+import org.noear.solon.core.handle.Context;
+import org.noear.solon.core.wrap.ParamWrap;
+
+/**
+ * @author noear 2023/7/15 created
+ */
+public class ExeTest {
+    public static void main(String[] args) {
+        Solon.start(ExeTest.class, args, app -> {
+            app.chainManager().defExecuteHandler(new ActionExecuteHandlerDefault() {
+                @Override
+                protected Object changeValueDo(Context ctx, ParamWrap p, String name, Class<?> type, String value) {
+                    if(type.isEnum()){
+
+                    }
+
+                    return super.changeValueDo(ctx, p, name, type, value);
+                }
+            });
+        });
+    }
+}
