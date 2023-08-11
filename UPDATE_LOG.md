0228257e747ced74cccd4984084752d447231733
noear <noear@live.cn>
2023-08-11 13:12:30 +0800

    增加 sa-token 插件，对 SaTokenDaoOfRedisson 实现

--- UPDATE_LOG.md
+++ UPDATE_LOG.md
@@ -28,6 +28,7 @@
 * 调整 solon-admin-server 界面细节
 * 修复 solon.docs.openapi2 插件 ApiImplicitParam::paramType 未生效的问题
 * 修复 solon.docs.openapi2 插件 ApiParam::hidden 未生效的问题
+* sa-token 升为 1.35.0.RC
 
 ### 2.4.2
 * 新增 lettuce-solon-plugin 插件
---
+++ solon-projects-other/solon-plugin-auth/sa-token-solon-plugin/src/main/java/cn/dev33/satoken/solon/error/SaSolonErrorCode.java
@@ -0,0 +1,17 @@
+package cn.dev33.satoken.solon.error;
+
+/**
+ * 定义 sa-token-solon-plugin 所有异常细分状态码 
+ * 
+ * @author click33
+ * @since 2022-10-30
+ */
+public interface SaSolonErrorCode {
+
+	/** 默认的拦截器异常处理函数 */
+	int CODE_20301 = 20301;
+
+	/** 默认的 Filter 异常处理函数 */
+	int CODE_20302 = 20302;
+
+}
