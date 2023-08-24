cda92b99cde0b0d294b11d2afb45b20e24ef3cff
noear <noear@live.cn>
2023-08-24 21:59:57 +0800

    优化分片下载测试demo

--- __test/src/main/java/webapp/demo2_mvc/DownController.java
+++ __test/src/main/java/webapp/demo2_mvc/DownController.java
@@ -1,7 +1,9 @@
 package webapp.demo2_mvc;
 
+import lombok.extern.slf4j.Slf4j;
 import org.noear.solon.annotation.Controller;
 import org.noear.solon.annotation.Mapping;
+import org.noear.solon.core.handle.Context;
 import org.noear.solon.core.handle.DownloadedFile;
 
 import java.io.File;
@@ -10,11 +12,13 @@ import java.io.IOException;
 /**
  * @author noear 2023/8/23 created
  */
+@Slf4j
 @Controller
 public class DownController {
     @Mapping("/demo2/range1/")
-    public DownloadedFile range1() throws IOException{
-        return new DownloadedFile(new File("/Users/noear/Movies/range_test2.mp4"));
+    public DownloadedFile range1(Context ctx) throws IOException {
+        log.debug(ctx.method() + ":: " + ctx.header("Range"));
+        return new DownloadedFile(new File("/Users/noear/Movies/range_test.mov"));
     }
 
     @Mapping("/demo2/range2/")
