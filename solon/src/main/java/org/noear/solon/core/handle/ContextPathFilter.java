df531833ebb414efb9f6688a8faec04805bd5323
noear <noear@live.cn>
2023-09-11 17:56:19 +0800

    添加自动配置的构造

--- solon/src/main/java/org/noear/solon/core/handle/ContextPathFilter.java
+++ solon/src/main/java/org/noear/solon/core/handle/ContextPathFilter.java
@@ -1,6 +1,7 @@
 package org.noear.solon.core.handle;
 
 import org.noear.solon.Solon;
+import org.noear.solon.Utils;
 
 /**
  * 提供 ContextPath 类似的功能（优先级要极高）
@@ -13,28 +14,38 @@ public class ContextPathFilter implements Filter {
     private final String contextPath1;
     private final boolean forced;
 
+    public ContextPathFilter(boolean forced) {
+        this(Solon.cfg().serverContextPath(), forced);
+    }
+
     /**
      * @param contextPath '/demo/'
      */
     public ContextPathFilter(String contextPath, boolean forced) {
-        String newPath = null;
-        if (contextPath.endsWith("/")) {
-            newPath = contextPath;
-        } else {
-            newPath = contextPath + "/";
-        }
-
-        if (newPath.startsWith("/")) {
-            this.contextPath1 = newPath;
-        } else {
-            this.contextPath1 = "/" + newPath;
-        }
-
-        this.contextPath0 = contextPath1.substring(0, contextPath1.length() - 1);
         this.forced = forced;
 
-        //有可能是 ContextPathFilter 是用户手动添加的！需要补一下配置
-        Solon.cfg().serverContextPath(this.contextPath1);
+        if (Utils.isEmpty(contextPath)) {
+            contextPath0 = null;
+            contextPath1 = null;
+        } else {
+            String newPath = null;
+            if (contextPath.endsWith("/")) {
+                newPath = contextPath;
+            } else {
+                newPath = contextPath + "/";
+            }
+
+            if (newPath.startsWith("/")) {
+                this.contextPath1 = newPath;
+            } else {
+                this.contextPath1 = "/" + newPath;
+            }
+
+            this.contextPath0 = contextPath1.substring(0, contextPath1.length() - 1);
+
+            //有可能是 ContextPathFilter 是用户手动添加的！需要补一下配置
+            Solon.cfg().serverContextPath(this.contextPath1);
+        }
     }
 
     public ContextPathFilter(String contextPath) {
@@ -44,14 +55,16 @@ public class ContextPathFilter implements Filter {
 
     @Override
     public void doFilter(Context ctx, FilterChain chain) throws Throwable {
-        if (ctx.pathNew().equals(contextPath0)) {
-            //www:888 加 abc 后，仍可以用 www:888/abc 打开
-            ctx.pathNew("/");
-        } else if (ctx.pathNew().startsWith(contextPath1)) {
-            ctx.pathNew(ctx.pathNew().substring(contextPath1.length() - 1));
-        } else {
-            if (forced) {
-                return;
+        if (contextPath0 != null) {
+            if (ctx.pathNew().equals(contextPath0)) {
+                //www:888 加 abc 后，仍可以用 www:888/abc 打开
+                ctx.pathNew("/");
+            } else if (ctx.pathNew().startsWith(contextPath1)) {
+                ctx.pathNew(ctx.pathNew().substring(contextPath1.length() - 1));
+            } else {
+                if (forced) {
+                    return;
+                }
             }
         }
 
