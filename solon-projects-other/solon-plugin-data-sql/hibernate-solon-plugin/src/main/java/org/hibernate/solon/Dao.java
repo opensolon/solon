61d6a646c9d98c0f5c026803d80e241b55050f4e
noear <noear@live.cn>
2023-09-17 23:46:51 +0800

    调整 hibernate-solon-plugin 代码

--- solon-projects-other/solon-plugin-data-sql/hibernate-solon-plugin/src/main/java/org/hibernate/solon/Dao.java
+++
@@ -1,9 +0,0 @@
-package org.hibernate.solon;
-
-import java.io.Serializable;
-
-public interface Dao<E> {
-    void saveOrUpdate(E entity);
-
-    void deleteById(Serializable id);
-}
--- solon-projects-other/solon-plugin-data-sql/hibernate-solon-plugin/src/main/java/org/hibernate/solon/integration/HibernatePluginImpl.java
+++ solon-projects-other/solon-plugin-data-sql/hibernate-solon-plugin/src/main/java/org/hibernate/solon/integration/HibernatePluginImpl.java
@@ -1,5 +1,6 @@
 package org.hibernate.solon.integration;
 
+import org.hibernate.solon.BaseMapperDefinition;
 import org.hibernate.solon.annotation.Db;
 import org.noear.solon.core.AppContext;
 import org.noear.solon.core.Plugin;
@@ -11,10 +12,18 @@ import javax.sql.DataSource;
  * @since 2.5
  */
 public class HibernatePluginImpl implements Plugin {
+    DbBeanBuilder dbBeanBuilder = new DbBeanBuilder();
+
+
+
     @Override
     public void start(AppContext context) throws Throwable {
         context.subWrapsOfType(DataSource.class, bw -> {
             HibernateAdapterManager.register(bw);
+        });
+
+        context.subBeansOfType(BaseMapperDefinition.class, b -> {
+            dbBeanBuilder.register(b);
         });
 
         context.beanInjectorAdd(Db.class, new DbBeanInjector());
