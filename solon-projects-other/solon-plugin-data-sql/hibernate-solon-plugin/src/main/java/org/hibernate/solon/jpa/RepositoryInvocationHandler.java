66b6a0718ab0990ce48d0365ded9fa83ad686e1c
noear <noear@live.cn>
2023-09-18 19:20:04 +0800

    优化 RepositoryInvocationHandler

--- solon-projects-other/solon-plugin-data-sql/hibernate-solon-plugin/src/main/java/org/hibernate/solon/jpa/RepositoryInvocationHandler.java
+++ solon-projects-other/solon-plugin-data-sql/hibernate-solon-plugin/src/main/java/org/hibernate/solon/jpa/RepositoryInvocationHandler.java
@@ -13,10 +13,12 @@ import java.lang.reflect.Method;
 public class RepositoryInvocationHandler implements InvocationHandler {
     private final SessionFactory sessionFactory;
     private final Class<?> repositoryInterface;
+    private final JapRepositoryProxyImpl repositoryProxy;
 
     public RepositoryInvocationHandler(SessionFactory sessionFactory, Class<?> repositoryInterface) {
         this.sessionFactory = sessionFactory;
         this.repositoryInterface = repositoryInterface;
+        this.repositoryProxy = new JapRepositoryProxyImpl(sessionFactory, repositoryInterface);
     }
 
     @Override
@@ -25,8 +27,7 @@ public class RepositoryInvocationHandler implements InvocationHandler {
                 || PagingAndSortingRepository.class == method.getDeclaringClass()
                 || JpaRepository.class == method.getDeclaringClass()) {
             //归属 JpaRepository 范围的
-            JapRepositoryProxyImpl repository = new JapRepositoryProxyImpl(sessionFactory, repositoryInterface);
-            return method.invoke(repository, args);
+            return method.invoke(repositoryProxy, args);
         } else {
             //无归属的 method 处理
         }
