ba208e1d23bc53a8b9dc2059f0baff2b0303fd6e
noear <noear@live.cn>
2023-09-20 00:30:18 +0800

    增加匹配程度接口

--- solon/src/main/java/org/noear/solon/core/route/Routing.java
+++ solon/src/main/java/org/noear/solon/core/route/Routing.java
@@ -9,10 +9,35 @@ import org.noear.solon.core.handle.MethodType;
  * @since 1.3
  */
 public interface Routing<T> {
+    /**
+     * 顺序位
+     */
     int index();
+
+    /**
+     * 路径
+     */
     String path();
+
+    /**
+     * 方法
+     */
     MethodType method();
+
+    /**
+     * 路由目标
+     */
     T target();
 
+    /**
+     * 是否匹配
+     */
     boolean matches(MethodType method2, String path2);
+
+    /**
+     * 匹配程度（0,不匹配；1,匹配路径；2,完全匹配）
+     *
+     * @since 2.5
+     */
+    int degrees(MethodType method2, String path2);
 }
