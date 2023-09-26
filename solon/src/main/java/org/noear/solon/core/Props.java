3bfaf65e7e6a4fccdd93cd0743ee8a777f81b6ef
noear <noear@live.cn>
2023-09-26 09:12:09 +0800

    简化 Props::getProp 处理提升性能

--- solon/src/main/java/org/noear/solon/core/Props.java
+++ solon/src/main/java/org/noear/solon/core/Props.java
@@ -198,15 +198,8 @@ public class Props extends Properties {
             doFind(keyStarts, (key, val) -> { //相对旧版，减少一次 forEach
                 if (key.startsWith(".")) {
                     key = key.substring(1); //去掉 .
-                    prop.put(key, val);
-
-                    if (key.contains("-")) {//为带 - 的 key ，增加副本
-                        String camelKey = buildCamelKey(key);
-                        prop.put(camelKey, val);
-                    }
-                } else if (key.startsWith("[")) {
-                    prop.put(key, val);
                 }
+                prop.put(key, val);
             });
 
             return prop;
@@ -269,7 +262,12 @@ public class Props extends Properties {
      */
     public Map<String,String> getMap(String keyStarts) {
         Map<String, String> map = new LinkedHashMap<>();
-        doFind(keyStarts, map::put);
+        doFind(keyStarts, (key, val) -> { //相对旧版，减少一次 forEach
+            if (key.startsWith(".")) {
+                key = key.substring(1); //去掉 .
+            }
+            map.put(key, val);
+        });
         return map;
     }
 
