cd1217fae23f074683340f5558644a32d57eb827
noear <noear@live.cn>
2023-10-11 15:44:11 +0800

    Http,Socket,WebSocket 注解保持 ElementType.TYPE 支持

--- solon/src/main/java/org/noear/solon/annotation/Socket.java
+++ solon/src/main/java/org/noear/solon/annotation/Socket.java
@@ -8,7 +8,7 @@ import java.lang.annotation.*;
  * @author noear
  * @since 1.3
  */
-@Target({ElementType.METHOD})
+@Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface Socket {
