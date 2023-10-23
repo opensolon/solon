db8e244e9ac82d179a1776ca09385ad3319f1d23
noear <noear@live.cn>
2023-10-23 19:17:11 +0800

    solon-admin 修复硬盘容量统计错误的问题

--- solon-projects/solon-detector/solon.health.detector/src/main/java/org/noear/solon/health/detector/impl/DiskDetector.java
+++ solon-projects/solon-detector/solon.health.detector/src/main/java/org/noear/solon/health/detector/impl/DiskDetector.java
@@ -22,7 +22,11 @@ import java.util.regex.Pattern;
 public class DiskDetector extends AbstractDetector {
     static final Logger log = LoggerFactory.getLogger(DiskDetector.class);
 
+    //Linux 查看硬盘数据命令,备用
+    // df -m
+    // lsblk -d -b -l -n -o SIZE,TYPE
     private static final Pattern linuxDiskPattern = Pattern.compile("\\s+([\\d]+)\\s+([\\d]+)\\s+([\\d]+)\\s+([\\d]+%)\\s+(/.*)", 42);
+    private static final Pattern linuxTotalDistPattern = Pattern.compile("\\s+([\\d]+)\\s+(\\s+)",42);
     private static final Pattern macDiskPattern = Pattern.compile("\\s+([\\d]+)\\s+([\\d]+)\\s+([\\d]+)\\s+([\\d]+%)\\s+\\d+\\s+\\d+\\s+\\d+%\\s+(/.*)", 42);
 
     @Override
@@ -37,53 +41,28 @@ public class DiskDetector extends AbstractDetector {
 
         long totals = 0;
         long totalUsed = 0;
-        if (osName.indexOf("windows") > -1) {//is windows
-            for (File file : File.listRoots()) {
-                Map<String, Object> disk = new HashMap<>();
+        boolean isMac=osName.indexOf("mac os") > -1;
+        File[] rootFiles= File.listRoots();
 
-                long total = file.getTotalSpace();
-                long free = file.getFreeSpace();
-                long used = total - free;
-                totals += total;
-                totalUsed += used;
-                String ratio = (total > 0L ? used * 100L / total : 0L) + "%";
-
-                disk.put("total", SizeUtil.formatByteSize(total));
-                disk.put("free", SizeUtil.formatByteSize(free));
-                disk.put("used", SizeUtil.formatByteSize(used));
-                disk.put("ratio", ratio);
-
-                details.put(file.getPath(), disk);
-            }
-        } else {
-            try {
-                String text = CmdUtil.execute("df", "-m");
-                boolean isMac = osName.indexOf("mac os") > -1;
-                List<String[]> disks = this.matcher(isMac ? macDiskPattern : linuxDiskPattern, text);
-
-                for (String[] disk : disks) {
-                    Map<String, Object> diskInfo = new HashMap<>();
-                    long total = Long.valueOf(disk[1]) * 1024L * 1024L;
-                    long used = Long.valueOf(disk[2]) * 1024L * 1024L;
-                    long free = Long.valueOf(disk[3]) * 1024L * 1024L;
-                    String ratio = disk[4];
-
-                    diskInfo.put("total", SizeUtil.formatByteSize(total));
-                    diskInfo.put("free", SizeUtil.formatByteSize(free));
-                    diskInfo.put("used", SizeUtil.formatByteSize(used));
-                    diskInfo.put("ratio", ratio);
-                    details.put(disk[5], diskInfo);
-
-                    totals += total;
-                    totalUsed += used;
-                }
-            } catch (Exception e) {
-                log.warn(e.getMessage(), e);
-                details.put("error", "Get Disk Failed:" + e.getMessage());
-            }
-
+        if (isMac) {
+            rootFiles=new File("/Volumes").listFiles();
         }
+        for (File file : rootFiles) {
+            Map<String, Object> disk = new HashMap<>();
+            long total = file.getTotalSpace();
+            long free = file.getFreeSpace();
+            long used = total - free;
+            totals += total;
+            totalUsed += used;
+            String ratio = (total > 0L ? used * 100L / total : 0L) + "%";
 
+            disk.put("total", SizeUtil.formatByteSize(total));
+            disk.put("free", SizeUtil.formatByteSize(free));
+            disk.put("used", SizeUtil.formatByteSize(used));
+            disk.put("ratio", ratio);
+            String path=isMac?file.getPath().replace("/Volumes/",""):file.getPath();
+            details.put(path, disk);
+        }
         Map<String, Object> info = new LinkedHashMap<>();
 
         info.put("total", SizeUtil.formatByteSize(totals));
