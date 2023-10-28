a82760e8b10b50bce057d5b71b2d7768d86a870c
noear <noear@live.cn>
2023-10-28 15:45:59 +0800

    mqtt-solon-cloud-plugin 实现方式重构

--- solon-projects-cloud/solon-cloud-event/mqtt-solon-cloud-plugin/src/main/java/org/noear/solon/cloud/extend/mqtt/service/MqttClientManager.java
+++ solon-projects-cloud/solon-cloud-event/mqtt-solon-cloud-plugin/src/main/java/org/noear/solon/cloud/extend/mqtt/service/MqttClientManager.java
@@ -3,14 +3,25 @@ package org.noear.solon.cloud.extend.mqtt.service;
 import org.eclipse.paho.client.mqttv3.IMqttAsyncClient;
 
 /**
+ * Mqtt 客户端管理器
+ *
  * @author noear
  * @since 2.0
  */
 public interface MqttClientManager {
+    /**
+     * 获取客户端
+     */
     IMqttAsyncClient getClient();
 
+    /**
+     * 添加连接回调
+     */
     void addCallback(ConnectCallback connectCallback);
 
+    /**
+     * 移除连接回调
+     */
     boolean removeCallback(ConnectCallback connectCallback);
 
     @FunctionalInterface
