0b3f05bbd258bba56242f0309ba919fcd05c4a83
noear <noear@live.cn>
2023-11-11 22:00:18 +0800

    添加 solon.boot.socketd 模块

---
+++ solon-projects/solon-boot/solon.boot.socketd/src/main/java/org/noear/solon/boot/socketd/XPluginImpl.java
@@ -0,0 +1,15 @@
+package org.noear.solon.boot.socketd;
+
+import org.noear.solon.core.AppContext;
+import org.noear.solon.core.Plugin;
+
+/**
+ * @author noear
+ * @since 2.6
+ */
+public class XPluginImpl implements Plugin {
+    @Override
+    public void start(AppContext context) throws Throwable {
+
+    }
+}
