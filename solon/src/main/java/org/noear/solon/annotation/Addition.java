f0e8279bfe38c10d18b385b284b5793286aef479
noear <noear@live.cn>
2024-01-28 12:54:13 +0800

    添加 `@Addition` 注解，调整 Mvc 注解处理逻辑

--- solon/src/main/java/org/noear/solon/annotation/Addition.java
+++ solon/src/main/java/org/noear/solon/annotation/Addition.java
@@ -3,13 +3,14 @@ package org.noear.solon.annotation;
 import java.lang.annotation.*;
 
 /**
- * 做为 @Mapping 的副词用
+ * 间接附加注解
  *
  * @author noear
- * @since 1.3
+ * @since 2.6
  */
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-public @interface Patch {
+public @interface Addition {
+    Class<? extends Annotation>[] value();
 }
--- solon/src/main/java/org/noear/solon/core/handle/HandlerLoader.java
+++ solon/src/main/java/org/noear/solon/core/handle/HandlerLoader.java
@@ -1,10 +1,7 @@
 package org.noear.solon.core.handle;
 
 import org.noear.solon.Utils;
-import org.noear.solon.annotation.After;
-import org.noear.solon.annotation.Before;
-import org.noear.solon.annotation.Mapping;
-import org.noear.solon.annotation.Options;
+import org.noear.solon.annotation.*;
 import org.noear.solon.core.BeanWrap;
 import org.noear.solon.core.util.ConsumerEx;
 import org.noear.solon.core.util.LogUtil;
@@ -133,20 +130,30 @@ public class HandlerLoader extends HandlerAide {
             bPath = "";
         }
 
-        Set<MethodType> b_method = new HashSet<>();
+        Set<MethodType> b_limitMethodSet = new HashSet<>();
+        Set<MethodType> b_addinMethodSet = new HashSet<>();
 
-        loadControllerAide(b_method);
+        MethodTypeUtil.findAndFill(b_limitMethodSet, t -> bw.clz().getAnnotation(t) != null);
+        loadControllerAide(b_addinMethodSet);
+        if (b_limitMethodSet.size() == 0 && bMapping != null) {
+            //如果没有独立注解，尝试获取 Mapping 上的方式
+            for (MethodType b_mt : bMapping.method()) {
+                if (b_mt != MethodType.ALL) {
+                    b_limitMethodSet.add(b_mt);
+                }
+            }
+        }
 
         //只支持 public 函数为 Action
         for (Method method : findMethods(bw.clz())) {
-            loadActionItem(slots, all, method, b_method);
+            loadActionItem(slots, all, method, b_limitMethodSet, b_addinMethodSet);
         }
     }
 
     /**
      * 加载 Action item 处理
      */
-    protected void loadActionItem(HandlerSlots slots, boolean all, Method method, Set<MethodType> b_method) {
+    protected void loadActionItem(HandlerSlots slots, boolean all, Method method, Set<MethodType> b_limitMethodSet, Set<MethodType> b_addinMethodSet) {
         Mapping m_map = method.getAnnotation(Mapping.class);
 
         //检测注解和限制
@@ -163,33 +170,29 @@ public class HandlerLoader extends HandlerAide {
         }
 
         String m_path;
-        Set<MethodType> m_method = new HashSet<>();
+        Set<MethodType> m_limitMethodSet = new HashSet<>(b_limitMethodSet);
+        Set<MethodType> m_addinMethodSet = new HashSet<>(b_addinMethodSet);
 
         //获取 action 的 methodTypes
-        MethodTypeUtil.findAndFill(m_method, t -> method.getAnnotation(t) != null);
+        MethodTypeUtil.findAndFill(m_limitMethodSet, t -> method.getAnnotation(t) != null);
 
-        //构建path and method
+        //构建 path and method
         if (m_map != null) {
             m_path = Utils.annoAlias(m_map.value(), m_map.path());
 
-            if (m_method.size() == 0) {
+            if (m_limitMethodSet.size() == 0) {
                 //如果没有找到，则用Mapping上自带的
-                m_method.addAll(Arrays.asList(m_map.method()));
+                m_limitMethodSet.addAll(Arrays.asList(m_map.method()));
             }
         } else {
             m_path = method.getName();
 
-            if (m_method.size() == 0) {
-                //获取 controller 的 methodTypes
-                MethodTypeUtil.findAndFill(m_method, t -> bw.clz().getAnnotation(t) != null);
-            }
-
-            if (m_method.size() == 0) {
+            if (m_limitMethodSet.size() == 0) {
                 //如果没有找到，则用Mapping上自带的；或默认
                 if (bMapping == null) {
-                    m_method.add(MethodType.HTTP);
+                    m_limitMethodSet.add(MethodType.HTTP);
                 } else {
-                    m_method.addAll(Arrays.asList(bMapping.method()));
+                    m_limitMethodSet.addAll(Arrays.asList(bMapping.method()));
                 }
             }
         }
@@ -201,15 +204,16 @@ public class HandlerLoader extends HandlerAide {
             Action action = createAction(bw, method, m_map, newPath, bRemoting);
 
             //m_method 必须之前已准备好，不再动  //用于支持 Cors
-            loadActionAide(method, action, m_method);
-            if (b_method.size() > 0 &&
-                    m_method.contains(MethodType.HTTP) == false &&
-                    m_method.contains(MethodType.ALL) == false) {
+            loadActionAide(method, action, m_addinMethodSet);
+
+            if (m_limitMethodSet.size() > 0 &&
+                    m_limitMethodSet.contains(MethodType.HTTP) == false &&
+                    m_limitMethodSet.contains(MethodType.ALL) == false) {
                 //用于支持 Cors
-                m_method.addAll(b_method);
+                m_limitMethodSet.addAll(m_addinMethodSet);
             }
 
-            for (MethodType m1 : m_method) {
+            for (MethodType m1 : m_limitMethodSet) {
                 if (m_map == null) {
                     slots.add(newPath, m1, action);
                 } else {
@@ -223,7 +227,7 @@ public class HandlerLoader extends HandlerAide {
     /**
      * 加载控制器助理（Before、After）
      */
-    protected void loadControllerAide(Set<MethodType> methodSet) {
+    protected void loadControllerAide(Set<MethodType> addinMethodSet) {
         for (Annotation anno : bw.clz().getAnnotations()) {
             if (anno instanceof Before) {
                 addDo(((Before) anno).value(), (b) -> this.before(bw.context().getBeanOrNew(b)));
@@ -235,9 +239,15 @@ public class HandlerLoader extends HandlerAide {
                         addDo(((Before) anno2).value(), (b) -> this.before(bw.context().getBeanOrNew(b)));
                     } else if (anno2 instanceof After) {
                         addDo(((After) anno2).value(), (f) -> this.after(bw.context().getBeanOrNew(f)));
-                    } else if (anno2 instanceof Options) {
+                    } else if (anno2 instanceof Addition) {
                         //用于支持 Cors
-                        methodSet.add(MethodType.OPTIONS);
+                        Addition additionAnno = (Addition) anno2;
+                        for (Class<?> annoClz : additionAnno.value()) {
+                            MethodType methodType = MethodTypeUtil.valueOf(annoClz.getSimpleName().toUpperCase());
+                            if (methodType != MethodType.UNKNOWN) {
+                                addinMethodSet.add(methodType);
+                            }
+                        }
                     }
                 }
             }
@@ -247,7 +257,7 @@ public class HandlerLoader extends HandlerAide {
     /**
      * 加载动作助理（Before、After）
      */
-    protected void loadActionAide(Method method, Action action, Set<MethodType> methodSet) {
+    protected void loadActionAide(Method method, Action action, Set<MethodType> addinMethodSet) {
         for (Annotation anno : method.getAnnotations()) {
             if (anno instanceof Before) {
                 addDo(((Before) anno).value(), (b) -> action.before(bw.context().getBeanOrNew(b)));
@@ -259,11 +269,14 @@ public class HandlerLoader extends HandlerAide {
                         addDo(((Before) anno2).value(), (b) -> action.before(bw.context().getBeanOrNew(b)));
                     } else if (anno2 instanceof After) {
                         addDo(((After) anno2).value(), (f) -> action.after(bw.context().getBeanOrNew(f)));
-                    } else if (anno2 instanceof Options) {
+                    } else if (anno2 instanceof Addition) {
                         //用于支持 Cors
-                        if (methodSet.contains(MethodType.HTTP) == false &&
-                                methodSet.contains(MethodType.ALL) == false) {
-                            methodSet.add(MethodType.OPTIONS);
+                        Addition additionAnno = (Addition) anno2;
+                        for (Class<?> annoClz : additionAnno.value()) {
+                            MethodType methodType = MethodTypeUtil.valueOf(annoClz.getSimpleName().toUpperCase());
+                            if (methodType != MethodType.UNKNOWN) {
+                                addinMethodSet.add(methodType);
+                            }
                         }
                     }
                 }
