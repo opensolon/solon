0064a4e70ebfd1bd025f6bab4db84e3a4a8436e3
sppan <sppan@tencent.com>
2024-03-06 10:00:17 +0800

    optimize: 多级嵌套代码不优雅

--- solon/src/main/java/org/noear/solon/core/mvc/ActionParamResolver.java
+++ solon/src/main/java/org/noear/solon/core/mvc/ActionParamResolver.java
@@ -1,11 +1,15 @@
 package org.noear.solon.core.mvc;
 
+import java.lang.reflect.AnnotatedElement;
 import org.noear.solon.Utils;
-import org.noear.solon.annotation.*;
+import org.noear.solon.annotation.Body;
+import org.noear.solon.annotation.Cookie;
+import org.noear.solon.annotation.Header;
+import org.noear.solon.annotation.Param;
+import org.noear.solon.annotation.Path;
+import org.noear.solon.annotation.PathVar;
 import org.noear.solon.core.Constants;
 import org.noear.solon.core.handle.ActionParam;
-
-import java.lang.reflect.AnnotatedElement;
 
 /**
  * Action 参数分析器
@@ -14,21 +18,33 @@ import java.lang.reflect.AnnotatedElement;
  * @since 2.7
  */
 public class ActionParamResolver {
+
     /**
      * 分析
-     * */
+     */
     public static void resolve(ActionParam vo, AnnotatedElement element) {
-        if (resolveBody(vo, element) == false) {
-            if (resolveParam(vo, element) == false) {
-                if (resolvePathVar(vo, element) == false) {
-                    if (resolvePath(vo, element) == false) {
-                        if (resolveHeader(vo, element) == false) {
-                            resolveCookie(vo, element);
-                        }
-                    }
-                }
-            }
+        // 分析 Body 注解
+        if (resolveBody(vo, element)) {
+            return;
         }
+        // 分析 Param 注解
+        if (resolveParam(vo, element)) {
+            return;
+        }
+        // 分析 PathVar 注解
+        if (resolvePathVar(vo, element)) {
+            return;
+        }
+        // 分析 Path 注解
+        if (resolvePath(vo, element)) {
+            return;
+        }
+        // 分析 Header 注解
+        if (resolveHeader(vo, element)) {
+            return;
+        }
+        // 分析 Cookie 注解
+        resolveCookie(vo, element);
     }
 
     /**
