b3923b7901f83a596f86828b6f43310cb8ea690c
CY-1 <516183795@qq.com>
2024-03-14 01:20:39 +0800

    调用有返回值的异步方法时打印日志

--- solon-projects/solon-scheduling/solon.scheduling/src/main/java/org/noear/solon/scheduling/async/AsyncExecutorDefault.java
+++ solon-projects/solon-scheduling/solon.scheduling/src/main/java/org/noear/solon/scheduling/async/AsyncExecutorDefault.java
@@ -3,6 +3,8 @@ package org.noear.solon.scheduling.async;
 import org.noear.solon.core.aspect.Invocation;
 import org.noear.solon.core.util.RunUtil;
 import org.noear.solon.scheduling.annotation.Async;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.Future;
 
@@ -13,9 +15,15 @@ import java.util.concurrent.Future;
  * @since 2.4
  */
 public class AsyncExecutorDefault implements AsyncExecutor {
+
+    private static final Logger logger = LoggerFactory.getLogger(AsyncExecutorDefault.class);
     @Override
     public Future submit(Invocation inv, Async anno) throws Throwable{
-        if (inv.method().getReturnType().isAssignableFrom(Future.class)) {
+        Class<?> returnType = inv.method().getReturnType();
+        if(!"void".equals(returnType.getTypeName())){
+            logger.warn("调用了带有返回值的异步方法");
+        }
+        if (returnType.isAssignableFrom(Future.class)) {
             return (Future) inv.invoke();
         } else {
             return RunUtil.async(() -> {
