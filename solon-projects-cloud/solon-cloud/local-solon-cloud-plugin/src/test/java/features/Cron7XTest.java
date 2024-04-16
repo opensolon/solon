488bb39742983e961b23071047ba2644cb99b864
noear <noear@live.cn>
2024-04-16 22:28:06 +0800

    添加两个单测

---
+++ solon-projects-cloud/solon-cloud/local-solon-cloud-plugin/src/test/java/features/Cron7XTest.java
@@ -0,0 +1,23 @@
+package features;
+
+import org.junit.jupiter.api.Test;
+import org.noear.solon.cloud.extend.local.impl.job.Cron7X;
+
+import java.time.ZoneOffset;
+
+/**
+ * @author noear 2024/4/16 created
+ */
+public class Cron7XTest {
+    @Test
+    public void cron7x1() {
+        Cron7X cron7x = Cron7X.parse("12ms");
+        assert cron7x.getInterval() == 12L;
+    }
+
+    @Test
+    public void cron7x2() {
+        Cron7X cron7x = Cron7X.parse("* * * * * ?+08");
+        assert cron7x.getZone() == ZoneOffset.ofHours(8);
+    }
+}
