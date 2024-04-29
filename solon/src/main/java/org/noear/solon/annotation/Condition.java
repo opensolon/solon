79a79ee6cd290a1c56b572eca6d55ee86599dbd5
noear <noear@live.cn>
2024-04-29 16:54:42 +0800

    调整 Condition::onMissingBean 标为弃用

--- solon/src/main/java/org/noear/solon/annotation/Condition.java
+++ solon/src/main/java/org/noear/solon/annotation/Condition.java
@@ -27,11 +27,17 @@ public @interface Condition {
 
     /**
      * 缺少 bean type
+     *
+     * @deprecated 2.7
      * */
+    @Deprecated
     Class<?> onMissingBean() default Void.class;
 
     /**
      * 缺少 bean name
+     *
+     * @deprecated 2.7
      * */
+    @Deprecated
     String onMissingBeanName() default "";
 }
