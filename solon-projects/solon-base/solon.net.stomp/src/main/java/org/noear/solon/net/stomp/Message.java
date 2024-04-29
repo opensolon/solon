acb58abed43a0c63a06c60807e26d9808b61a3a5
limliu <261010267@qq.com>
2024-04-29 11:58:58 +0800

    补充注释

--- solon-projects/solon-base/solon.net.stomp/src/main/java/org/noear/solon/net/stomp/Message.java
+++ solon-projects/solon-base/solon.net.stomp/src/main/java/org/noear/solon/net/stomp/Message.java
@@ -11,14 +11,43 @@ import java.util.List;
  */
 public interface Message {
 
+    /**
+     * 添加head
+     *
+     * @param key 键, key参考#Header
+     * @param val 值
+     * @return
+     */
     Message addHeader(String key, String val);
 
+    /**
+     * 获取head集合
+     *
+     * @return
+     */
     List<Header> getHeaders();
 
+    /**
+     * 基于指定key获取head中对应的值
+     *
+     * @param key 参考#Header
+     * @return
+     */
     String header(String key);
 
+    /**
+     * 获取消息内容
+     *
+     * @return
+     */
     String getPayload();
 
+    /**
+     * 获取命令, 如send...等
+     * 参考#Commands
+     *
+     * @return
+     */
     String getCommand();
 
 }
