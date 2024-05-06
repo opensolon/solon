b694ae29251c707b3b9c6954b3607fd5938e04ec
noear <noear@live.cn>
2024-05-06 21:24:35 +0800

    优化 solon.boot.smarthttp 在 multipart 的 name 解码处理

--- solon-projects/solon-boot/solon.boot.smarthttp/src/main/java/org/noear/solon/boot/smarthttp/http/uploadfile/HttpMultipart.java
+++ solon-projects/solon-boot/solon.boot.smarthttp/src/main/java/org/noear/solon/boot/smarthttp/http/uploadfile/HttpMultipart.java
@@ -6,46 +6,27 @@ import org.noear.solon.boot.ServerProps;
 import java.io.IOException;
 import java.io.InputStream;
 
+
 public class HttpMultipart {
-    private String name;
-    private String filename;
-    private HttpHeaderCollection headers;
-    private InputStream body;
+    public String name;
+    public String filename;
+    public HttpHeaderCollection headers;
+    public InputStream body;
 
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        try {
-            this.name = ServerProps.urlDecode(name);
-        } catch (Throwable ex) {
-            this.name = name;
-        }
     }
 
     public String getFilename() {
         return filename;
     }
 
-    public void setFilename(String filename) {
-        this.filename = filename;
-    }
-
     public HttpHeaderCollection getHeaders() {
         return headers;
     }
 
-    public void setHeaders(HttpHeaderCollection headers) {
-        this.headers = headers;
-    }
-
     public InputStream getBody() {
         return body;
-    }
-
-    public void setBody(InputStream body) {
-        this.body = body;
     }
 
     public String getString() throws IOException {
