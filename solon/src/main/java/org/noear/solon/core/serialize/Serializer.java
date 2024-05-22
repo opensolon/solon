b9cbf9d01f12a9816025a5dd3ca93f308bc94f60
noear <noear@live.cn>
2024-05-22 09:03:25 +0800

    内核添加 Serializer 接口，用于统一体系内的序列化

---
+++ solon/src/main/java/org/noear/solon/core/serialize/Serializer.java
@@ -0,0 +1,26 @@
+package org.noear.solon.core.serialize;
+
+import java.io.IOException;
+
+/**
+ * 序列化器
+ *
+ * @author noear
+ * @since 2.8
+ */
+public interface Serializer<T> {
+    /**
+     * 名字
+     */
+    String name();
+
+    /**
+     * 序列化
+     */
+    T serialize(Object fromObj) throws IOException;
+
+    /**
+     * 反序列化
+     */
+    Object deserialize(T data, Class<?> toClz) throws IOException;
+}
