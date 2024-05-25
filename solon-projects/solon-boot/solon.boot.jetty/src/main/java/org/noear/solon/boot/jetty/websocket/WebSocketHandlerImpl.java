31acc3a4053325afe4a4e5bd9e551fde7e120cb1
noear <noear@live.cn>
2024-05-25 13:05:29 +0800

    jetty 适配添加 WebSocket 子协议校验支持

--- solon-projects/solon-boot/solon.boot.jetty/src/main/java/org/noear/solon/boot/jetty/websocket/WebSocketHandlerImpl.java
+++ solon-projects/solon-boot/solon.boot.jetty/src/main/java/org/noear/solon/boot/jetty/websocket/WebSocketHandlerImpl.java
@@ -8,11 +8,15 @@ public class WebSocketHandlerImpl extends WebSocketHandler {
 
     @Override
     public void configure(WebSocketServletFactory factory) {
+        //由监听器内个性定制
         //factory.getPolicy().setIdleTimeout(10L * 60L * 1000L);
         //factory.getPolicy().setAsyncWriteTimeout(10L * 1000L);
 
         //注册览听器
-        factory.register(WebSocketListenerImpl.class);
+        //factory.register(WebSocketListenerImpl.class);
+
+        //设置生成器（握手、子协议、监听器）
+        factory.setCreator(new WebSocketCreatorImpl());
     }
 }
 
