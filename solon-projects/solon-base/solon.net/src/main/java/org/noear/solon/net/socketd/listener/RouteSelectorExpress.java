70444bc44e3bfbfec5ba3a3e5a0f6b7ecb42173d
noear <noear@live.cn>
2024-05-26 10:58:11 +0800

    优化 RouteSelectorExpress 的路由顺序（常量的，优于变量的）

--- solon-projects/solon-base/solon.net/src/main/java/org/noear/solon/net/socketd/listener/RouteSelectorExpress.java
+++ solon-projects/solon-base/solon.net/src/main/java/org/noear/solon/net/socketd/listener/RouteSelectorExpress.java
@@ -3,6 +3,7 @@ package org.noear.solon.net.socketd.listener;
 import org.noear.socketd.transport.core.Listener;
 import org.noear.socketd.transport.core.listener.RouteSelector;
 import org.noear.solon.core.handle.MethodType;
+import org.noear.solon.core.route.Routing;
 import org.noear.solon.core.route.RoutingDefault;
 import org.noear.solon.core.route.RoutingTable;
 import org.noear.solon.core.route.RoutingTableDefault;
@@ -24,7 +25,13 @@ public class RouteSelectorExpress implements RouteSelector<Listener> {
 
     @Override
     public void put(String path, Listener listener) {
-        inner.add(new RoutingDefault<>(path, MethodType.SOCKET, 0, listener));
+        Routing<Listener> routing = new RoutingDefault<>(path, MethodType.SOCKET, 0, listener);
+
+        if (path.contains("*") || path.contains("{")) {
+            inner.add(routing);
+        } else {
+            inner.add(0, routing);
+        }
     }
 
     @Override
