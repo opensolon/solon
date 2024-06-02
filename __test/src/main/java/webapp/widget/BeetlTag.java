1809626de5eb08e4478a70a7c9c8c1623d28fcbd
noear <noear@live.cn>
2024-06-02 09:47:33 +0800

    添加 beetl, enjoy 定制 tag 注入测试

--- __test/src/main/java/webapp/widget/BeetlTag.java
+++ __test/src/main/java/webapp/widget/BeetlTag.java
@@ -3,11 +3,13 @@ package webapp.widget;
 import org.beetl.core.tag.Tag;
 import org.noear.solon.annotation.Component;
 import org.noear.solon.annotation.Inject;
+import org.noear.solon.annotation.Singleton;
 import webapp.dso.EmptyService;
 
 /**
  * @author noear 2024/6/2 created
  */
+@Singleton(false)
 @Component("view:beetlTag")
 public class BeetlTag extends Tag {
     @Inject
--- __test/src/main/java/webapp/widget/EnjoyTag.java
+++ __test/src/main/java/webapp/widget/EnjoyTag.java
@@ -6,11 +6,13 @@ import com.jfinal.template.io.Writer;
 import com.jfinal.template.stat.Scope;
 import org.noear.solon.annotation.Component;
 import org.noear.solon.annotation.Inject;
+import org.noear.solon.annotation.Singleton;
 import webapp.dso.EmptyService;
 
 /**
  * @author noear 2024/6/2 created
  */
+@Singleton(false)
 @Component("view:enjoyTag")
 public class EnjoyTag extends Directive {
     @Inject
@@ -22,5 +24,10 @@ public class EnjoyTag extends Directive {
         System.out.println("enjoyTag:::ok:::");
 
         stat.exec(env, scope, writer);
+    }
+
+    @Override
+    public boolean hasEnd() {
+        return true;
     }
 }
