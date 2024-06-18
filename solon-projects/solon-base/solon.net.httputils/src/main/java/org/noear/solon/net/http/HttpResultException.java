8996a1dd3dcb0ae81a241a7c2b5d8f95f75c2280
noear <noear@live.cn>
2024-06-18 14:19:28 +0800

    添加 solon.net.httputils

---
+++ solon-projects/solon-base/solon.net.httputils/src/main/java/org/noear/solon/net/http/HttpResultException.java
@@ -0,0 +1,13 @@
+package org.noear.solon.net.http;
+
+import org.noear.solon.exception.SolonException;
+
+/**
+ * @author noear
+ * @since 1.5
+ */
+public class HttpResultException extends SolonException {
+    public HttpResultException(String message) {
+        super(message);
+    }
+}
