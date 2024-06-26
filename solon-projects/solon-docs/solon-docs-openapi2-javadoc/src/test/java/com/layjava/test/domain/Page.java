7e1dcee360cec69dee79a03fa2051e156ff01d4e
noear <noear@live.cn>
2024-06-26 20:08:59 +0800

    移除多余模块

--- solon-projects/solon-docs/solon-docs-openapi2-javadoc/src/test/java/com/layjava/test/domain/Page.java
+++
@@ -1,9 +0,0 @@
-package com.layjava.test.domain;
-
-/**
- * @author noear 2024/6/17 created
- */
-public interface Page {
-    int getPageNumber();
-    int getPageSize();
-}
--- solon-projects/solon-docs/solon-openapi2-knife4j/src/test/java/com/swagger/demo/model/UserType.java
+++
@@ -1,9 +0,0 @@
-package com.swagger.demo.model;
-
-/**
- * @author noear 2023/9/3 created
- */
-public enum UserType {
-    TYPE1,
-    TYPE2,
-}
