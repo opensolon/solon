7859e6efa380172eac74467e0a86f3f03eb3e912
noear <noear@live.cn>
2024-08-30 18:41:26 +0800

    solon-web-rx 添加 x-ndjson 单测代码

--- solon-projects/solon-web/solon-web-rx/src/test/java/features/AppTest.java
+++ solon-projects/solon-web/solon-web-rx/src/test/java/features/AppTest.java
@@ -1,4 +1,4 @@
-package labs;
+package features;
 
 import demo.App;
 import org.junit.jupiter.api.Test;
@@ -9,14 +9,27 @@ import org.noear.solon.test.SolonTest;
 /**
  * @author noear 2024/8/30 created
  */
-@SolonTest(App.class)
+@SolonTest(value = App.class,args = "--server.port=7071")
 public class AppTest extends HttpTester {
     @Test
     public void test1() throws Exception {
         assert path("/t1?name=d").get().equals("Hello d");
+    }
+
+    @Test
+    public void test2() throws Exception {
         assert path("/t2?name=d").get().equals("[\"Hello d\",\"hello2 d\"]");
+    }
+
+    @Test
+    public void test3() throws Exception {
         assert path("/t2?name=d").header("accept", MimeType.APPLICATION_X_NDJSON_VALUE).get().equals("Hello d\n" +
                 "hello2 d");
+
+    }
+
+    @Test
+    public void test4() throws Exception {
         assert path("/t3?name=d").get().equals("Hello d\n" +
                 "hello2 d");
     }
