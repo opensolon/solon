6c644c999bb6d375f3c741fead59199165bc93fb
noear <noear@live.cn>
2024-09-11 11:11:39 +0800

    smarthttp 升为 1.6.0

--- solon-projects/solon-boot/solon-boot-smarthttp/src/main/java/org/noear/solon/boot/smarthttp/XPluginImp.java
+++ solon-projects/solon-boot/solon-boot-smarthttp/src/main/java/org/noear/solon/boot/smarthttp/XPluginImp.java
@@ -37,7 +37,7 @@ public final class XPluginImp implements Plugin {
 
 
     public static String solon_boot_ver() {
-        return "smart http 1.5/" + Solon.version();
+        return "smart http 1.6/" + Solon.version();
     }
 
     private SmHttpServerComb _server;
