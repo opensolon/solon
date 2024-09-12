39d4d4b56597da2c11ed2cbac86cea4477106d0b
noear <noear@live.cn>
2024-09-12 09:42:10 +0800

    取消 HandlerProxy ，不太合理（尤其是对本地网关）

--- solon/src/main/java/org/noear/solon/core/AppContext.java
+++ solon/src/main/java/org/noear/solon/core/AppContext.java
@@ -371,12 +371,13 @@ public class AppContext extends BeanContainer {
         if (bw.raw() instanceof Handler) {
             Mapping mapping = annoEl.getAnnotation(Mapping.class);
             if (mapping != null) {
-                Handler handler = new HandlerProxy(bw); //支持原型
+                Handler handler = bw.raw();
                 Set<MethodType> v0 = FactoryManager.getGlobal().mvcFactory().findMethodTypes(new HashSet<>(), t -> annoEl.getAnnotation(t) != null);
                 if (v0.size() == 0) {
                     v0 = new HashSet<>(Arrays.asList(mapping.method()));
                 }
                 Solon.app().add(mapping, v0, handler);
+                singletonHint = "Handler";
             }
         }
 
