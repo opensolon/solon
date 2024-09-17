cdfabfeca3692bb933d158d3f0af82101f54d93b
noear <noear@live.cn>
2024-09-17 23:00:11 +0800

    添加序列化执行单测

---
+++ solon-projects/solon-serialization/solon-serialization-jackson/src/test/java/features/action/TestExecutor.java
@@ -0,0 +1,41 @@
+package features.action;
+
+import org.junit.jupiter.api.Test;
+import org.noear.solon.Solon;
+import org.noear.solon.annotation.Controller;
+import org.noear.solon.annotation.Mapping;
+import org.noear.solon.core.handle.ContextEmpty;
+import org.noear.solon.test.SolonTest;
+
+/**
+ * @author noear 2024/9/17 created
+ */
+@SolonTest
+public class TestExecutor {
+    @Test
+    public void test() throws Throwable {
+        ContextEmpty ctx = new ContextEmpty();
+        ctx.headerMap().add("Content-Type", "text/json");
+        ctx.pathNew("/");
+        ctx.bodyNew("{\"name\":\"noear\",\"label\":\"A\"}");
+
+        Solon.app().tryHandle(ctx);
+
+        System.out.println(ctx.result);
+
+        assert "Hello noear A".equals(ctx.result);
+    }
+
+    @Controller
+    public static class Demo {
+        @Mapping
+        public String hello(String name, Label label) {
+            return "Hello " + name + " " + label;
+        }
+    }
+
+    public enum Label {
+        A,
+        B
+    }
+}
