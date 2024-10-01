529835bcffe857643769d9edc8338cc45b462f27
noear <noear@live.cn>
2024-10-01 21:14:30 +0800

    添加 7 个单测

---
+++ solon-projects/solon-boot/solon-boot-jetty/src/test/java/features/jetty/App.java
@@ -0,0 +1,20 @@
+package features.jetty;
+
+import org.noear.solon.Solon;
+import org.noear.solon.annotation.Controller;
+import org.noear.solon.annotation.Mapping;
+
+/**
+ * @author noear 2024/10/1 created
+ */
+@Controller
+public class App {
+    public static void main(String[] args) {
+        Solon.start(ServerText.class, args);
+    }
+
+    @Mapping("hello")
+    public String hello() {
+        return "hello";
+    }
+}
---
+++ solon-projects/solon-boot/solon-boot-jlhttp/src/test/java/features/jlhttp/ServerText.java
@@ -0,0 +1,14 @@
+package features.jlhttp;
+
+import org.junit.jupiter.api.Test;
+import org.noear.solon.test.HttpTester;
+import org.noear.solon.test.SolonTest;
+
+@SolonTest(App.class)
+public class ServerText extends HttpTester {
+
+    @Test
+    public void test() throws Exception {
+        assert "hello".equals(path("/hello").get());
+    }
+}
