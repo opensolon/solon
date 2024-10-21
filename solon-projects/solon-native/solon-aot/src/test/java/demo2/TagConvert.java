c470bae9c69d045e6f9c1aabef00c317a43702bc
noear <noear@live.cn>
2024-10-21 22:03:57 +0800

    aot 调整代理示例

--- solon-projects/solon-native/solon-aot/src/test/java/demo2/TagConvert.java
+++ solon-projects/solon-native/solon-aot/src/test/java/demo2/TagConvert.java
@@ -15,8 +15,11 @@
  */
 package demo2;
 
+import org.noear.solon.annotation.Component;
+
 /**
  * @author noear 2022/9/30 created
  */
+@Component
 public class TagConvert implements BasicConvert {
 }
--- solon-projects/solon-native/solon-aot/src/test/java/demo2/TagDao.java
+++ solon-projects/solon-native/solon-aot/src/test/java/demo2/TagDao.java
@@ -15,8 +15,11 @@
  */
 package demo2;
 
+import org.noear.solon.annotation.Component;
+
 /**
  * @author noear 2022/9/30 created
  */
+@Component
 public class TagDao implements BaseMapper<Tag> {
 }
