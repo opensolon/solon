40c88576510753776fd1946412704096a700c354
noear <noear@live.cn>
2024-10-22 10:01:26 +0800

    solon DateUtil 增加三种格式解析

--- solon/src/main/java/org/noear/solon/core/util/DateUtil.java
+++ solon/src/main/java/org/noear/solon/core/util/DateUtil.java
@@ -35,16 +35,19 @@ public class DateUtil {
     private static final String FORMAT_24_ISO08601 = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
     private static final String FORMAT_23_a = "yyyy-MM-dd HH:mm:ss,SSS";
     private static final String FORMAT_23_b = "yyyy-MM-dd HH:mm:ss.SSS";
+    private static final String FORMAT_23_t = "yyyy-MM-dd'T'HH:mm:ss.SSS";
     private static final String FORMAT_22 = "yyyyMMddHHmmssSSSZ";//z: +0000
     private static final String FORMAT_19_ISO = "yyyy-MM-dd'T'HH:mm:ss";
     private static final String FORMAT_19_a = "yyyy-MM-dd HH:mm:ss";
     private static final String FORMAT_19_b = "yyyy/MM/dd HH:mm:ss";
     private static final String FORMAT_19_c = "yyyy.MM.dd HH:mm:ss";
+    private static final String FORMAT_18 = "HH:mm:ss.SSS+HH:mm";
     private static final String FORMAT_17 = "yyyyMMddHHmmssSSS";
     private static final String FORMAT_16_a = "yyyy-MM-dd HH:mm";
     private static final String FORMAT_16_b = "yyyy/MM/dd HH:mm";
     private static final String FORMAT_16_c = "yyyy.MM.dd HH:mm";
     private static final String FORMAT_14 = "yyyyMMddHHmmss";
+    private static final String FORMAT_12 = "HH:mm:ss.SSS";
     private static final String FORMAT_10_a = "yyyy-MM-dd";
     private static final String FORMAT_10_b = "yyyy/MM/dd";
     private static final String FORMAT_10_c = "yyyy.MM.dd";
@@ -56,6 +59,10 @@ public class DateUtil {
      * 解析
      */
     public static Date parse(String val) throws ParseException {
+        if (val == null) {
+            return null;
+        }
+
         final int len = val.length();
         String ft = null;
 
@@ -71,7 +78,9 @@ public class DateUtil {
                 ft = FORMAT_24_ISO08601;
             }
         } else if (len == 23) {
-            if (val.charAt(19) == ',') {
+            if (val.charAt(10) == 'T') {
+                ft = FORMAT_23_t;
+            } else if (val.charAt(19) == ',') {
                 ft = FORMAT_23_a;
             } else {
                 ft = FORMAT_23_b;
@@ -91,6 +100,8 @@ public class DateUtil {
                     ft = FORMAT_19_a;
                 }
             }
+        } else if (len == 18) {
+            ft = FORMAT_18;
         } else if (len == 17) {
             ft = FORMAT_17;
         } else if (len == 16) {
@@ -104,6 +115,8 @@ public class DateUtil {
             }
         } else if (len == 14) {
             ft = FORMAT_14;
+        } else if (len == 12) {
+            ft = FORMAT_12;
         } else if (len == 10) {
             char c1 = val.charAt(4);
             if (c1 == '/') {
@@ -158,7 +171,7 @@ public class DateUtil {
 
     /**
      * 转为 GMT 字符串
-     * */
+     */
     public static String toGmtString(Date date) {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
         sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
