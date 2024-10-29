6d34eb278abfbb96587a5c98d16f1c8c7121e376
noear <noear@live.cn>
2024-10-29 09:57:06 +0800

    优化控制器 非 Mapping 方法的代理处理

--- solon-projects/solon-base/solon-mvc/src/main/java/org/noear/solon/core/mvc/ActionLoaderDefault.java
+++ solon-projects/solon-base/solon-mvc/src/main/java/org/noear/solon/core/mvc/ActionLoaderDefault.java
@@ -151,8 +151,10 @@ public class ActionLoaderDefault extends HandlerAide implements ActionLoader {
         for (Method method : ClassUtil.findPublicMethods(bw.clz())) {
             loadActionItem(slots, all, method, b_limitMethodSet, b_addinMethodSet);
 
-            //是否需要自动代理
-            enableProxy = enableProxy || bw.context().beanInterceptorHas(method);
+            //是否需要自动代理 //只支持 public
+            if (Modifier.isPublic(method.getModifiers())) {
+                enableProxy = enableProxy || bw.context().beanInterceptorHas(method);
+            }
         }
 
         //是否需要自动代理
@@ -179,7 +181,7 @@ public class ActionLoaderDefault extends HandlerAide implements ActionLoader {
         } else {
             //如果有注解，不是 public 时，则告警提醒（以后改为异常）//v2.5
             if (Modifier.isPublic(method.getModifiers()) == false) {
-                LogUtil.global().warn("This mapping method is not public: " + method.getDeclaringClass().getName() + ":" + method.getName());
+                LogUtil.global().warn("This @Mapping method is not public: " + method.getDeclaringClass().getName() + ":" + method.getName());
             }
         }
 
