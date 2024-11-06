3c3706bb2c4d5331242055872fb8ab76bc26b50d
noear <noear@live.cn>
2024-11-06 11:44:16 +0800

    新增 @Multipart 注解

---
+++ solon/src/main/java/org/noear/solon/annotation/Multipart.java
@@ -0,0 +1,15 @@
+package org.noear.solon.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * 做为 @Mapping 的副词用，申明支持多部分
+ *
+ * @author noear
+ * @since 3.0
+ */
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Multipart {
+}
