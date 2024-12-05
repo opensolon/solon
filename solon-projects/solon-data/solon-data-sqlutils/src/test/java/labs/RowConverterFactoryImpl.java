0d1ae11d8eb65df604c40acc069297468e9992ba
noear <noear@live.cn>
2024-12-05 17:07:57 +0800

    add demo

---
+++ solon-projects/solon-data/solon-data-sqlutils/src/test/java/labs/RowConverterFactoryImpl.java
@@ -0,0 +1,52 @@
+package labs;
+
+import org.noear.snack.ONode;
+import org.noear.solon.annotation.Component;
+import org.noear.solon.data.sql.bound.RowConverter;
+import org.noear.solon.data.sql.bound.RowConverterFactory;
+
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @author noear 2024/12/5 created
+ */
+@Component
+public class RowConverterFactoryImpl implements RowConverterFactory<Object> {
+    @Override
+    public RowConverter<Object> create(Class<?> tClass) {
+        return new RowConverterImpl(tClass);
+    }
+
+    private static class RowConverterImpl implements RowConverter<Object> {
+        private final Class<?> tClass;
+        private ResultSetMetaData metaData;
+
+        public RowConverterImpl(Class<?> tClass) {
+            this.tClass = tClass;
+        }
+
+        @Override
+        public Object convert(ResultSet rs) throws SQLException {
+            if (metaData == null) {
+                metaData = rs.getMetaData();
+            }
+
+            Map<String, Object> map = new LinkedHashMap<>();
+            for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                String name = metaData.getColumnName(i);
+                Object value = rs.getObject(i);
+                map.put(name, value);
+            }
+
+            if (Map.class == tClass) {
+                return map;
+            } else {
+                return ONode.load(map).toObject(tClass);
+            }
+        }
+    }
+}
