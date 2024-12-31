7795f687c29dff5d050feb1c92cb241d3bd68b9a
noear <noear@live.cn>
2024-12-31 15:52:53 +0800

    添加两个单测

---
+++ __test/src/main/java/webapp/demo2_mvc/Void2Controller.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2017-2024 noear.org and authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package webapp.demo2_mvc;
+
+import org.noear.solon.annotation.Controller;
+import org.noear.solon.annotation.Mapping;
+import org.noear.solon.core.handle.Context;
+import org.noear.solon.core.handle.Render;
+import org.noear.solon.core.handle.Result;
+
+/**
+ * @author noear 2024/12/31 created
+ */
+@Controller
+public class Void2Controller implements Render {
+    @Mapping("/demo2/void2")
+    public void void2() {
+
+    }
+
+    @Override
+    public void render(Object data, Context ctx) throws Throwable {
+        ctx.render(Result.succeed(data));
+    }
+}
