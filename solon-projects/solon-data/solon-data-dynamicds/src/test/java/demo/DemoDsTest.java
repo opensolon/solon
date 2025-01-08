1cf9ce2c551f7dac4c8e19ed3c7423faa0b93add
noear <noear@live.cn>
2025-01-09 07:50:52 +0800

    调整 demo

--- solon-projects/solon-data/solon-data-dynamicds/src/test/java/demo/DemoDsTest.java
+++ solon-projects/solon-data/solon-data-dynamicds/src/test/java/demo/DemoDsTest.java
@@ -34,44 +34,50 @@ import java.util.Properties;
  */
 
 public class DemoDsTest {
-@Configuration
-public class Config {
-    @Bean("db_user")
-    public DataSource dsUser(@Inject("$demo.ds.db_user}") DynamicDataSource dataSource) {
-        return dataSource;
+    @Configuration
+    public class Config {
+        @Bean("db_user")
+        public DataSource dsUser(@Inject("$demo.ds.db_user}") DynamicDataSource dataSource) {
+            return dataSource;
+        }
+
+        //@Bean("db_user")
+        public DataSource dsUser2(@Inject("$demo.ds.db_user}") Properties props) {
+            //手动构建，可以不用配置：type, strict
+            Map<String, DataSource> dsMap = DsUtils.buildDsMap(props, DataSource.class);
+            DataSource dsDef = dsMap.get("default");
+
+            DynamicDataSource tmp = new DynamicDataSource();
+            tmp.setStrict(true);
+            tmp.setTargetDataSources(dsMap);
+            tmp.setDefaultTargetDataSource(dsDef);
+
+            return tmp;
+        }
     }
 
-    //@Bean("db_user")
-    public DataSource dsUser2(@Inject("$demo.ds.db_user}") Properties props) {
-        //手动构建，可以不用配置：type, strict
-        Map<String, DataSource> dsMap = DsUtils.buildDsMap(props, DataSource.class);
-        DataSource dsDef = dsMap.get("default");
+    @Component
+    public class UserService {
+        @Db("db_user")
+        UserMapper userMapper;
 
-        DynamicDataSource tmp = new DynamicDataSource();
-        tmp.setStrict(true);
-        tmp.setTargetDataSources(dsMap);
-        tmp.setDefaultTargetDataSource(dsDef);
+        @DynamicDs //使用默认源
+        public void addUser() {
+            userMapper.inserUser();
+        }
 
-        return tmp;
-    }
-}
-@Component
-public class UserService{
-    @Db("db_user")
-    UserMapper userMapper;
+        @DynamicDs("db_user_1") //使用 db_user_1 源
+        public void getUserList() {
+            userMapper.selectUserList();
+        }
 
-    @DynamicDs //使用默认源
-    public void addUser(){
-        userMapper.inserUser();
+        public void getUserList2() {
+            DynamicDsKey.setCurrent("db_user_2"); //使用 db_user_2 源
+            try {
+                userMapper.selectUserList();
+            } finally {
+                DynamicDsKey.remove();
+            }
+        }
     }
-
-    @DynamicDs("db_user_1") //使用 db_user_1 源
-    public void getUserList(){
-        userMapper.selectUserList();
-    }
-    public void getUserList2(){
-        DynamicDsKey.setCurrent("db_user_2"); //使用 db_user_2 源
-        userMapper.selectUserList();
-    }
-}
 }
