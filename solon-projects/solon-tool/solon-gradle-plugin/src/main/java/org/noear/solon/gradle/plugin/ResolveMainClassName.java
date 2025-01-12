505c98bc89f7398f296bf4e38f8958b1912b9693
noear <noear@live.cn>
2025-01-12 11:29:16 +0800

    Copyright 时间调整到 2025

--- solon-projects/solon-tool/solon-gradle-plugin/src/main/java/org/noear/solon/gradle/plugin/ResolveMainClassName.java
+++ solon-projects/solon-tool/solon-gradle-plugin/src/main/java/org/noear/solon/gradle/plugin/ResolveMainClassName.java
@@ -15,7 +15,7 @@
  */
 
 /*
- * Copyright 2017-2024 noear.org and authors
+ * Copyright 2017-2025 noear.org and authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
