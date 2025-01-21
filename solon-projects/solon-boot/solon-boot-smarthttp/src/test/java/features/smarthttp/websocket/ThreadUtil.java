1ebae37ccc2f818ff5b83966cbf4c9d9976859c3
jaime <r67yfyt@126.com>
2025-01-21 22:37:50 +0800

    feat: smart http的websocket通信兼容虚拟线程

---
+++ solon-projects/solon-boot/solon-boot-smarthttp/src/test/java/features/smarthttp/websocket/ThreadUtil.java
@@ -0,0 +1,27 @@
+package features.smarthttp.websocket;
+
+/**
+ * 线程工具类
+ *
+ * @author jaime
+ * @version 1.0
+ * @since 2025/1/21
+ */
+public class ThreadUtil {
+
+    /**
+     * 判断是否是虚拟线程
+     *
+     * @return boolean
+     */
+    public static boolean isVirtualThread() {
+        Thread thread = Thread.currentThread();
+        try {
+            Class<?> v = Class.forName("java.lang.BaseVirtualThread");
+            Thread currentThread = Thread.currentThread();
+            return v.isAssignableFrom(currentThread.getClass());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
