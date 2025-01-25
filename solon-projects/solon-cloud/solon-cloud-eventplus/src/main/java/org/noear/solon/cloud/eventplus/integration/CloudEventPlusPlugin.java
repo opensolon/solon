8f9365043f59be0606a16829a7852c5c91590bb6
noear <noear@live.cn>
2025-01-25 23:31:57 +0800

    调整 solon-cloud-eventplus 插件实现命名

--- solon-projects/solon-cloud/solon-cloud-eventplus/src/main/java/org/noear/solon/cloud/eventplus/integration/CloudEventPlusPlugin.java
+++ solon-projects/solon-cloud/solon-cloud-eventplus/src/main/java/org/noear/solon/cloud/eventplus/integration/CloudEventPlusPlugin.java
@@ -25,7 +25,7 @@ import org.noear.solon.core.Plugin;
  * @author iYarnFog
  * @since 1.5
  */
-public class XPluginImp implements Plugin {
+public class CloudEventPlusPlugin implements Plugin {
 
     @Override
     public void start(AppContext context) {
