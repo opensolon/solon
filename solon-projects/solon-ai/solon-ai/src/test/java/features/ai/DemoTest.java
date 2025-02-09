ac08dee0a5d812aabcc47c3a513963ddb2ef0372
noear <noear@live.cn>
2025-02-09 11:03:19 +0800

    solon-ai 完善 demo

--- solon-projects/solon-ai/solon-ai/src/test/java/features/ai/DemoTest.java
+++ solon-projects/solon-ai/solon-ai/src/test/java/features/ai/DemoTest.java
@@ -17,19 +17,15 @@ import java.io.IOException;
  * @author noear 2025/1/28 created
  */
 public class DemoTest {
-    private String apiUrl = "http://127.0.0.1:11434/api/chat";
-    private String model = "deepseek-r1:1.5b";//"llama3.2";
+    private static final String apiUrl = "http://127.0.0.1:11434/api/chat";
+    private static final String model = "deepseek-r1:1.5b";//"llama3.2";
 
     @Test
     public void case1() throws IOException {
-        ChatModel chatModel = ChatModel.of(apiUrl)
-                .model(model)
-                .build();
+        ChatModel chatModel = ChatModel.of(apiUrl).model(model).build();
 
         //一次性返回
-        ChatResponse resp = chatModel
-                .prompt("hello")
-                .call();
+        ChatResponse resp = chatModel.prompt("hello").call();
 
         //打印消息
         System.out.println(resp.getMessage().getContent());
@@ -38,20 +34,18 @@ public class DemoTest {
 
     @Test
     public void case2() {
-        ChatModel chatModel = ChatModel.of(apiUrl)
-                .model(model)
-                .build();
+        ChatModel chatModel = ChatModel.of(apiUrl).model(model).build();
 
         //流返回(sse)
-        Publisher<ChatResponse> publisher = chatModel
-                .prompt("hello")
-                .stream();
+        Publisher<ChatResponse> publisher = chatModel.prompt("hello").stream();
 
         publisher.subscribe(new SimpleSubscriber<ChatResponse>()
                 .doOnNext(resp -> {
                     System.out.println(resp.getMessage().getContent());
                 }).doOnComplete(() -> {
                     System.out.println("::完成!");
+                }).doOnError(err -> {
+                    err.printStackTrace();
                 }));
     }
 
@@ -86,6 +80,8 @@ public class DemoTest {
         publisher.subscribe(new SimpleSubscriber<ChatResponse>()
                 .doOnNext(resp -> {
                     System.out.println(resp.getMessage().getContent());
+                }).doOnComplete(() -> {
+                    System.out.println("::完成!");
                 }).doOnError(err -> {
                     err.printStackTrace();
                 }));
