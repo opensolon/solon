521dac2809a604320a4485b989f611f5b36980d6
noear <noear@live.cn>
2025-02-20 16:43:21 +0800

    solon-ai 添加 image 模型相关类

---
+++ solon-projects/solon-ai/solon-ai/src/main/java/org/noear/solon/ai/image/dialect/ImageDialect.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2017-2025 noear.org and authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.noear.solon.ai.image.dialect;
+
+import org.noear.solon.ai.image.ImageConfig;
+import org.noear.solon.ai.image.ImageOptions;
+import org.noear.solon.ai.image.ImageResponse;
+
+import java.util.List;
+
+/**
+ * 图像方言
+ *
+ * @author noear
+ * @since 3.1
+ */
+public interface ImageDialect {
+    /**
+     * 匹配检测
+     *
+     * @param config 聊天配置
+     */
+    boolean matched(ImageConfig config);
+
+    /**
+     * 构建请求数据
+     *
+     * @param config  聊天配置
+     * @param options 聊天选项
+     */
+    String buildRequestJson(ImageConfig config, ImageOptions options, List<String> input);
+
+    /**
+     * 分析响应数据
+     *
+     * @param config 聊天配置
+     */
+    ImageResponse parseResponseJson(ImageConfig config, String respJson);
+}
