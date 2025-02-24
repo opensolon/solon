0a7f8d9d0b2cf12c9e243a55a5765fb8836be2bd
linziguan <linziguan@live.com>
2025-02-24 10:22:50 +0800

    提交excel读取功能

--- solon-projects/solon-ai/solon-ai-load-excel/src/main/java/org/noear/solon/ai/rag/loader/ExcelLoader.java
+++ solon-projects/solon-ai/solon-ai-load-excel/src/main/java/org/noear/solon/ai/rag/loader/ExcelLoader.java
@@ -16,20 +16,20 @@
 package org.noear.solon.ai.rag.loader;
 
 import org.noear.solon.ai.rag.Document;
-import org.noear.solon.ai.rag.DocumentLoader;
 
+import cn.hutool.json.JSONArray;
 import cn.hutool.poi.excel.ExcelReader;
 import cn.hutool.poi.excel.ExcelUtil;
-
 import java.io.InputStream;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 /**
  * Excel 文档加载器
+ * <p>读取excel表格内容，并转换为json</p>
  *
- * @author noear
+ * @author linziguan
  * @since 3.1
  */
 public class ExcelLoader extends AbstractDocumentLoader {
@@ -42,9 +42,39 @@ public class ExcelLoader extends AbstractDocumentLoader {
 	
     @Override
     public List<Document> load() {
-    	ExcelReader reader = ExcelUtil.getReader(this.inputStream);
-    	List<Map<String, Object>> readAll = reader.readAll();
+    	JSONArray jsonArray = new JSONArray();
+    	List<Document> docs = new ArrayList<>();
     	
-        return Collections.emptyList();
+    	ExcelReader reader = ExcelUtil.getReader(this.inputStream);
+    	int numberOfSheets = reader.getWorkbook().getNumberOfSheets();
+    	for(int s=0;s<numberOfSheets;s++) {
+    		List<Map<String, Object>> readAll = reader.setSheet(s).readAll();
+    		if(readAll.size()>0) {
+    			// 第一行标题
+            	Map<String, Object> titleRow = readAll.get(0);
+            	readAll.remove(0);
+            	
+            	// 每200行切分一段
+            	int batchSize = 200;
+        		int totalSize = readAll.size();
+                int numBatches = (totalSize + batchSize - 1) / batchSize; // 计算总批次数
+                for (int i = 0; i < numBatches; i++) {
+                	int start = i * batchSize;
+                    int end = Math.min(start + batchSize, totalSize); // 确保最后一组不会越界
+
+                    List<Map<String, Object>> batch = readAll.subList(start, end);
+                    
+                    jsonArray.addAll(batch);
+                    
+                    Document doc = new Document(jsonArray.toString());
+                    docs.add(doc);
+                }
+    		}
+    	}
+    	
+    	
+    	
+//        return Collections.emptyList();
+    	return docs;
     }
 }
