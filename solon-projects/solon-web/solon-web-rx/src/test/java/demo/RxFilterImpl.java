d177ff152e6101bf1b3e5d57b8d6cba7a0be44fe
noear <noear@live.cn>
2025-03-03 15:21:49 +0800

    test: 调整示例

--- solon-projects/solon-web/solon-web-rx/src/test/java/demo/RxFilterImpl.java
+++ solon-projects/solon-web/solon-web-rx/src/test/java/demo/RxFilterImpl.java
@@ -1,6 +1,9 @@
 package demo;
 
 import org.noear.solon.annotation.Component;
+import org.noear.solon.core.handle.Context;
+import org.noear.solon.core.handle.Filter;
+import org.noear.solon.core.handle.FilterChain;
 import org.noear.solon.rx.Completable;
 import org.noear.solon.rx.handle.RxContext;
 import org.noear.solon.rx.handle.RxFilter;
@@ -10,7 +13,15 @@ import org.noear.solon.rx.handle.RxFilterChain;
  * @author noear 2025/2/16 created
  */
 @Component
-public class RxFilterImpl implements RxFilter {
+public class RxFilterImpl implements Filter, RxFilter {
+    @Override
+    public void doFilter(Context ctx, FilterChain chain) throws Throwable {
+        try {
+            chain.doFilter(ctx);
+        } catch (Throwable ex) {
+            System.out.println("RxFilterImpl.doFilter error");
+        }
+    }
 
     @Override
     public Completable doFilter(RxContext ctx, RxFilterChain chain) {
