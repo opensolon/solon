1a0e862c3397bea3cd0966d7c917bee046796a22
noear <noear@live.cn>
2025-03-21 00:10:58 +0800

    添加 solon-flow TaskAsyncComponent 类

--- solon-projects/solon-flow/solon-flow/src/test/java/demo/flow/async/AsyncTaskComponent.java
+++ solon-projects/solon-flow/solon-flow/src/test/java/demo/flow/async/AsyncTaskComponent.java
@@ -1,34 +1,13 @@
 package demo.flow.async;
 
-import org.noear.solon.Utils;
 import org.noear.solon.flow.ChainContext;
 import org.noear.solon.flow.Node;
-import org.noear.solon.flow.TaskComponent;
+import org.noear.solon.flow.TaskAsyncComponent;
 
-/**
- * @author noear 2025/3/20 created
- */
-public class AsyncTaskComponent implements TaskComponent {
+public class AsyncTaskComponent extends TaskAsyncComponent {
     @Override
-    public void run(ChainContext context, Node node) throws Throwable {
-        //中断当前分支
-        context.interrupt();
-
-        //异步执行
-        Utils.async(() -> {
-            //做真正的任务
-            System.out.println("node: " + node.id());
-            //向后驱动
-            next(context, node);
-        });
-    }
-
-    private void next(ChainContext context, Node node) {
-        try {
-            context.engine().next(node, context);
-        } catch (Throwable ex) {
-            ex.printStackTrace();
-            context.stop();
-        }
+    protected void asyncRun(ChainContext context, Node node) throws Throwable {
+        //做真正的任务
+        System.out.println("node: " + node.id());
     }
 }
