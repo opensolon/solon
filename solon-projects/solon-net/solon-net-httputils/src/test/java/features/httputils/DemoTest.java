c7f7f77a626f7d82cbb39f09a6560537c672b13a
noear <noear@live.cn>
2025-03-25 20:06:52 +0800

    加个单测

---
+++ solon-projects/solon-net/solon-net-httputils/src/test/java/features/httputils/DemoTest.java
@@ -0,0 +1,37 @@
+package features.httputils;
+
+import org.junit.jupiter.api.Test;
+import org.noear.solon.net.http.HttpUtils;
+import org.noear.solon.rx.SimpleSubscriber;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author noear 2025/3/25 created
+ */
+public class DemoTest {
+    @Test
+    public void case1() throws Exception {
+        CountDownLatch latch = new CountDownLatch(1);
+        HttpUtils.http("http://localhost:8080/test/functioncall/sse")
+                .data("prompt", "今天杭州的天气情况？")
+                .execAsTextStream("GET")
+                .subscribe(new SimpleSubscriber<String>()
+                        .doOnSubscribe(subscription -> {
+                            System.out.println("开始");
+                            subscription.request(Long.MAX_VALUE);
+                        })
+                        .doOnNext(item -> {
+                            System.out.println(item);
+                        }).doOnComplete(() -> {
+                            System.out.println("完成");
+                            System.out.println("===================================");
+                            latch.countDown();
+                        }).doOnError(err -> {
+                            err.printStackTrace();
+                            latch.countDown();
+                        }));
+
+        latch.await();
+    }
+}
