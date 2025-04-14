d8e7c08a988d829630c9ed93d6ce9220ccbe180b
noear <noear@live.cn>
2025-04-14 16:04:10 +0800

    修复 solon-net-httputils 流式半刷时会卡的问题

--- solon-projects/solon-net/solon-net-httputils/src/main/java/org/noear/solon/net/http/impl/jdk/JdkInputStreamWrapper.java
+++ solon-projects/solon-net/solon-net-httputils/src/main/java/org/noear/solon/net/http/impl/jdk/JdkInputStreamWrapper.java
@@ -36,8 +36,39 @@ public class JdkInputStreamWrapper extends InputStream {
 
     @Override
     public int read() throws IOException {
-        return in.read();
+        return this.in.read();
     }
+
+    @Override
+    public int read(byte[] b, int off, int len) throws IOException {
+        return this.in.read(b, off, len);
+    }
+
+    @Override
+    public long skip(long n) throws IOException {
+        return this.in.skip(n);
+    }
+
+    @Override
+    public int available() throws IOException {
+        return this.in.available();
+    }
+
+    @Override
+    public synchronized void mark(int readlimit) {
+        this.in.mark(readlimit);
+    }
+
+    @Override
+    public synchronized void reset() throws IOException {
+        this.in.reset();
+    }
+
+    @Override
+    public boolean markSupported() {
+        return this.in.markSupported();
+    }
+
 
     @Override
     public void close() throws IOException {
