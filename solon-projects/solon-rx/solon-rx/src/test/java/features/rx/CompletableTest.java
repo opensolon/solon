0a800ba9037655737876e79e994d5a0d7de02a8e
noear <noear@live.cn>
2025-04-18 23:57:41 +0800

    添加 solon-rx Completable:then 单测

--- solon-projects/solon-rx/solon-rx/src/test/java/features/rx/CompletableTest.java
+++ solon-projects/solon-rx/solon-rx/src/test/java/features/rx/CompletableTest.java
@@ -5,13 +5,14 @@ import org.noear.solon.rx.Completable;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @author noear 2025/4/18 created
  */
 public class CompletableTest {
     @Test
-    public void testThen() {
+    public void testThenOk() {
         List<String> list = new ArrayList<>();
 
         Completable.create(emitter -> {
@@ -27,5 +28,30 @@ public class CompletableTest {
         assert list.size() == 2;
         assert "a".equals(list.get(0));
         assert "b".equals(list.get(1));
+    }
+
+    @Test
+    public void testThenErr() {
+        List<String> list = new ArrayList<>();
+        AtomicReference<Throwable> errorHolder = new AtomicReference<>();
+
+        Completable.create(emitter -> {
+                    list.add("a");
+                    emitter.onError(new RuntimeException("a"));
+                }).then(Completable.create((emitter1) -> {
+                    list.add("b");
+                    emitter1.onComplete();
+                })).doOnError(err -> {
+                    errorHolder.set(err);
+                })
+                .subscribe();
+
+        System.out.println(list);
+
+        assert list.size() == 1;
+        assert "a".equals(list.get(0));
+
+        assert errorHolder.get() != null;
+        assert errorHolder.get() instanceof RuntimeException;
     }
 }
