5bab21487ee66e0808e313385f0fbb1bad602e8f
noear <noear@live.cn>
2025-04-30 11:03:08 +0800

    调整 solon Param 注解，添加字段支持

--- solon/src/main/java/org/noear/solon/annotation/Param.java
+++ solon/src/main/java/org/noear/solon/annotation/Param.java
@@ -25,7 +25,7 @@ import java.lang.annotation.*;
  * @author noear
  * @since 1.0
  * */
-@Target({ElementType.PARAMETER})
+@Target({ElementType.PARAMETER, ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface Param {
