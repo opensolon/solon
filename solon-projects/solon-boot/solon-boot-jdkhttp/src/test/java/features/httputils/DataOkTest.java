d933ec2b03ae4c34ddba24514eda49d6167f22d4
noear <noear@live.cn>
2025-05-16 19:48:48 +0800

    添加 solon-net-httputils 新的单测

--- solon-projects/solon-boot/solon-boot-jdkhttp/src/test/java/features/httputils/DataOkTest.java
+++ solon-projects/solon-boot/solon-boot-jdkhttp/src/test/java/features/httputils/DataOkTest.java
@@ -6,6 +6,9 @@ import org.noear.solon.net.http.HttpUtils;
 import org.noear.solon.net.http.impl.okhttp.OkHttpUtilsFactory;
 import org.noear.solon.test.SolonTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * @author noear 2024/10/27 created
  */
@@ -73,5 +76,48 @@ public class DataOkTest {
     @Test
     public void redirect_case307() throws Exception {
         assert http("http://localhost:8080/redirect/jump?code=307").get().equals("ok");
+    }
+
+
+    @Test
+    public void redirect_case301b() throws Exception {
+        AtomicInteger codeHolder = new AtomicInteger();
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        http("http://localhost:8080/redirect/jump?code=301").execAsync("GET")
+                .thenAccept(resp -> {
+                    codeHolder.set(resp.code());
+                    countDownLatch.countDown();
+                });
+        countDownLatch.await();
+
+        assert codeHolder.get() == 200;
+    }
+
+    @Test
+    public void redirect_case302b() throws Exception {
+        AtomicInteger codeHolder = new AtomicInteger();
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        http("http://localhost:8080/redirect/jump?code=302").execAsync("GET")
+                .thenAccept(resp -> {
+                    codeHolder.set(resp.code());
+                    countDownLatch.countDown();
+                });
+        countDownLatch.await();
+
+        assert codeHolder.get() == 200;
+    }
+
+    @Test
+    public void redirect_case307b() throws Exception {
+        AtomicInteger codeHolder = new AtomicInteger();
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        http("http://localhost:8080/redirect/jump?code=307").execAsync("GET")
+                .thenAccept(resp -> {
+                    codeHolder.set(resp.code());
+                    countDownLatch.countDown();
+                });
+        countDownLatch.await();
+
+        assert codeHolder.get() == 200;
     }
 }
