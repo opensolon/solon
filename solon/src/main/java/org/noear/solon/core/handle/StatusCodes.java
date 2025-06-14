5964d8aec9a2d1044cbee3089079860cc97cb632
noear <noear@live.cn>
2025-06-15 07:11:22 +0800

    添加 solon StatusCodes 类

--- solon/src/main/java/org/noear/solon/core/handle/StatusCodes.java
+++ solon/src/main/java/org/noear/solon/core/handle/StatusCodes.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.noear.solon.core.util;
+package org.noear.solon.core.handle;
 
 /**
  * 状态码
