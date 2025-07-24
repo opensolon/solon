339dbf5ee778c967c9bc9a175782f57a936fd8d4
noear <noear@live.cn>
2025-07-24 15:52:37 +0800

    添加 solon-net-httputils ssl 定制支持

--- solon-projects/solon-net/solon-net-httputils/src/main/java/org/noear/solon/net/http/impl/HttpSslSupplierDefault.java
+++ solon-projects/solon-net/solon-net-httputils/src/main/java/org/noear/solon/net/http/impl/HttpSslSupplierDefault.java
@@ -4,9 +4,9 @@ import org.noear.solon.Utils;
 import org.noear.solon.net.http.HttpSslSupplier;
 
 import javax.net.ssl.*;
-import java.security.KeyStore;
 import java.security.SecureRandom;
-import java.util.Arrays;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
 /**
  * SSL 客户端
@@ -31,7 +31,7 @@ public class HttpSslSupplierDefault implements HttpSslSupplier {
             try {
                 if (sslContext == null) {
                     sslContext = SSLContext.getInstance("SSL");
-                    sslContext.init(null, new TrustManager[]{getX509TrustManager()}, new SecureRandom());
+                    sslContext.init(null, new TrustManager[]{new DefaultX509TrustManager()}, new SecureRandom());
                 }
             } catch (Exception e) {
                 throw new IllegalStateException(e);
@@ -59,21 +59,21 @@ public class HttpSslSupplierDefault implements HttpSslSupplier {
         return hostnameVerifier;
     }
 
-    public X509TrustManager getX509TrustManager() {
-        X509TrustManager trustManager = null;
-        try {
-            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-            trustManagerFactory.init((KeyStore) null);
-            TrustManager[] trustManagers = trustManagerFactory.getTrustManagers();
-            if (trustManagers.length != 1 || !(trustManagers[0] instanceof X509TrustManager)) {
-                throw new IllegalStateException("Unexpected default trust managers:" + Arrays.toString(trustManagers));
-            }
-            trustManager = (X509TrustManager) trustManagers[0];
-        } catch (Exception e) {
-            e.printStackTrace();
+    public static class DefaultX509TrustManager implements X509TrustManager {
+        @Override
+        public void checkClientTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+
         }
 
-        return trustManager;
+        @Override
+        public void checkServerTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+
+        }
+
+        @Override
+        public X509Certificate[] getAcceptedIssuers() {
+            return new X509Certificate[0];
+        }
     }
 
     public static class DefaultHostnameVerifier implements HostnameVerifier {
