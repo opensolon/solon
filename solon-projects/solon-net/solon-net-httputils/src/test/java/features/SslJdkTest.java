f181f7a796459df0229b0aaf1a929101c2b59cf2
noear <noear@live.cn>
2025-08-09 12:08:54 +0800

    优化 solon-net-httputils ssl 单测

--- solon-projects/solon-net/solon-net-httputils/src/test/java/features/SslJdkTest.java
+++ solon-projects/solon-net/solon-net-httputils/src/test/java/features/SslJdkTest.java
@@ -3,18 +3,17 @@ package features;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.noear.solon.net.http.HttpException;
-import org.noear.solon.net.http.HttpExtension;
 import org.noear.solon.net.http.HttpUtils;
 import org.noear.solon.net.http.impl.jdk.JdkHttpUtilsFactory;
 
-import java.io.IOException;
+import java.util.function.Predicate;
 
 /**
  * @author noear 2024/10/4 created
  */
 public class SslJdkTest {
     static HttpUtils http(String url) {
-        return  JdkHttpUtilsFactory.getInstance().http(url);
+        return JdkHttpUtilsFactory.getInstance().http(url);
     }
 
     @Test
@@ -29,38 +28,22 @@ public class SslJdkTest {
     }
 
     @Test
-    public void case11() throws Exception {
-        String html = http("https://solon.noear.org/").get();
-        System.out.println(html);
-
-        assert html != null;
-        assert html.contains("Solon");
+    public void caseSslOk() {
+        testUrlDo("https://solon.noear.org/", html -> html.contains("Solon"));
+        testUrlDo("https://www.bilibili.com/", html -> html.contains("bilibili"));
+        testUrlDo("https://www.taobao.com/", html -> html.contains("taobao"));
+        testUrlDo("https://www.csdn.net/", html -> html.contains("csdn"));
+        testUrlDo("https://i.cnblogs.com/", html -> html.contains("cnblogs"));
+        testUrlDo("https://www.guancha.cn/", html -> html.contains("guancha"));
     }
 
-    @Test
-    public void case12() throws Exception {
-        String html = http("https://www.bilibili.com/").get();
+    private void testUrlDo(String url, Predicate<String> test) {
+        System.out.println("------------test do: " + url);
+
+        String html = http(url).get();
         System.out.println(html);
 
-        assert html != null;
-        assert html.contains("bilibili");
-    }
-
-    @Test
-    public void case13() throws Exception {
-        String html = http("https://www.taobao.com/").get();
-        System.out.println(html);
-
-        assert html != null;
-        assert html.contains("taobao");
-    }
-
-    @Test
-    public void case14() throws Exception {
-        String html = http("https://www.csdn.net/").get();
-        System.out.println(html);
-
-        assert html != null;
-        assert html.contains("csdn");
+        Assertions.assertNotNull(html);
+        Assertions.assertTrue(test.test(html), "test failed:" + url);
     }
 }
