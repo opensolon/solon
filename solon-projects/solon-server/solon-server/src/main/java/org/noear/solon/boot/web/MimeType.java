f492751dc974b9e7109a77df115be3b8e1264a09
noear <noear@live.cn>
2025-08-13 12:16:01 +0800

    调整包结构 (solon-server)solon.server 部分替代 solon.boot

--- solon-projects/solon-server/solon-server/src/main/java/org/noear/solon/boot/web/MimeType.java
+++ solon-projects/solon-server/solon-server/src/main/java/org/noear/solon/boot/web/MimeType.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.noear.solon.server.util;
+package org.noear.solon.boot.web;
 
 import org.noear.solon.Utils;
 
