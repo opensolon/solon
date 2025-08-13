83c8e9b7091b4b1a26bdad8637154d0409650d90
noear <noear@live.cn>
2025-08-13 12:47:36 +0800

    调整包结构 (solon-server)solon.server 部分替代 solon.boot

---
+++ solon-projects/solon-server/solon-server/src/main/java/org/noear/solon/boot/web/RedirectUtils.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2017-2025 noear.org and authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.noear.solon.boot.web;
+
+import org.noear.solon.Solon;
+import org.noear.solon.Utils;
+
+/**
+ * Web 跳转工具
+ *
+ * @author noear
+ * @since 1.11
+ * @deprecated 3.5
+ * */
+@Deprecated
+public class RedirectUtils {
+    /**
+     * 获取跳转地址
+     */
+    public static String getRedirectPath(String location) {
+        if (Utils.isEmpty(Solon.cfg().serverContextPath())) {
+            return location;
+        }
+
+        if (location.startsWith("/")) {
+            if (location.startsWith(Solon.cfg().serverContextPath())) {
+                return location;
+            } else {
+                return Solon.cfg().serverContextPath() + location.substring(1);
+            }
+        } else {
+            return location;
+        }
+    }
+}
