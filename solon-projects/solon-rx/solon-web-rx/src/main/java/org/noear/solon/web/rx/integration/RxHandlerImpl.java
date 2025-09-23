3b601e72c2fb61fb40226f8c00851b82c36a280c
noear <noear@live.cn>
2025-09-23 20:23:58 +0800

    添加注释

--- solon-projects/solon-rx/solon-web-rx/src/main/java/org/noear/solon/web/rx/integration/RxHandlerImpl.java
+++ solon-projects/solon-rx/solon-web-rx/src/main/java/org/noear/solon/web/rx/integration/RxHandlerImpl.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017-2025 noear.org and authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.noear.solon.web.rx.integration;
 
 import org.noear.solon.core.handle.Context;
@@ -8,7 +23,8 @@ import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
 
 /**
- * @author noear 2025/2/16 created
+ * @author noear
+ * @since 3.0
  */
 public class RxHandlerImpl implements RxHandler {
     private static final byte[] CRLF = "\n".getBytes();
