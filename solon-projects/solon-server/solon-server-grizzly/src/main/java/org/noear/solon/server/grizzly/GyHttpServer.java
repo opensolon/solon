509aac73818eb244c1131a411842add4f6b00bad
noear <noear@live.cn>
2025-09-25 11:37:52 +0800

    完成 solon-server-grizzly 适配

--- solon-projects/solon-server/solon-server-grizzly/src/main/java/org/noear/solon/server/grizzly/GyHttpServer.java
+++ solon-projects/solon-server/solon-server-grizzly/src/main/java/org/noear/solon/server/grizzly/GyHttpServer.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017-2025 noear.org and authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.noear.solon.server.grizzly;
 
 import org.glassfish.grizzly.http.server.*;
@@ -9,12 +24,10 @@ import org.noear.solon.server.ServerProps;
 import org.noear.solon.server.grizzly.http.GyHttpContextHandler;
 import org.noear.solon.server.prop.impl.HttpServerProps;
 
-import java.util.concurrent.ThreadFactory;
-
 /**
  *
- * @author noear 2025/9/5 created
- *
+ * @author noear
+ * @since 3.6
  */
 public class GyHttpServer implements ServerLifecycle {
     private HttpServer httpServer;
