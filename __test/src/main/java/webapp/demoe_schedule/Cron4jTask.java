09d60014857d816e865d36223756aa05f9901dcf
noear <noear@live.cn>
2025-09-25 09:09:16 +0800

    清理 __test 不必要的依赖包

--- __test/src/main/java/webapp/demoe_schedule/Cron4jTask.java
+++ __test/src/main/java/webapp/demoe_schedule/Cron4jTask.java
@@ -15,17 +15,17 @@
  */
 package webapp.demoe_schedule;
 
-import it.sauronsoftware.cron4j.Task;
-import it.sauronsoftware.cron4j.TaskExecutionContext;
-import org.noear.solon.extend.cron4j.Cron4j;
-
-import java.util.Date;
-
-@Cron4j(cron5x = "*/1 * * * *")
-public class Cron4jTask extends Task {
-
-    @Override
-    public void execute(TaskExecutionContext context) throws RuntimeException {
-        System.out.println("我是定时任务: Cron4jTask(*/1 * * * *) -- " + new Date().toString());
-    }
-}
+//import it.sauronsoftware.cron4j.Task;
+//import it.sauronsoftware.cron4j.TaskExecutionContext;
+//import org.noear.solon.extend.cron4j.Cron4j;
+//
+//import java.util.Date;
+//
+//@Cron4j(cron5x = "*/1 * * * *")
+//public class Cron4jTask extends Task {
+//
+//    @Override
+//    public void execute(TaskExecutionContext context) throws RuntimeException {
+//        System.out.println("我是定时任务: Cron4jTask(*/1 * * * *) -- " + new Date().toString());
+//    }
+//}
