fde4fef2a136bf3b1eb60fec4ba4407b5b69a130
noear <noear@live.cn>
2025-09-25 09:09:16 +0800

    清理 __test 不必要的依赖包

--- __test/src/main/java/webapp/demoe_schedule/Cron4jRun2.java
+++ __test/src/main/java/webapp/demoe_schedule/Cron4jRun2.java
@@ -15,15 +15,15 @@
  */
 package webapp.demoe_schedule;
 
-import org.noear.solon.extend.cron4j.Cron4j;
-
-import java.util.Date;
-
-@Cron4j(cron5x = "*/1 * * * *")
-public class Cron4jRun2 implements Runnable {
-
-    @Override
-    public void run() {
-        System.out.println("我是定时任务: Cron4jRun2(*/1 * * * *) -- " + new Date().toString());
-    }
-}
+//import org.noear.solon.extend.cron4j.Cron4j;
+//
+//import java.util.Date;
+//
+//@Cron4j(cron5x = "*/1 * * * *")
+//public class Cron4jRun2 implements Runnable {
+//
+//    @Override
+//    public void run() {
+//        System.out.println("我是定时任务: Cron4jRun2(*/1 * * * *) -- " + new Date().toString());
+//    }
+//}
