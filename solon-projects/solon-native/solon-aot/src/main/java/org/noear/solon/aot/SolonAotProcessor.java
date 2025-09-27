24961a35a35e8421f9c33fad347bee89b6dec5e8
noear <noear@live.cn>
2025-09-27 09:42:25 +0800

    调整 solon 内部的 MvcFactory 替换为 ActionLoaderFactory（同时 solon-handle 替换 solon-mvc），更通用，更合理

--- solon-projects/solon-native/solon-aot/src/main/java/org/noear/solon/aot/SolonAotProcessor.java
+++ solon-projects/solon-native/solon-aot/src/main/java/org/noear/solon/aot/SolonAotProcessor.java
@@ -267,7 +267,7 @@ public class SolonAotProcessor {
             addReflectConfigDo(metadata, "org.noear.solon.extend.impl.ScannerExt");
             addReflectConfigDo(metadata, "org.noear.solon.extend.impl.LogUtilExt");
             addReflectConfigDo(metadata, "org.noear.solon.extend.impl.ProxyBinderExt");
-            addReflectConfigDo(metadata, "org.noear.solon.core.mvc.MvcFactoryDefault");
+            addReflectConfigDo(metadata, "org.noear.solon.extend.impl.ActionLoaderFactoryExt");
 
             FileWriter fileWriter = getFileWriter("reflect-config.json");
             fileWriter.write(metadata.toReflectionJson());
