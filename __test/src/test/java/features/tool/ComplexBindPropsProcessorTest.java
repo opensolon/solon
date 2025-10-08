6c19702a3256a625be83953b7bded8575dc1d1d3
fuzi1996 <imfuzi@163.com>
2025-10-08 14:03:43 +0800

    ft:使用google 编译框架优化单侧

--- __test/src/test/java/features/tool/ComplexBindPropsProcessorTest.java
+++ __test/src/test/java/features/tool/ComplexBindPropsProcessorTest.java
@@ -1,27 +1,31 @@
 package features.tool;
 
+import com.google.testing.compile.Compilation;
+import com.google.testing.compile.JavaFileObjects;
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
+import org.noear.solon.configurationprocessor.ConfigurationMetadataAnnotationProcessor;
 import org.noear.solon.configurationprocessor.json.JSONArray;
 import org.noear.solon.configurationprocessor.json.JSONObject;
-import org.noear.solon.core.util.IoUtil;
-import org.noear.solon.test.SolonTest;
-import webapp.App;
 import webapp.demoz_tool.AConfig;
 import webapp.demoz_tool.ComplexDemoProp;
 
-import java.io.InputStream;
+import javax.tools.JavaFileObject;
+import javax.tools.StandardLocation;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.function.Consumer;
 
+import static com.google.testing.compile.Compiler.javac;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.junit.jupiter.api.Assertions.*;
 
-
-@SolonTest(App.class)
 public class ComplexBindPropsProcessorTest {
 
     private static final String METADATA_PATH = "META-INF/solon/solon-configuration-metadata.json";
@@ -32,14 +36,32 @@ public class ComplexBindPropsProcessorTest {
     public void test_metadata_generate() throws Throwable {
         String canonicalName = ComplexDemoProp.class.getCanonicalName();
 
-        InputStream resourceAsStream =
-                this.getClass().getClassLoader().getResourceAsStream(METADATA_PATH);
-        DocumentContext ctx = JsonPath.parse(resourceAsStream);
+        Path basePath = Paths.get("src/main/java");
 
-        System.out.println(IoUtil.transferToString(this.getClass().getClassLoader().getResourceAsStream(METADATA_PATH), "UTF-8"));
+        JavaFileObject complexDemoProp = JavaFileObjects.forSourceLines(canonicalName,
+                Files.readAllLines(basePath.resolve("webapp/demoz_tool/ComplexDemoProp.java")));
+        JavaFileObject aConfig = JavaFileObjects.forSourceLines(AConfig.class.getCanonicalName(),
+                Files.readAllLines(basePath.resolve("webapp/demoz_tool/AConfig.java")));
+        JavaFileObject bConfig = JavaFileObjects.forSourceLines("webapp.demoz_tool.BConfig",
+                Files.readAllLines(basePath.resolve("webapp/demoz_tool/BConfig.java")));
+
+        Compilation compilation = javac()
+                .withProcessors(new ConfigurationMetadataAnnotationProcessor())
+                .compile(complexDemoProp, aConfig, bConfig);
+
+        com.google.testing.compile.CompilationSubject.assertThat(compilation).succeeded();
+        com.google.testing.compile.CompilationSubject.assertThat(compilation).generatedFile(StandardLocation.CLASS_OUTPUT, METADATA_PATH);
+
+        Optional<JavaFileObject> metadataFile = compilation.generatedFile(StandardLocation.CLASS_OUTPUT, METADATA_PATH);
+        assertTrue(metadataFile.isPresent());
+
+        String metadataContent = metadataFile.get().getCharContent(false).toString();
+        System.out.println(metadataContent);
+
+        DocumentContext ctx = JsonPath.parse(metadataContent);
 
         checkProperty(ctx, "cdemo.aConfig.arrays", "java.lang.String[]", AConfig.class.getCanonicalName());
-        checkProperty(ctx, "cdemo.aConfig.defaultValue", "java.lang.Boolean", AConfig.class.getCanonicalName());
+        checkProperty(ctx, "cdemo.aConfig.defaultValue", "java.lang.Boolean", AConfig.class.getCanonicalName(), v -> assertEquals(true, v));
         checkProperty(ctx, "cdemo.aConfig.ids", "java.util.List<java.lang.Long>", AConfig.class.getCanonicalName());
         checkProperty(ctx, "cdemo.aConfig.name", "java.lang.String", AConfig.class.getCanonicalName());
         checkProperty(ctx, "cdemo.aConfigMap", "java.util.Map<java.lang.String,webapp.demoz_tool.AConfig>", canonicalName);
@@ -86,11 +108,29 @@ public class ComplexBindPropsProcessorTest {
 
     @Test
     public void test_metadata_properties() throws Throwable {
-        // 读取元数据文件
-        InputStream resourceAsStream =
-                this.getClass().getClassLoader().getResourceAsStream(METADATA_PATH);
-        String metaDataContent = IoUtil.transferToString(resourceAsStream, "UTF-8");
-        JSONObject object = new JSONObject(metaDataContent);
+        Path basePath = Paths.get("src/main/java");
+
+        JavaFileObject demoProps = JavaFileObjects.forSourceLines("webapp.demoz_tool.DemoProps",
+                Files.readAllLines(basePath.resolve("webapp/demoz_tool/DemoProps.java")));
+        JavaFileObject demoEnum = JavaFileObjects.forSourceLines("webapp.demoz_tool.DemoEnum",
+                Files.readAllLines(basePath.resolve("webapp/demoz_tool/DemoEnum.java")));
+        JavaFileObject demoEnumWithValue = JavaFileObjects.forSourceLines("webapp.demoz_tool.DemoEnumWithValue",
+                Files.readAllLines(basePath.resolve("webapp/demoz_tool/DemoEnumWithValue.java")));
+
+        Compilation compilation = javac()
+                .withProcessors(new ConfigurationMetadataAnnotationProcessor())
+                .compile(demoProps, demoEnum, demoEnumWithValue);
+
+
+        com.google.testing.compile.CompilationSubject.assertThat(compilation).succeeded();
+        com.google.testing.compile.CompilationSubject.assertThat(compilation).generatedFile(StandardLocation.CLASS_OUTPUT, METADATA_PATH);
+
+        Optional<JavaFileObject> metadataFile = compilation.generatedFile(StandardLocation.CLASS_OUTPUT, METADATA_PATH);
+        assertTrue(metadataFile.isPresent());
+
+        String metadataContent = metadataFile.get().getCharContent(false).toString();
+        System.out.println(metadataContent);
+        JSONObject object = new JSONObject(metadataContent);
 
         // 验证properties数组存在且不为空
         assertTrue(object.has("properties"));
