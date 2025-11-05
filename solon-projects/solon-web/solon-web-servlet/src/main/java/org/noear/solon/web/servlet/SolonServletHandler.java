9c3be36d632646fb17e6612a8b8af139b47529fd
noear <noear@live.cn>
2025-11-05 08:58:31 +0800

    v3.5: 优化 solon-server 不再默认输出 TEXT_PLAIN_UTF8_VALUE（允许空 content-type）

--- solon-projects/solon-web/solon-web-servlet/src/main/java/org/noear/solon/web/servlet/SolonServletHandler.java
+++ solon-projects/solon-web/solon-web-servlet/src/main/java/org/noear/solon/web/servlet/SolonServletHandler.java
@@ -40,7 +40,7 @@ public class SolonServletHandler extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         SolonServletContext ctx = new SolonServletContext(request, response);
-        ctx.contentType(MimeType.TEXT_PLAIN_UTF8_VALUE);
+        //ctx.contentType(MimeType.TEXT_PLAIN_UTF8_VALUE);
 
         preHandle(ctx);
 
