e5befe5d93cc9330d77af27375f6a5ece35986a1
noear <noear@live.cn>
2025-11-18 08:43:38 +0800

    Create ClassAsOne.java

---
+++ __test/src/test/java/labs/ClassAsOne.java
@@ -0,0 +1,36 @@
+package labs;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+/**
+ * @author noear 2025/3/15 created
+ */
+public class ClassAsOne {
+    public static void main(String[] args) throws IOException {
+        print("/solon-projects/solon-rx/solon-rx/src/main/java/org/noear/solon/rx");
+        //print("/src/test/java/features/");
+    }
+
+    public static void print(String dir) throws IOException {
+        String directoryPath = System.getProperty("user.dir") + dir;
+
+        try (Stream<Path> paths = Files.walk(Paths.get(directoryPath))) {
+            paths.filter(Files::isRegularFile)
+                    .filter(path -> path.toString().endsWith(".java"))
+                    .forEach(path -> {
+                        try {
+                            System.out.println("// file: " + path.getFileName());
+                            String classCode = new String(Files.readAllBytes(path));
+                            System.out.println(classCode);
+                            System.out.println("\n\n");
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+        }
+    }
+}
