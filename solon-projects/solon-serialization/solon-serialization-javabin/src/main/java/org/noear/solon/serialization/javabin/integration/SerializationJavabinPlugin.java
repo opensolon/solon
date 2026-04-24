f9955fdd8cbfe8182793f84bcbb0fad42f71571f
noear <noear@live.cn>
2026-04-24 11:46:40 +0800

    新增 solon-serialization-javabin 插件（统一 JavabinSerializer 管理）

--- solon-projects/solon-serialization/solon-serialization-javabin/src/main/java/org/noear/solon/serialization/javabin/integration/SerializationJavabinPlugin.java
+++ solon-projects/solon-serialization/solon-serialization-javabin/src/main/java/org/noear/solon/serialization/javabin/integration/SerializationJavabinPlugin.java
@@ -37,7 +37,6 @@ public class SerializationJavabinPlugin implements Plugin {
 
         JavabinSerializer serializer = JavabinSerializer.getInstance();
         context.wrapAndPut(JavabinSerializer.class, serializer);
-        context.app().serializers().register("@java-bin", serializer);
     }
 
     static void applyConfig(JavabinClassFilter filter) {
