6c713754cf5d0f0fa3520a7f06c5f3d7deaab10e
Sanjay Malakar <iamsanjaymalakar@gmail.com>
2026-08-26 15:32:17 -0700

    fix(maven-plugin): close all pooled RandomAccessFiles in FilePool.close

--- solon-projects/solon-tool/solon-maven-plugin/src/main/java/org/noear/solon/maven/plugin/tools/data/RandomAccessDataFile.java
+++ solon-projects/solon-tool/solon-maven-plugin/src/main/java/org/noear/solon/maven/plugin/tools/data/RandomAccessDataFile.java
@@ -262,15 +262,29 @@ public class RandomAccessDataFile implements RandomAccessData {
 
 		public void close() throws IOException {
 			this.available.acquireUninterruptibly(this.size);
+			IOException first = null;
 			try {
 				RandomAccessFile pooledFile = this.files.poll();
 				while (pooledFile != null) {
-					pooledFile.close();
+					try {
+						pooledFile.close();
+					}
+					catch (IOException e) {
+						if (first == null) {
+							first = e;
+						}
+						else {
+							first.addSuppressed(e);
+						}
+					}
 					pooledFile = this.files.poll();
 				}
 			}
 			finally {
 				this.available.release(this.size);
+			}
+			if (first != null) {
+				throw first;
 			}
 		}
 
